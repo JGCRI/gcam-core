862720213d8d89da91a17609e2e050663da1e810
Josh Lurz <jlurz24@gmail.com>
2003-05-14 21:33:03 +0000

    Made the Logger parent responsible for printing to the screen to simplify subclasses.

--- CIAM/PlainTextLogger.cpp
+++ CIAM/PlainTextLogger.cpp
@@ -34,69 +34,61 @@ void PlainTextLogger::close(){
 //! Logs a single message.
 void PlainTextLogger::logCompleteMessage( const int line, const string& file, const WarningLevel warningLevelIn, const string& message ) {
 	
-	stringstream buffer;
-	bool printColon = false;
-
-	// Print the tabs.
-	if ( printLogNest ) {
-		for ( int nest = 0; nest < currentNestLevel; nest++ ) {
-			for ( int space = 0; space < logTabSize; space++ ) {
-				buffer << " ";
-			}
-		}
-	}
-	
-	// Print the date.
-	if ( printLogDateStamp ) {
-		buffer << getDateString() << " ";
-		printColon = true;
-	}
-	
-	// Print the timestamp.
-	if ( printLogTimeStamp ) {
-		buffer << getTimeString() << " ";
-		printColon = true;
-	}
-	
-	// Print the warning level
-	if ( printLogWarningLevel ) {
-		buffer << "Level " << warningLevelIn << " ";
-		printColon = true;
-	}
-	
-	// Print the file name.
-	if ( printLogFileName ) {
-		printColon = true;
-		if ( printLogFullPath ) {
-			buffer << file;
-		}
-		else {
-			buffer << getFileNameFromPath( file );
-		}
-	}
-	
-	// Print the line number
-	if ( printLogLineNumber ) {
-		printColon = true;
-		buffer << "(" << line << ")";
-	}
-	
-	if( printColon ){
-		buffer << ":";
-	}
-
-	// Print the message
-	buffer << message;
-	
-	string fullMessage;
-	fullMessage = buffer.str();
-
 	// Decide whether to print the message
 	if ( warningLevelIn >= minLogWarningLevel ){
-		logFile << fullMessage << endl;
-	}
-
-	if ( warningLevelIn >= minToScreenWarningLevel ) {
-		cout << "Log Message: " <<  fullMessage << endl;
+		stringstream buffer;
+		bool printColon = false;
+		
+		// Print the tabs.
+		if ( printLogNest ) {
+			for ( int nest = 0; nest < currentNestLevel; nest++ ) {
+				for ( int space = 0; space < logTabSize; space++ ) {
+					buffer << " ";
+				}
+			}
+		}
+		
+		// Print the date.
+		if ( printLogDateStamp ) {
+			buffer << getDateString() << " ";
+			printColon = true;
+		}
+		
+		// Print the timestamp.
+		if ( printLogTimeStamp ) {
+			buffer << getTimeString() << " ";
+			printColon = true;
+		}
+		
+		// Print the warning level
+		if ( printLogWarningLevel ) {
+			buffer << "Level " << warningLevelIn << " ";
+			printColon = true;
+		}
+		
+		// Print the file name.
+		if ( printLogFileName ) {
+			printColon = true;
+			if ( printLogFullPath ) {
+				buffer << file;
+			}
+			else {
+				buffer << getFileNameFromPath( file );
+			}
+		}
+		
+		// Print the line number
+		if ( printLogLineNumber ) {
+			printColon = true;
+			buffer << "(" << line << ")";
+		}
+		
+		if( printColon ){
+			buffer << ":";
+		}
+		
+		buffer << message;
+		
+		logFile << buffer.str() << endl;
 	}
 }
