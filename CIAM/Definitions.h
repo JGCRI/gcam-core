80a41a896e3b86a0631a43f8d75df012c23eba47
Josh Lurz <jlurz24@gmail.com>
2003-08-05 21:25:46 +0000

    Added isEqual function to check for equivalence of doubles.

--- CIAM/Definitions.h
+++ CIAM/Definitions.h
@@ -22,32 +22,34 @@
 // This only disables the warning if it is called directly before defining a class.
 class msVC6_4786WorkAround {
 public:
-	msVC6_4786WorkAround() {}
+   msVC6_4786WorkAround() {}
 };
 static msVC6_4786WorkAround emptyStatic;
 #endif
 
 #include <limits>
+#include <cmath>
+
 // VC 6.0 does not define min and max in <algorithm>
 #if defined(_MSC_VER) && _MSC_VER < 1300
 
 #ifndef min
 namespace std {
-template<class T>
-const T& min(const T& x, const T& y)
-{
-	return ( x < y ? x : y);
-}
+   template<class T>
+      const T& min(const T& x, const T& y)
+   {
+      return ( x < y ? x : y);
+   }
 }
 #endif // min
- 
+
 #ifndef max
 namespace std {
-template<class T>
-const T& max(const T& x, const T& y)
-{
-return ( x > y ? x : y);
-}
+   template<class T>
+      const T& max(const T& x, const T& y)
+   {
+      return ( x > y ? x : y);
+   }
 }
 #endif // max
 
@@ -56,10 +58,10 @@ return ( x > y ? x : y);
 // This allows for memory leak debugging.
 #if defined(_MSC_VER)
 #   ifdef _DEBUG
-        // usually the followin two lines are defined in Microsoft's generated stdafx.h
+// usually the followin two lines are defined in Microsoft's generated stdafx.h
 #       define VC_EXTRALEAN // do not include rarely used parts
 #       include <afxwin.h>  // MFC core und standard components
-         // extra definition for check whether all needed headers are included
+// extra definition for check whether all needed headers are included
 #       undef SEARCH_MEMORY_LEAKS_ENABLED
 #       define SEARCH_MEMORY_LEAKS_ENABLED
 #   endif   // _DEBUG
@@ -71,21 +73,27 @@ return ( x > y ? x : y);
 
 
 namespace std {
-template <class T>
-//! Helper function to determine the sign of a number.
-   const int sign( const T number ) {
+   template <class T>
+      //! Helper function to determine the sign of a number.
+      const int sign( const T number ) {
       return ( number < 0 )?(-1):(1);
    }
+   
+   template <class T>
+      //! Helper function to check for validity of numbers. 
+      const bool isValidNumber( const T number ) {
+      bool tempval =  ( number == number );
+      if ( numeric_limits<double>::infinity() != 0 ) {
+         tempval = tempval && ( number != std::numeric_limits<T>::infinity() );
+      }
+      return tempval;
+   }
 
-template <class T>
-//! Helper function to check for validity of numbers. 
-const bool isValidNumber( const T number ) {
-bool tempval =  ( number == number );
-if ( numeric_limits<double>::infinity() != 0 ) {
-   tempval = tempval && ( number != std::numeric_limits<T>::infinity() );
-}
-return tempval;
-}
+   //! Helper function to determine if two doubles are equal.
+   inline const bool isEqual( const double firstNumber, const double secondNumber ) {
+      const double SMALL_NUM = 1E-10;
+      return ( fabs( firstNumber - secondNumber ) < SMALL_NUM );
+   }
 }
 
 #endif // _DEFINITIONS_H_
