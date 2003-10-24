18fc77921834622d6bfa828daaeabc3b44531db0
Josh Lurz <jlurz24@gmail.com>
2003-10-24 19:34:00 +0000

    -Added new function which return SMALL_NUM and VERY_SMALL_NUM
    -Documented.

--- CIAM/Util.h
+++ CIAM/Util.h
@@ -5,7 +5,7 @@
 #endif
 
 /*! 
-* \file Util.h	
+* \file Util.h  
 * \ingroup CIAM
 * \brief A set of commonly used functions.
 * 
@@ -19,44 +19,105 @@
 #include <limits>
 #include <cmath>
 #include <fstream>
+#include <iostream>
+#include <string>
 
 namespace util {
-   template <class T>
-      //! Helper function to determine the sign of a number.
-      const int sign( const T number ) {
-      return ( number < 0 )?(-1):(1);
-   }
-   
-   template <class T>
-      //! Helper function to check for validity of numbers. 
-      const bool isValidNumber( const T number ) {
-      bool tempval =  ( number == number );
-      if ( std::numeric_limits<double>::infinity() != 0 ) {
-         tempval = tempval && ( number != std::numeric_limits<T>::infinity() );
-      }
-      return tempval;
+    /*! \brief A function to determine the sign of a number.
+    * \param number A templated parameter which must be comparable to 0.
+    * \return Returns -1 if the number is less than 0, +1 otherwise.
+    */
+    template <class T>
+    const int sign( const T number ) {
+        return ( number < 0 )?(-1):(1);
+    }
+
+    /*! \brief A function to check for the validity of numbers. 
+    *
+    * Occasionally after calculations numbers are no longer in the range of
+    * real numbers, however C++ will continue to perform calculations on them.
+    * This function can then be used to check if numbers are not NaN or Infinity.
+    *   
+    * \warning Some compiler/platforms do not support these checks. In that case this function will always return true. 
+    * \warning Do not try to perform this check without using this function. It will fail on some compilers.
+    * \param number A templated parameter which must be comparable to 0.
+    * \return Returns -1 if the number is less than 0, +1 otherwise.
+    */
+    template <class T>
+    const bool isValidNumber( const T number ) {
+        bool tempval = ( number == number );
+        if ( std::numeric_limits<double>::infinity() != 0 ) {
+            tempval = tempval && ( number != std::numeric_limits<T>::infinity() );
+        }
+        return tempval;
+    }
+
+    /*! \brief A function to determine if two doubles are equal.
+    *
+    * Due to inaccuracies in machine arithmatic, it is virtually impossible for two doubles with decimal values
+    * that are calculated with different methods to be exactly equal. This function checks if the two values
+    * are within a very small threshhold. 
+    *
+    * \warning Do not compare two doubles using the == operator. Use this function instead. 
+    * \param firstNumber The first double to compare.
+    * \param secondNumber The second double to compare.
+    * \return Whether the two doubles are within SMALL_NUM of equivalence. 
+    */
+    inline const bool isEqual( const double firstNumber, const double secondNumber ) {
+        const double SMALL_NUM = 1E-10;
+        return ( fabs( firstNumber - secondNumber ) < SMALL_NUM );
+    }
+
+    /*! \brief A function to check if a file was opened successfully.
+    *
+    * When C++ opens a file, for input or output, it does not check whether it was opened successfully.
+    * If the file has not been opened correctly, the program will often not fail as FORTRAN programs do,
+    * but instead it will have unexpected behavior. This function will check if the file is open, and 
+    * if it is not it will print an error message before calling abort. 
+    * 
+    * \todo This function should be more flexible, for use in cases where the missing file is a non-fatal problem.
+    * \param streamIn A templated parameter that must have the function is_open. This was done so that one function could 
+    * check both input files and output files.
+    * \param fName The name of the file streamIn references, so that the error message can be more informative.
+    */
+    template <class T>
+    inline void checkIsOpen( const T& streamIn, const std::string& fName ) {
+        if( !streamIn.is_open() ) {
+            std::cerr << "Severe Error: File could not be opened for writing." << std::endl;
+            abort();
+        }
+    }
+
+    /*! \brief A function to replace spaces with underscores.
+    *
+    * This function will replace all spaces in a string with underscores. Each space will be replaced by 
+    * exactly one underscore, multiple spaces are not concatonated. Other whitespace characters are not
+    * replaced. 
+    * 
+    * \todo This function currently generated a compiler warning, which should be fixed. 
+    * \param stringIn The string in which spaces should be replaced by underscores.
+    */
+    inline void replaceSpaces( std::string& stringIn ) {
+        static const std::basic_string<char>::size_type npos = -1;
+        std::basic_string <char>::size_type index;
+
+        while( stringIn.find_first_of( " " ) != npos ) {
+            index = stringIn.find_first_of( " " );
+            stringIn.replace( index, 1, "_" );
+        }
+    }
+
+    //! Static function which returns SMALL_NUM. This avoid initialization problems. 
+   static inline double getSmallNumber() {
+      const double SMALL_NUM = 1e-6;
+      return SMALL_NUM;
    }
 
-   //! Helper function to determine if two doubles are equal.
-   inline const bool isEqual( const double firstNumber, const double secondNumber ) {
-      const double SMALL_NUM = 1E-10;
-      return ( fabs( firstNumber - secondNumber ) < SMALL_NUM );
-   }
-   
-   //! Function to check for proper opening of an output file.
-   inline void checkIsOpen( std::ofstream& streamIn ) {
-      if( !streamIn.is_open() ) {
-         abort();
-      }
-   }
-
-   //! Function to check for proper opening of an input file.
-   inline void checkIsOpen( std::ifstream& streamIn ) {
-      if( !streamIn.is_open() ) {
-         abort();
-      }
+   //! Static function which returns VERY_SMALL_NUM. This avoids initialization problems. 
+   static inline double getVerySmallNumber() {
+      const double VERY_SMALL_NUM = 1e-8;
+      return VERY_SMALL_NUM;
    }
 }
-
 #endif // _UTIL_H_
 
