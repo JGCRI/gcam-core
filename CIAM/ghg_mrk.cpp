0bdb6643dfa4e719faabace92ed07b6b6c5b52e6
Josh Lurz <jlurz24@gmail.com>
2003-11-19 22:28:13 +0000

    -Fixed a quirk in the constructor

--- CIAM/ghg_mrk.cpp
+++ CIAM/ghg_mrk.cpp
@@ -28,170 +28,170 @@ extern Scenario* scenario;
 
 //! Default construtor.
 ghg_mrk::ghg_mrk(){
-   const Modeltime* modeltime = scenario->getModeltime();
-	const int maxper = modeltime->getmaxper();
-   constraint.resize( maxper );
-   emission.resize( modeltime->getmaxper() ); // emissions (tgC or MTC)
+    const Modeltime* modeltime = scenario->getModeltime();
+    const int maxper = modeltime->getmaxper();
+    constraint.resize( maxper );
+    emission.resize( maxper ); // emissions (tgC or MTC)
 }
 
 //! Clear member variables.
 void ghg_mrk::clear(){
-	name = "";
-	unit = "";
-	market = "";
-	constraint.clear();
-	emission.clear();
+    name = "";
+    unit = "";
+    market = "";
+    constraint.clear();
+    emission.clear();
 }
 
 //! Create GHG markets
 void ghg_mrk::setMarket( const string& regionName ) {
-	
-	Marketplace* marketplace = scenario->getMarketplace();
 
-	// name is GHG name
-	marketplace->createMarket( regionName, market, name, Marketplace::GHG );
+    Marketplace* marketplace = scenario->getMarketplace();
+
+    // name is GHG name
+    marketplace->createMarket( regionName, market, name, Marketplace::GHG );
     marketplace->setMarketToSolve ( name, regionName );
-	/* no need to use market.setPriceVector here unless GHG markets need
-        initial prices read-in for the base year */  
+    /* no need to use market.setPriceVector here unless GHG markets need
+    initial prices read-in for the base year */  
 }
 
 
 //! Initializes data members from XML.
 void ghg_mrk::XMLParse( const DOMNode* node ){
-	
-	const Modeltime* modeltime = scenario->getModeltime();
-	DOMNodeList* nodeList;
-	DOMNode* curr = 0;
-	string nodeName;
-	
-	// PRECONDITION
-	/*! \pre assume we are passed a valid node.*/
-	assert( node );
 
-	// get the name attribute.
-	name = XMLHelper<string>::getAttrString( node, "name" );
-	
-	#if( _DEBUG )
-		cout << "\t Greenhouse gas market name set as " << name << endl;
-	#endif
-	
-	// get all child nodes.
-	nodeList = node->getChildNodes();
+    const Modeltime* modeltime = scenario->getModeltime();
+    DOMNodeList* nodeList;
+    DOMNode* curr = 0;
+    string nodeName;
 
-	// loop through the child nodes.
-	for( int i = 0; i < static_cast<int>( nodeList->getLength() ); i++ ){
-		curr = nodeList->item( i );
-		nodeName = XMLHelper<string>::safeTranscode( curr->getNodeName() );
-		
-      if( nodeName == "#text" ) {
-         continue;
-      }
+    // PRECONDITION
+    /*! \pre assume we are passed a valid node.*/
+    assert( node );
 
-		else if( nodeName == "market" ){
-			market = XMLHelper<string>::getValueString( curr ); // should be only one market
-		}
+    // get the name attribute.
+    name = XMLHelper<string>::getAttrString( node, "name" );
 
-		else if( nodeName == "constraint" ){
-         XMLHelper<double>::insertValueIntoVector( curr, constraint, modeltime );
-		}
-      else {
+#if( _DEBUG )
+    cout << "\t Greenhouse gas market name set as " << name << endl;
+#endif
+
+    // get all child nodes.
+    nodeList = node->getChildNodes();
+
+    // loop through the child nodes.
+    for( int i = 0; i < static_cast<int>( nodeList->getLength() ); i++ ){
+        curr = nodeList->item( i );
+        nodeName = XMLHelper<string>::safeTranscode( curr->getNodeName() );
+
+        if( nodeName == "#text" ) {
+            continue;
+        }
+
+        else if( nodeName == "market" ){
+            market = XMLHelper<string>::getValueString( curr ); // should be only one market
+        }
+
+        else if( nodeName == "constraint" ){
+            XMLHelper<double>::insertValueIntoVector( curr, constraint, modeltime );
+        }
+        else {
             cout << "Unrecognized text string: " << nodeName << " found while parsing ghgmarket." << endl;
-         }
-   }
+        }
+    }
 }
 
 //! Writes datamembers to datastream in XML format.
 void ghg_mrk::toXML( ostream& out ) const {
-	
-	// write the beginning tag.
-	Tabs::writeTabs( out );
-	out << "<ghgmarket name=\"" << name << "\">" << endl;
-	
-	// increase the indent.
-	Tabs::increaseIndent();
 
-	// write the xml for the class members.
-	XMLWriteElement( unit, "unit", out );
-	// write out the market string.
-	XMLWriteElement( market, "market", out );
-	
-	for( vector<double>::const_iterator i = constraint.begin(); i != constraint.end(); i++ ){
-		Tabs::increaseIndent();
-		Tabs::writeTabs( out );
-		
-		out << "<period>" << endl;
-		
-		Tabs::increaseIndent();
-		XMLWriteElement( *i, "constraint", out );
-		Tabs::decreaseIndent();
+    // write the beginning tag.
+    Tabs::writeTabs( out );
+    out << "<ghgmarket name=\"" << name << "\">" << endl;
 
-		Tabs::writeTabs( out );
-		out << "</period>" << endl;	
-		Tabs::decreaseIndent();
-	}
+    // increase the indent.
+    Tabs::increaseIndent();
 
-	// finished writing xml for the class members.
-	
-	// decrease the indent.
-	Tabs::decreaseIndent();
-	
-	// write the closing tag.
-	Tabs::writeTabs( out );
-	out << "</ghgmarket>" << endl;
+    // write the xml for the class members.
+    XMLWriteElement( unit, "unit", out );
+    // write out the market string.
+    XMLWriteElement( market, "market", out );
+
+    for( vector<double>::const_iterator i = constraint.begin(); i != constraint.end(); i++ ){
+        Tabs::increaseIndent();
+        Tabs::writeTabs( out );
+
+        out << "<period>" << endl;
+
+        Tabs::increaseIndent();
+        XMLWriteElement( *i, "constraint", out );
+        Tabs::decreaseIndent();
+
+        Tabs::writeTabs( out );
+        out << "</period>" << endl;	
+        Tabs::decreaseIndent();
+    }
+
+    // finished writing xml for the class members.
+
+    // decrease the indent.
+    Tabs::decreaseIndent();
+
+    // write the closing tag.
+    Tabs::writeTabs( out );
+    out << "</ghgmarket>" << endl;
 }
 
 //! Writes datamembers to datastream in XML format.
 void ghg_mrk::toDebugXML( const int period, ostream& out ) const {
-	
-	// write the beginning tag.
-	Tabs::writeTabs( out );
-	out << "<ghgmarket name=\"" << name << "\">" << endl;
-	
-	// increase the indent.
-	Tabs::increaseIndent();
 
-	// Write the xml for the class members.
-	XMLWriteElement( unit, "unit", out );
+    // write the beginning tag.
+    Tabs::writeTabs( out );
+    out << "<ghgmarket name=\"" << name << "\">" << endl;
 
-	// write out the market string.
-	XMLWriteElement( market, "market", out );
-	
-	// Write the constraint for the current year
-	XMLWriteElement( constraint[ period ], "constraint", out );
+    // increase the indent.
+    Tabs::increaseIndent();
 
-	XMLWriteElement( emission[ period ], "emission", out );
-	// finished writing xml for the class members.
-	
-	// decrease the indent.
-	Tabs::decreaseIndent();
-	
-	// write the closing tag.
-	Tabs::writeTabs( out );
-	out << "</ghgmarket>" << endl;
+    // Write the xml for the class members.
+    XMLWriteElement( unit, "unit", out );
+
+    // write out the market string.
+    XMLWriteElement( market, "market", out );
+
+    // Write the constraint for the current year
+    XMLWriteElement( constraint[ period ], "constraint", out );
+
+    XMLWriteElement( emission[ period ], "emission", out );
+    // finished writing xml for the class members.
+
+    // decrease the indent.
+    Tabs::decreaseIndent();
+
+    // write the closing tag.
+    Tabs::writeTabs( out );
+    out << "</ghgmarket>" << endl;
 }
 
 //! Set emissions.
 void ghg_mrk::setEmission( const double amount, const int per )
 {
-	emission[ per ] = amount; // emissions (tgC or MTC)
+    emission[ per ] = amount; // emissions (tgC or MTC)
 }
 
 //! Show emission name.
 string ghg_mrk::getName() const
 {
-	return name;
+    return name;
 }
 
 //! Return emissions target.
 double ghg_mrk::getConstraint( const int per ) const
 {
-	return constraint[ per ]; // emissions constraint (tgC or MTC)
+    return constraint[ per ]; // emissions constraint (tgC or MTC)
 }
 
 //! Return emissions.
 double ghg_mrk::getEmission( const int per ) const
 {
-	return emission[ per ]; // emissions (tgC or MTC)
+    return emission[ per ]; // emissions (tgC or MTC)
 
 }
 
