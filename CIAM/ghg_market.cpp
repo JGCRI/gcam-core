0facb736649ef74630e7b9d66ae2826912a2e21c
Josh Lurz <jlurz24@gmail.com>
2003-11-24 17:38:16 +0000

    -Added definitions for virtual functions to set the initial price and the price from the last period. These take care of the situations where if there is no constraint the carbon price should be zero, but it should be one in the first period with a constraint.
    -Modified the code that removes markets in NR to solve a GHG market with zero price but positive excess demand and supply.
    
    -Documented.

--- CIAM/ghg_market.cpp
+++ CIAM/ghg_market.cpp
@@ -26,16 +26,45 @@ string GHGMarket::getType() const {
    return "GHGMarket";
 }
 
+/* \brief Initialize the GHGMarket price.
+* \detailed This method checks first if the price has already been initialized.
+* If it has been initialized, the price is left unchanged. Otherwise the method checks the value of the constraint,
+* or supply, to determine how to initialize the price. If supply is 0, price is set to 0. If the supply 
+* is greater than 0, the price is initialized to 1.
+* \author Josh Lurz
+*/
 void GHGMarket::initPrice() {
-   price = 1;
+    // If price is near zero it needs to be initialized.
+    if( price < util::getSmallNumber() ){
+        // If supply is 0 price should be 0.
+        if( supply < util::getSmallNumber() ){
+            price = 0;
+        }
+        else {
+            price = 1;
+        }
+    }
 }
 
 void GHGMarket::setPrice( const double priceIn ) {
     Market::setPrice( priceIn );
 }
 
+/* \brief Initialize the GHGMarket price from last period's price.
+* \detailed This method first checks if the lastPrice was 0. This would mean that last period's constraint was 
+* 0. If it is, then it checks if the constraint in the current period is greater than 0. In this case price is 
+* set to 1 as this is the initial constrained period. Otherwise price is set to the previous period's price as
+* is done in the normal market.
+* \param lastPrice Previous period's price. 
+* \author Josh Lurz
+*/
 void GHGMarket::setPriceFromLast( const double lastPrice ) {
-   Market::setPriceFromLast( lastPrice );
+    if( lastPrice < util::getSmallNumber() && supply > util::getSmallNumber() ){
+        price = 1;
+    }
+    else {
+        price = lastPrice;
+    }
 }
 
 double GHGMarket::getPrice() const {
@@ -66,12 +95,26 @@ void GHGMarket::addToSupply( const double supplyIn ) {
     Market::addToSupply( supplyIn );
 }
 
+/* \brief This method determines whether to solve a GHGMarket with the solution mechanism.
+* \detailed This method only returns that the solution mechanism should attempt to solve the market
+* if the supply is positive. This prevents the solution mechanism from trying to solve unconstrained 
+* time periods.
+* \return Whether to solve the market.
+* \author Sonny Kim
+*/
 bool GHGMarket::shouldSolve() const {
 
    // Don't solve if  there is no constraint
    return ( solveMarket && supply > 1E-6 );
 }
 
+/* \brief This method determines whether to solve a GHGMarket with the NR solution mechanism.
+* \detailed This method only returns that the NR solution mechanism should attempt to solve the market
+* if the supply and demand is positive, and the excess demand is not miniscule. 
+* \todo This needs a more detailed explanation, but the logic is convoluded.
+* \return Whether to solve the market in NR.
+* \author Sonny Kim
+*/
 bool GHGMarket::shouldSolveNR() const {
    bool doSolveMarket = false;
    // Check if this market is supposed to be solved & if a significant demand exists
@@ -79,7 +122,7 @@ bool GHGMarket::shouldSolveNR() const {
       doSolveMarket = true;
    }
    
-   if ( ( supply < util::getSmallNumber() ) ||  ( price < util::getSmallNumber() && ( demand - supply ) < util::getSmallNumber() ) || ( price < util::getSmallNumber() ) ) {
+   if ( ( supply < util::getSmallNumber() ) ||  ( price < util::getSmallNumber() && ( demand - supply ) < util::getSmallNumber() ) ) {
       doSolveMarket = false; 
    }
 
