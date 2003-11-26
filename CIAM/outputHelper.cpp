acb6ddccab03abf02505b9c11b553c3b9623d7fc
Josh Lurz <jlurz24@gmail.com>
2003-11-26 17:15:11 +0000

    -Added error handling code so that the DB will run on XP/7.0

--- CIAM/outputHelper.cpp
+++ CIAM/outputHelper.cpp
@@ -22,6 +22,7 @@
 #include <ctime>
 #include "scenario.h" 
 #include "World.h"
+#include <comdef.h>
 
 using namespace std;
 
@@ -72,27 +73,38 @@ void dboutput4(string var1name,string var2name,string var3name,string var4name,
 	    // COleVariant does not take int, must be short or long
 	    // now write to the database
 	    // MUST Update before adding another
-	    DBoutrst.AddNew(); // now the current record is this empty new one
-	    tm* lt = localtime(&ltime); // struct for time components
-	    // run id MiniCAM style
-	    long int RunID = lt->tm_sec + lt->tm_min * 100 + lt->tm_hour * 10000
+        try {
+	        DBoutrst.AddNew(); // now the current record is this empty new one
+        
+	        tm* lt = localtime(&ltime); // struct for time components
+	        // run id MiniCAM style
+	        long RunID = lt->tm_sec + lt->tm_min * 100 + lt->tm_hour * 10000
                    + lt->tm_mday * 1000000 
 				   + (lt->tm_mon * 100000000 + 100000000); // 0 is january
-	    DBoutrst.SetField(0L, COleVariant(RunID,VT_I4));
-	    DBoutrst.SetField(1L, COleVariant(short int(iter->second)));
-	    DBoutrst.SetField(3L, COleVariant(var2name.c_str(), VT_BSTRT));
-	    DBoutrst.SetField(4L, COleVariant(var3name.c_str(), VT_BSTRT));
-	    DBoutrst.SetField(5L, COleVariant(var4name.c_str(), VT_BSTRT));
-	    DBoutrst.SetField(6L, COleVariant(uname.c_str(), VT_BSTRT));
-        // get scenario name and output to dbout
-        string scenarioName = scenario->getName();
-	    DBoutrst.SetField(7L, COleVariant(scenarioName.c_str(), VT_BSTRT));
-	    for (i=0;i< static_cast<int>( dout.size() );i++) {
-		    j = 8+i;
-		    DBoutrst.SetField(j, COleVariant(dout[i]));
-	    }
-	    DBoutrst.Update(); // save and write the record
-    }
+
+	        DBoutrst.SetField(0L, COleVariant(RunID,VT_I4)); // Bad line.
+	        DBoutrst.SetField(1L, COleVariant(short(iter->second)));
+	        DBoutrst.SetField(3L, COleVariant(var2name.c_str(), VT_BSTRT));
+	        DBoutrst.SetField(4L, COleVariant(var3name.c_str(), VT_BSTRT));
+	        DBoutrst.SetField(5L, COleVariant(var4name.c_str(), VT_BSTRT));
+	        DBoutrst.SetField(6L, COleVariant(uname.c_str(), VT_BSTRT));
+            // get scenario name and output to dbout
+            string scenarioName = scenario->getName();
+	        DBoutrst.SetField(7L, COleVariant(scenarioName.c_str(), VT_BSTRT));
+	        for (i=0;i< static_cast<int>( dout.size() );i++) {
+		        j = 8+i;
+		        DBoutrst.SetField(j, COleVariant(dout[i]));
+	        }
+	        DBoutrst.Update(); // save and write the record
+        
+            } catch( _com_error e ){
+                printf("Error:*************************************************\n");
+                printf("Code = %08lx\n", e.Error());
+                printf("Message = %s\n", e.ErrorMessage());
+                printf("Source = %s\n", (LPCSTR) e.Source());
+                printf("Description = %s\n", (LPCSTR) e.Description());
+            }
+        }
     // if region not found in regionMap, do nothing
 #endif
 }
