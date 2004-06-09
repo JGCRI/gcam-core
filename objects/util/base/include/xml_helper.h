2e31da90ce232a9116f5e4a1038d530e32be178c
Josh Lurz <jlurz24@gmail.com>
2004-06-09 15:01:05 +0000

    -Removing functions added yesterday, I had already added versions of them before. -Keeping improved documentation for them.

--- objects/util/base/include/xml_helper.h
+++ objects/util/base/include/xml_helper.h
@@ -427,42 +427,42 @@ void XMLWriteElement( const T value, const std::string elementName, std::ostream
    out << "</" << elementName << ">" << std::endl;
 }
 
-//! Function to write the opening XML tag.
-/*! 
-* This function is used to write a single opening XML tag and an optional year and name to the output stream
-* in XML format. If the year or name is not passed in, the function will not print it. The function increases
-* the indent level after writing the tag so that subsequent elements are correctly indented. 
+/*! \brief Write an opening XML tag.
+* \detailed This function is used to write an opening XML tag and an optional name and year to the output stream.
+* The name and year are optional attributes. The name and year may be left out, or only the year may be left out, but
+* the year cannot be written without a year unless the empty string is included in the function arguments, due
+* to the way that C++ default arguments work. If the arguments are left out, the function will not write the attribute. 
+* The function increases the indent level after writing the tag so that subsequent elements are correctly indented.
 * \param elementName Name of the element.
 * \param out Stream to print to.
-* \param tabs A tabs object responsible for printing the correct number of tabs. 
-* \param year Optional year value to print as an attribute.
+* \param Tabs The number of tabs to print before the element. 
 * \param name Optional name value to print as an attribute.
+* \param year Optional year value to print as an attribute.
 */
 inline void XMLWriteOpeningTag( const std::string& elementName, std::ostream& out, Tabs* tabs,  const int year = 0, const std::string& name = "" ) {
 
     tabs->writeTabs( out );
-
     out << "<" << elementName;
-
+    
+    if( year ){
+        out << " year=\"" << year << "\"";
+    }
+    
     if ( name != "" ) {
         out << " name=\"" << name << "\"";
-    }
-
-    if( year != 0 ){
-        out << " year=\"" << year << "\"";
     }
 
     out << ">" << std::endl;
     tabs->increaseIndent();
 } 
 
-//! Function to write the closing XML tag.
-/*! 
-* This function is used to write a single closing XML tag. It decreases the indent before writing the tag.
-* Closing tags cannot have attributes. 
+/*!  \brief Write a closing XML tag.
+* \detailed This function is used to write a closing XML tag. It decreases the indent before writing the tag,
+* and adds a newline.
+* \note Closing tags cannot have attributes. 
 * \param elementName Name of the element.
 * \param out Stream to print to.
-* \param tabs A tabs object responsible for printing the correct number of tabs. 
+* \param Tabs The number of tabs to print before the element. 
 */
 inline void XMLWriteClosingTag( const std::string& elementName, std::ostream& out, Tabs* tabs ) {
     
@@ -657,46 +657,5 @@ void parseContainerNode( const xercesc::DOMNode* node, std::vector<U>& insertToV
     }
 }
 
-/*! \brief Write an opening XML tag.
-* \detailed This function is used to write an opening XML tag and an optional name and year to the output stream.
-* The name and year are optional attributes. The name and year may be left out, or only the year may be left out, but
-* the year cannot be written without a year unless the empty string is included in the function arguments, due
-* to the way that C++ default arguments work. If the arguments are left out, the function will not write the attribute. 
-* The function increases the indent level after writing the tag so that subsequent elements are correctly indented.
-* \param elementName Name of the element.
-* \param out Stream to print to.
-* \param Tabs The number of tabs to print before the element. 
-* \param name Optional name value to print as an attribute.
-* \param year Optional year value to print as an attribute.
-*/
-inline void writeOpeningTag( const std::string& elementName, std::ostream& out, Tabs* tabs, const std::string& name = "", const int year = 0 ) {
-    // Write tabs and open the tag.
-    tabs->writeTabs( out );
-    out << "<" << elementName;
-    
-    // Write attributes.
-    if ( name != "" ) {
-        out << " name=\"" << name << "\"";
-    }
-    if( year ){
-        out << " year=\"" << year << "\"";
-    }
-    // Close the tag.
-    out << ">" << std::endl;
-    tabs->increaseIndent();
-} 
-
-/*!  \brief Write a closing XML tag.
-* \detailed This function is used to write a closing XML tag. It decreases the indent before writing the tag,
-* and adds a newline.
-* \note Closing tags cannot have attributes. 
-* \param elementName Name of the element.
-* \param out Stream to print to.
-*/
-inline void writeClosingTag( const std::string& elementName, std::ostream& out, Tabs* tabs ) {
-    tabs->decreaseIndent();
-    tabs->writeTabs( out );
-    out << "</" << elementName;
-    out << ">" << std::endl;
-} 
 #endif // _XML_HELPER_H_
+
