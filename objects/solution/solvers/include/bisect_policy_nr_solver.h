901cd77da268b4698c75b252ca9d53300aebb5b3
Josh Lurz <jlurz24@gmail.com>
2004-11-12 15:21:37 +0000

    -New solution mechanism

---
+++ objects/solution/solvers/include/bisect_policy_nr_solver.h
@@ -0,0 +1,44 @@
+#ifndef _BISECT_POLICY_NR_SOLVER_H_
+#define _BISECT_POLICY_NR_SOLVER_H_
+#if defined(_MSC_VER)
+#pragma once
+#endif
+
+#include <memory>
+#include "solution/solvers/include/solver.h"
+
+/*! 
+* \file bisect_policy_nr_solver.h
+* \ingroup Objects
+* \brief The BisectPolicyNRSolver class header file. 
+*
+* \author Josh Lurz
+* \date $Date$
+* \version $Revision$
+*/
+
+class SolverComponent;
+class Marketplace;
+class World;
+/*! 
+* \ingroup Objects
+* \brief A class which defines an An instance of the Solver class which uses bisection first and then Newton-Rhaphson.
+* \author Josh Lurz
+*/
+
+class BisectPolicyNRSolver: public Solver {
+public:
+    BisectPolicyNRSolver( Marketplace* marketplaceIn, World* worldIn );
+    virtual ~BisectPolicyNRSolver();
+    static const std::string& getName();
+    virtual void init();
+    virtual bool solve( const int period );
+private:
+    std::auto_ptr<SolverComponent> mLogNewtonRaphson; //!< LogNewtonRaphson solver component.
+    std::auto_ptr<SolverComponent> mBisectAll; //!< BisectAll solver component.
+    std::auto_ptr<SolverComponent> mBisectOne; //!< BisectOne solver component.
+    std::auto_ptr<SolverComponent> mBisectPolicy; //!< BisectPolicy solver component.
+};
+
+#endif // _bisect_policy_nr_solver_
+
