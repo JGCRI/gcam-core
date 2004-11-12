ccf9a6d168a6a69f0bede284319502cfde2ef142
Josh Lurz <jlurz24@gmail.com>
2004-11-12 14:58:02 +0000

    -Use new loggers

--- objects/containers/source/merge_runner.cpp
+++ objects/containers/source/merge_runner.cpp
@@ -14,11 +14,11 @@
 #include "util/base/include/xml_helper.h"
 #include "util/base/include/configuration.h"
 #include "containers/include/scenario.h"
+#include "util/logger/include/ilogger.h"
 
 using namespace std;
 using namespace xercesc;
 
-extern ofstream bugoutfile, logfile;
 extern Scenario* scenario;
 
 //! Constructor
@@ -27,25 +27,14 @@ MergeRunner::MergeRunner(){
 
 //! Destructor
 MergeRunner::~MergeRunner(){
-    // Close All Text Files
-    bugoutfile.close();
-    logfile.close();
 }
 
 //! Setup the scenario.
 bool MergeRunner::setupScenario( Timer& timer, const string aName, const list<string> aScenComponents ){
-    // Open various files.
-    const Configuration* conf = Configuration::getInstance();
-    const string logFileName = conf->getFile( "logOutFileName" );
-    logfile.open( logFileName.c_str(), ios::out );
-    util::checkIsOpen( logfile, logFileName  );
-    
-    const string bugOutFileName = conf->getFile( "bugOutFileName" );
-    bugoutfile.open( bugOutFileName.c_str(), ios::out );
-    util::checkIsOpen( bugoutfile, bugOutFileName );
-    
+
     // Parse the input file.
     XercesDOMParser* parser = XMLHelper<void>::getParser();
+    const Configuration* conf = Configuration::getInstance();
     DOMNode* root = XMLHelper<void>::parseXML( conf->getFile( "xmlInputFileName" ), parser );
 
     // Use a smart pointer for scenario so that if the main program exits before the end the memory is freed correctly. 
@@ -55,35 +44,31 @@ bool MergeRunner::setupScenario( Timer& timer, const string aName, const list<st
     
     // Override scenario name from data file with that from configuration file
     string overrideName = conf->getString( "scenarioName" ) + aName;
-    if ( overrideName != "") {
+    if ( overrideName != "" ) {
         mScenario->setName( overrideName );
     }
 
     // Fetch the listing of Scenario Components.
     const list<string> scenComponents = conf->getScenarioComponents();
-    
+
+    // Get the main log file.
+    ILogger& mainLog = ILogger::getLogger( "main_log" );
+    mainLog.setLevel( ILogger::NOTICE );
+
     // Iterate over the vector.
     typedef list<string>::const_iterator ScenCompIter;
     for( ScenCompIter currComp = scenComponents.begin(); currComp != scenComponents.end(); ++currComp ) {
-        if ( Configuration::getInstance()->getBool( "debugChecking" ) ) { 
-            cout << "Parsing " << *currComp << " scenario component." << endl;
-        }
+        mainLog << "Parsing " << *currComp << " scenario component." << endl;
         root = XMLHelper<void>::parseXML( *currComp, parser );
         mScenario->XMLParse( root );
     }
-
-    if ( Configuration::getInstance()->getBool( "debugChecking" ) ) { 
-        cout << "XML parsing complete." << endl;
-    }
-    logfile << "XML parsing complete." << endl;
+    
+    mainLog << "XML parsing complete." << endl;
 
     // Print data read in time.
     timer.save();
-    if ( Configuration::getInstance()->getBool( "timestamp" ) ) { 
-        timer.print( cout, "XML Readin Time:" );
-    }
-    timer.print( logfile, "XML Readin Time:" );
-
+    mainLog.setLevel( ILogger::DEBUG );
+    timer.print( mainLog, "XML Readin Time:" );
     return true;
 }
 /*! \brief Does nothing, needed for interface.
