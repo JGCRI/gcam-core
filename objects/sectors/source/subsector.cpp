04da8b0528ab9ce7080356d452306fea92e888e4
Steven J Smith <ssmith@pnnl.gov>
2004-12-02 18:24:45 +0000

    Added global market outputs for price and supply to output.csv file. Fixed so output.csv is written again. Removed derivedTechInitCalc method. Added option to control share weight scaling for calibrated subsectors via input (can not scale at all, can scale to arbitrary year, can keep share weight constant at calibrated value).

--- objects/sectors/source/subsector.cpp
+++ objects/sectors/source/subsector.cpp
@@ -76,7 +76,7 @@ Subsector::Subsector( const string regionName, const string sectorName ){
     calibrationStatus.resize( maxper, false );
     fixedShare.resize( maxper );
     capLimited.resize( maxper, false );
-    scaleYear = modeltime->getendyear(); // default year to scale share weight to after calibration
+    scaleYear = modeltime->getper_to_yr( maxper ); // default year to scale share weight to after calibration
 }
 
 /*! \brief Default destructor.
