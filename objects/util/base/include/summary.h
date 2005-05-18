f9eaca2361fa275279ce0fb51b959ff31161b734
Josh Lurz <jlurz24@gmail.com>
2005-05-18 19:08:46 +0000

    -Cleanup approved by Sonny

--- objects/util/base/include/summary.h
+++ objects/util/base/include/summary.h
@@ -6,7 +6,7 @@
 
 /*! 
 * \file summary.h
-* \ingroup CIAM
+* \ingroup Objects
 * \brief The Summary class header file.
 * \author Sonny Kim
 * \date $Date$
@@ -17,7 +17,7 @@
 #include <string>
 
 /*! 
-* \ingroup CIAM
+* \ingroup Objects
 * \brief An object which contains variables for reporting.
 * \author Sonny Kim
 */
@@ -25,31 +25,34 @@
 class Summary
 {
 private:
-	std::map<std::string, double> fuelcons;  //!< map of fuel name and amount consumed
-	std::map<std::string, double> pecons;  //!< map of primary energy consumption
-	std::map<std::string, double> peprod;  //!< map of primary energy production
-	std::map<std::string, double> petrade;  //!< map of primary energy trade
-	std::map<std::string, double> emission;  //!< map of ghg emissions
-	std::map<std::string, double> emissfuel;  //!< map of ghg emissions implicit in fuel
-	std::map<std::string, double> emissind;  //!< map of indirect ghg emissions
-	std::map<std::string, double> sequesteredAmount;  //!< map of sequestered amount of emissions
+    typedef std::map<std::string, double> SummaryItem;
+    typedef SummaryItem::const_iterator CSummaryIterator;
+    typedef SummaryItem::iterator SummaryIterator;
+	SummaryItem fuelcons;  //!< map of fuel name and amount consumed
+	SummaryItem pecons;  //!< map of primary energy consumption
+	SummaryItem peprod;  //!< map of primary energy production
+	SummaryItem petrade;  //!< map of primary energy trade
+	SummaryItem emission;  //!< map of ghg emissions
+	SummaryItem emissfuel;  //!< map of ghg emissions implicit in fuel
+	SummaryItem emissind;  //!< map of indirect ghg emissions
+	SummaryItem sequesteredAmount;  //!< map of sequestered amount of emissions
 public:
 	Summary(); // default construtor
 	void initfuelcons( const std::string& fname, const double value );
 	void initpeprod( const std::string& fname, const double value );
-	std::map<std::string, double> getfuelcons() const;
-	std::map<std::string, double> getpecons() const;
-	std::map<std::string, double> getpetrade() const;
-	std::map<std::string, double> getemission() const;
-	std::map<std::string, double> getemfuelmap() const;
-	std::map<std::string, double> getemindmap() const;
-	std::map<std::string, double> getSequesteredAmountMap() const;
-    void updatefuelcons( const std::map<std::string, double>& fuelinfo);
+	const SummaryItem& getfuelcons() const;
+	const SummaryItem& getpecons() const;
+	const SummaryItem& getpetrade() const;
+	const SummaryItem& getemission() const;
+	const SummaryItem& getemfuelmap() const;
+	const SummaryItem& getemindmap() const;
+	const SummaryItem& getSequesteredAmountMap() const;
+    void updatefuelcons( const SummaryItem& fuelinfo);
 	void updatepetrade();
-	void updateemiss( const std::map<std::string, double>& ghginfo );
-	void updateemfuelmap( const std::map<std::string, double>& ghginfo );
-	void updateemindmap( const std::map<std::string, double>& ghginfo );
-	void updateSequesteredAmountMap( const std::map<std::string, double>& ghginfo );
+	void updateemiss( const SummaryItem& ghginfo );
+	void updateemfuelmap( const SummaryItem& ghginfo );
+	void updateemindmap( const SummaryItem& ghginfo );
+	void updateSequesteredAmountMap( const SummaryItem& ghginfo );
     void clearfuelcons();
 	void clearpeprod();
 	void clearemiss();
--- objects/util/base/source/summary.cpp
+++ objects/util/base/source/summary.cpp
@@ -1,6 +1,6 @@
 /*! 
 * \file summary.cpp
-* \ingroup CIAM
+* \ingroup Objects
 * \brief summary class source file.
 * \author Sonny Kim
 * \date $Date$
@@ -33,43 +33,36 @@ void Summary::initfuelcons( const string& fname, const double value ){
 }
 
 void Summary::initpeprod( const string& fname, const double value ){
-
 	peprod[ fname ] += value;
 	peprod[ "zTotal" ] += value;
 }
 
-map<string, double> Summary::getfuelcons() const {
-	
+const Summary::SummaryItem& Summary::getfuelcons() const {
 	return fuelcons;
 }
 
-map<string, double> Summary::getpecons() const {
-	
+const Summary::SummaryItem& Summary::getpecons() const {	
 	return pecons;
 }
 
-map<string, double> Summary::getpetrade() const {
+const Summary::SummaryItem& Summary::getpetrade() const {
 	return petrade;
 }
 
-map<string, double> Summary::getemission() const {
-	
+const Summary::SummaryItem& Summary::getemission() const {
 	return emission;
 }
 
-map<string, double> Summary::getemfuelmap() const {
-	
+const Summary::SummaryItem& Summary::getemfuelmap() const {
 	return emissfuel;
 }
 
-map<string, double> Summary::getemindmap() const {
-
+const Summary::SummaryItem& Summary::getemindmap() const {
 	return emissind;
 }
 
 //! return map of sequestered amount of emissions
-map<string, double> Summary::getSequesteredAmountMap() const {
-
+const Summary::SummaryItem& Summary::getSequesteredAmountMap() const {
 	return sequesteredAmount;
 }
 
@@ -79,14 +72,9 @@ to the summary object maps fuelcons and pecons.
 
 The iterator fmap is used to traverse the fuelinfo map.
 */ 
-void Summary::updatefuelcons( const map<string, double>& fuelinfo ) {
-	
-	string str;
-	typedef map<string,double>:: const_iterator CI;
-    CI fmap;
-        
+void Summary::updatefuelcons( const SummaryItem& fuelinfo ) {
 	// map all primary and secondary fuel consumption
-	for (fmap=fuelinfo.begin(); fmap!=fuelinfo.end(); ++fmap) {	// iterate to one less than the end
+	for (CSummaryIterator fmap=fuelinfo.begin(); fmap!=fuelinfo.end(); ++fmap) {	// iterate to one less than the end
 		fuelcons[fmap->first] += fmap->second; // Add values from the passed map to fuelcons
         // Don't need a zTotal b/c the fuels are uncomparable. 
 	}
@@ -95,8 +83,8 @@ void Summary::updatefuelcons( const map<string, double>& fuelinfo ) {
    const vector<string> primaryFuelList = scenario->getWorld()->getPrimaryFuelList();
    
    for( vector<string>::const_iterator fuelIter = primaryFuelList.begin(); fuelIter != primaryFuelList.end(); fuelIter++ ) {
-	   fmap=fuelinfo.find( *fuelIter );
-	   if(fmap!=fuelinfo.end()) {
+	   CSummaryIterator fmap=fuelinfo.find( *fuelIter );
+	   if( fmap!=fuelinfo.end() ) {
 		   pecons[fmap->first] += fmap->second;
 		   pecons["zTotal"] += fmap->second;
 	   }
@@ -104,65 +92,56 @@ void Summary::updatefuelcons( const map<string, double>& fuelinfo ) {
 }
 
 void Summary::updatepetrade() {
-
 	// map all primary and secondary fuel consumption
-	for ( map<string,double>::const_iterator fmap = peprod.begin(); fmap != peprod.end(); ++fmap ) {
+	for ( CSummaryIterator fmap = peprod.begin(); fmap != peprod.end(); ++fmap ) {
 		petrade[ fmap->first ] = peprod[ fmap->first ] - pecons[ fmap->first ];
 	}
 }
 
-void Summary::updateemiss( const map<string, double>& ghginfo ) {
-	
+void Summary::updateemiss( const SummaryItem& ghginfo ) {
 	// map all primary and secondary fuel consumption
-	for ( map<string,double>::const_iterator fmap = ghginfo.begin(); fmap != ghginfo.end(); ++fmap){
+	for ( CSummaryIterator fmap = ghginfo.begin(); fmap != ghginfo.end(); ++fmap){
 		emission[ fmap->first ] += fmap->second;
 	}
 }
 
-void Summary::updateemfuelmap( const map<string, double>& ghginfo ) {
-
+void Summary::updateemfuelmap( const SummaryItem& ghginfo ) {
 	// map all primary and secondary fuel consumption
-	for ( map<string,double>::const_iterator fmap = ghginfo.begin(); fmap != ghginfo.end(); ++fmap ) {
+	for ( CSummaryIterator fmap = ghginfo.begin(); fmap != ghginfo.end(); ++fmap ) {
 		emissfuel[ fmap->first ] += fmap->second;
 	}
 }
 
-void Summary::updateemindmap( const map<string, double>& ghginfo ) {
-
+void Summary::updateemindmap( const SummaryItem& ghginfo ) {
 	// map all primary and secondary fuel consumption
-	for ( map<string,double>::const_iterator fmap = ghginfo.begin(); fmap != ghginfo.end(); ++fmap ) {
+	for ( CSummaryIterator fmap = ghginfo.begin(); fmap != ghginfo.end(); ++fmap ) {
 		emissind[ fmap->first ] += fmap->second;
 	}
 }
 
 //! update the map of sequestered amount of emissions
-void Summary::updateSequesteredAmountMap( const map<string, double>& ghginfo ) {
-
+void Summary::updateSequesteredAmountMap( const SummaryItem& ghginfo ) {
 	// map sequestered amount of CO2 for secondary fuels and zTotal
-	for ( map<string,double>::const_iterator fmap = ghginfo.begin(); fmap != ghginfo.end(); ++fmap ) {
+	for ( CSummaryIterator fmap = ghginfo.begin(); fmap != ghginfo.end(); ++fmap ) {
 		sequesteredAmount[ fmap->first ] += fmap->second;
 	}
 }
 
 void Summary::clearfuelcons() {
-	
 	fuelcons.clear();
 	pecons.clear();
 }
 
 void Summary::clearpeprod() {
-
 	peprod.clear();
 	petrade.clear();
 }
 
 void Summary::clearemiss() {
-
 	emission.clear();
 }
 
 void Summary::clearemfuelmap() {
-	
 	emissfuel.clear();
 }
 
