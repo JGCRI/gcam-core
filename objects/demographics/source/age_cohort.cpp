7aec6d4fa60740c741cf86b947158f93310ca613
Josh Lurz <jlurz24@gmail.com>
2005-06-01 22:47:00 +0000

    -Doc fix

--- objects/demographics/source/age_cohort.cpp
+++ objects/demographics/source/age_cohort.cpp
@@ -36,7 +36,8 @@ AgeCohort::AgeCohort() {
     mUpperAgeBound = -1;
 }
 
-//! Default Destructor. Needed becuase we are using auto_ptr so that we avoid destroying incomplete types.
+//! Default Destructor. Needed because we are using auto_ptr so that we avoid
+//! destroying incomplete types.
 AgeCohort::~AgeCohort(){
 }
 
