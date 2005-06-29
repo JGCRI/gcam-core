16ba8d1d14be46c5688761313d5210d90b77852c
Josh Lurz <jlurz24@gmail.com>
2005-06-29 15:14:09 +0000

    -Comment cleanup

--- objects/marketplace/include/market_locator.h
+++ objects/marketplace/include/market_locator.h
@@ -19,18 +19,24 @@
 
 /*!
 * \ingroup Objects
-* \brief This class is responsible for rapidly looking up the location of a market within the marketplace
-* given region and good names.
-* \details This class is designed to efficiently lookup the location of a market within the Marketplace
-* given the region of the object requesting the lookup, and the name of the good they want to lookup 
-* information about. This object is setup initially by the Marketplace through calls to Marketplace::addMarket. 
-* During this function, the Marketplace gives the MarketLocator the name of a market area, a region, a good name,
-* and a lookup number to use if the MarketLocator does not already know the location of the market. The MarketLocator
-* stores this information in a pair of lists. The first list contains nodes which represent market areas. These 
-* nodes each store a list of sectors and their corresponding sector numbers. This first list is only used during
-* the market creation process. The second list stored by the MarketLocator is a list of nodes representing regions,
-* each containing a list of sectors and their market numbers. This is the list which is used to determine a market
-* number from a region name and good name throughout the model run.
+* \brief This class is responsible for rapidly looking up the location of a
+*        market within the marketplace given region and good names.
+* \details This class is designed to efficiently lookup the location of a market
+*          within the Marketplace given the region of the object requesting the
+*          lookup, and the name of the good they want to lookup information
+*          about. This object is setup initially by the Marketplace through
+*          calls to Marketplace::addMarket. During this function, the
+*          Marketplace gives the MarketLocator the name of a market area, a
+*          region, a good name, and a lookup number to use if the MarketLocator
+*          does not already know the location of the market. The MarketLocator
+*          stores this information in a pair of lists. The first list contains
+*          nodes which represent market areas. These nodes each store a list of
+*          sectors and their corresponding sector numbers. This first list is
+*          only used during the market creation process. The second list stored
+*          by the MarketLocator is a list of nodes representing regions, each
+*          containing a list of sectors and their market numbers. This is the
+*          list which is used to determine a market number from a region name
+*          and good name throughout the model run.
 * \author Josh Lurz
 */
 class MarketLocator
@@ -41,20 +47,29 @@ public:
     int addMarket( const std::string& aMarket, const std::string& aRegion, const std::string& aGoodName,
         const int aUniqueNumber );
     int getMarketNumber( const std::string& aRegion, const std::string& aGoodName ) const;
-     //! An identifier returned by the various functions if the market does not exist.
+     
+    //! An identifier returned by the various functions if the market does not
+     //! exist.
     static const int MARKET_NOT_FOUND = -1;
 private:
-    /*! \brief A single node in a list of goods which contains the name of the good and its market location.
+    /*! \brief A single node in a list of goods which contains the name of the
+    *          good and its market location.
     */
     class GoodNode {
     public:
         GoodNode( const std::string& aName, int aMarketNumber );
-        const std::string mName; //!< The good name.
-        const int mNumber; //!< The market number.
+        
+        //! The good name.
+        const std::string mName;
+        
+         //! The market number.
+        const int mNumber;
     };
 
-    /*! \brief A single node in a list of Regions or Markets which contains the name of the Region or Market
-    * and a list of good names and market locations. */
+    /*! \brief A single node in a list of Regions or Markets which contains the
+    *          name of the Region or Market and a list of good names and market
+    *          locations. 
+    */
     class RegionOrMarketNode {
     public:
         RegionOrMarketNode( const std::string& aName );
@@ -70,11 +85,15 @@ private:
         int getMarketNumber( const std::string& aGoodName ) const;
     private:
         typedef std::list<GoodNode*> GoodList;
-        mutable GoodList mGoodList; //!< A list of Good nodes.
-        const std::string mName; //!< The region or market area name.
+        
+        //! A list of Good nodes.
+        mutable GoodList mGoodList;
+
+        //! The region or market area name.
+        const std::string mName;
     };
-    /*! \brief A list of RegionOrMarketNodes which can be searched quickly to find a particular 
-    * region or market.
+    /*! \brief A list of RegionOrMarketNodes which can be searched quickly to
+    *          find a particular region or market.
     * \details This list is stored in alphabetical order for rapid searching.
     */
     class RegionOrMarketList {
@@ -84,10 +103,17 @@ private:
         int getMarketNumber( const std::string& aRegion, const std::string& aGoodName ) const;
         int addToList( const std::string& aName, const std::string& aGoodNameName, int aUniqueNumber );
     private:
-        mutable RegionOrMarketNode* mCachedPosition; //!< The last RegionNode found in a lookup.
-        std::vector<RegionOrMarketNode*> mList; //!< A list of RegionOrMarket nodes.
+        //! The last RegionNode found in a lookup.
+        mutable RegionOrMarketNode* mCachedPosition;
+
+        //! A list of RegionOrMarket nodes.
+        std::vector<RegionOrMarketNode*> mList;
     };
-    std::auto_ptr<RegionOrMarketList> mMarketList; //!< A list of market areas.
-    std::auto_ptr<RegionOrMarketList> mRegionOrMarketList; //!< A list of regions.
+
+    //! A list of market areas.
+    std::auto_ptr<RegionOrMarketList> mMarketList;
+
+    //! A list of regions.
+    std::auto_ptr<RegionOrMarketList> mRegionOrMarketList;
 };
 #endif // _MARKET_LOCATOR_H_
