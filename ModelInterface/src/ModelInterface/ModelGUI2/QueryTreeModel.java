07561aaa12e90934e6128239b606d2a4e9d1809a
Pralit Patel <pralit.patel@pnnl.gov>
2006-07-28 22:15:22 +0000

    Added undo/redo for adding and removeing queries.

--- ModelInterface/src/ModelInterface/ModelGUI2/QueryTreeModel.java
+++ ModelInterface/src/ModelInterface/ModelGUI2/QueryTreeModel.java
@@ -1,12 +1,20 @@
 package ModelInterface.ModelGUI2;
 
+import ModelInterface.InterfaceMain;
 import ModelInterface.ModelGUI2.queries.QueryGenerator;
+import ModelInterface.ModelGUI2.undo.QueryAddRemoveUndoableEdit;
 
 import java.util.ArrayList;
+import java.util.Iterator;
+
 import javax.swing.tree.TreeModel;
 import javax.swing.tree.TreePath;
 import javax.swing.event.TreeModelListener;
 import javax.swing.event.TreeModelEvent;
+
+import javax.swing.undo.UndoManager;
+import javax.swing.undo.UndoableEdit;
+
 import org.w3c.dom.Node;
 import org.w3c.dom.NodeList;
 import org.w3c.dom.Element;
@@ -14,10 +22,10 @@ import org.w3c.dom.Document;
 
 public class QueryTreeModel implements TreeModel {
 	protected QueryGroup root;
-	protected ArrayList tmListeners;
+	protected ArrayList<TreeModelListener> tmListeners;
 	public QueryTreeModel(Node n) {
 		root = new QueryGroup("All", recCreateTree(n));
-		tmListeners = new ArrayList();
+		tmListeners = new ArrayList<TreeModelListener>();
 		addTreeModelListener(new QueryTreeModelListener());
 	}
 	protected ArrayList recCreateTree(Node n) {
@@ -76,40 +84,53 @@ public class QueryTreeModel implements TreeModel {
 		tmListeners.remove(l);
 	}
 	public void valueForPathChanged(TreePath path, Object newValue) {
-		// error? the use should not be alowed to change values in the tree directly
+		// error? the user should not be alowed to change values in the tree directly
 	}
 	public void add(TreePath path, QueryGenerator qg) {
-		QueryGroup parent;
-		if(path.getLastPathComponent() instanceof QueryGroup) {
-			parent = (QueryGroup)path.getLastPathComponent();
-		} else {
-			parent = (QueryGroup)path.getParentPath().getLastPathComponent();
-		}
-		parent.getQueryList().add(qg);
+		addValue(path, qg);
 	}
 	public void add(TreePath path, String gN) {
-		QueryGroup parent;
+		addValue(path, new QueryGroup(gN, new ArrayList()));
+	}
+	private void addValue(TreePath path, Object val) {
+		TreePath parentPath;
 		if(path.getLastPathComponent() instanceof QueryGroup) {
-			parent = (QueryGroup)path.getLastPathComponent();
+			parentPath = path;
 		} else {
-			parent = (QueryGroup)path.getParentPath().getLastPathComponent();
+			parentPath = path.getParentPath();
 		}
-		parent.getQueryList().add(new QueryGroup(gN, new ArrayList()));
+		QueryGroup parent = (QueryGroup)parentPath.getLastPathComponent();
+		int[] index = new int[1];
+		Object[] children = new Object[1];
+		children[0] = val;
+		index[0] = parent.getQueryList().size();
+		parent.getQueryList().add(val);
+		fireTreeNodesInserted(this, parentPath, index, children);
 	}
 	public void remove(TreePath path) {
-		if(path.getLastPathComponent() instanceof QueryGroup) {
+		Object[] lastComp = new Object[1];
+		int[] index;
+		lastComp[0] = path.getLastPathComponent();
+		if(lastComp[0] instanceof QueryGroup) {
 			// ask sure to remove all of this group
 		}
-		if(path.getLastPathComponent().equals(root)) {
+		if(lastComp[0].equals(root)) {
+			Object[] children = root.getQueryList().toArray();
+			index = new int[children.length];
+			for(int i = 0; i < index.length; ++i) {
+				index[i] = 1;
+			}
 			root.getQueryList().clear();
+			fireTreeNodesRemoved(this, path, index, children);
 		} else {
-			((QueryGroup)path.getParentPath().getLastPathComponent()).getQueryList().remove(path.getLastPathComponent());
+			index = new int[1];
+			index[0] = ((QueryGroup)path.getParentPath().getLastPathComponent()).getQueryList().indexOf(lastComp[0]);
+			((QueryGroup)path.getParentPath().getLastPathComponent()).getQueryList().remove(index[0]);
+			fireTreeNodesRemoved(this, path.getParentPath(), index, lastComp);
 		}
 	}
 	public Node getAsNode(Document doc) {
-		//Node ret = doc.createElement("queries");
 		addQueryGroup(doc, doc.getDocumentElement(), root);
-		//return ret;
 		return doc.getDocumentElement();
 	}
 	protected void addQueryGroup(Document doc, Node currNode, QueryGroup currGroup) {
@@ -125,7 +146,31 @@ public class QueryTreeModel implements TreeModel {
 			currNode.appendChild(tempNode);
 		}
 	}
-	protected class QueryGroup {
+	public void fireTreeNodesChanged(Object source, TreePath path, int[] childIndices, Object[] children) {
+		TreeModelEvent e = new TreeModelEvent(source, path, childIndices, children);
+		for(Iterator<TreeModelListener> it = tmListeners.iterator(); it.hasNext(); ) {
+			it.next().treeNodesChanged(e);
+		}
+	}
+	public void fireTreeNodesInserted(Object source, TreePath path, int[] childIndices, Object[] children) {
+		TreeModelEvent e = new TreeModelEvent(source, path, childIndices, children);
+		for(Iterator<TreeModelListener> it = tmListeners.iterator(); it.hasNext(); ) {
+			it.next().treeNodesInserted(e);
+		}
+	}
+	public void fireTreeNodesRemoved(Object source, TreePath path, int[] childIndices, Object[] children) {
+		TreeModelEvent e = new TreeModelEvent(source, path, childIndices, children);
+		for(Iterator<TreeModelListener> it = tmListeners.iterator(); it.hasNext(); ) {
+			it.next().treeNodesRemoved(e);
+		}
+	}
+	public void fireTreeStructureChanged(Object source, TreePath path, int[] childIndices, Object[] children) {
+		TreeModelEvent e = new TreeModelEvent(source, path, childIndices, children);
+		for(Iterator<TreeModelListener> it = tmListeners.iterator(); it.hasNext(); ) {
+			it.next().treeStructureChanged(e);
+		}
+	}
+	public class QueryGroup {
 		ArrayList qList;
 		String groupName;
 		public QueryGroup(String name, ArrayList list) {
@@ -143,18 +188,31 @@ public class QueryTreeModel implements TreeModel {
 		}
 	}
 	protected class QueryTreeModelListener implements TreeModelListener {
+		private UndoManager undoManager;
+		private InterfaceMain main;
+		public QueryTreeModelListener() {
+			main = InterfaceMain.getInstance();
+			undoManager = main.getUndoManager();
+		}
 		public void treeNodesChanged(TreeModelEvent e) {
 			// don't care about this
 		}
 		public void treeNodesInserted(TreeModelEvent e) {
-			System.out.println("Insert being called from "+e.getSource());
+			if(e.getSource() instanceof QueryTreeModel) {
+				addEdit(new QueryAddRemoveUndoableEdit(e, true));
+			}
 		}
 		public void treeNodesRemoved(TreeModelEvent e) {
-			System.out.println("Remove being called from "+e.getSource());
-			// create undoable edit
+			if(e.getSource() instanceof QueryTreeModel) {
+				addEdit(new QueryAddRemoveUndoableEdit(e, false));
+			}
 		}
 		public void treeStructureChanged(TreeModelEvent e) {
 			// don't care about this
+		}
+		private void addEdit(UndoableEdit e) {
+			undoManager.addEdit(e);
+			main.refreshUndoRedo();
 		}
 	}
 }
