692415114a55387c8f4e05086820e2dec91a1d59
Patrick Luckow <pluckow@synapse-energy.com>
2010-07-06 18:29:20 +0000

    Fixes primary energy queries for multiple electric sectors (Proposal 053)

--- Main_User_Workspace/cvs/objects/util/base/include/version.h
+++ Main_User_Workspace/cvs/objects/util/base/include/version.h
@@ -43,7 +43,7 @@
  * NOTE: ADD 1 TO LATEST SUBVERSION REVISION NUMBER
  */
 //! The latest SVN revision number for identification of the build.
-#define __REVISION_NUMBER__ "3726" 
+#define __REVISION_NUMBER__ "3727" 
 /*****************************************************************************/
 
 //! GCAM model version.
