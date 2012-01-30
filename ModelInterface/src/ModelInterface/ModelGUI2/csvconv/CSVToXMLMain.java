f467dc0a62514205c105201357239f15196834d6
Pralit Patel <pralit.patel@pnnl.gov>
2012-01-30 21:15:44 +0000

    Add the ability run batch CSV conversion from the stand alone commandline CSVToXMLMain.

--- ModelInterface/src/ModelInterface/ModelGUI2/csvconv/CSVToXMLMain.java
+++ ModelInterface/src/ModelInterface/ModelGUI2/csvconv/CSVToXMLMain.java
@@ -1,12 +1,12 @@
 package ModelInterface.ModelGUI2.csvconv;
 
 import java.io.File;
+import java.net.URI;
 import java.io.FileInputStream;
 import java.io.DataInputStream;
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
 import java.io.FileWriter;
-import java.io.File;
 
 import java.util.ArrayList;
 import java.util.HashMap;
@@ -16,8 +16,13 @@ import javax.swing.JOptionPane;
 import javax.swing.JFrame;
 
 import org.w3c.dom.Document;
+import org.w3c.dom.Node;
+import org.w3c.dom.NodeList;
+import org.w3c.dom.Element;
 import com.sun.org.apache.xml.internal.serialize.OutputFormat;
 import com.sun.org.apache.xml.internal.serialize.XMLSerializer;
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
 
 /**
  * A stand alone driver to run the CSV to XML conversion tool.
@@ -31,40 +36,48 @@ import com.sun.org.apache.xml.internal.serialize.XMLSerializer;
 public class CSVToXMLMain {
     public static void main(String[] args) {
         if(args.length < 3) {
-            System.err.println("Usage: CSVToXMLMain <CSV file> [<CSV file> ..] <header file> <output XML file>");
-            System.exit(1);
         }
         try {
-            File xmlOutputFile = new File(args[args.length - 1]);
-            File headerFile = new File(args[args.length - 2]);
-            File[] csvFiles = new File[args.length - 2];
-            for(int i = 0; i < args.length - 2; ++i) {
-                csvFiles[i] = new File(args[i]);
+            if(args.length == 1) {
+                // Assuming we are running a batch file
+                System.out.println("Running batch file: "+args[0]);
+                runFromBatch(new File(args[0]));
+            } else if(args.length >= 3) {
+                // Assuming we are getting the conversion file names directly from the argument list
+                File xmlOutputFile = new File(args[args.length - 1]);
+                File headerFile = new File(args[args.length - 2]);
+                File[] csvFiles = new File[args.length - 2];
+                for(int i = 0; i < args.length - 2; ++i) {
+                    csvFiles[i] = new File(args[i]);
+                }
+                Document doc = runCSVConversion(csvFiles, headerFile, null);
+                writeFile(xmlOutputFile, doc);
+            } else {
+                System.err.println("Usage: CSVToXMLMain <CSV file> [<CSV file> ..] <header file> <output XML file>");
+                System.err.println("   or: CSVToXMLMain <batch file>");
+                System.exit(1);
             }
-            Document doc = runCSVConversion(csvFiles, headerFile, null);
-            writeFile(xmlOutputFile, doc);
         } catch (Exception e) {
             e.printStackTrace();
             System.exit(1);
         }
-
     }
 
     /**
-	 * Takes a CSV file, and Headers file, then processes the files by building
-	 * a new tree with the DOMTreeBuilder class.  The resulting XML document is
+     * Takes a CSV file, and Headers file, then processes the files by building
+     * a new tree with the DOMTreeBuilder class.  The resulting XML document is
      * returned
-	 * 
-	 * @param csvFiles
-	 *            the CSV files
-	 * @param headerFile
-	 *            the Headers file
+     * 
+     * @param csvFiles
+     *            the CSV files
+     * @param headerFile
+     *            the Headers file
      * @param parentFrame
      *            A GUI frame which may be used to display error messages to
      *            if it is not null.
      * @return
      *            The generated XML dom document
-	 */
+     */
     public static Document runCSVConversion(File[] csvFiles, File headerFile, JFrame parentFrame) {
         StringTokenizer st;
         String intValueStr = null;
@@ -240,40 +253,118 @@ public class CSVToXMLMain {
     }
 
     /**
-	 * Writes the DOM document to the specified file.
+     * Writes the DOM document to the specified file.
      * Note this is used in many places and should really be in a utility
      * however in this case we need that utility to have no extra library
      * dependencies.
-	 * 
-	 * @param file
-	 *            where the XML tree will be written to
-	 * @param thDoc
-	 *            the tree that should be written
-	 * @return whether the file was actually written or not
-	 */
-	public static boolean writeFile(File file, Document theDoc) {
-		// specify output formating properties
-		OutputFormat format = new OutputFormat(theDoc);
-		format.setEncoding("UTF-8");
-		format.setLineSeparator("\r\n");
-		format.setIndenting(true);
-		format.setIndent(3);
-		format.setLineWidth(0);
-		format.setPreserveSpace(false);
-		format.setOmitDocumentType(true);
+     * 
+     * @param file
+     *            where the XML tree will be written to
+     * @param thDoc
+     *            the tree that should be written
+     * @return whether the file was actually written or not
+     */
+    public static boolean writeFile(File file, Document theDoc) {
+        // specify output formating properties
+        OutputFormat format = new OutputFormat(theDoc);
+        format.setEncoding("UTF-8");
+        format.setLineSeparator("\r\n");
+        format.setIndenting(true);
+        format.setIndent(3);
+        format.setLineWidth(0);
+        format.setPreserveSpace(false);
+        format.setOmitDocumentType(true);
 
-		// create the searlizer and have it print the document
+        // create the searlizer and have it print the document
 
-		try {
-			FileWriter fw = new FileWriter(file);
-			XMLSerializer serializer = new XMLSerializer(fw, format);
-			serializer.asDOMSerializer();
-			serializer.serialize(theDoc);
-			fw.close();
-		} catch (java.io.IOException e) {
-			System.err.println("Error outputing tree: " + e);
-			return false;
-		}
-		return true;
-	}
+        try {
+            FileWriter fw = new FileWriter(file);
+            XMLSerializer serializer = new XMLSerializer(fw, format);
+            serializer.asDOMSerializer();
+            serializer.serialize(theDoc);
+            fw.close();
+        } catch (java.io.IOException e) {
+            System.err.println("Error outputing tree: " + e);
+            return false;
+        }
+        return true;
+    }
+
+    /**
+     * Run CSV to XML conversion(s) by reading header, csv, and output filenames from a
+     * ModelInterface style batch command.  This code is adapated from
+     * ModelInterface.ModelGUI2.InputViewer.runBatch which could not be called directly
+     * to avoid dependencies.  Note many conversions could be specified in a single batch
+     * file.  Non CSV conversion commands will produce a warning and skipped.
+     * @param batchFile The batch file to parse and run.
+     * @throws Exception Any error from parsing the batch file or during a conversion.
+     */
+    private static void runFromBatch(File batchFile) throws Exception {
+        // Convert the file path to a URI before parsing to
+        // ensure the document has a valid heirarchical URI.
+        final URI docURI = batchFile.toURI();
+        
+        // Attempt to parse the document.
+        DocumentBuilder parser = DocumentBuilderFactory.newInstance().newDocumentBuilder();
+        Document loadedDocument = parser.parse(docURI.getPath());
+
+        // Parse the batch file looking for CSV file commands and parsing out the header, csv,
+        // and output files.
+        NodeList rootChildren = loadedDocument.getDocumentElement().getChildNodes();
+        for(int rootIndex = 0; rootIndex < rootChildren.getLength(); ++rootIndex) {
+            Node currClassNode = rootChildren.item(rootIndex);
+            // only interested in InputViewer which is the class typically used to do the converion
+            if(currClassNode.getNodeType() != Node.ELEMENT_NODE) {
+                continue;
+            }
+            if(currClassNode.getNodeName().equals("class") && ((Element)currClassNode).getAttribute("name").equals("ModelInterface.ModelGUI2.InputViewer")) {
+                NodeList commands = currClassNode.getChildNodes();
+                for(int commandIndex = 0; commandIndex < commands.getLength(); ++commandIndex ) {
+                    Node command = commands.item(commandIndex);
+                    // Only interesed in the action CSV file
+                    if(command.getNodeType() != Node.ELEMENT_NODE) {
+                        continue;
+                    }
+                    String actionCommand = ((Element)command).getAttribute("name");
+                    if(actionCommand == null) {
+                        continue;
+                    }
+
+                    if(actionCommand.equals("CSV file")) {
+                        File headerFile = null;
+                        File outFile = null;
+                        ArrayList<File> csvFiles = new ArrayList<File>();
+                        // read file names for header file, csv files, and the output file
+                        NodeList fileNameChildren = command.getChildNodes();
+                        for(int j = 0; j < fileNameChildren.getLength(); ++j) {
+                            Node fileNode = fileNameChildren.item(j);
+                            if(fileNode.getNodeType() != Node.ELEMENT_NODE) {
+                                continue;
+                            }
+                            File tempFile = new File(fileNode.getTextContent());
+                            // find header, csv, and output files
+                            if(fileNode.getNodeName().equals("headerFile")) {
+                                headerFile = tempFile;
+                            } else if(fileNode.getNodeName().equals("outFile")) {
+                                outFile = tempFile;
+                            } else if(fileNode.getNodeName().equals("csvFile")) {
+                                csvFiles.add(tempFile);
+                            } else {
+                                System.out.println("Unknown tag while parsing CSV file command: "+fileNode.getNodeName());
+                            }
+                        }
+                        // run the conversion and save the results
+                        File[] csvFilesArr = new File[csvFiles.size()];
+                        csvFilesArr = csvFiles.toArray(csvFilesArr);
+                        Document doc = runCSVConversion(csvFilesArr, headerFile, null);
+                        writeFile(outFile, doc);
+                    } else {
+                        System.out.println("Invalid command: "+actionCommand+", only CSV file can be run in this mode");
+                    }
+                }
+            } else {
+                System.out.println("Invalid class, only ModelInterface.ModelGUI2.InputViewer can be run in this mode");
+            }
+        }
+    }
 }
