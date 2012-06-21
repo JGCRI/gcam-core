ec5f0e045563aa6ae2c9244a621b4cc80564b89e
Pralit Patel <pralit.patel@pnnl.gov>
2012-06-21 15:49:56 +0000

    Clean up old code which is now part of the ModelInterface

--- ConfigurationEditor/manifest.mk
+++
@@ -1,4 +0,0 @@
-Manifest-Version: 1.0
-Class-Path: ./jars/xalan.jar ./jars/xercesImpl.jar ./jars/xml-apis.jar ./jars/elall.jar ./jars/elcore.jar ./jars/explicit.jar
-Main-Class: configurationeditor.Main
-
