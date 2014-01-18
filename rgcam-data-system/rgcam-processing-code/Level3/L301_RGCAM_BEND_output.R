daa2368969691fb3b7725d2355f3435fba7f4acc
Page Kyle <pkyle@pnnl.gov>
2014-01-18 03:08:30 +0000

    BEND post-processing update

--- rgcam-data-system/rgcam-processing-code/Level3/L301_RGCAM_BEND_output.R
+++ rgcam-data-system/rgcam-processing-code/Level3/L301_RGCAM_BEND_output.R
@@ -13,6 +13,7 @@ state_region_ind <- readmap( "state_region_ind" )
 RGCAM_BEND_technology <- readdata( "RGCAM_BEND_technology" )
 RGCAM_bld_energy <- readdata( "RGCAM_bld_energy" )
 RGCAM_bld_service <- readdata( "RGCAM_bld_service" )
+RGCAM_indtrn_elec <- readdata( "RGCAM_indtrn_elec" )
 
 # -----------------------------------------------------------------------------
 # 2. Aggregations and calculations
@@ -85,6 +86,30 @@ L301_RGCAM_BEND_energy_Tbtu_2005HVAC <- aggregate( L301_RGCAM_BEND_energy_Tbtu[
 L301_RGCAM_BEND_energy_Tbtu_2005 <- rbind( L301_RGCAM_BEND_energy_Tbtu_2005HVAC, L301_RGCAM_BEND_energy_Tbtu_2005 )
 L301_RGCAM_BEND_energy_Tbtu_2005 <- cast( L301_RGCAM_BEND_energy_Tbtu_2005, state_name + BEND_sector + BEND_service ~ BEND_fuel, value = "X2005" )
 
+#Non-buildings electricity use. Need to write out the comm non-building, transportation, and industry. Keep separate for now
+L301_RGCAM_nonbld_elec <- subset( RGCAM_bld_energy, sector == "comm non-building" & subsector == "electricity" )
+L301_RGCAM_nonbld_elec$BEND_sector <- "Commercial non-building"
+L301_RGCAM_nonbld_elec$BEND_fuel <- "electricity"
+L301_RGCAM_nonbld_elec <- L301_RGCAM_nonbld_elec[ c( "state_name", "BEND_sector", "BEND_fuel", X_GCAM_out_years ) ]
+
+#For industry and transportation, first need to remove the repeated pass-through sectors to avoid double counting
+RGCAM_indtrn_elec <- subset( RGCAM_indtrn_elec, !sector %in% c( "elect_td_ind", "elect_td_trn" ) )
+RGCAM_indtrn_elec$state_name <- state_region_ind$state_name[ match( RGCAM_indtrn_elec$region, state_region_ind$state ) ]
+RGCAM_indtrn_elec$BEND_sector[ grepl( "trn", RGCAM_indtrn_elec$sector ) ] <- "Transportation"
+RGCAM_indtrn_elec$BEND_sector[ !grepl( "trn", RGCAM_indtrn_elec$sector ) ] <- "Industry"
+RGCAM_indtrn_elec$BEND_fuel <- "Electricity"
+L301_RGCAM_indtrn_elec <- aggregate( RGCAM_indtrn_elec[ X_GCAM_out_years ],
+      by=as.list( RGCAM_indtrn_elec[ c( "state_name", "BEND_sector", "BEND_fuel" ) ] ), sum )
+
+#Merge these tables (commercial non-building, industry, and transportation) and convert the unit
+L301_RGCAM_BEND_other_elec <- rbind( L301_RGCAM_nonbld_elec, L301_RGCAM_indtrn_elec )
+L301_RGCAM_BEND_other_elec[ X_GCAM_out_years ] <- round( L301_RGCAM_BEND_other_elec[ X_GCAM_out_years ] / conv_Tbtu_EJ, BEND_Tbtu_digits )
+
+#Write out the service demands, aggregated by state and sector and service. Heating and cooling only.
+L301_RGCAM_BEND_service_agg_Tbtu <- aggregate( L301_RGCAM_BEND_service_Tbtu[ X_GCAM_out_years ],
+      by=as.list( L301_RGCAM_BEND_service_Tbtu[ c( "state_name", "BEND_sector", "BEND_service" ) ] ), sum )
+L301_RGCAM_BEND_service_agg_Tbtu <- subset( L301_RGCAM_BEND_service_agg_Tbtu, BEND_service %in% c( "Heating", "Cooling", "Ventilation" ) )
+
 # -----------------------------------------------------------------------------
 # 3. Output
 #Add comments for each table
@@ -92,12 +117,15 @@ comments.L301_RGCAM_BEND_energy_Tbtu <- c( "Energy consumption by state / sector
 comments.L301_RGCAM_BEND_efficiency <- c( "Energy efficiency by state / sector / service / fuel / technology","Unitless COP" )
 comments.L301_RGCAM_BEND_shares <- c( "Service shares by fuel / technology within state / sector / service","Unitless portion" )
 comments.L301_RGCAM_BEND_energy_Tbtu_2005 <- c( "Energy consumption by state / sector / selected service / fuel","Trillion btu/yr" )
+comments.L301_RGCAM_BEND_other_elec <- c( "Electricity consumption by state / non-building sector","Trillion btu/yr" )
+comments.L301_RGCAM_BEND_service_agg_Tbtu <- c( "Service demand by state / sector / selected service","Trillion btu/yr" )
 
 #write tables as CSV files
 writedata( L301_RGCAM_BEND_energy_Tbtu,fn="L301_RGCAM_BEND_energy_Tbtu", comments=comments.L301_RGCAM_BEND_energy_Tbtu )
 writedata( L301_RGCAM_BEND_efficiency,fn="L301_RGCAM_BEND_efficiency", comments=comments.L301_RGCAM_BEND_efficiency )
 writedata( L301_RGCAM_BEND_shares,fn="L301_RGCAM_BEND_shares", comments=comments.L301_RGCAM_BEND_shares )
-writedata( L301_RGCAM_BEND_energy_Tbtu_2005,fn="L301_RGCAM_BEND_energy_Tbtu_2005", comments=comments.L301_RGCAM_BEND_energy_Tbtu_2005 )
+writedata( L301_RGCAM_BEND_other_elec,fn="L301_RGCAM_BEND_other_elec", comments=comments.L301_RGCAM_BEND_other_elec )
+writedata( L301_RGCAM_BEND_service_agg_Tbtu,fn="L301_RGCAM_BEND_service_agg_Tbtu", comments=comments.L301_RGCAM_BEND_service_agg_Tbtu )
 
 # Every script should finish with this line
 logstop()
