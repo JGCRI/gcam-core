990ca707ce13a2c2e0df2a4a09f39163c5eb0944
Pralit Patel <pralit.patel@pnnl.gov>
2016-10-10 13:01:38 -0400

    Bump GCAM version number and ability to print the version and exit via command line arguments.

--- cvs/objects/util/base/include/version.h
+++ cvs/objects/util/base/include/version.h
@@ -41,10 +41,10 @@
  * NOTE: ADD 1 TO LATEST SUBVERSION REVISION NUMBER
  */
 //! The latest SVN revision number for identification of the build.
-#define __REVISION_NUMBER__ "6539" 
+#define __REVISION_NUMBER__ "gcam-v4.3" 
 /*****************************************************************************/
 
 //! GCAM model version.
-#define __ObjECTS_VER__ "4.1" 
+#define __ObjECTS_VER__ "4.3" 
 
 #endif // _VERSION_H_ 
