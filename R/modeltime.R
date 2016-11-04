4ced54315e604512fee76570ee135d33b16ecdf5
Ben Bond-Lamberty <bondlamberty@pnnl.gov>
2016-11-04 09:17:55 -0400

    Re-implement `modeltime`, with test code

--- R/modeltime.R
+++ R/modeltime.R
@@ -1,16 +1,44 @@
 
-#' Title
+#' modeltime
 #'
-#' Description
+#' Construct the \code{modeltime} data structures
 #'
 #' @param argument_name description of the argument
-#' @return
+#' @return A named list with all modeltime components
 #' @keywords
 #' @seealso
-#' @author
+#' @author BBL
 #' @export
 #'
 #' @examples
 modeltime <- function() {
-  exists("modeltime.test")
+
+  # Calculate the read-in timesteps in the model
+  GCAM_timesteps <- diff(modeltime.YEARS)
+
+  L200.ModelTime <- tibble(start.year.timestep    = GCAM_timesteps[1],
+                           start.year             = min(modeltime.BASE_YEARS),
+                           final.calibration.year = max(modeltime.BASE_YEARS),
+                           end.year               = max(modeltime.FUTURE_YEARS))
+
+  changeyears <- c(FALSE, GCAM_timesteps[-1] != GCAM_timesteps[-length(GCAM_timesteps)])
+  GCAM_interyears <- modeltime.YEARS[changeyears]
+  GCAM_interyear.timesteps <- GCAM_timesteps[changeyears]
+
+  L200.ModelTimeInterYears <- tibble(inter.year.timestep = GCAM_interyear.timesteps,
+                                     inter.year          = GCAM_interyears)
+
+  L200.MAGICC <- tibble(last.historical.year    = modeltime.MAGICC_LAST_HISTORICAL_YEAR,
+                        bc.unit.forcing         = modeltime.MAGICC_BC_UNIT_FORCING,
+                        carbon.model.start.year = modeltime.MAGICC_C_START_YEAR)
+
+  L200.hector <- tibble(hector.end.year         = modeltime.HECTOR_END_YEAR,
+                        emissions.switch.year   = modeltime.HECTOR_EMISSIONS_YEAR,
+                        hector.ini.file         = modeltime.HECTOR_INI_FILE,
+                        carbon.model.start.year = modeltime.MAGICC_C_START_YEAR)
+
+  list("L200.ModelTime" = L200.ModelTime,
+       "L200.ModelTimeInterYears" = L200.ModelTimeInterYears,
+       "L200.MAGICC" = L200.MAGICC,
+       "L200.hector" = L200.hector)
 }
