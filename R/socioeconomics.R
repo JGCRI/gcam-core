73ff42e98079242bdd9c1a973e326e23c3529c09
Ben Bond-Lamberty <bondlamberty@pnnl.gov>
2016-12-01 21:14:05 -0500

    Added socioeconomics chunks
    One reads a file off disk and puts it into the driver’s data list

---
+++ R/socioeconomics.R
@@ -0,0 +1,75 @@
+
+
+#' module_socioeconomics_inputs
+#'
+#' Load the \code{socioeconomics} input data
+#'
+#' @param command API command to execute
+#' @param ... other optional parameters, depending on command
+#' @return Depends on \code{command}
+#' @author BBL
+#' @export
+module_socioeconomics_inputs <- function(command, ...) {
+  if(command == driver.DECLARE_INPUTS) {
+    return(NULL)   # module_socioeconomics_inputs doesn't depend on anything
+  } else if(command == driver.DECLARE_OUTPUTS) {
+    return("socioeconomics.USDA_GDP_MER")
+  } else if(command == driver.MAKE) {
+    # Read an input file and return data
+    # TODO: obviously this will be extracted to a load function later
+    fqfn <- system.file("extdata", "USDA_GDP_MER.csv", package = "gcamdata")
+    df <- readr::read_csv(fqfn, comment = "#")
+    return(list("socioeconomics.USDA_GDP_MER" = df))
+  } else {
+    stop("Unknown command")
+  }
+}
+
+
+#' module_socioeconomics_L100.GDP_hist
+#'
+#' Construct the \code{socioeconomics} data structures
+#'
+#' @param command API command to execute
+#' @param ... other optional parameters, depending on command
+#' @return Depends on \code{command}
+#' @author BBL
+#' @export
+module_socioeconomics_L100.GDP_hist <- function(command, ...) {
+  if(command == driver.DECLARE_INPUTS) {
+    return("socioeconomics.USDA_GDP_MER")
+  } else if(command == driver.DECLARE_OUTPUTS) {
+    return("L100.gdp_mil90usd_ctry_Yh")
+  } else if(command == driver.MAKE) {
+    socioeconomics_L100.GDP_hist_makedata(...)
+  } else {
+    stop("Unknown command")
+  }
+}
+
+
+#' socioeconomics_L100.GDP_hist_makedata
+#'
+#' @param all_data A named list, holding all data system products so far
+#' @return A named list with all socioeconomics data
+#' @importFrom assertthat assert_that
+socioeconomics_L100.GDP_hist_makedata <- function(all_data) {
+
+  #printlog( "Historical GDP downscaled to modern country" )
+  USDA_GDP_MER <- all_data[["socioeconomics.USDA_GDP_MER"]]
+  assert_that(is.data.frame(USDA_GDP_MER))
+  # At present the GDP database used requires no downscaling and all
+  # major countries are included, so really no processing steps are needed.
+  # All that happens in this file right now is subsetting the years that
+  # will be required by later files, and converting the units to GCAM's
+  # GDP unit (million 1990 USD)
+  #  L100.gdp_mil90usd_ctry_Yh <- na.omit(
+  #   data.frame(
+  #     USDA_GDP_MER[ "iso" ],
+  #     USDA_GDP_MER[ X_historical_years ] * conv_bil_mil / conv_1990_2005_USD ) )
+  #
+  # comments.L100.gdp_mil90usd_ctry_Yh <- c( "Historical GDP downscaled to country (iso)","Unit = million 1990 US dollars" )
+
+  #  list("L100.gdp_mil90usd_ctry_Yh" = L100.gdp_mil90usd_ctry_Yh)
+}
+
