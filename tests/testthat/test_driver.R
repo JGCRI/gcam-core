77d4d0d552fd3dbf803d26ccf621f8d41b210c81
Ben Bond-Lamberty <bondlamberty@pnnl.gov>
2016-12-06 05:17:26 -0500

    Full driver test (may get expensive later)

--- tests/testthat/test_driver.R
+++ tests/testthat/test_driver.R
@@ -8,3 +8,10 @@ test_that("errors with bad input", {
   expect_error(driver(1))
   expect_error(driver("1"))
 })
+
+test_that("runs", {
+  capture_output({
+    out <- driver()
+  })
+  expect_true(is.list(out))
+})
