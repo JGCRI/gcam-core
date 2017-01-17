1c9b8411e644a2ec798791f8fc71c7095fd873bf
Ryna Cui <yiyun.cui@pnnl.gov>
2017-01-17 16:01:16 -0500

    US producer price of ag commodities is calibrated to 2010 data (updated from FAO), aggregation is weighted by 2010 production (level0), fodder crops and pasture price is calibrated to 2009 data (level0)

--- input/gcam-data-system/aglu-processing-code/level1/LB132.ag_an_For_Prices_USA_C_2005.R
+++ input/gcam-data-system/aglu-processing-code/level1/LB132.ag_an_For_Prices_USA_C_2005.R
@@ -25,10 +25,12 @@ sourcedata( "COMMON_ASSUMPTIONS", "unit_conversions", extension = ".R" )
 sourcedata( "AGLU_ASSUMPTIONS", "A_aglu_data", extension = ".R" )
 FAO_ag_items_PRODSTAT <- readdata( "AGLU_MAPPINGS", "FAO_ag_items_PRODSTAT" )
 FAO_an_items_PRODSTAT <- readdata( "AGLU_MAPPINGS", "FAO_an_items_PRODSTAT" )
-FAO_USA_ag_an_P_USDt_PRICESTAT <- readdata( "AGLU_LEVEL0_DATA", "FAO_USA_ag_an_P_USDt_PRICESTAT" )
+# Use updated US.PP data from FAO
+FAO_USA_ag_an_P_USDt_PRICESTAT <- readdata( "AGLU_LEVEL0_DATA", "FAO_USA_ag_an_P_USDt_PRICESTAT_updated" )
 FAO_USA_For_Exp_t_USD_FORESTAT <- readdata( "AGLU_LEVEL0_DATA", "FAO_USA_For_Exp_t_USD_FORESTAT" )
 USDA_Alfalfa_prices_USDt <- readdata( "AGLU_LEVEL0_DATA", "USDA_Alfalfa_prices_USDt" )
-L100.FAO_ag_Prod_t <- readdata( "AGLU_LEVEL1_DATA", "L100.FAO_ag_Prod_t" )
+# Use level0 production data instead of level1 with the 5-yr rolling average
+FAO_ag_Prod_t_PRODSTAT <- readdata( "AGLU_LEVEL0_DATA", "FAO_ag_Prod_t_PRODSTAT" )
 FAO_USA_an_Prod_t_PRODSTAT <- readdata( "AGLU_LEVEL0_DATA", "FAO_USA_an_Prod_t_PRODSTAT" )
 
 # -----------------------------------------------------------------------------
@@ -53,16 +55,16 @@ FAO_USA_ag_an_P_USDt_PRICESTAT[
 #Subset only the relevant country/item combinations from the ag prodstat database
 #NOTE: This mostly excludes fodder crops and "not elsewhere specified" crops. It also excludes about seven crops that are
 #minor enough to not distort price estimates, but their omission may cause problems if a large number of commodities is used
-L132.FAO_ag_Prod_t <- L100.FAO_ag_Prod_t[
-      vecpaste( L100.FAO_ag_Prod_t[ c( "countries", "item" ) ] ) %in%
+L132.FAO_ag_Prod_t <- FAO_ag_Prod_t_PRODSTAT[
+      vecpaste( FAO_ag_Prod_t_PRODSTAT[ c( "countries", "item" ) ] ) %in%
       vecpaste( FAO_USA_ag_an_P_USDt_PRICESTAT[ c( "countries", "item" ) ] ), ]
 
-printlog( "Computing average prices and production quantities of all commodities" )
-FAO_USA_ag_an_P_USDt_PRICESTAT$avg <- rowMeans( FAO_USA_ag_an_P_USDt_PRICESTAT[ X_model_price_years ], na.rm = TRUE )
-FAO_USA_ag_an_P_USDt_PRICESTAT$avg[ FAO_USA_ag_an_P_USDt_PRICESTAT$item == "Oil palm fruit" ] <- cost_OilPalmFruit_05USDt
-L132.FAO_ag_Prod_t$avg <- rowMeans( L132.FAO_ag_Prod_t[ X_model_price_years ], na.rm = TRUE )
-FAO_USA_an_Prod_t_PRODSTAT$avg <- rowMeans( FAO_USA_an_Prod_t_PRODSTAT[ X_model_price_years ], na.rm = TRUE )
-FAO_USA_For_Exp_t_USD_FORESTAT$avg <- rowMeans( FAO_USA_For_Exp_t_USD_FORESTAT[ X_model_price_years ], na.rm = TRUE )
+printlog( "Using 2010 prices and production quantities of all commodities" )
+FAO_USA_ag_an_P_USDt_PRICESTAT$avg <- FAO_USA_ag_an_P_USDt_PRICESTAT$X2010
+#FAO_USA_ag_an_P_USDt_PRICESTAT$avg[ FAO_USA_ag_an_P_USDt_PRICESTAT$item == "Oil palm fruit" ] <- cost_OilPalmFruit_05USDt
+L132.FAO_ag_Prod_t$avg <- L132.FAO_ag_Prod_t$X2010
+FAO_USA_an_Prod_t_PRODSTAT$avg <- FAO_USA_an_Prod_t_PRODSTAT$X2010
+FAO_USA_For_Exp_t_USD_FORESTAT$avg <- FAO_USA_For_Exp_t_USD_FORESTAT$X2010
 
 #Remove NA rows
 FAO_USA_ag_an_P_USDt_PRICESTAT <- FAO_USA_ag_an_P_USDt_PRICESTAT[ !is.na( FAO_USA_ag_an_P_USDt_PRICESTAT$avg) , ] 
@@ -73,7 +75,7 @@ FAO_USA_an_Prod_t_PRODSTAT <- FAO_USA_an_Prod_t_PRODSTAT[ !is.na( FAO_USA_an_Pro
 printlog( "Part 1: Primary agricultural goods and animal products" )
 printlog( "Calculating revenue by commodity as production times price" )
 #Primary agricultural goods
-L132.ag_V_USA_Cfao_fby <- data.frame( L132.FAO_ag_Prod_t[ c( "iso", "item" ) ],
+L132.ag_V_USA_Cfao_fby <- data.frame( L132.FAO_ag_Prod_t[ c( "countries", "item" ) ],
       Prod_t = L132.FAO_ag_Prod_t$avg )
 L132.ag_V_USA_Cfao_fby$Price_USDt <- FAO_USA_ag_an_P_USDt_PRICESTAT$avg[
       match( L132.ag_V_USA_Cfao_fby$item, FAO_USA_ag_an_P_USDt_PRICESTAT$item ) ]
@@ -95,20 +97,21 @@ L132.an_V_USA_Cfao_fby[[C]] <- FAO_an_items_PRODSTAT[[C]][ match( L132.an_V_USA_
 #Remove any fodder crops
 L132.ag_V_USA_Cfao_fby <- L132.ag_V_USA_Cfao_fby[ !L132.ag_V_USA_Cfao_fby[[C]] %in% c( "FodderHerb", "FodderGrass" ), ]
 
-#Aggregate by GCAM crop names and compute average prices
+#Aggregate by GCAM crop names and compute average prices, convert to 1975USD
+conv_2010_1975_USD <- round( conv_1990_1975_USD / conv_1990_2010_USD, digits = 4 )
 printlog( "Aggregating by GCAM commodity and computing average prices" )
 #Primary agricultural goods
 L132.ag_V_USA_Cnf_fby <- aggregate( L132.ag_V_USA_Cfao_fby[ c( "Prod_t", "V_USD" ) ], by=as.list( L132.ag_V_USA_Cfao_fby[ C ] ), sum )
-L132.ag_V_USA_Cnf_fby$Price_USDt <- L132.ag_V_USA_Cnf_fby$V_USD / L132.ag_V_USA_Cnf_fby$Prod_t
+L132.ag_V_USA_Cnf_fby$Price_USDt <- L132.ag_V_USA_Cnf_fby$V_USD / L132.ag_V_USA_Cnf_fby$Prod_t * conv_2010_1975_USD
 
 #Animal products
 L132.an_V_USA_C_fby <- aggregate( L132.an_V_USA_Cfao_fby[ c( "Prod_t", "V_USD" ) ], by=as.list( L132.an_V_USA_Cfao_fby[ C ] ), sum )
-L132.an_V_USA_C_fby$Price_USDt <- L132.an_V_USA_C_fby$V_USD / L132.an_V_USA_C_fby$Prod_t
+L132.an_V_USA_C_fby$Price_USDt <- L132.an_V_USA_C_fby$V_USD / L132.an_V_USA_C_fby$Prod_t * conv_2010_1975_USD
 
 printlog( "Part 2: Fodder crops and pasture" )
-#Calculate average FodderHerb prices from alfalfa prices
+#Calculate FodderHerb prices from 2009 alfalfa prices, since 2010 is not available in data system
 L132.ag_V_USA_FodderHerb_fby <- data.frame( GCAM_commodity = "FodderHerb", Prod_t = NA, V_USD = NA,
-      Price_USDt = mean(USDA_Alfalfa_prices_USDt$avg[ USDA_Alfalfa_prices_USDt$year %in% model_price_years ] ) )
+      Price_USDt = mean(USDA_Alfalfa_prices_USDt$avg[ USDA_Alfalfa_prices_USDt$year %in% 2009 ] ) * conv_2009_1975_USD )
 L132.ag_V_USA_FodderGrass_fby <- L132.ag_V_USA_FodderHerb_fby
 L132.ag_V_USA_FodderGrass_fby$GCAM_commodity <- "FodderGrass"
 L132.ag_V_USA_FodderGrass_fby$Price_USDt <- L132.ag_V_USA_FodderHerb_fby$Price_USDt * priceratio_grass_alfalfa
@@ -124,17 +127,17 @@ printlog( "Part 3: Forest products" )
 L132.For_V_USA_fby <- data.frame( GCAM_commodity = "Forest",
       Exp_m3 = FAO_USA_For_Exp_t_USD_FORESTAT$avg[ grepl( "Quantity", FAO_USA_For_Exp_t_USD_FORESTAT$element ) ],
       ExpV_USD = FAO_USA_For_Exp_t_USD_FORESTAT$avg[ grepl( "Value", FAO_USA_For_Exp_t_USD_FORESTAT$element ) ] * 1000 )
-L132.For_V_USA_fby$Price_USDm3 <- L132.For_V_USA_fby$ExpV_USD / L132.For_V_USA_fby$Exp_m3
+L132.For_V_USA_fby$Price_USDm3 <- L132.For_V_USA_fby$ExpV_USD / L132.For_V_USA_fby$Exp_m3 * conv_2010_1975_USD
 
 #Convert to model units, and combine into a single table
 printlog( "Part 4: Converting to model units and merging into a single table" )
-L132.ag_V_USA_C_fby$calPrice <- round( L132.ag_V_USA_C_fby$Price_USDt * conv_2004_1975_USD / conv_t_kg,
+L132.ag_V_USA_C_fby$calPrice <- round( L132.ag_V_USA_C_fby$Price_USDt / conv_t_kg,
       digits = digits_calPrice )
 L132.ag_V_USA_C_fby$unit <- "1975$/kg"
-L132.an_V_USA_C_fby$calPrice <- round( L132.an_V_USA_C_fby$Price_USDt * conv_t_metric_short * conv_2004_1975_USD / conv_t_kg,
+L132.an_V_USA_C_fby$calPrice <- round( L132.an_V_USA_C_fby$Price_USDt * conv_t_metric_short / conv_t_kg,
       digits = digits_calPrice)
 L132.an_V_USA_C_fby$unit <- "1975$/kg"
-L132.For_V_USA_fby$calPrice <- round( L132.For_V_USA_fby$Price_USDm3 * conv_2004_1975_USD, digits = digits_calPrice )
+L132.For_V_USA_fby$calPrice <- round( L132.For_V_USA_fby$Price_USDm3, digits = digits_calPrice )
 L132.For_V_USA_fby$unit <- "1975$/m3"
 
 pricecols <- c( C, "calPrice", "unit" )
