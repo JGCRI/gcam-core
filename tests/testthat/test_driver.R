4d546ef46802baa07957e3521239acc894487c1d
Ben Bond-Lamberty <bondlamberty@pnnl.gov>
2017-01-19 07:04:11 -0500

    Trying to figure out why failing on Travis

--- tests/testthat/test_driver.R
+++ tests/testthat/test_driver.R
@@ -13,6 +13,7 @@ test_that("errors with bad input", {
 })
 
 test_that("catches non-unique outputs", {
+  # Create a couple (fake) chunks that produce the same thing
   chunknames <- c("test1", "test2")
   with_mock(
     find_chunks = function(...) tibble(name = chunknames),
@@ -26,6 +27,7 @@ test_that("catches non-unique outputs", {
 })
 
 test_that("catches unmarked file inputs", {
+  # Create a (fake) chunk that hasn't marked its file inputs correctly
   chunknames <- c("test1", "test2")
   with_mock(
     find_chunks = function(...) tibble(name = chunknames),
@@ -34,26 +36,12 @@ test_that("catches unmarked file inputs", {
                                         from_file = c(TRUE, FALSE)),
     chunk_outputs = function(...) tibble(name = chunknames,
                                          output = c("o1", "o2")),
-    expect_error(driver(), regexp = "Unfound inputs not marked as from file")
-  )
-})
-
-test_that("catches lying chunks", {
-  chunknames <- c("test1")
-  with_mock(
-    find_chunks = function(...) tibble(name = chunknames),
-    chunk_inputs = function(...) tibble(name = chunknames,
-                                        input = "i1",
-                                        from_file = TRUE),
-    chunk_outputs = function(...) tibble(name = chunknames,
-                                         output = "o1"),
-    run_chunk = function(...) { o2 <- tibble(); return_data(o2) },
-    load_csv_files = function(...) { i1 <- tibble(); return_data(i1) },
-    expect_error(driver(), regexp = "is not returning what it promised")
+    expect_error(driver(), regexp = "not marked as from file")
   )
 })
 
 test_that("catches stuck", {
+  # Create a couple (fake) chunks that depend on each other
   chunknames <- c("test1", "test2")
   with_mock(
     find_chunks = function(...) tibble(name = chunknames),
@@ -66,10 +54,18 @@ test_that("catches stuck", {
   )
 })
 
-
-test_that("runs", {
-  # x <- capture_output({
-  #   out <- driver()
-  # })
-  # expect_true(is.list(out))
+test_that("catches lying chunks", {
+  # Create a (fake) chunk that declares and produces different outputs
+  chunknames <- c("test1")
+  with_mock(
+    find_chunks = function(...) tibble(name = chunknames),
+    chunk_inputs = function(...) tibble(name = chunknames,
+                                        input = "i1",
+                                        from_file = TRUE),
+    chunk_outputs = function(...) tibble(name = chunknames,
+                                         output = "o1"),
+    load_csv_files = function(...) { i1 <- tibble(); return_data(i1) },
+    run_chunk = function(...) { o2 <- tibble(); return_data(o2) },
+    expect_error(driver(), regexp = "is not returning what it promised")
+  )
 })
