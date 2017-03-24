3e24aac8cf66895f46830af355caae8cfe171627
Ben Bond-Lamberty <bondlamberty@pnnl.gov>
2017-03-24 15:28:36 -0400

    Test code for dstrace

---
+++ tests/testthat/test_dstrace.R
@@ -0,0 +1,36 @@
+# Test the dstrace utility
+
+context("dstrace")
+
+test_that("handles bad input", {
+
+  expect_error(dstrace(1, empty_data()))
+  expect_error(dstrace("1", 1))
+  expect_error(dstrace("1", empty_data(), recurse = 1))
+
+  # Make a fake data structure
+  tibble() %>%
+    add_title("o1")  ->
+    o1
+
+  tibble() %>%
+    add_title("o2") %>%
+    add_flags(FLAG_INPUT_DATA) ->
+    o2
+
+  tibble() %>%
+    add_title("o3") %>%
+    add_precursors("o1", "o2") ->
+    o3
+
+  all_data <- add_data(list("o1" = o1, "o2" = o2, "o3" = o3), empty_data())
+
+  # Identifies objects with no precursors
+  expect_output(dstrace("o1", all_data), regexp = "No precursors")
+  # Identifies objects read from file
+  expect_output(dstrace("o2", all_data), regexp = "read from file")
+  # Traces precursors
+  expect_output(dstrace("o3", all_data), regexp = "Precursor: o1")
+  expect_output(dstrace("o3", all_data), regexp = "Precursor: o2")
+
+})
