575b2d8f3f89f7706a77cf3c9c7f745e645681a6
Ben Bond-Lamberty <bondlamberty@pnnl.gov>
2017-03-31 11:44:07 -0400

    Fixed an input file problem and a few minor formatting issues

--- R/zchunk_LB111.ag_resbio_R_C.R
+++ R/zchunk_LB111.ag_resbio_R_C.R
@@ -1,6 +1,6 @@
 #' module_aglu_LB111.ag_resbio_R_C
 #'
-#' Calculate the production-weighted parameters of residue biomass by GCAM region and commodity
+#' Calculate the production-weighted parameters of residue biomass by GCAM region and commodity.
 #'
 #' @param command API command to execute
 #' @param ... other optional parameters, depending on command
@@ -14,7 +14,6 @@
 #' @importFrom dplyr filter mutate select
 #' @importFrom tidyr gather spread
 #' @author RC March 2017
-#' @export
 module_aglu_LB111.ag_resbio_R_C <- function(command, ...) {
   if(command == driver.DECLARE_INPUTS) {
     return(c(FILE = "common/iso_GCAM_regID",
@@ -40,27 +39,27 @@ module_aglu_LB111.ag_resbio_R_C <- function(command, ...) {
       select(-year) %>%
       rename(prod = value) %>%
       full_join(Various_ag_resbio_data, by = "item") %>%
-    # Drop rows with NA values ( dropping commodities that are not in the resbio dataset )
+      # Drop rows with NA values ( dropping commodities that are not in the resbio dataset )
       na.omit() %>%
-    # also drop rows where production weights are zero, as these would return missing values later on
+      # also drop rows where production weights are zero, as these would return missing values later on
       filter(prod != 0) %>%
-    # Multiply by production to get weights, change to long-format for easier calculation
+      # Multiply by production to get weights, change to long-format for easier calculation
       gather(resbio_params, value, -iso, -item, -prod) %>%
       mutate(value = value * prod) %>%
-    # Add vectors for GCAM regions and commodities, collapse, and divide by production to get residue biomass values
+      # Add vectors for GCAM regions and commodities, collapse, and divide by production to get residue biomass values
       left_join_error_no_match(iso_GCAM_regID[c( "iso", "GCAM_region_ID")], by = "iso") %>%
       left_join_error_no_match(FAO_ag_items_PRODSTAT[c( "item", "GCAM_commodity")], by = "item") %>%
       group_by(GCAM_region_ID, GCAM_commodity, resbio_params) %>%
       summarize_if(is.numeric, sum) %>%
-    # Dividing by production to get weighted average residue biomass parameters by region and crop
+      # Dividing by production to get weighted average residue biomass parameters by region and crop
       ungroup() %>%
       mutate(value = value / prod) %>%
       select(-prod) %>%
       spread(resbio_params, value) %>%
 
-    # Produce outputs
+      # Produce outputs
       add_title("Weighted average residue biomass parameters by GCAM region / commodity") %>%
-      add_units("units: varied") %>%
+      add_units("Varied") %>%
       add_comments("Calculate the HarvestIndex, ErosCtrl, ResEnergy, Root_Shoot, and WaterContent of residue biomass") %>%
       add_comments("These parameters are weighted by production when calculating the average by GCAM region and commodity") %>%
       add_legacy_name("L111.ag_resbio_R_C") %>%
@@ -68,7 +67,6 @@ module_aglu_LB111.ag_resbio_R_C <- function(command, ...) {
                      "aglu/FAO_ag_items_PRODSTAT",
                      "L100.FAO_ag_Prod_t",
                      "aglu/Various_ag_resbio_data") %>%
-      # typical flags, but there are others--see `constants.R`
       add_flags(FLAG_NO_XYEAR) ->
       L111.ag_resbio_R_C
 
