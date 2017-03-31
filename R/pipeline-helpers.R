e105b9f99aaf4aa209c9b425c4f1560fce69878e
Ben Bond-Lamberty <bondlamberty@pnnl.gov>
2017-03-31 10:46:58 -0400

    repeat_add_columns function
    Takes the place of `repeat_and_add_vector`

--- R/pipeline-helpers.R
+++ R/pipeline-helpers.R
@@ -45,17 +45,45 @@ left_join_error_no_match <- function(d, ...) {
 #' @param rule Rule to use; see \code{\link{approx}} and details
 #' @details This was \code{gcam_interp} in the original data system.
 #' @return Interpolated values.
+#' @importFrom assertthat assert_that
 #' @export
 #' @examples
 #' df <- data.frame(year = 1:5, value = c(1, 2, NA, 4, 5))
 #' approx_fun(df$year, df$value, rule = 2)
 approx_fun <- function(year, value, rule = 1) {
-  assertthat::assert_that(is.numeric(year))
-  assertthat::assert_that(is.numeric(value))
+  assert_that(is.numeric(year))
+  assert_that(is.numeric(value))
 
   if(rule == 1 | rule == 2 ) {
     stats::approx(as.vector(year), value, rule = rule, xout = year)$y
   } else {
     stop("Not implemented yet!")
   }
+}
+
+
+
+#' repeat_add_columns
+#'
+#' Repeat a data frame for each entry in a second, binding the columns together.
+#'
+#' @param x Data frame (tibble) to repeat
+#' @param y A copy of \code{x} is created for each row of this tibble
+#' @return A repeated \code{x} with columns from \code{y} added.
+#' @details This corresponds to \code{repeat_and_add_vector} in the old data system.
+#' @importFrom assertthat assert_that
+#' @author BBL
+#' @export
+#' @examples
+#' x <- tibble::tibble(x = 1:3)
+#' y <- tibble::tibble(y = c(4, 5), z = c(6, 7))
+#' repeat_add_columns(x, y)
+repeat_add_columns <- function(x, y) {
+  assert_that(tibble::is_tibble(x))
+  assert_that(tibble::is_tibble(y))
+
+  x %>%
+    mutate(UNIQUE_JOIN_FIELD = 1) %>%
+    full_join(mutate(y, UNIQUE_JOIN_FIELD = 1), by = "UNIQUE_JOIN_FIELD") %>%
+    select(-UNIQUE_JOIN_FIELD)
 }
