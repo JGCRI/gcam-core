b424c57d4638a1b9bfb64d3624731403ce94638e
Ben Bond-Lamberty <bondlamberty@pnnl.gov>
2017-03-31 14:34:42 -0400

    Updates - files and documentation

--- R/zchunk_LA117.tradbio.R
+++ R/zchunk_LA117.tradbio.R
@@ -1,4 +1,4 @@
-#' module_energy_LA116.geo
+#' module_energy_LA117.tradbio
 #'
 #' Briefly describe what this chunk does.
 #'
@@ -6,32 +6,31 @@
 #' @param ... other optional parameters, depending on command
 #' @return Depends on \code{command}: either a vector of required inputs,
 #' a vector of output names, or (if \code{command} is "MAKE") all
-#' the generated outputs: \code{L116.RsrcCurves_EJ_R_geo}, \code{L116.RsrcCurves_EJ_R_EGS}. The corresponding file in the
-#' original data system was \code{LA116.geo.R} (energy level1).
+#' the generated outputs: \code{L117.RsrcCurves_EJ_R_tradbio}. The corresponding file in the
+#' original data system was \code{LA117.tradbio.R} (energy level1).
 #' @details Describe in detail what this chunk does.
 #' @importFrom assertthat assert_that
 #' @importFrom dplyr filter mutate select
 #' @importFrom tidyr gather spread
 #' @author YourInitials CurrentMonthName 2017
 #' @export
-module_energy_LA116.geo_DISABLED <- function(command, ...) {
+module_energy_LA117.tradbio_DISABLED <- function(command, ...) {
   if(command == driver.DECLARE_INPUTS) {
-    return(c(FILE = "common/iso_GCAM_regID",
-             "L100.Land_type_area_ha",
-             FILE = "energy/A16.geo_curves",
-             FILE = "energy/A16.EGS_curves"))
+    return(c(FILE = "energy/A_regions",
+             FILE = "common/iso_GCAM_regID",
+             "L1011.en_bal_EJ_R_Si_Fi_Yh",
+             FILE = "energy/A17.tradbio_curves"))
   } else if(command == driver.DECLARE_OUTPUTS) {
-    return(c("L116.RsrcCurves_EJ_R_geo",
-             "L116.RsrcCurves_EJ_R_EGS"))
+    return(c("L117.RsrcCurves_EJ_R_tradbio"))
   } else if(command == driver.MAKE) {
 
     all_data <- list(...)[[1]]
 
     # Load required inputs
+    A_regions <- get_data(all_data, "energy/A_regions")
     iso_GCAM_regID <- get_data(all_data, "common/iso_GCAM_regID")
-    Land_type_area_ha <- get_data(all_data, "Land_type_area_ha")
-    A16.geo_curves <- get_data(all_data, "energy/A16.geo_curves")
-    A16.EGS_curves <- get_data(all_data, "energy/A16.EGS_curves")
+    L1011.en_bal_EJ_R_Si_Fi_Yh <- get_data(all_data, "L1011.en_bal_EJ_R_Si_Fi_Yh")
+    A17.tradbio_curves <- get_data(all_data, "energy/A17.tradbio_curves")
 
     # ===================================================
     # TRANSLATED PROCESSING CODE GOES HERE...
@@ -50,8 +49,6 @@ module_energy_LA116.geo_DISABLED <- function(command, ...) {
     #
     # NOTE: there are 'match' calls in this code. You probably want to use left_join_error_no_match
     # For more information, see https://github.com/JGCRI/gcamdata/wiki/Name-That-Function
-    # NOTE: This code uses vecpaste
-    # This function can be removed; see https://github.com/JGCRI/gcamdata/wiki/Name-That-Function
     # NOTE: This code uses repeat_and_add_vector
     # This function can be removed; see https://github.com/JGCRI/gcamdata/wiki/Name-That-Function
     # ===================================================
@@ -66,23 +63,13 @@ module_energy_LA116.geo_DISABLED <- function(command, ...) {
       add_units("units") %>%
       add_comments("comments describing how data generated") %>%
       add_comments("can be multiple lines") %>%
-      add_legacy_name("L116.RsrcCurves_EJ_R_geo") %>%
+      add_legacy_name("L117.RsrcCurves_EJ_R_tradbio") %>%
       add_precursors("precursor1", "precursor2", "etc") %>%
       # typical flags, but there are others--see `constants.R`
       add_flags(FLAG_NO_TEST, FLAG_LONG_YEAR_FORM, FLAG_NO_XYEAR) ->
-      L116.RsrcCurves_EJ_R_geo
-    tibble() %>%
-      add_title("descriptive title of data") %>%
-      add_units("units") %>%
-      add_comments("comments describing how data generated") %>%
-      add_comments("can be multiple lines") %>%
-      add_legacy_name("L116.RsrcCurves_EJ_R_EGS") %>%
-      add_precursors("precursor1", "precursor2", "etc") %>%
-      # typical flags, but there are others--see `constants.R`
-      add_flags(FLAG_NO_TEST, FLAG_LONG_YEAR_FORM, FLAG_NO_XYEAR) ->
-      L116.RsrcCurves_EJ_R_EGS
+      L117.RsrcCurves_EJ_R_tradbio
 
-    return_data(L116.RsrcCurves_EJ_R_geo, L116.RsrcCurves_EJ_R_EGS)
+    return_data(L117.RsrcCurves_EJ_R_tradbio)
   } else {
     stop("Unknown command")
   }
