4c318e06fe72e41565e8936d6f2fe34527ec3741
rynacui <yiyun.cui@pnnl.gov>
2017-04-03 15:38:25 -0400

    add more comments

--- R/zchunk_LA100.GTAP_downscale_ctry.R
+++ R/zchunk_LA100.GTAP_downscale_ctry.R
@@ -12,7 +12,7 @@
 #' @importFrom assertthat assert_that
 #' @importFrom dplyr filter mutate select
 #' @importFrom tidyr gather spread
-#' @author YourInitials CurrentMonthName 2017
+#' @author RC April 2017
 #' @export
 module_aglu_LA100.GTAP_downscale_ctry <- function(command, ...) {
   if(command == driver.DECLARE_INPUTS) {
@@ -32,24 +32,24 @@ module_aglu_LA100.GTAP_downscale_ctry <- function(command, ...) {
     L100.LDS_value_milUSD <- get_data(all_data, "L100.LDS_value_milUSD")
     L100.LDS_ag_prod_t <- get_data(all_data, "L100.LDS_ag_prod_t")
 
-    # Create the iso and GTAP_region mapping file
+    # Create the iso - GTAP_region mapping file
     AGLU_ctry %>%
       select(iso, GTAP_region) %>%
       distinct(iso, .keep_all = TRUE) ->
       GTAP_ctry
 
+    # Prepare land value for entire GTAP region
     FAO_ag_items_PRODSTAT %>%
       select(GTAP_use) %>%
       unique() %>%
       na.omit() %>%
       repeat_add_columns(unique(L100.LDS_ag_prod_t[c("iso", "GLU")])) %>%
       left_join_error_no_match(GTAP_ctry, by = "iso") %>%
-    # Match in the land value for the entire GTAP region. These will be multiplied by country shares
+      # Match in the land value for the entire GTAP region. These will be multiplied by country shares
       left_join(L100.LDS_value_milUSD, by = c("GTAP_region", "GLU", "GTAP_use")) %>%
       mutate(value = if_else(is.na(value), 0, value)) ->
       LV_Rgtap
 
-    # Compute the country-within-GTAP region shares for each of the commodity classes
     # Compute production by GTAP region
     L100.LDS_ag_prod_t %>%
       left_join_error_no_match(GTAP_ctry, by = "iso") %>%
@@ -60,6 +60,7 @@ module_aglu_LA100.GTAP_downscale_ctry <- function(command, ...) {
       ungroup() %>%
       rename(prod_rgn = value) ->
       Ag_Prod_Rgtap
+
     # Compute production by ctry iso
     L100.LDS_ag_prod_t %>%
       left_join(FAO_ag_items_PRODSTAT[c("GTAP_crop", "GTAP_use")], by = "GTAP_crop") %>%
@@ -68,12 +69,13 @@ module_aglu_LA100.GTAP_downscale_ctry <- function(command, ...) {
       summarise_if(is.numeric, sum) %>%
       ungroup() %>%
       rename(prod_ctry = value) %>%
-    # Share = production by country and GTAP use / production by GTAP region and GTAP use
+      # Compute the country-within-GTAP region shares for each of the commodity classes
+      # Share = production by country and GTAP use / production by GTAP region and GTAP use
       left_join_error_no_match(GTAP_ctry, by = "iso") %>%
       left_join_error_no_match(Ag_Prod_Rgtap, by = c( "GTAP_region", "GLU", "GTAP_use" )) %>%
       mutate(share = prod_ctry / prod_rgn) %>%
       mutate(share = if_else(is.na(share), 0, share )) %>%
-    # Multiply the land values by the shares
+      # Multiply the land values by the shares
       right_join(LV_Rgtap, by = c("iso", "GTAP_region", "GLU", "GTAP_use")) %>%
       mutate(value = value * share) %>%
       mutate(value = if_else(is.na(value), 0, value)) %>%
@@ -89,7 +91,6 @@ module_aglu_LA100.GTAP_downscale_ctry <- function(command, ...) {
                      "aglu/FAO_ag_items_PRODSTAT",
                      "L100.LDS_value_milUSD",
                      "L100.LDS_ag_prod_t") %>%
-      # typical flags, but there are others--see `constants.R`
       add_flags(FLAG_NO_XYEAR) ->
       L100.GTAP_LV_milUSD
 
