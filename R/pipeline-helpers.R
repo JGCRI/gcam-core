305d84d51d195e2fd5d20b996570c7253fef5cc2
Robert Link <robert.link@pnnl.gov>
2017-04-03 15:42:19 -0400

    Remove \dontrun tag from examples in pipeline-helpers.R

--- R/pipeline-helpers.R
+++ R/pipeline-helpers.R
@@ -145,15 +145,13 @@ standardize_iso <- function(d, col='iso')
 #' sensitive operations are complete.
 #' @param d The data to have integer column names protected or unprotected
 #' @return Tibble with the integer column names protected
-#' @importFrom dplyr %>%
 #' @export
 #' @examples
-#' \dontrun{
+#' library(magrittr)
 #' df <- dplyr::tibble(iso=c('bad','dum'), `2005`=c(123.45, NA), `2050`=c(867, 5309))
 #' protect_integer_cols(df) %>%
 #'     dplyr::select_if(function(col){!any(is.na(col))}) %>%
 #'     unprotect_integer_cols
-#'}
 protect_integer_cols <- function(d)
 {
   names(d) <- sub('^([0-9]+)$', 'X\\1', names(d))
