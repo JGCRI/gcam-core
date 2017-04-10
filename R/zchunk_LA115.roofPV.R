3c77a7568206a8f872d84fd7a505cc13972c23ad
stan656 <aaron.staniszewski@pnnl.gov>
2017-04-10 16:38:56 -0400

    Still has 3 errors when testing package, as mentioned on slack

--- R/zchunk_LA115.roofPV.R
+++ R/zchunk_LA115.roofPV.R
@@ -14,12 +14,12 @@
 #' @importFrom tidyr gather spread
 #' @author YourInitials CurrentMonthName 2017
 #' @export
-module_energy_LA115.roofPV_DISABLED <- function(command, ...) {
+module_energy_LA115.roofPV <- function(command, ...) {
   if(command == driver.DECLARE_INPUTS) {
     return(c(FILE = "common/iso_GCAM_regID",
              FILE = "energy/A15.roofPV_curves",
              FILE = "energy/A15.roofPV_TechChange",
-             "L100.Pop_thous_ctry_Yh"))
+             FILE = "socioeconomics/L100.Pop_thous_ctry_Yh"))
   } else if(command == driver.DECLARE_OUTPUTS) {
     return(c("L115.RsrcCurves_EJ_R_roofPV"))
   } else if(command == driver.MAKE) {
@@ -29,11 +29,39 @@ module_energy_LA115.roofPV_DISABLED <- function(command, ...) {
     # Load required inputs
     iso_GCAM_regID <- get_data(all_data, "common/iso_GCAM_regID")
     A15.roofPV_curves <- get_data(all_data, "energy/A15.roofPV_curves")
-    A15.roofPV_TechChange <- get_data(all_data, "energy/A15.roofPV_TechChange")
-    L100.Pop_thous_ctry_Yh <- get_data(all_data, "L100.Pop_thous_ctry_Yh")
+    A15.roofPV_TechChange <- get_data(all_data, "energy/A15.roofPV_TechChange") #not used
+    L100.Pop_thous_ctry_Yh <- get_data(all_data, "socioeconomics/L100.Pop_thous_ctry_Yh")
 
     # ===================================================
     # TRANSLATED PROCESSING CODE GOES HERE...
+
+    #Categorizing countries and regions to GCAM Region ID; selecting population in 2010
+    L100.Pop_thous_ctry_Yh %>%
+      select(iso, X2010) %>%
+      left_join(iso_GCAM_regID %>%
+                  select(-country_name)) %>%
+      select(-iso) ->
+      x
+
+    #Sum of population by regions
+    x %>%
+      group_by(region_GCAM3) %>%
+      summarise(popSum = sum(X2010)) ->
+      pop_RG3
+
+
+    #Building resource curves by GCAM Region ID
+    x %>%
+      left_join(pop_RG3, by = "region_GCAM3") %>%
+      left_join(A15.roofPV_curves, by = "region_GCAM3") %>%
+      mutate(maxSubResource = maxSubResource*X2010/popSum) %>%
+      rename(`curve.exponent` = `curve-exponent`, `mid.price` = `mid-price`) %>%
+      group_by(GCAM_region_ID, resource, subresource, curve.exponent, gdpSupplyElast, subResourceCapacityFactor) %>%
+      summarise(maxSubResource = sum(maxSubResource),
+                mid.price = median(mid.price)) %>%
+      select(GCAM_region_ID, resource, subresource, maxSubResource, mid.price, curve.exponent, gdpSupplyElast, subResourceCapacityFactor) ->
+      L115.RsrcCurves_EJ_R_roofPV
+
     #
     # If you find a mistake/thing to update in the old code and
     # fixing it will change the output data, causing the tests to fail,
@@ -56,15 +84,15 @@ module_energy_LA115.roofPV_DISABLED <- function(command, ...) {
     # Note that all precursor names (in `add_precursor`) must be in this chunk's inputs
     # There's also a `same_precursors_as(x)` you can use
     # If no precursors (very rare) don't call `add_precursor` at all
-    tibble() %>%
+    L115.RsrcCurves_EJ_R_roofPV %>%
       add_title("descriptive title of data") %>%
       add_units("units") %>%
       add_comments("comments describing how data generated") %>%
       add_comments("can be multiple lines") %>%
       add_legacy_name("L115.RsrcCurves_EJ_R_roofPV") %>%
-      add_precursors("precursor1", "precursor2", "etc") %>%
+      add_precursors("common/iso_GCAM_regID", "energy/A15.roofPV_curves", "energy/A15.roofPV_TechChange", "socioeconomics/L100.Pop_thous_ctry_Yh") %>%
       # typical flags, but there are others--see `constants.R`
-      add_flags(FLAG_NO_TEST, FLAG_LONG_YEAR_FORM, FLAG_NO_XYEAR) ->
+      add_flags(FLAG_NO_TEST) ->
       L115.RsrcCurves_EJ_R_roofPV
 
     return_data(L115.RsrcCurves_EJ_R_roofPV)
