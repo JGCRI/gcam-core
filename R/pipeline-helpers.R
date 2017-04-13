9ecd800adf235ca72305138a7027cdd69bf51e59
Ben Bond-Lamberty <bondlamberty@pnnl.gov>
2017-04-13 05:38:10 -0400

    Add a `gdp_deflator` function
    Generalize the many GDP conversion factors in the old data system code.
    Addresses issue #243

--- R/pipeline-helpers.R
+++ R/pipeline-helpers.R
@@ -167,3 +167,63 @@ unprotect_integer_cols <- function(d) {
   names(d) <- sub('^X([0-9]+)$', '\\1', names(d))
   d
 }
+
+
+#' gdp_deflator
+#'
+#' Calculate a gross domestic product (GDP) implicit price deflator between two years.
+#'
+#' @param year Year for which to calculate deflator
+#' @param base_year Base year in deflator calculation
+#' @details The GDP deflator is a measure of price inflation/deflation with respect to a specific base year.
+#' @note This returns slightly different values if \code{OLD_DATA_SYSTEM_BEHAVIOR} is TRUE.
+#' @return Deflator (GDP in \code{from_year} divided by GDP in \code{to_year}).
+#' @source U.S. Bureau of Economic Analysis, Gross domestic product (implicit price deflator) [A191RD3A086NBEA], retrieved from FRED, Federal Reserve Bank of St. Louis; https://fred.stlouisfed.org/series/A191RD3A086NBEA, April 12, 2017
+#' @author BBL
+#' @export
+#' @examples
+#' gdp_deflator(2010, base_year = 1990)
+gdp_deflator <- function(year, base_year) {
+
+  if(OLD_DATA_SYSTEM_BEHAVIOR) {
+    if(year == 2010 & base_year == 1990) return(1.510)   # conv_1990_2010_USD
+    if(year == 2007 & base_year == 1990) return(1.470)   # conv_1990_2007_USD
+    if(year == 2005 & base_year == 1990) return(1.383)   # conv_1990_2005_USD
+    if(year == 1975 & base_year == 1990) return(0.4649)  # conv_1990_1975_USD
+    if(year == 1975 & base_year == 1996) return(0.4049)  # conv_1996_1975_USD
+    if(year == 1975 & base_year == 1997) return(0.3983)  # conv_1997_1975_USD
+    if(year == 1975 & base_year == 1998) return(0.3939)  # conv_1998_1975_USD
+    if(year == 1975 & base_year == 1999) return(0.3883)  # conv_1999_1975_USD
+    if(year == 1975 & base_year == 2000) return(0.380)   # conv_2000_1975_USD
+    if(year == 1975 & base_year == 2001) return(0.3711)  # conv_2001_1975_USD
+    if(year == 1975 & base_year == 2002) return(0.3647)  # conv_2002_1975_USD
+    if(year == 1975 & base_year == 2003) return(0.3571)  # conv_2003_1975_USD
+    if(year == 1975 & base_year == 2004) return(0.3472)  # conv_2004_1975_USD
+    if(year == 1975 & base_year == 2005) return(0.3362)  # conv_2005_1975_USD
+    if(year == 1975 & base_year == 2006) return(0.3257)  # conv_2006_1975_USD
+    if(year == 1975 & base_year == 2007) return(0.317)   # conv_2007_1975_USD
+    if(year == 1975 & base_year == 2008) return(0.3104)  # conv_2008_1975_USD
+    if(year == 1975 & base_year == 2009) return(0.3104)  # conv_2009_1975_USD
+  }
+
+  # This time series is the BEA "A191RD3A086NBEA" product
+  # Downloaded April 13, 2017 from https://fred.stlouisfed.org/series/A191RD3A086NBEA
+  gdp_years <- 1929:2016
+  gdp <- c(9.896, 9.535, 8.555, 7.553, 7.345, 7.749, 7.908, 8.001, 8.347,
+           8.109, 8.033, 8.131, 8.68, 9.369, 9.795, 10.027, 10.288, 11.618,
+           12.887, 13.605, 13.581, 13.745, 14.716, 14.972, 15.157, 15.298,
+           15.559, 16.091, 16.625, 17.001, 17.237, 17.476, 17.669, 17.886,
+           18.088, 18.366, 18.702, 19.227, 19.786, 20.627, 21.642, 22.784,
+           23.941, 24.978, 26.337, 28.703, 31.361, 33.083, 35.135, 37.602,
+           40.706, 44.377, 48.52, 51.53, 53.565, 55.466, 57.24, 58.395,
+           59.885, 61.982, 64.392, 66.773, 68.996, 70.569, 72.248, 73.785,
+           75.324, 76.699, 78.012, 78.859, 80.065, 81.887, 83.754, 85.039,
+           86.735, 89.12, 91.988, 94.814, 97.337, 99.246, 100, 101.221,
+           103.311, 105.214, 106.913, 108.828, 109.998, 111.445)
+  names(gdp) <- gdp_years
+
+  assert_that(year %in% gdp_years)
+  assert_that(base_year %in% gdp_years)
+
+  gdp[as.character(year)] / gdp[as.character(base_year)]
+}
