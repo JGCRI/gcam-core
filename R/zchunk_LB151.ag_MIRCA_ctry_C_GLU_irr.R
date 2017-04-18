1854417c73dc51a8fd51982ef9f65ff36507e83b
kvcalvin <katherine.calvin@pnnl.gov>
2017-04-18 14:28:44 -0400

    Adding documentation and comments

--- R/zchunk_LB151.ag_MIRCA_ctry_C_GLU_irr.R
+++ R/zchunk_LB151.ag_MIRCA_ctry_C_GLU_irr.R
@@ -1,6 +1,6 @@
 #' module_aglu_LB151.ag_MIRCA_ctry_C_GLU_irr
 #'
-#' Briefly describe what this chunk does.
+#' Separates country and harvested area into irrigated and rainfed by country, GLU, and GTAP_crop.
 #'
 #' @param command API command to execute
 #' @param ... other optional parameters, depending on command
@@ -8,11 +8,14 @@
 #' a vector of output names, or (if \code{command} is "MAKE") all
 #' the generated outputs: \code{L151.ag_irrHA_ha_ctry_crop}, \code{L151.ag_rfdHA_ha_ctry_crop}, \code{L151.ag_irrProd_t_ctry_crop}, \code{L151.ag_rfdProd_t_ctry_crop}. The corresponding file in the
 #' original data system was \code{LB151.ag_MIRCA_ctry_C_GLU_irr.R} (aglu level1).
-#' @details Describe in detail what this chunk does.
+#' @details Separates country and harvested area into irrigated and rainfed by country, GLU, and GTAP_crop.
+#' This chunk uses MIRCA irrigated and rainfed harvested area to separate harvested area from the LDS. This
+#' is then combined with ratios of irrigated to rainfed yield from the FAO CROSIT database to compute
+#' irrigated and rainfed production.
 #' @importFrom assertthat assert_that
 #' @importFrom dplyr filter mutate select
 #' @importFrom tidyr gather spread replace_na
-#' @author YourInitials CurrentMonthName 2017
+#' @author KVC April 2017
 #' @export
 module_aglu_LB151.ag_MIRCA_ctry_C_GLU_irr <- function(command, ...) {
   if(command == driver.DECLARE_INPUTS) {
@@ -90,7 +93,6 @@ module_aglu_LB151.ag_MIRCA_ctry_C_GLU_irr <- function(command, ...) {
       select(-rfdHA) ->                                                         # Remove extra columns
       L151.ag_irrHA_ha_ctry_crop
 
-
     # Calculate production on irrigated and rainfed land, by country, GTAP crop, and GLU
     # Use the ratio of irrigated to rainfed yield from the FAO CROSIT database,
     # in combination with the harvested area data computed above to separate irrigated
@@ -105,68 +107,74 @@ module_aglu_LB151.ag_MIRCA_ctry_C_GLU_irr <- function(command, ...) {
     # Next, Calculate the yield ratio for each country and crop, based on the CROSIT database
     L100.LDS_ag_HA_ha %>%
       select(iso, GLU, GTAP_crop) %>%
-      left_join(Unique_AGLU_ctry[ c( "iso", "CROSIT_country_ID") ], by = "iso" ) %>%
-      left_join(FAO_ag_items_PRODSTAT[ c("GTAP_crop","CROSIT_cropID")], by = "GTAP_crop" ) %>%
-      left_join(FAO_ag_CROSIT[ c( "country_ID", "crop_ID", "Yield_kgHa_irrigated", "Yield_kgHa_rainfed", "year" )],
+      left_join(Unique_AGLU_ctry[ c( "iso", "CROSIT_country_ID") ], by = "iso" ) %>%                                    # Map in CROSIT country ids
+      left_join(FAO_ag_items_PRODSTAT[ c("GTAP_crop","CROSIT_cropID")], by = "GTAP_crop" ) %>%                          # Map in CROSIT crop ids
+      left_join(FAO_ag_CROSIT[ c( "country_ID", "crop_ID", "Yield_kgHa_irrigated", "Yield_kgHa_rainfed", "year" )],     # Map in CROSIT irrigated and rainfed yields
                  by = c( "CROSIT_country_ID" = "country_ID", "CROSIT_cropID" = "crop_ID")) %>%
+
+      # CROSIT data includes historic and future years, we only want the historic year. Additionally, CROSIT data is missing some crops
+      # we don't want to remove those. We'll set their yield ratio to 1 later.
       filter(year == CROSIT_HISTORICAL_YEAR | is.na(year)) %>%
-      mutate(yieldratio = Yield_kgHa_irrigated / Yield_kgHa_rainfed) %>%
+      mutate(yieldratio = Yield_kgHa_irrigated / Yield_kgHa_rainfed) %>%                                                # Compute yield ratio
       select(-Yield_kgHa_irrigated, -Yield_kgHa_rainfed) %>%
       mutate(yieldratio=if_else( is.na(yieldratio), 1, yieldratio )) %>%                                                # Replace NAs with 1
       mutate(yieldratio=if_else( yieldratio==0, 1, yieldratio )) %>%                                                    # Replace zeros with 1
       mutate(yieldratio=if_else( is.infinite(yieldratio), 1, yieldratio ))  ->                                          # Replace Infs with 1
-
       Yieldratio_ctry_crop
 
     # Now, use the yield ratio to solve for the production shares (irrigated versus rainfed)
+    # This calculation preserves the harvested area shares and yield ratios computed above.
     ag_HA_ha_ctry_crop %>%
-      left_join( Yieldratio_ctry_crop[ c( "iso", "GLU", "GTAP_crop", "yieldratio") ], by = c( "iso", "GLU", "GTAP_crop") ) %>%
-      mutate(irrshareHA=irrHA/(irrHA + rfdHA)) %>%
-      mutate(irrshareProd=(irrshareHA*yieldratio)/((irrshareHA*yieldratio)+(1-irrshareHA))) %>%
-      right_join(L100.LDS_ag_prod_t, by=c("iso", "GLU", "GTAP_crop")) %>%
-      mutate(irrProd=value*irrshareProd, rfdProd=value*(1-irrshareProd)) ->
+      left_join( Yieldratio_ctry_crop[ c( "iso", "GLU", "GTAP_crop", "yieldratio") ], by = c( "iso", "GLU", "GTAP_crop") ) %>%  # Map in the yield ratio
+      mutate(irrshareHA=irrHA/(irrHA + rfdHA)) %>%                                                                              # Compute the share of irrigated harvested area
+      mutate(irrshareProd=(irrshareHA*yieldratio)/((irrshareHA*yieldratio)+(1-irrshareHA))) %>%                                 # Compute the share of irrigated production
+      right_join(L100.LDS_ag_prod_t, by=c("iso", "GLU", "GTAP_crop")) %>%                                                       # Map in the total production
+      mutate(irrProd=value*irrshareProd, rfdProd=value*(1-irrshareProd)) ->                                                     # Compute irrigated and rainfed production
       ag_Prod_t_ctry_crop
 
     # Split irrigated and rainfed production into separate dataframes
     ag_Prod_t_ctry_crop %>%
       select(iso,GLU,GTAP_crop,rfdProd) ->                                      # Remove extra columns
       L151.ag_rfdProd_t_ctry_crop
-    readr::write_csv(L151.ag_rfdProd_t_ctry_crop, "~/Documents/ag_rfdProd_t_ctry_crop.csv")
 
     ag_Prod_t_ctry_crop %>%
       select(iso,GLU,GTAP_crop,irrProd) ->                                      # Remove extra columns
       L151.ag_irrProd_t_ctry_crop
 
     L151.ag_irrHA_ha_ctry_crop %>%
-      add_title("descriptive title of data") %>%
-      add_units("units") %>%
-      add_comments("comments describing how data generated") %>%
-      add_comments("can be multiple lines") %>%
+      add_title("Irrigated harvested area by country, GTAP crop, GLU") %>%
+      add_units("Hectares") %>%
+      add_comments("Uses shares of irrigated area from MIRCA, combined with total harvested area, to calculate irrigated harvested area") %>%
+      add_comments("Data is for a single year (circa 2000)") %>%
       add_legacy_name("L151.ag_irrHA_ha_ctry_crop") %>%
       add_precursors("aglu/FAO_ag_items_PRODSTAT","L100.LDS_ag_HA_ha","L100.MIRCA_irrHA_ha","L100.MIRCA_rfdHA_ha") ->
       L151.ag_irrHA_ha_ctry_crop
     L151.ag_rfdHA_ha_ctry_crop %>%
-      add_title("descriptive title of data") %>%
-      add_units("units") %>%
-      add_comments("comments describing how data generated") %>%
-      add_comments("can be multiple lines") %>%
+      add_title("Irrigated harvested area by country, GTAP crop, GLU") %>%
+      add_units("Hectares") %>%
+      add_comments("Uses shares of rainfed area from MIRCA, combined with total harvested area, to calculate rainfed harvested area") %>%
+      add_comments("Data is for a single year (circa 2000)") %>%
       add_legacy_name("L151.ag_rfdHA_ha_ctry_crop") %>%
       add_precursors("aglu/FAO_ag_items_PRODSTAT","L100.LDS_ag_HA_ha","L100.MIRCA_irrHA_ha","L100.MIRCA_rfdHA_ha") ->
       L151.ag_rfdHA_ha_ctry_crop
     L151.ag_irrProd_t_ctry_crop %>%
-      add_title("descriptive title of data") %>%
-      add_units("units") %>%
-      add_comments("comments describing how data generated") %>%
-      add_comments("can be multiple lines") %>%
+      add_title("Irrigated production by country, GTAP crop, GLU") %>%
+      add_units("tons") %>%
+      add_comments("Combines the MIRCA shares of irrigated/rainfed area with the FAO CROSIT ratios of irrigated to rainfed yield") %>%
+      add_comments("to compute irrigated production shares. These are then combined with total production to calculate irrigated production.") %>%
+      add_comments("In the event of missing yield information, irrigated and rainfed yields are assumed equal (and thus production shares equal)") %>%
+      add_comments("Data is for a single year (circa 2000)") %>%
       add_legacy_name("L151.ag_irrProd_t_ctry_crop") %>%
       add_precursors("common/iso_GCAM_regID","aglu/AGLU_ctry","aglu/FAO_ag_items_PRODSTAT","aglu/FAO_ag_CROSIT",
                      "L100.LDS_ag_HA_ha","L100.LDS_ag_prod_t","L100.MIRCA_irrHA_ha","L100.MIRCA_rfdHA_ha") ->
       L151.ag_irrProd_t_ctry_crop
     L151.ag_rfdProd_t_ctry_crop %>%
-      add_title("descriptive title of data") %>%
-      add_units("units") %>%
-      add_comments("comments describing how data generated") %>%
-      add_comments("can be multiple lines") %>%
+      add_title("Rainfed production by country, GTAP crop, GLU") %>%
+      add_units("tons") %>%
+      add_comments("Combines the MIRCA shares of irrigated/rainfed area with the FAO CROSIT ratios of irrigated to rainfed yield") %>%
+      add_comments("to compute rainfed production shares. These are then combined with total production to calculate rainfed production.") %>%
+      add_comments("In the event of missing yield information, irrigated and rainfed yields are assumed equal (and thus production shares equal)") %>%
+      add_comments("Data is for a single year (circa 2000)") %>%
       add_legacy_name("L151.ag_rfdProd_t_ctry_crop") %>%
       add_precursors("common/iso_GCAM_regID","aglu/AGLU_ctry","aglu/FAO_ag_items_PRODSTAT","aglu/FAO_ag_CROSIT",
                      "L100.LDS_ag_HA_ha","L100.LDS_ag_prod_t","L100.MIRCA_irrHA_ha","L100.MIRCA_rfdHA_ha") ->
