5985efdc92d17a6066203e1097de79a419f3f47a
Ben Bond-Lamberty <bondlamberty@pnnl.gov>
2017-04-20 10:16:23 -0400

    Mostly formatting tweaks

--- R/zchunk_LA123.electricity.R
+++ R/zchunk_LA123.electricity.R
@@ -13,7 +13,6 @@
 #' @importFrom dplyr filter mutate select
 #' @importFrom tidyr gather spread
 #' @author FF, April 2017
-#' @export
 module_energy_LA123.electricity <- function(command, ...) {
   if(command == driver.DECLARE_INPUTS) {
     return(c(FILE = "energy/enduse_fuel_aggregation",
@@ -34,106 +33,122 @@ module_energy_LA123.electricity <- function(command, ...) {
     A23.chp_elecratio <- get_data(all_data, "energy/A23.chp_elecratio")
     get_data(all_data, "temp-data-inject/L1011.en_bal_EJ_R_Si_Fi_Yh") %>%
       gather(year,value,-GCAM_region_ID,-fuel,-sector) %>%
-      mutate(year = as.integer(substr(year, 2, 5))) -> L1011.en_bal_EJ_R_Si_Fi_Yh
+      mutate(year = as.integer(substr(year, 2, 5))) ->
+      L1011.en_bal_EJ_R_Si_Fi_Yh
 
     # ===================================================
 
     #Creates end use fuel for electricity to be used to create L123.in_EJ_R_elec_F_Yh
     enduse_fuel_aggregation %>%
       select(fuel,electricity) %>%
-      filter(!is.na(electricity)) -> enduse_fuel_aggregation_electricity
-
+      filter(!is.na(electricity)) ->
+      enduse_fuel_aggregation_electricity
 
     # Creates L123.in_EJ_R_elec_F_Yh based on L1011.en_bal_EJ_R_Si_Fi_Yh and enduse_fuel_aggregation_electricity
     # Calculates the inpunts by fuel (based on electricity input fuels), region ID and sector (electricity)
     L1011.en_bal_EJ_R_Si_Fi_Yh %>%
       filter(sector == "in_electricity generation") %>%
       mutate(sector = "electricity generation") %>%
-      left_join(enduse_fuel_aggregation_electricity,by="fuel") %>%
-      mutate(fuel=electricity,input=value) %>%
-      select(-electricity,-value) %>%
+      left_join(enduse_fuel_aggregation_electricity, by = "fuel") %>%
+      mutate(fuel = electricity, input = value) %>%
+      select(-electricity, -value) %>%
       filter(fuel %in% ELECTRICITY_INPUT_FUELS) %>%
-      group_by(GCAM_region_ID, sector,fuel,year ) %>%
-      summarise_each(funs(sum)) -> L123.in_EJ_R_elec_F_Yh
+      group_by(GCAM_region_ID, sector, fuel, year ) %>%
+      summarise_each(funs(sum)) ->
+      L123.in_EJ_R_elec_F_Yh
 
     # Creates L123.out_EJ_R_elec_F_Yh based on L1011.en_bal_EJ_R_Si_Fi_Yh and enduse_fuel_aggregation_electricity
     # Calculates the electricity outputs by fuel, region ID and sector (electricity generation)
     L1011.en_bal_EJ_R_Si_Fi_Yh %>%
       filter(sector == "out_electricity generation") %>%
       mutate(sector = "electricity generation") %>%
-      left_join(enduse_fuel_aggregation_electricity,by="fuel") %>%
-      mutate(fuel=electricity,outputs=value) %>%
-      select(-electricity,-value) %>%
-      group_by(GCAM_region_ID, sector,fuel,year) %>%
+      left_join(enduse_fuel_aggregation_electricity, by = "fuel") %>%
+      mutate(fuel = electricity, outputs = value) %>%
+      select(-electricity, -value) %>%
+      group_by(GCAM_region_ID, sector, fuel, year) %>%
       summarise_each(funs(sum)) %>%
-      filter(!is.na(fuel)) -> L123.out_EJ_R_elec_F_Yh
+      filter(!is.na(fuel)) ->
+      L123.out_EJ_R_elec_F_Yh
 
     # Long form of L123.in_EJ_R_elec_F_Yh (previously created) to be used to calculate efficiencies
     # based on electricity inputs and outputs by fuel and year.
 
     # Calculates electricity generation efficiencies (L123.eff_R_elec_F_Yh) by region, fuel and year
     L123.out_EJ_R_elec_F_Yh %>%
-      semi_join(L123.in_EJ_R_elec_F_Yh,by=c("GCAM_region_ID", "fuel","year")) %>%
-      left_join(L123.in_EJ_R_elec_F_Yh, by=c("GCAM_region_ID","sector","fuel","year")) %>%
-      mutate(value=outputs/input) %>%
-      select(-outputs,-input) -> L123.eff_R_elec_F_Yh
+      semi_join(L123.in_EJ_R_elec_F_Yh, by = c("GCAM_region_ID", "fuel", "year")) %>%
+      left_join(L123.in_EJ_R_elec_F_Yh, by = c("GCAM_region_ID", "sector", "fuel", "year")) %>%
+      mutate(value = outputs / input) %>%
+      select(-outputs, -input) ->
+      L123.eff_R_elec_F_Yh
 
     # Taking care of NA, 0, and INF values generatted in previous step (efficiency calculations) and updating
     # the efficiency output L123.eff_R_elec_F_Yh
     L123.eff_R_elec_F_Yh %>%
-      mutate(value =if_else(!is.na(value),value,DEFAULT_ELECTRIC_EFFICIENCY)) %>%
-      mutate(value =if_else(value==0,DEFAULT_ELECTRIC_EFFICIENCY,value)) %>%
-      mutate(value =if_else(value==Inf,DEFAULT_ELECTRIC_EFFICIENCY,value)) -> L123.eff_R_elec_F_Yh
+      mutate(value = if_else(!is.na(value),value,DEFAULT_ELECTRIC_EFFICIENCY)) %>%
+      mutate(value = if_else(value == 0, DEFAULT_ELECTRIC_EFFICIENCY, value)) %>%
+      mutate(value = if_else(is.infinite(value), DEFAULT_ELECTRIC_EFFICIENCY, value)) ->
+      L123.eff_R_elec_F_Yh
 
     # Tibble (Output_efficiency_based) created to adjust electricity outputs (L123.out_EJ_R_elec_F_Yh)
     # based on above modified efficiency calculations
 
     L123.in_EJ_R_elec_F_Yh %>%
-      left_join(L123.eff_R_elec_F_Yh, by=c("GCAM_region_ID","sector","fuel","year")) %>%
-      mutate(outputs_new=input*value) %>%
-      select(-input,-value) -> Output_efficiency_based
+      left_join(L123.eff_R_elec_F_Yh, by = c("GCAM_region_ID", "sector", "fuel", "year")) %>%
+      mutate(outputs_new = input * value) %>%
+      select(-input, -value) ->
+      Output_efficiency_based
 
     # Adjusting electricity outputs based on new efficiencies
     L123.out_EJ_R_elec_F_Yh %>%
-      left_join(Output_efficiency_based,by=c("GCAM_region_ID","sector","fuel","year")) %>%
-      mutate(outputs =if_else(!is.na(outputs_new),outputs_new,outputs)) %>%
-      select(-outputs_new) -> L123.out_EJ_R_elec_F_Yh
+      left_join(Output_efficiency_based, by = c("GCAM_region_ID", "sector", "fuel", "year")) %>%
+      mutate(outputs = if_else(!is.na(outputs_new), outputs_new, outputs)) %>%
+      select(-outputs_new) ->
+      L123.out_EJ_R_elec_F_Yh
 
     #2: CHP calculations
 
     # filter end use fuel in the industry sector and remove any NA generated.
     enduse_fuel_aggregation %>%
-      select(fuel,industry) %>%
-      filter(!is.na(industry))->enduse_fuel_aggregation_industry
+      select(fuel, industry) %>%
+      filter(!is.na(industry)) ->
+      enduse_fuel_aggregation_industry
 
     # Create Industry CHP estimates by fuel, region and industry sector.
     L1011.en_bal_EJ_R_Si_Fi_Yh %>%
       filter(sector == "out_chp_elec") %>%
       mutate(sector = "chp_elec") %>%
-      left_join(enduse_fuel_aggregation_industry,by="fuel") %>%
-      mutate(fuel=industry) %>%
+      left_join(enduse_fuel_aggregation_industry, by = "fuel") %>%
+      mutate(fuel = industry) %>%
       select(-industry) %>%
-      mutate(fuel= replace(fuel,fuel== "heat",NA)) %>%
-      mutate(fuel= replace(fuel,fuel== "electricity",NA)) %>%
-      group_by(GCAM_region_ID, sector,fuel,year) %>%
+      mutate(fuel = replace(fuel, fuel == "heat", NA)) %>%
+      mutate(fuel = replace(fuel, fuel == "electricity", NA)) %>%
+      group_by(GCAM_region_ID, sector, fuel, year) %>%
       summarise_each(funs(sum)) %>%
-      filter(!is.na(fuel)) -> L123.out_EJ_R_indchp_F_Yh
+      filter(!is.na(fuel)) ->
+      L123.out_EJ_R_indchp_F_Yh
 
     # Estimates inputs for CHP in the industry sector based on outputs and the electricity ration by fuel (output/elec_ratio)
     L123.out_EJ_R_indchp_F_Yh %>%
       full_join(filter(A23.chp_elecratio,fuel!="hydrogen"), by = "fuel") %>%
-      mutate(outputs_ratio=value/elec_ratio) %>%
-      select(-value,-elec_ratio) -> L123.in_EJ_R_indchp_F_Yh
+      mutate(outputs_ratio = value / elec_ratio) %>%
+      select(-value, -elec_ratio) ->
+      L123.in_EJ_R_indchp_F_Yh
 
-   # Rename necessary outputs to value for long test flag
-   L123.out_EJ_R_elec_F_Yh %>%
-       mutate(value=outputs)%>%select(-outputs)->L123.out_EJ_R_elec_F_Yh
+    # Rename necessary outputs to value for long test flag
+    L123.out_EJ_R_elec_F_Yh %>%
+      mutate(value=outputs) %>%
+      select(-outputs) ->
+      L123.out_EJ_R_elec_F_Yh
 
-   L123.in_EJ_R_elec_F_Yh %>%
-       mutate(value=input)%>%select(-input)->L123.in_EJ_R_elec_F_Yh
+    L123.in_EJ_R_elec_F_Yh %>%
+      mutate(value = input) %>%
+      select(-input) ->
+      L123.in_EJ_R_elec_F_Yh
 
-   L123.in_EJ_R_indchp_F_Yh %>%
-      mutate(value=outputs_ratio)%>%select(-outputs_ratio)->L123.in_EJ_R_indchp_F_Yh
+    L123.in_EJ_R_indchp_F_Yh %>%
+      mutate(value = outputs_ratio) %>%
+      select(-outputs_ratio) ->
+      L123.in_EJ_R_indchp_F_Yh
 
     #Save results and write CSV files
     L123.out_EJ_R_elec_F_Yh %>%
@@ -144,6 +159,7 @@ module_energy_LA123.electricity <- function(command, ...) {
       add_precursors("energy/enduse_fuel_aggregation", "energy/A23.chp_elecratio") %>%
       add_flags(FLAG_NO_XYEAR,FLAG_LONG_YEAR_FORM) ->
       L123.out_EJ_R_elec_F_Yh
+
     L123.in_EJ_R_elec_F_Yh %>%
       add_title("Inputs to electricity sector by GCAM region / fuel / historical year") %>%
       add_units("EJ") %>%
@@ -152,6 +168,7 @@ module_energy_LA123.electricity <- function(command, ...) {
       add_precursors("energy/enduse_fuel_aggregation", "energy/A23.chp_elecratio") %>%
       add_flags(FLAG_NO_XYEAR,FLAG_LONG_YEAR_FORM) ->
       L123.in_EJ_R_elec_F_Yh
+
     L123.eff_R_elec_F_Yh %>%
       add_title("Electric sector efficiencies by GCAM region / fuel / historical year") %>%
       add_units("Unitless") %>%
@@ -160,6 +177,7 @@ module_energy_LA123.electricity <- function(command, ...) {
       add_precursors("energy/enduse_fuel_aggregation", "energy/A23.chp_elecratio") %>%
       add_flags(FLAG_NO_XYEAR,FLAG_LONG_YEAR_FORM) ->
       L123.eff_R_elec_F_Yh
+
     L123.out_EJ_R_indchp_F_Yh %>%
       add_title("Industrial CHP electricity generation by GCAM region / fuel / historical year") %>%
       add_units("EJ") %>%
@@ -168,10 +186,10 @@ module_energy_LA123.electricity <- function(command, ...) {
       add_precursors("energy/enduse_fuel_aggregation", "energy/A23.chp_elecratio") %>%
       add_flags(FLAG_NO_XYEAR,FLAG_LONG_YEAR_FORM) ->
       L123.out_EJ_R_indchp_F_Yh
+
     L123.in_EJ_R_indchp_F_Yh %>%
       add_title("Inputs to industrial CHP by GCAM region / fuel / historical year") %>%
       add_units("EJ") %>%
-      add_comments("comments describing how data generated") %>%
       add_comments("Written by LA123.electricity.R") %>%
       add_legacy_name("L123.in_EJ_R_indchp_F_Yh") %>%
       add_precursors("energy/enduse_fuel_aggregation", "energy/A23.chp_elecratio") %>%
