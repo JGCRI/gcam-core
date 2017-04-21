b4a913f32ae04fcf021f9827f293fe0a64e5119e
Ben Bond-Lamberty <bondlamberty@pnnl.gov>
2017-04-21 08:30:37 -0400

    Rewrite for flexibility and better pipeline usage

--- tests/testthat/test_module_helpers.R
+++ tests/testthat/test_module_helpers.R
@@ -5,31 +5,32 @@ context("module_helpers")
 test_that("get_water_inputs_for_mapping", {
 
   # Bad inputs
-  expect_error(get_water_inputs_for_mapping(1, tibble()))
-  expect_error(get_water_inputs_for_mapping(tibble(), 2))
+  expect_error(get_water_inputs_for_mapping(1, character(), tibble()))
+  expect_error(get_water_inputs_for_mapping(character(), 1, tibble()))
+  expect_error(get_water_inputs_for_mapping(character(), character(), 1))
+  expect_error(get_water_inputs_for_mapping(character(), character(), tibble(), 1))
 
   not_irr <- "not_irrigation"
   water_mapping <- tibble(water.sector = c(IRRIGATION, not_irr),
                           supplysector = c("water_td_irr", "water_td_pri"))
 
   # Mapped GLU
-  d <- tibble(water.sector = c(IRRIGATION, IRRIGATION, not_irr, not_irr),
-              water_type = c(MAPPED_WATER_TYPES[1], MAPPED_WATER_TYPES[2],
-                             MAPPED_WATER_TYPES[1], MAPPED_WATER_TYPES[2]),
-              GLU = c("GLU01", "GLU02", "GLU03", "GLU04"))
-  res <- expect_is(get_water_inputs_for_mapping(d, water_mapping), "character")
-  expect_equal(length(res), nrow(d))
+  water_sector <- c(IRRIGATION, IRRIGATION, not_irr, not_irr)
+  water_type <- c(MAPPED_WATER_TYPES[1], MAPPED_WATER_TYPES[2],
+                  MAPPED_WATER_TYPES[1], MAPPED_WATER_TYPES[2])
+  GLU <- c("GLU01", "GLU02", "GLU03", "GLU04")
+  res <- expect_is(get_water_inputs_for_mapping(water_sector, water_type, water_mapping, GLU), "character")
+  expect_equal(length(res), length(water_sector))
 
   # No GLU, but no mapped water types in irrigation
-  d <- tibble(water.sector = c(IRRIGATION, IRRIGATION, not_irr, not_irr),
-              water_type = c("x", "y", MAPPED_WATER_TYPES[1], MAPPED_WATER_TYPES[2]))
-  res <- expect_is(get_water_inputs_for_mapping(d, water_mapping), "character")
-  expect_equal(length(res), nrow(d))
-  expect_equal(d$water_type[1:2], res[1:2])   # should have original names
+  water_sector <- c(IRRIGATION, IRRIGATION, not_irr, not_irr)
+  water_type <- c("x", "y", MAPPED_WATER_TYPES[1], MAPPED_WATER_TYPES[2])
+  res <- expect_is(get_water_inputs_for_mapping(water_sector, water_type, water_mapping), "character")
+  expect_equal(length(res), length(water_sector))
+  expect_equal(water_type[1:2], res[1:2])   # should have original names
 
   # No GLU - error
-  d <- tibble(water.sector = c(IRRIGATION, IRRIGATION, not_irr, not_irr),
-              water_type = c(MAPPED_WATER_TYPES[1], "y",
-                             MAPPED_WATER_TYPES[1], MAPPED_WATER_TYPES[2]))
-  expect_error(get_water_inputs_for_mapping(d, water_mapping))
+  water_sector <- c(IRRIGATION, IRRIGATION, not_irr, not_irr)
+  water_type = c(MAPPED_WATER_TYPES[1], "y", MAPPED_WATER_TYPES[1], MAPPED_WATER_TYPES[2])
+  expect_error(get_water_inputs_for_mapping(water_sector, water_type, water_mapping))
 })
