d6ded33b41a9c6d11e5341cc54ac894a15b79f60
rynacui <yiyun.cui@pnnl.gov>
2017-05-04 18:28:10 -0400

    transform code

--- R/zchunk_LB152.ag_GTAP_R_C_GLU_irr.R
+++ R/zchunk_LB152.ag_GTAP_R_C_GLU_irr.R
@@ -14,7 +14,7 @@
 #' @importFrom tidyr gather spread
 #' @author YourInitials CurrentMonthName 2017
 #' @export
-module_aglu_LB152.ag_GTAP_R_C_GLU_irr_DISABLED <- function(command, ...) {
+module_aglu_LB152.ag_GTAP_R_C_GLU_irr <- function(command, ...) {
   if(command == driver.DECLARE_INPUTS) {
     return(c(FILE = "common/iso_GCAM_regID",
              FILE = "aglu/FAO_ag_items_PRODSTAT",
@@ -39,69 +39,91 @@ module_aglu_LB152.ag_GTAP_R_C_GLU_irr_DISABLED <- function(command, ...) {
     L151.ag_irrProd_t_ctry_crop <- get_data(all_data, "L151.ag_irrProd_t_ctry_crop")
     L151.ag_rfdProd_t_ctry_crop <- get_data(all_data, "L151.ag_rfdProd_t_ctry_crop")
 
-    # ===================================================
-    # TRANSLATED PROCESSING CODE GOES HERE...
-    #
-    # If you find a mistake/thing to update in the old code and
-    # fixing it will change the output data, causing the tests to fail,
-    # (i) open an issue on GitHub, (ii) consult with colleagues, and
-    # then (iii) code a fix:
-    #
-    # if(OLD_DATA_SYSTEM_BEHAVIOR) {
-    #   ... code that replicates old, incorrect behavior
-    # } else {
-    #   ... new code with a fix
-    # }
-    #
-    #
-    # NOTE: there are 'match' calls in this code. You probably want to use left_join_error_no_match
-    # For more information, see https://github.com/JGCRI/gcamdata/wiki/Name-That-Function
-    # ===================================================
+    # Add region and crop lookup vectors to GTAP tables
+    # Collaps ag commodity data into GCAM regions and commodities, and convert to appropriate units (bm2 and Mt)
+    L151.ag_irrHA_ha_ctry_crop %>%
+      left_join_keep_first_only(select(iso_GCAM_regID, iso, GCAM_region_ID), by = "iso") %>%                      # Match country iso with GCAM region
+      left_join_keep_first_only(select(FAO_ag_items_PRODSTAT, GTAP_crop, GCAM_commodity), by = "GTAP_crop") %>%   # Match GTAP crop with GCAM commodity
+      na.omit %>%                                                                                                 # Drop "NatRubber" not belong to any GCAM commodity
+      mutate(irrHA = irrHA * CONV_HA_BM2) %>%                                                                     # Convert the harvest area unit from hectare to billion m2
+      group_by(GCAM_region_ID, GCAM_commodity, GLU) %>%                                                           # Aggregate to GCAM region and commodity by each GLU
+      summarise(irrHA = sum(irrHA)) %>%
+      ungroup() ->
+      L152.ag_irrHA_bm2_R_C_GLU
+
+    L151.ag_rfdHA_ha_ctry_crop %>%
+      left_join_keep_first_only(select(iso_GCAM_regID, iso, GCAM_region_ID), by = "iso") %>%                      # Match country iso with GCAM region
+      left_join_keep_first_only(select(FAO_ag_items_PRODSTAT, GTAP_crop, GCAM_commodity), by = "GTAP_crop") %>%   # Match GTAP crop with GCAM commodity
+      na.omit %>%                                                                                                 # Drop "NatRubber" not belong to any GCAM commodity
+      mutate(rfdHA = rfdHA * CONV_HA_BM2) %>%                                                                     # Convert the harvest area unit from hectare to billion m2
+      group_by(GCAM_region_ID, GCAM_commodity, GLU) %>%                                                           # Aggregate to GCAM region and commodity by each GLU
+      summarise(rfdHA = sum(rfdHA)) %>%
+      ungroup() ->
+      L152.ag_rfdHA_bm2_R_C_GLU
+
+    L151.ag_irrProd_t_ctry_crop %>%
+      left_join_keep_first_only(select(iso_GCAM_regID, iso, GCAM_region_ID), by = "iso") %>%                      # Match country iso with GCAM region
+      left_join_keep_first_only(select(FAO_ag_items_PRODSTAT, GTAP_crop, GCAM_commodity), by = "GTAP_crop") %>%   # Match GTAP crop with GCAM commodity
+      na.omit %>%                                                                                                 # Drop "NatRubber" not belong to any GCAM commodity
+      mutate(irrProd = irrProd * CONV_TON_MEGATON) %>%                                                            # Convert the production unit from ton to megaton
+      group_by(GCAM_region_ID, GCAM_commodity, GLU) %>%                                                           # Aggregate to GCAM region and commodity by each GLU
+      summarise(irrProd = sum(irrProd)) %>%
+      ungroup() ->
+      L152.ag_irrProd_Mt_R_C_GLU
+
+    L151.ag_rfdProd_t_ctry_crop %>%
+      left_join_keep_first_only(select(iso_GCAM_regID, iso, GCAM_region_ID), by = "iso") %>%                      # Match country iso with GCAM region
+      left_join_keep_first_only(select(FAO_ag_items_PRODSTAT, GTAP_crop, GCAM_commodity), by = "GTAP_crop") %>%   # Match GTAP crop with GCAM commodity
+      na.omit %>%                                                                                                 # Drop "NatRubber" not belong to any GCAM commodity
+      mutate(rfdProd = rfdProd * CONV_TON_MEGATON) %>%                                                            # Convert the production unit from ton to megaton
+      group_by(GCAM_region_ID, GCAM_commodity, GLU) %>%                                                           # Aggregate to GCAM region and commodity by each GLU
+      summarise(rfdProd = sum(rfdProd)) %>%
+      ungroup() ->
+      L152.ag_rfdProd_Mt_R_C_GLU
 
     # Produce outputs
-    # Temporary code below sends back empty data frames marked "don't test"
-    # Note that all precursor names (in `add_precursor`) must be in this chunk's inputs
-    # There's also a `same_precursors_as(x)` you can use
-    # If no precursors (very rare) don't call `add_precursor` at all
-    tibble() %>%
+    L152.ag_irrHA_bm2_R_C_GLU %>%
       add_title("descriptive title of data") %>%
       add_units("units") %>%
       add_comments("comments describing how data generated") %>%
       add_comments("can be multiple lines") %>%
       add_legacy_name("L152.ag_irrHA_bm2_R_C_GLU") %>%
-      add_precursors("precursor1", "precursor2", "etc") %>%
-      # typical flags, but there are others--see `constants.R`
-      add_flags(FLAG_LONG_YEAR_FORM, FLAG_NO_XYEAR) ->
+      add_precursors("common/iso_GCAM_regID",
+                     "aglu/FAO_ag_items_PRODSTAT",
+                     "L151.ag_irrHA_ha_ctry_crop") ->
       L152.ag_irrHA_bm2_R_C_GLU
-    tibble() %>%
+
+    L152.ag_rfdHA_bm2_R_C_GLU %>%
       add_title("descriptive title of data") %>%
       add_units("units") %>%
       add_comments("comments describing how data generated") %>%
       add_comments("can be multiple lines") %>%
       add_legacy_name("L152.ag_rfdHA_bm2_R_C_GLU") %>%
-      add_precursors("precursor1", "precursor2", "etc") %>%
-      # typical flags, but there are others--see `constants.R`
-      add_flags(FLAG_LONG_YEAR_FORM, FLAG_NO_XYEAR) ->
+      add_precursors("common/iso_GCAM_regID",
+                     "aglu/FAO_ag_items_PRODSTAT",
+                     "L151.ag_rfdHA_ha_ctry_crop") ->
       L152.ag_rfdHA_bm2_R_C_GLU
-    tibble() %>%
+
+    L152.ag_irrProd_Mt_R_C_GLU %>%
       add_title("descriptive title of data") %>%
       add_units("units") %>%
       add_comments("comments describing how data generated") %>%
       add_comments("can be multiple lines") %>%
       add_legacy_name("L152.ag_irrProd_Mt_R_C_GLU") %>%
-      add_precursors("precursor1", "precursor2", "etc") %>%
-      # typical flags, but there are others--see `constants.R`
-      add_flags(FLAG_LONG_YEAR_FORM, FLAG_NO_XYEAR) ->
+      add_precursors("common/iso_GCAM_regID",
+                     "aglu/FAO_ag_items_PRODSTAT",
+                     "L151.ag_irrProd_t_ctry_crop") ->
       L152.ag_irrProd_Mt_R_C_GLU
-    tibble() %>%
+
+    L152.ag_rfdProd_Mt_R_C_GLU %>%
       add_title("descriptive title of data") %>%
       add_units("units") %>%
       add_comments("comments describing how data generated") %>%
       add_comments("can be multiple lines") %>%
       add_legacy_name("L152.ag_rfdProd_Mt_R_C_GLU") %>%
-      add_precursors("precursor1", "precursor2", "etc") %>%
-      # typical flags, but there are others--see `constants.R`
-      add_flags(FLAG_LONG_YEAR_FORM, FLAG_NO_XYEAR) ->
+      add_precursors("common/iso_GCAM_regID",
+                     "aglu/FAO_ag_items_PRODSTAT",
+                     "L151.ag_rfdProd_t_ctry_crop") ->
       L152.ag_rfdProd_Mt_R_C_GLU
 
     return_data(L152.ag_irrHA_bm2_R_C_GLU, L152.ag_rfdHA_bm2_R_C_GLU, L152.ag_irrProd_Mt_R_C_GLU, L152.ag_rfdProd_Mt_R_C_GLU)
