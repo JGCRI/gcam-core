9a3d722d2617858d1e5cfa3523c019777aa80b5a
yalingliupnnl <yaling.liu@pnnl.gov>
2017-05-04 14:34:46 -0400

    temporary commit

--- R/zchunk_L210.water.demand.primary.R
+++ R/zchunk_L210.water.demand.primary.R
@@ -14,7 +14,7 @@
 #' @importFrom tidyr gather spread
 #' @author YourInitials CurrentMonthName 2017
 #' @export
-module_water_L210.water.demand.primary_DISABLED <- function(command, ...) {
+module_water_L210.water.demand.primary <- function(command, ...) {
   if(command == driver.DECLARE_INPUTS) {
     return(c(FILE = "common/GCAM_region_names",
              FILE = "water/A03.sector",
@@ -37,6 +37,36 @@ module_water_L210.water.demand.primary_DISABLED <- function(command, ...) {
     # ===================================================
     # TRANSLATED PROCESSING CODE GOES HERE...
     #
+    A21.globaltech_coef %>% select(supplysector,subsector,technology) -> A21.tmp1
+    A22.globaltech_coef %>% filter( grepl('nuclear', supplysector)) %>% select(supplysector,subsector,technology) -> A22.tmp
+    A21.A22.tmp <- rbind(A21.tmp1,A22.tmp)
+    L210.TechCoef.v1 <- merge(L110.water_demand_primary_R_S_W_m3_GJ,A21.A22.tmp,by="supplysector")
+
+    # L210.TechCoef.v1 %>% filter( grepl('nuclear', supplysector))
+
+    # Avoid double acounting unconventional oil in the regional oil sector as it is already
+    # accounted for in unconventional oil production.
+    dontuse1 <- data.frame(supp=c("regional oil"))
+    dontuse2 <- data.frame(subs=c("unconventional oil"))
+    L210.TechCoef.v1  %>% filter(!(supplysector %in% dontuse1$supp & subsector %in% dontuse2$subs)) -> L210.TechCoef.v2
+    L210.TechCoef.v2 %>% mutate(water_sector ="mining") -> L210.TechCoef.v3
+
+
+
+
+    ############################
+    #old data system
+    #############################
+    L210.TechCoef$minicam.energy.input <- get_water_inputs_for_mapping( L210.TechCoef, A03.sector )
+    L210.TechCoef <- merge( L210.TechCoef, GCAM_region_names )
+    L210.TechCoef$market.name <- L210.TechCoef[[reg]]
+    # Set the coef for all years
+    L210.orig_num_rows <- nrow( L210.TechCoef )
+    L210.TechCoef <- L210.TechCoef[ rep( 1:nrow( L210.TechCoef ), times=length( model_years ) ), ]
+    L210.TechCoef$year <- model_years[ sort( rep( 1:length( model_years ), times=L210.orig_num_rows ) ) ]
+    L210.TechCoef <- L210.TechCoef[, names_TechCoef ]
+
+
     # If you find a mistake/thing to update in the old code and
     # fixing it will change the output data, causing the tests to fail,
     # (i) open an issue on GitHub, (ii) consult with colleagues, and
@@ -66,7 +96,8 @@ module_water_L210.water.demand.primary_DISABLED <- function(command, ...) {
       add_comments("comments describing how data generated") %>%
       add_comments("can be multiple lines") %>%
       add_legacy_name("L210.TechCoef") %>%
-      add_precursors("precursor1", "precursor2", "etc") %>%
+      add_precursors("common/GCAM_region_names", "water/A03.sector", "energy/A21.globaltech_coef",
+                      "energy/A22.globaltech_coef","L110.water_demand_primary_R_S_W_m3_GJ")                      %>%
       # typical flags, but there are others--see `constants.R`
       add_flags(FLAG_LONG_YEAR_FORM, FLAG_NO_XYEAR) ->
       L210.TechCoef
