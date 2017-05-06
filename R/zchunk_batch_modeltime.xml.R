317b3c9d6cec4820d65a73bd774e6541e44358c5
Ben Bond-Lamberty <bondlamberty@pnnl.gov>
2017-05-05 21:39:10 -0400

    Documentation and version update

--- R/zchunk_batch_modeltime.xml.R
+++ R/zchunk_batch_modeltime.xml.R
@@ -1,6 +1,6 @@
 #' module_modeltime_batch_modeltime.xml
 #'
-#' Briefly describe what this chunk does.
+#' Construct XML data structure for \code{modeltime.xml}.
 #'
 #' @param command API command to execute
 #' @param ... other optional parameters, depending on command
@@ -8,12 +8,6 @@
 #' a vector of output names, or (if \code{command} is "MAKE") all
 #' the generated outputs: \code{modeltime.xml}. The corresponding file in the
 #' original data system was \code{batch_modeltime.xml.R} (modeltime XML).
-#' @details Describe in detail what this chunk does.
-#' @importFrom assertthat assert_that
-#' @importFrom dplyr filter mutate select
-#' @importFrom tidyr gather spread
-#' @author YourInitials CurrentMonthName 2017
-#' @export
 module_modeltime_batch_modeltime.xml <- function(command, ...) {
   if(command == driver.DECLARE_INPUTS) {
     return(c("L200.ModelTime",
