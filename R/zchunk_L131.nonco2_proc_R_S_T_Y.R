22870fea33603f23f38c4a5df224437ed56bb51f
Ben Bond-Lamberty <bondlamberty@pnnl.gov>
2017-05-08 12:16:02 -0400

    Spacing tweaks

--- R/zchunk_L131.nonco2_proc_R_S_T_Y.R
+++ R/zchunk_L131.nonco2_proc_R_S_T_Y.R
@@ -83,7 +83,6 @@ module_emissions_L131.nonco2_proc_R_S_T_Y <- function(command, ...) {
       mutate(tech_share = tech_emissions.x / sector_emissions) -> # calculate tech_share
       L131.nonco2_pct_R_prc_S_S_2005
 
-
     # Third: Disaggregate EDGAR emissions to subsectors
     # First aggregate all EDGAR data and subset for processing sectors
 
@@ -93,8 +92,8 @@ module_emissions_L131.nonco2_proc_R_S_T_Y <- function(command, ...) {
       bind_rows(filter(EDGAR_gases, Non.CO2 != "NMVOC")) %>%
       left_join(EDGAR_sector, by = c("IPCC_description", "IPCC")) %>%
       rename(EDGAR_agg_sector = agg_sector) %>%
-      mutate( iso = tolower( ISO_A3 ), ISO_A3 = NULL ) %>%
-      change_iso_code('rou', 'rom') %>%
+      mutate( iso = tolower(ISO_A3), ISO_A3 = NULL ) %>%
+      change_iso_code("rou", "rom") %>%
       left_join(iso_GCAM_regID, by = "iso") %>%
       filter(EDGAR_agg_sector %in% c("industry_processes", "chemicals", "landfills", "wastewater", "aerosols",
                                      "metals", "foams", "solvents", "semiconductors")) %>%
