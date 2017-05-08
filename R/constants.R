be54ad03e83bebe30ea539a68da665ce282032e9
Steve Smith (PNNL-JGCRI) <ssmith@pnnl.gov>
2017-05-08 17:14:46 -0400

    SJS Updates

--- R/constants.R
+++ R/constants.R
@@ -95,6 +95,7 @@ CONV_HA_BM2 <- 1e-5
 CONV_THA_KGM2 <- 0.1   # tons C/ha -> kg C/m2
 CONV_GG_TG <- 0.001 # gigagrams to tegagrams
 CONV_TST_TG <- 0.000907 # thousand short tons to Tg
+CONV_KG_TO_TG <- 0.000000001
 
 # Cubic meters (m3) to billion cubic meters (bm3)
 CONV_M3_BM3 <- 1e-09
--- R/zchunk_L123.bcoc_awb_R_S_T_Y.R
+++ R/zchunk_L123.bcoc_awb_R_S_T_Y.R
@@ -1,6 +1,6 @@
 #' module_emissions_L123.bcoc_awb_R_S_T_Y
 #'
-#' Briefly describe what this chunk does.
+#' Produces BC and OC AWB emission factors for each Ag production technology.
 #'
 #' @param command API command to execute
 #' @param ... other optional parameters, depending on command
@@ -8,17 +8,17 @@
 #' a vector of output names, or (if \code{command} is "MAKE") all
 #' the generated outputs: \code{L123.bcoc_tgmt_R_awb_2000}. The corresponding file in the
 #' original data system was \code{L123.bcoc_awb_R_S_T_Y.R} (emissions level1).
-#' @details Describe in detail what this chunk does.
+#' @details Use year 2000 BC and OC AWB emissions to calculate emission factors for each ag production technology.
 #' @importFrom assertthat assert_that
 #' @importFrom dplyr filter mutate select
 #' @importFrom tidyr gather spread
-#' @author YourInitials CurrentMonthName 2017
+#' @author SJS May 2017
 #' @export
-module_emissions_L123.bcoc_awb_R_S_T_Y_DISABLED <- function(command, ...) {
+module_emissions_L123.bcoc_awb_R_S_T_Y <- function(command, ...) {
   if(command == driver.DECLARE_INPUTS) {
     return(c(FILE = "common/iso_GCAM_regID",
              "L103.ag_Prod_Mt_R_C_Y_GLU",
-             "L121.AWBshare_R_C_Y_GLU",
+             FILE = "temp-data-inject/L121.AWBshare_R_C_Y_GLU",
              FILE = "emissions/RCP_BC_2000",
              FILE = "emissions/RCP_OC_2000"))
   } else if(command == driver.DECLARE_OUTPUTS) {
@@ -30,43 +30,78 @@ module_emissions_L123.bcoc_awb_R_S_T_Y_DISABLED <- function(command, ...) {
     # Load required inputs
     iso_GCAM_regID <- get_data(all_data, "common/iso_GCAM_regID")
     L103.ag_Prod_Mt_R_C_Y_GLU <- get_data(all_data, "L103.ag_Prod_Mt_R_C_Y_GLU")
-    L121.AWBshare_R_C_Y_GLU <- get_data(all_data, "L121.AWBshare_R_C_Y_GLU")
+    L121.AWBshare_R_C_Y_GLU <- get_data(all_data, "temp-data-inject/L121.AWBshare_R_C_Y_GLU")
     RCP_BC_2000 <- get_data(all_data, "emissions/RCP_BC_2000")
     RCP_OC_2000 <- get_data(all_data, "emissions/RCP_OC_2000")
 
-    # ===================================================
-    # TRANSLATED PROCESSING CODE GOES HERE...
-    #
-    # If you find a mistake/thing to update in the old code and
-    # fixing it will change the output data, causing the tests to fail,
-    # (i) open an issue on GitHub, (ii) consult with colleagues, and
-    # then (iii) code a fix:
-    #
-    # if(OLD_DATA_SYSTEM_BEHAVIOR) {
-    #   ... code that replicates old, incorrect behavior
-    # } else {
-    #   ... new code with a fix
-    # }
-    #
-    #
-    # NOTE: there are `merge` and/or 'match' calls in this code. Be careful!
-    # For more information, see https://github.com/JGCRI/gcamdata/wiki/Merge-and-Match
-    # ===================================================
+    # Extract 2000 emissions data for AWB emissions and assign emission species identifier
+    RCP_BC_2000 %>%
+      select(iso, awb) %>%
+      mutate(Non.CO2 = "BC_AWB") -> BC_AWB_2000
+
+    RCP_OC_2000 %>%
+      select(iso, awb) %>%
+      mutate(Non.CO2 = "OC_AWB") -> OC_AWB_2000
+
+    # Aggregate BC and OC emissions to GCAM regions and convert to Tg
+    if(OLD_DATA_SYSTEM_BEHAVIOR) {
+      # Old system used match and ended up with just the first value instead of summing all values
+      bind_rows(OC_AWB_2000, BC_AWB_2000) %>%
+      left_join(iso_GCAM_regID, by = "iso") %>% #Not all iso's will necesarilly have an input value. This is ok.
+      group_by(Non.CO2, GCAM_region_ID) %>%
+      summarise( awb = first( awb ) )  %>%
+      filter( !is.na(awb) ) %>%
+      mutate( awb = awb * CONV_KG_TO_TG ) %>%
+      ungroup() ->
+      BCOC_AWB_2000
+    } else {
+        bind_rows(OC_AWB_2000, BC_AWB_2000) %>%
+        left_join(iso_GCAM_regID, by = "iso") %>% #Not all iso's will necesarilly have an input value. This is ok.
+        group_by(Non.CO2, GCAM_region_ID) %>%
+        summarize_if(is.numeric , sum) %>%
+        filter( !is.na(awb) ) %>%
+        mutate( awb = awb * CONV_KG_TO_TG ) %>%
+        ungroup() ->
+        BCOC_AWB_2000
+   }
+
+    # Extract only 2000 data from ag production data
+    filter( L103.ag_Prod_Mt_R_C_Y_GLU, year == 2000 ) %>%
+      select( -year ) %>%
+      mutate(GLU = as.character(GLU), GCAM_commodity = as.character(GCAM_commodity) ) ->
+      L103.ag_Prod_Mt_R_C_Y_GLU_2000
+
+    # Allocate aggreate regional year 2000 AWB emissions into GCAM crop and GLU
+    # This is done using shares from L121.AWBshare_R_C_Y_GLU, which is calculated in another chunk
+    filter(L121.AWBshare_R_C_Y_GLU, year == "X2000")  %>%
+      select( -year ) %>%
+      # Add BC and OC emissions data
+      repeat_add_columns( tibble::tibble( Non.CO2 = c("BC_AWB","OC_AWB") ) ) %>%
+      # Use left_join since adding additional identifier ("Non.CO2"), so will double number of rows
+      left_join_error_no_match(BCOC_AWB_2000, by = c( "GCAM_region_ID", "Non.CO2") ) %>%
+      # Calculate AWB emissions by crop and GLU by sharing out regional total awb emissions
+      # by the share for each individual GCAM technology
+      mutate( awb_emission = AWB_emiss_share * awb ) %>%
+      # Add production data
+      left_join_error_no_match( L103.ag_Prod_Mt_R_C_Y_GLU_2000, by = c("GCAM_region_ID", "GCAM_commodity", "GLU") ) %>%
+      # Calculate emission factor, which is
+      mutate( emfact = awb_emission / value ) %>%
+      select( -awb_emission, -value, -AWB_emiss_share, -awb )  %>%
+      # Replace NaNs with zeros
+      mutate_all( funs( replace(., is.na(.), 0))) ->
+      L121.AWB_Emissions_FactorR_C_Y_GLU
 
     # Produce outputs
-    # Temporary code below sends back empty data frames marked "don't test"
-    # Note that all precursor names (in `add_precursor`) must be in this chunk's inputs
-    # There's also a `same_precursors_as(x)` you can use
-    # If no precursors (very rare) don't call `add_precursor` at all
-    tibble() %>%
-      add_title("descriptive title of data") %>%
+    L121.AWB_Emissions_FactorR_C_Y_GLU %>%
+      add_title("Emission factors for BC and OC emissions by GCAM GLU and crop") %>%
       add_units("units") %>%
-      add_comments("comments describing how data generated") %>%
-      add_comments("can be multiple lines") %>%
+      add_comments("Derived from RCP year 2000 AWB emissions by region and allocated according to above ground non-harvested biomass") %>%
       add_legacy_name("L123.bcoc_tgmt_R_awb_2000") %>%
-      add_precursors("precursor1", "precursor2", "etc") %>%
-      # typical flags, but there are others--see `constants.R`
-      add_flags(FLAG_NO_TEST, FLAG_LONG_YEAR_FORM, FLAG_NO_XYEAR) ->
+      add_precursors("common/iso_GCAM_regID",
+                     "emissions/RCP_BC_2000",
+                     "emissions/RCP_OC_2000",
+                     "L103.ag_Prod_Mt_R_C_Y_GLU",
+                     "temp-data-inject/L121.AWBshare_R_C_Y_GLU") ->
       L123.bcoc_tgmt_R_awb_2000
 
     return_data(L123.bcoc_tgmt_R_awb_2000)
