ca7c32d3789050c3db4fa90748e96a2f7eb6d224
Steve Smith (PNNL-JGCRI) <ssmith@pnnl.gov>
2017-05-09 13:13:40 -0400

    Revised to respond to bbl comments.

--- R/zchunk_L123.bcoc_awb_R_S_T_Y.R
+++ R/zchunk_L123.bcoc_awb_R_S_T_Y.R
@@ -13,7 +13,7 @@
 #' @importFrom dplyr filter mutate select
 #' @importFrom tidyr gather spread
 #' @author SJS May 2017
-#' @export
+
 module_emissions_L123.bcoc_awb_R_S_T_Y <- function(command, ...) {
   if(command == driver.DECLARE_INPUTS) {
     return(c(FILE = "common/iso_GCAM_regID",
@@ -49,7 +49,8 @@ module_emissions_L123.bcoc_awb_R_S_T_Y <- function(command, ...) {
     if(OLD_DATA_SYSTEM_BEHAVIOR) {
       # Old system used match and ended up with just the first value instead of summing all values
       bind_rows(OC_AWB_2000, BC_AWB_2000) %>%
-      left_join(iso_GCAM_regID, by = "iso") %>% #Not all iso's will necesarilly have an input value. This is ok.
+      # Not all iso's will necesarilly have an input value. This is ok, but use left_join becuase of this.
+      left_join(iso_GCAM_regID, by = "iso") %>%
       group_by(Non.CO2, GCAM_region_ID) %>%
       summarise(awb = first(awb))  %>%
       filter(!is.na(awb)) %>%
@@ -58,7 +59,8 @@ module_emissions_L123.bcoc_awb_R_S_T_Y <- function(command, ...) {
       BCOC_AWB_2000
     } else {
         bind_rows(OC_AWB_2000, BC_AWB_2000) %>%
-        left_join(iso_GCAM_regID, by = "iso") %>% #Not all iso's will necesarilly have an input value. This is ok.
+        # Not all iso's will necesarilly have an input value. This is ok, but use left_join becuase of this.
+        left_join(iso_GCAM_regID, by = "iso") %>%
         group_by(Non.CO2, GCAM_region_ID) %>%
         summarize_if(is.numeric , sum) %>%
         filter(!is.na(awb)) %>%
@@ -69,17 +71,15 @@ module_emissions_L123.bcoc_awb_R_S_T_Y <- function(command, ...) {
 
     # Extract only 2000 data from ag production data
     filter(L103.ag_Prod_Mt_R_C_Y_GLU, year == 2000) %>%
-      select(-year) %>%
-      mutate(GLU = as.character(GLU), GCAM_commodity = as.character(GCAM_commodity)) ->
+      select(-year)  ->
       L103.ag_Prod_Mt_R_C_Y_GLU_2000
 
     # Allocate aggreate regional year 2000 AWB emissions into GCAM crop and GLU
     # This is done using shares from L121.AWBshare_R_C_Y_GLU, which is calculated in another chunk
-    filter(L121.AWBshare_R_C_Y_GLU, year == "X2000")  %>%
+    filter(L121.AWBshare_R_C_Y_GLU, year == "2000")  %>%
       select(-year) %>%
       # Add BC and OC emissions data
       repeat_add_columns(tibble::tibble(Non.CO2 = c("BC_AWB","OC_AWB"))) %>%
-      # Use left_join since adding additional identifier ("Non.CO2"), so will double number of rows
       left_join_error_no_match(BCOC_AWB_2000, by = c("GCAM_region_ID", "Non.CO2")) %>%
       # Calculate AWB emissions by crop and GLU by sharing out regional total awb emissions
       # by the share for each individual GCAM technology
@@ -96,7 +96,7 @@ module_emissions_L123.bcoc_awb_R_S_T_Y <- function(command, ...) {
     # Produce outputs
     L121.AWB_Emissions_FactorR_C_Y_GLU %>%
       add_title("Emission factors for BC and OC emissions by GCAM GLU and crop") %>%
-      add_units("units") %>%
+      add_units("kt/Mt") %>%
       add_comments("Derived from RCP year 2000 AWB emissions by region and allocated according to above ground non-harvested biomass") %>%
       add_legacy_name("L123.bcoc_tgmt_R_awb_2000") %>%
       add_precursors("common/iso_GCAM_regID",
