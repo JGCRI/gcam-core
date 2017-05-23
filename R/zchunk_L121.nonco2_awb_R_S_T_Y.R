dbe1ce2871a1b1668733c9df72d9580ef4e4396f
kdorheim <kalyn.dorheim@pnnl.gov>
2017-05-23 12:28:35 -0400

    Revert "Start"
    This reverts commit eca0b8e23a0673229ff3438591a2b2bcb702aeec.

--- R/zchunk_L121.nonco2_awb_R_S_T_Y.R
+++ R/zchunk_L121.nonco2_awb_R_S_T_Y.R
@@ -14,19 +14,22 @@
 #' @importFrom tidyr gather spread
 #' @author YourInitials CurrentMonthName 2017
 #' @export
-module_emissions_L121.nonco2_awb_R_S_T_Y <- function(command, ...) {
+module_emissions_L121.nonco2_awb_R_S_T_Y_DISABLED <- function(command, ...) {
   if(command == driver.DECLARE_INPUTS) {
     return(c(FILE = "common/iso_GCAM_regID",
-             FILE = "emissions/EDGAR/EDGAR_sector",
+             # ** NOTE *** the following file has been removed from the repo
+             # please talk to Kate about why and how to work around this. Thanks
+             # FILE = "emissions/EDGAR_nation",
+             FILE = "emissions/EDGAR_sector",
              "L103.ag_Prod_Mt_R_C_Y_GLU",
              "L111.ag_resbio_R_C",
-             FILE = "emissions/EDGAR/EDGAR_SO2",
-             FILE = "emissions/EDGAR/EDGAR_CO",
-             FILE = "emissions/EDGAR/EDGAR_NOx",
-             FILE = "emissions/EDGAR/EDGAR_NMVOC",
-             FILE = "emissions/EDGAR/EDGAR_CH4",
-             FILE = "emissions/EDGAR/EDGAR_N2O",
-             FILE = "emissions/EDGAR/EDGAR_NH3"))
+             FILE = "emissions/EDGAR_SO2",
+             FILE = "emissions/EDGAR_CO",
+             FILE = "emissions/EDGAR_NOx",
+             FILE = "emissions/EDGAR_NMVOC",
+             FILE = "emissions/EDGAR_CH4",
+             FILE = "emissions/EDGAR_N2O",
+             FILE = "emissions/EDGAR_NH3"))
   } else if(command == driver.DECLARE_OUTPUTS) {
     return(c("L121.AWBshare_R_C_Y_GLU",
              "L121.nonco2_tg_R_awb_C_Y_GLU"))
@@ -36,54 +39,64 @@ module_emissions_L121.nonco2_awb_R_S_T_Y <- function(command, ...) {
 
     # Load required inputs
     iso_GCAM_regID <- get_data(all_data, "common/iso_GCAM_regID")
-    EDGAR_sector <- get_data(all_data, "emissions/EDGAR/EDGAR_sector")
+    EDGAR_nation <- get_data(all_data, "emissions/EDGAR_nation")
+    EDGAR_sector <- get_data(all_data, "emissions/EDGAR_sector")
     L103.ag_Prod_Mt_R_C_Y_GLU <- get_data(all_data, "L103.ag_Prod_Mt_R_C_Y_GLU")
     L111.ag_resbio_R_C <- get_data(all_data, "L111.ag_resbio_R_C")
-    EDGAR_SO2 <- get_data(all_data, "emissions/EDGAR/EDGAR_SO2")
-    EDGAR_CO <- get_data(all_data, "emissions/EDGAR/EDGAR_CO")
-    EDGAR_NOx <- get_data(all_data, "emissions/EDGAR/EDGAR_NOx")
-    EDGAR_NMVOC <- get_data(all_data, "emissions/EDGAR/EDGAR_NMVOC")
-    EDGAR_CH4 <- get_data(all_data, "emissions/EDGAR/EDGAR_CH4")
-    EDGAR_N2O <- get_data(all_data, "emissions/EDGAR/EDGAR_N2O")
-    EDGAR_NH3 <- get_data(all_data, "emissions/EDGAR/EDGAR_NH3")
+    EDGAR_SO2 <- get_data(all_data, "emissions/EDGAR_SO2")
+    EDGAR_CO <- get_data(all_data, "emissions/EDGAR_CO")
+    EDGAR_NOx <- get_data(all_data, "emissions/EDGAR_NOx")
+    EDGAR_NMVOC <- get_data(all_data, "emissions/EDGAR_NMVOC")
+    EDGAR_CH4 <- get_data(all_data, "emissions/EDGAR_CH4")
+    EDGAR_N2O <- get_data(all_data, "emissions/EDGAR_N2O")
+    EDGAR_NH3 <- get_data(all_data, "emissions/EDGAR_NH3")
 
     # ===================================================
-    #lenght(d)
-    #L103.ag_Prod_Mt_R_C_Y_GLU %>%
-    #  left_join_error_no_match(L111.ag_resbio_R_C, by = c("GCAM_region_ID", "GCAM_commodity")) -> out
-
-
-
-
+    # TRANSLATED PROCESSING CODE GOES HERE...
+    #
+    # If you find a mistake/thing to update in the old code and
+    # fixing it will change the output data, causing the tests to fail,
+    # (i) open an issue on GitHub, (ii) consult with colleagues, and
+    # then (iii) code a fix:
+    #
+    # if(OLD_DATA_SYSTEM_BEHAVIOR) {
+    #   ... code that replicates old, incorrect behavior
+    # } else {
+    #   ... new code with a fix
+    # }
+    #
+    #
+    # NOTE: there are 'match' calls in this code. You probably want to use left_join_error_no_match
+    # For more information, see https://github.com/JGCRI/gcamdata/wiki/Name-That-Function
+    # NOTE: This code uses vecpaste
+    # This function can be removed; see https://github.com/JGCRI/gcamdata/wiki/Name-That-Function
+    # NOTE: This code uses repeat_and_add_vector
+    # This function can be removed; see https://github.com/JGCRI/gcamdata/wiki/Name-That-Function
     # ===================================================
 
     # Produce outputs
+    # Temporary code below sends back empty data frames marked "don't test"
+    # Note that all precursor names (in `add_precursor`) must be in this chunk's inputs
+    # There's also a `same_precursors_as(x)` you can use
+    # If no precursors (very rare) don't call `add_precursor` at all
     tibble() %>%
       add_title("descriptive title of data") %>%
       add_units("units") %>%
       add_comments("comments describing how data generated") %>%
       add_comments("can be multiple lines") %>%
       add_legacy_name("L121.AWBshare_R_C_Y_GLU") %>%
-      add_precursors("common/iso_GCAM_regID") %>%
-      add_precursors("emissions/EDGAR/EDGAR_sector", "emissions/EDGAR/EDGAR_SO2","emissions/EDGAR/EDGAR_CO",
-                     "emissions/EDGAR/EDGAR_NOx", "emissions/EDGAR/EDGAR_NMVOC", "emissions/EDGAR/EDGAR_CH4",
-                     "emissions/EDGAR/EDGAR_N2O", "emissions/EDGAR/EDGAR_NH3") %>%
-      add_precursors("L103.ag_Prod_Mt_R_C_Y_GLU", "L111.ag_resbio_R_C") %>%
+      add_precursors("precursor1", "precursor2", "etc") %>%
       # typical flags, but there are others--see `constants.R`
       add_flags(FLAG_LONG_YEAR_FORM, FLAG_NO_XYEAR) ->
       L121.AWBshare_R_C_Y_GLU
-
     tibble() %>%
       add_title("descriptive title of data") %>%
       add_units("units") %>%
       add_comments("comments describing how data generated") %>%
       add_comments("can be multiple lines") %>%
       add_legacy_name("L121.nonco2_tg_R_awb_C_Y_GLU") %>%
-      add_precursors("common/iso_GCAM_regID") %>%
-      add_precursors("emissions/EDGAR/EDGAR_sector", "emissions/EDGAR/EDGAR_SO2","emissions/EDGAR/EDGAR_CO",
-                     "emissions/EDGAR/EDGAR_NOx", "emissions/EDGAR/EDGAR_NMVOC", "emissions/EDGAR/EDGAR_CH4",
-                     "emissions/EDGAR/EDGAR_N2O", "emissions/EDGAR/EDGAR_NH3") %>%
-      add_precursors("L103.ag_Prod_Mt_R_C_Y_GLU", "L111.ag_resbio_R_C") %>%
+      add_precursors("precursor1", "precursor2", "etc") %>%
+      # typical flags, but there are others--see `constants.R`
       add_flags(FLAG_LONG_YEAR_FORM, FLAG_NO_XYEAR) ->
       L121.nonco2_tg_R_awb_C_Y_GLU
 
