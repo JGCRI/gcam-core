221b2bc1a3a4282adcfbdaf1c2dcf76610ee7a51
kvcalvin <katherine.calvin@pnnl.gov>
2017-06-09 16:12:17 -0400

    Process fertilizer coefficients
    Copy coefficients to each of the four management technologies (irr/rfd
    x hi/lo)

--- R/zchunk_L2062.ag_Fert_irr_mgmt.R
+++ R/zchunk_L2062.ag_Fert_irr_mgmt.R
@@ -45,19 +45,37 @@ module_aglu_L2062.ag_Fert_irr_mgmt <- function(command, ...) {
       mutate(year = as.integer(substr(year, 2, 5))) ->
       L142.ag_Fert_IO_R_C_Y_GLU
 
+    # Process Fertilizer Coefficients: Copy coefficients to all four technologies (irr/rfd + hi/lo)
+    L142.ag_Fert_IO_R_C_Y_GLU %>%
+      filter(year %in% BASE_YEARS) %>%
+      left_join(GCAM_region_names, by="GCAM_region_ID") %>%
+      left_join(basin_to_country_mapping[ c("GLU_code", "GLU_name")], by=c("GLU" = "GLU_code")) %>%
+      repeat_add_columns(tibble::tibble(IRR_RFD = c("IRR", "RFD")) ) %>%
+      repeat_add_columns(tibble::tibble(MGMT = c("hi", "lo")) ) %>%
+      mutate(AgSupplySector = GCAM_commodity,
+             AgSupplySubsector = paste(GCAM_commodity, GLU_name, sep="_"),
+             AgProductionTechnology = paste(GCAM_commodity, GLU_name, IRR_RFD, MGMT, sep="_")) %>%
+      mutate(minicam.energy.input = "N fertilizer") %>%
+      rename(coefficient = value) %>%
+      select(region, AgSupplySector, AgSupplySubsector, AgProductionTechnology, minicam.energy.input, year, coefficient) ->
+      L2062.AgCoef_Fert_ag_irr_mgmt
 
+    # Copy 2010 coefficients to all future years, then bind with historic coefficients
+    L2062.AgCoef_Fert_ag_irr_mgmt %>%
+      filter(year == max(BASE_YEARS)) %>%
+      select(-year) %>%
+      repeat_add_columns(tibble::tibble(year = FUTURE_YEARS)) %>%
+      bind_rows(L2062.AgCoef_Fert_ag_irr_mgmt) ->
+      L2062.AgCoef_Fert_ag_irr_mgmt
 
     # Produce outputs
-    tibble() %>%
+    L2062.AgCoef_Fert_ag_irr_mgmt %>%
       add_title("descriptive title of data") %>%
       add_units("units") %>%
       add_comments("comments describing how data generated") %>%
       add_comments("can be multiple lines") %>%
       add_legacy_name("L2062.AgCoef_Fert_ag_irr_mgmt") %>%
-      add_precursors("common/GCAM_region_names", "water/basin_to_country_mapping", "aglu/A_Fodderbio_chars",
-                     "temp-data-inject/L142.ag_Fert_IO_R_C_Y_GLU", "temp-data-inject/L205.AgCost_ag", "temp-data-inject/L205.AgCost_bio") %>%
-      # typical flags, but there are others--see `constants.R`
-      add_flags(FLAG_LONG_YEAR_FORM, FLAG_NO_XYEAR) ->
+      add_precursors("common/GCAM_region_names", "water/basin_to_country_mapping", "temp-data-inject/L142.ag_Fert_IO_R_C_Y_GLU") ->
       L2062.AgCoef_Fert_ag_irr_mgmt
     tibble() %>%
       add_title("descriptive title of data") %>%
@@ -68,7 +86,7 @@ module_aglu_L2062.ag_Fert_irr_mgmt <- function(command, ...) {
       add_precursors("common/GCAM_region_names", "water/basin_to_country_mapping", "aglu/A_Fodderbio_chars",
                      "temp-data-inject/L142.ag_Fert_IO_R_C_Y_GLU", "temp-data-inject/L205.AgCost_ag", "temp-data-inject/L205.AgCost_bio") %>%
       # typical flags, but there are others--see `constants.R`
-      add_flags(FLAG_LONG_YEAR_FORM, FLAG_NO_XYEAR) ->
+      add_flags(FLAG_LONG_YEAR_FORM, FLAG_NO_XYEAR, FLAG_NO_TEST) ->
       L2062.AgCoef_Fert_bio_irr_mgmt
     tibble() %>%
       add_title("descriptive title of data") %>%
@@ -79,7 +97,7 @@ module_aglu_L2062.ag_Fert_irr_mgmt <- function(command, ...) {
       add_precursors("common/GCAM_region_names", "water/basin_to_country_mapping", "aglu/A_Fodderbio_chars",
                      "temp-data-inject/L142.ag_Fert_IO_R_C_Y_GLU", "temp-data-inject/L205.AgCost_ag", "temp-data-inject/L205.AgCost_bio") %>%
       # typical flags, but there are others--see `constants.R`
-      add_flags(FLAG_LONG_YEAR_FORM, FLAG_NO_XYEAR) ->
+      add_flags(FLAG_LONG_YEAR_FORM, FLAG_NO_XYEAR, FLAG_NO_TEST) ->
       L2062.AgCost_ag_irr_mgmt_adj
     tibble() %>%
       add_title("descriptive title of data") %>%
@@ -90,7 +108,7 @@ module_aglu_L2062.ag_Fert_irr_mgmt <- function(command, ...) {
       add_precursors("common/GCAM_region_names", "water/basin_to_country_mapping", "aglu/A_Fodderbio_chars",
                      "temp-data-inject/L142.ag_Fert_IO_R_C_Y_GLU", "temp-data-inject/L205.AgCost_ag", "temp-data-inject/L205.AgCost_bio") %>%
       # typical flags, but there are others--see `constants.R`
-      add_flags(FLAG_LONG_YEAR_FORM, FLAG_NO_XYEAR) ->
+      add_flags(FLAG_LONG_YEAR_FORM, FLAG_NO_XYEAR, FLAG_NO_TEST) ->
       L2062.AgCost_bio_irr_mgmt_adj
 
     return_data(L2062.AgCoef_Fert_ag_irr_mgmt, L2062.AgCoef_Fert_bio_irr_mgmt, L2062.AgCost_ag_irr_mgmt_adj, L2062.AgCost_bio_irr_mgmt_adj)
