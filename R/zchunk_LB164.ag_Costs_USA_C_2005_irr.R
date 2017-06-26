2e172e7e858ace37ce8d848c73df0f9897cbcfdc
abigailsnyder <abigail.snyder@pnnl.gov>
2017-06-26 16:29:03 -0400

    update to reflect one of the inputs is a temp-data-inject: LB161 aglu

--- R/zchunk_LB164.ag_Costs_USA_C_2005_irr.R
+++ R/zchunk_LB164.ag_Costs_USA_C_2005_irr.R
@@ -22,7 +22,7 @@ module_aglu_LB164.ag_Costs_USA_C_2005_irr <- function(command, ...) {
              FILE = "aglu/USDA_cost_data",
              "L100.LDS_ag_HA_ha",
              "L133.ag_Cost_75USDkg_C",
-             "L161.ag_irrHA_frac_R_C_GLU"))
+             FILE = "temp-data-inject/L161.ag_irrHA_frac_R_C_GLU"))
   } else if(command == driver.DECLARE_OUTPUTS) {
     return(c("L164.ag_Cost_75USDkg_C"))
   } else if(command == driver.MAKE) {
@@ -36,28 +36,9 @@ module_aglu_LB164.ag_Costs_USA_C_2005_irr <- function(command, ...) {
     USDA_cost_data <- get_data(all_data, "aglu/USDA_cost_data")
     L100.LDS_ag_HA_ha <- get_data(all_data, "L100.LDS_ag_HA_ha")
     L133.ag_Cost_75USDkg_C <- get_data(all_data, "L133.ag_Cost_75USDkg_C")
-    L161.ag_irrHA_frac_R_C_GLU <- get_data(all_data, "L161.ag_irrHA_frac_R_C_GLU")
+    L161.ag_irrHA_frac_R_C_GLU <- get_data(all_data, "temp-data-inject/L161.ag_irrHA_frac_R_C_GLU")
 
-    # ===================================================
-    # TRANSLATED PROCESSING CODE GOES HERE...
-    #
-    # If you find a mistake/thing to update in the old code and
-    # fixing it will change the output data, causing the tests to fail,
-    # (i) open an issue on GitHub, (ii) consult with colleagues, and
-    # then (iii) code a fix:
-    #
-    # if(OLD_DATA_SYSTEM_BEHAVIOR) {
-    #   ... code that replicates old, incorrect behavior
-    # } else {
-    #   ... new code with a fix
-    # }
-    #
-    #
-    # NOTE: there are 'match' calls in this code. You probably want to use left_join_error_no_match
-    # For more information, see https://github.com/JGCRI/gcamdata/wiki/Name-That-Function
-    # NOTE: This code uses vecpaste
-    # This function can be removed; see https://github.com/JGCRI/gcamdata/wiki/Name-That-Function
-    # ===================================================
+
 
     # Produce outputs
     tibble() %>%
@@ -72,7 +53,7 @@ module_aglu_LB164.ag_Costs_USA_C_2005_irr <- function(command, ...) {
                      "aglu/USDA_cost_data",
                      "L100.LDS_ag_HA_ha",
                      "L133.ag_Cost_75USDkg_C",
-                     "L161.ag_irrHA_frac_R_C_GLU") %>%
+                     "temp-data-inject/L161.ag_irrHA_frac_R_C_GLU") %>%
       add_flags(FLAG_LONG_YEAR_FORM, FLAG_NO_XYEAR) ->
       L164.ag_Cost_75USDkg_C
 
