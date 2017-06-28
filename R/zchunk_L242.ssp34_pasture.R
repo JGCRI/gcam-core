27793eda5176bc44b0aed4f5b19525733a7e697b
Ben Bond-Lamberty <bondlamberty@pnnl.gov>
2017-06-28 06:19:35 -0400

    Initial setup

---
+++ R/zchunk_L242.ssp34_pasture.R
@@ -0,0 +1,94 @@
+#' module_aglu_L242.ssp34_pasture
+#'
+#' Briefly describe what this chunk does.
+#'
+#' @param command API command to execute
+#' @param ... other optional parameters, depending on command
+#' @return Depends on \code{command}: either a vector of required inputs,
+#' a vector of output names, or (if \code{command} is "MAKE") all
+#' the generated outputs: \code{L242.LN2_HistUnmgdAllocation_SSP34}, \code{L242.LN2_UnmgdAllocation_SSP34}, \code{L242.LN2_HistMgdAllocation_SSP34}, \code{L242.LN2_MgdAllocation_SSP34}. The corresponding file in the
+#' original data system was \code{L242.ssp34_pasture.R} (aglu level2).
+#' @details Describe in detail what this chunk does.
+#' @importFrom assertthat assert_that
+#' @importFrom dplyr filter mutate select
+#' @importFrom tidyr gather spread
+#' @author YourInitials CurrentMonthName 2017
+#' @export
+module_aglu_L242.ssp34_pasture <- function(command, ...) {
+  if(command == driver.DECLARE_INPUTS) {
+    return(c(FILE = "common/GCAM_region_names",
+             FILE = "water/basin_to_country_mapping",
+             FILE = "aglu/GCAMLandLeaf_CdensityLT",
+             FILE = "aglu/A_LandLeaf_Unmgd2",
+             FILE = "aglu/A_LandLeaf2",
+             "L125.LC_bm2_R_LT_Yh_GLU",
+             "L102.pcgdp_thous90USD_Scen_R_Y"))
+  } else if(command == driver.DECLARE_OUTPUTS) {
+    return(c("L242.LN2_HistUnmgdAllocation_SSP34",
+             "L242.LN2_UnmgdAllocation_SSP34",
+             "L242.LN2_HistMgdAllocation_SSP34",
+             "L242.LN2_MgdAllocation_SSP34"))
+  } else if(command == driver.MAKE) {
+
+    all_data <- list(...)[[1]]
+
+    # Load required inputs
+    GCAM_region_names <- get_data(all_data, "common/GCAM_region_names")
+    basin_to_country_mapping <- get_data(all_data, "water/basin_to_country_mapping")
+    GCAMLandLeaf_CdensityLT <- get_data(all_data, "aglu/GCAMLandLeaf_CdensityLT")
+    A_LandLeaf_Unmgd2 <- get_data(all_data, "aglu/A_LandLeaf_Unmgd2")
+    A_LandLeaf2 <- get_data(all_data, "aglu/A_LandLeaf2")
+    L125.LC_bm2_R_LT_Yh_GLU <- get_data(all_data, "L125.LC_bm2_R_LT_Yh_GLU")
+    L102.pcgdp_thous90USD_Scen_R_Y <- get_data(all_data, "L102.pcgdp_thous90USD_Scen_R_Y")
+
+    # Produce outputs
+
+    tibble() %>%
+      add_title("descriptive title of data") %>%
+      add_units("units") %>%
+      add_comments("comments describing how data generated") %>%
+      add_comments("can be multiple lines") %>%
+      add_legacy_name("L242.LN2_HistUnmgdAllocation_SSP34") %>%
+      add_precursors("common/GCAM_region_names",
+                     "water/basin_to_country_mapping",
+                     "aglu/GCAMLandLeaf_CdensityLT",
+                     "aglu/A_LandLeaf_Unmgd2",
+                     "aglu/A_LandLeaf2",
+                     "L125.LC_bm2_R_LT_Yh_GLU",
+                     "L102.pcgdp_thous90USD_Scen_R_Y") %>%
+      # typical flags, but there are others--see `constants.R`
+      add_flags(FLAG_LONG_YEAR_FORM, FLAG_NO_XYEAR) ->
+      L242.LN2_HistUnmgdAllocation_SSP34
+
+    tibble() %>%
+      add_title("descriptive title of data") %>%
+      add_units("units") %>%
+      add_comments("comments describing how data generated") %>%
+      add_comments("can be multiple lines") %>%
+      add_legacy_name("L242.LN2_UnmgdAllocation_SSP34") %>%
+      same_precursors_as(L242.LN2_HistUnmgdAllocation_SSP34) ->
+      L242.LN2_UnmgdAllocation_SSP34
+
+    tibble() %>%
+      add_title("descriptive title of data") %>%
+      add_units("units") %>%
+      add_comments("comments describing how data generated") %>%
+      add_comments("can be multiple lines") %>%
+      add_legacy_name("L242.LN2_HistMgdAllocation_SSP34") %>%
+      same_precursors_as(L242.LN2_HistUnmgdAllocation_SSP34) ->
+      L242.LN2_HistMgdAllocation_SSP34
+
+    tibble() %>%
+      add_title("descriptive title of data") %>%
+      add_units("units") %>%
+      add_comments("comments describing how data generated") %>%
+      add_comments("can be multiple lines") %>%
+      add_legacy_name("L242.LN2_MgdAllocation_SSP34") %>%
+      same_precursors_as(L242.LN2_HistUnmgdAllocation_SSP34) ->
+      L242.LN2_MgdAllocation_SSP34
+
+    return_data(L242.LN2_HistUnmgdAllocation_SSP34, L242.LN2_UnmgdAllocation_SSP34, L242.LN2_HistMgdAllocation_SSP34, L242.LN2_MgdAllocation_SSP34)
+  } else {
+    stop("Unknown command")
+  }
+}
