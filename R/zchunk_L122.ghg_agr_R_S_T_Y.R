7a5091077dd4bcaacf747a400628e48b9feaa02a
russellhz <russell.horowitz@pnnl.gov>
2017-06-30 17:13:06 -0400

    Scaling

--- R/zchunk_L122.ghg_agr_R_S_T_Y.R
+++ R/zchunk_L122.ghg_agr_R_S_T_Y.R
@@ -35,33 +35,79 @@ module_emissions_L122.ghg_agr_R_S_T_Y <- function(command, ...) {
 
     # Load required inputs
     iso_GCAM_regID <- get_data(all_data, "common/iso_GCAM_regID")
-    EDGAR_nation <- get_data(all_data, "emissions/EDGAR/EDGAR_nation")
+    EDGAR_nation <- get_data(all_data, "emissions/EDGAR/EDGAR_nation") %>%
+      distinct()
     EDGAR_sector <- get_data(all_data, "emissions/EDGAR/EDGAR_sector")
     L103.ag_Prod_Mt_R_C_Y_GLU <- get_data(all_data, "L103.ag_Prod_Mt_R_C_Y_GLU")
     L122.LC_bm2_R_HarvCropLand_C_Yh_GLU <- get_data(all_data, "L122.LC_bm2_R_HarvCropLand_C_Yh_GLU")
     L142.ag_Fert_IO_R_C_Y_GLU <- get_data(all_data, "temp-data-inject/L142.ag_Fert_IO_R_C_Y_GLU")
-    EDGAR_CH4 <- get_data(all_data, "emissions/EDGAR/EDGAR_CH4")
-    EDGAR_N2O <- get_data(all_data, "emissions/EDGAR/EDGAR_N2O")
-    EDGAR_NH3 <- get_data(all_data, "emissions/EDGAR/EDGAR_NH3")
-    EDGAR_NOx <- get_data(all_data, "emissions/EDGAR/EDGAR_NOx")
+    EDGAR_CH4 <- get_data(all_data, "emissions/EDGAR/EDGAR_CH4") %>%
+      gather(year, value, matches(YEAR_PATTERN)) %>%
+      mutate(Non.CO2 = "CH4_AGR")
+    EDGAR_N2O <- get_data(all_data, "emissions/EDGAR/EDGAR_N2O") %>%
+      gather(year, value, matches(YEAR_PATTERN)) %>%
+      mutate(Non.CO2 = "N20_AGR")
+    EDGAR_NH3 <- get_data(all_data, "emissions/EDGAR/EDGAR_NH3") %>%
+      gather(year, value, matches(YEAR_PATTERN)) %>%
+      mutate(Non.CO2 = "NH3_AGR")
+    EDGAR_NOx <- get_data(all_data, "emissions/EDGAR/EDGAR_NOx") %>%
+      gather(year, value, matches(YEAR_PATTERN)) %>%
+      mutate(Non.CO2 = "NOx_AGR")
 
     # ===================================================
-    # TRANSLATED PROCESSING CODE GOES HERE...
-    #
-    # If you find a mistake/thing to update in the old code and
-    # fixing it will change the output data, causing the tests to fail,
-    # (i) open an issue on GitHub, (ii) consult with colleagues, and
-    # then (iii) code a fix:
-    #
-    # if(OLD_DATA_SYSTEM_BEHAVIOR) {
-    #   ... code that replicates old, incorrect behavior
-    # } else {
-    #   ... new code with a fix
-    # }
-    #
-    #
-    # NOTE: there are `merge` and/or 'match' calls in this code. Be careful!
-    # For more information, see https://github.com/JGCRI/gcamdata/wiki/Merge-and-Match
+    # Compute shares of regional cropland allocation by crop type, and regional production of each crop by each GLU
+    # In the downscaling from (geopolitical) region to crop and GLU, we use land area to go from region to region/crop, and
+    # production to go from region/crop to region/GLU/crop
+
+    # Land area shares (region/crop within region)
+    L122.CropAreaShare_R_C_Y <- L122.LC_bm2_R_HarvCropLand_C_Y_GLU %>%
+      filter(year %in% HISTORICAL_YEARS) %>%
+      group_by(year, GCAM_region_ID, GCAM_commodity) %>%
+      summarise(value = sum(value)) %>%
+      group_by(year, GCAM_region_ID) %>%
+      # Crop area by region and year
+      mutate(crop_area_total = sum(value)) %>%
+      ungroup() %>%
+      mutate(crop_area_share = value / crop_area_total)
+
+    # Production shares (region/GLU/crop within region/crop)
+    L122.ProdGLUshare_R_C_Y_GLU <- L103.ag_Prod_Mt_R_C_Y_GLU %>%
+      filter(year %in% HISTORICAL_YEARS) %>%
+      group_by(GCAM_region_ID, GCAM_commodity, year) %>%
+      # Production by crop, region, and year
+      mutate(Prod_R_C = sum(value)) %>%
+      ungroup() %>%
+      mutate(prod_share_GLU = value / Prod_R_C) %>%
+      replace_na(list(prod_share_GLU = 0))
+
+    # Emissions shares: product of region/crop shares and region/crop/glu shares
+    L122.EmissShare_R_C_Y_GLU <- L122.ProdGLUshare_R_C_Y_GLU %>%
+      left_join_error_no_match(L122.CropAreaShare_R_C_Y, by = c("GCAM_region_ID", "GCAM_commodity", "year")) %>%
+      transmute(GCAM_region_ID, GCAM_commodity, GLU, year, emiss_share = prod_share_GLU * crop_area_share)
+
+    L122.EmissShare_R_C_Y_GLU$emiss_share <- with( L122.EmissShare_R_C_Y_GLU, prod_share_GLU * crop_area_share )
+    L122.EmissShare_R_C_Y_GLU <- L122.EmissShare_R_C_Y_GLU[ c( R_C_Y_GLU, "emiss_share" ) ]
+
+    # Compute EDGAR emissions by region
+    L122.EDGAR <- bind_rows(EDGAR_CH4, EDGAR_N2O, EDGAR_NH3, EDGAR_NOx) %>%
+      # Peat fire and forest fire post burn decay not in EDGAR_sector
+      fast_left_join(EDGAR_sector %>% select(IPCC, sector = agg_sector), by = "IPCC") %>%
+      # ATA, SEA, and AIR not in EDGAR_nation
+      fast_left_join(EDGAR_nation, by = "ISO_A3") %>%
+      # umi, bvt, sgs, iot, atf, and hmd not in iso_GCAM_regID
+      fast_left_join(iso_GCAM_regID, by = "iso") %>%
+      group_by(sector, Non.CO2, iso, IPCC) %>%
+      filter(!any(is.na(value)))
+      select(GCAM_region_ID, sector, Non.CO2, year, value) %>%
+      group_by(GCAM_region_ID, sector, Non.CO2) %>%
+      filter(!any(is.na(value)))
+
+    #Drop unnecessary columns, aggregate by region, and melt
+    L122.EDGAR <- na.omit( L122.EDGAR )
+    L122.EDGAR <- aggregate( L122.EDGAR[ X_EDGAR_historical_years ],
+                             by = L122.EDGAR[ c( "GCAM_region_ID", "Non.CO2", "sector")], sum)
+    L122.EDGAR.melt <- melt( L122.EDGAR, id.vars = c( "GCAM_region_ID", "Non.CO2", "sector" ), variable.name = Y)
+
     # ===================================================
 
     # Produce outputs
