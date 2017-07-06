1be3223e431dbb9797cffcd5c556ec4e23d6b2d5
abigailsnyder <abigail.snyder@pnnl.gov>
2017-07-06 13:21:42 -0400

    remove disabled from namespace and update some inputs

--- R/zchunk_L2042.resbio_input_irr_mgmt.R
+++ R/zchunk_L2042.resbio_input_irr_mgmt.R
@@ -14,15 +14,17 @@
 #' @importFrom tidyr gather spread
 #' @author YourInitials CurrentMonthName 2017
 #' @export
-module_aglu_L2042.resbio_input_irr_mgmt_DISABLED <- function(command, ...) {
+module_aglu_L2042.resbio_input_irr_mgmt <- function(command, ...) {
   if(command == driver.DECLARE_INPUTS) {
     return(c(FILE = "common/GCAM_region_names",
-             "L2041.AgResBio_For",
-             "L2041.GlobalResBio_Mill",
-             "L2041.AgResBio_ag_irr",
-             "L2041.AgResBioCurve_For",
-             "L2041.StubResBioCurve_Mill",
-             "L2041.AgResBioCurve_ag_irr"))
+             FILE = "water/basin_to_country_mapping",
+             FILE = "aglu/A_agSupplySector",
+             FILE = "aglu/A_demand_technology",
+             FILE = "aglu/A_resbio_curves",
+             FILE = "aglu/A_bio_frac_prod_R",
+             "L111.ag_resbio_R_C",
+             "L103.ag_Prod_Mt_R_C_Y_GLU",
+             FILE = "L123.For_Prod_bm3_R_Y_GLU"))
   } else if(command == driver.DECLARE_OUTPUTS) {
     return(c("L2042.AgResBio_For",
              "L2042.AgResBioCurve_For",
@@ -36,44 +38,38 @@ module_aglu_L2042.resbio_input_irr_mgmt_DISABLED <- function(command, ...) {
 
     # Load required inputs
     GCAM_region_names <- get_data(all_data, "common/GCAM_region_names")
-    L2041.AgResBio_For <- get_data(all_data, "L2041.AgResBio_For")
-    L2041.GlobalResBio_Mill <- get_data(all_data, "L2041.GlobalResBio_Mill")
-    L2041.AgResBio_ag_irr <- get_data(all_data, "L2041.AgResBio_ag_irr")
-    L2041.AgResBioCurve_For <- get_data(all_data, "L2041.AgResBioCurve_For")
-    L2041.StubResBioCurve_Mill <- get_data(all_data, "L2041.StubResBioCurve_Mill")
-    L2041.AgResBioCurve_ag_irr <- get_data(all_data, "L2041.AgResBioCurve_ag_irr")
+    basin_to_country_mapping <- get_data(all_data, "water/basin_to_country_mapping")
+    A_agSupplySector <- get_data(all_data, "aglu/A_agSupplySector")
+    A_demand_technology <- get_data(all_data, "aglu/A_demand_technology")
+    A_resbio_curves <- get_data(all_data, "aglu/A_resbio_curves")
+    A_bio_frac_prod_R <- get_data(all_data, "aglu/A_bio_frac_prod_R")
+    L111.ag_resbio_R_C <- get_data(all_data, "L111.ag_resbio_R_C")
+    L103.ag_Prod_Mt_R_C_Y_GLU <- get_data(all_data, "L103.ag_Prod_Mt_R_C_Y_GLU")
+    L123.For_Prod_bm3_R_Y_GLU <- get_data(all_data, "L123.For_Prod_bm3_R_Y_GLU")
 
-    # ===================================================
-    # TRANSLATED PROCESSING CODE GOES HERE...
-    #
-    # If you find a mistake/thing to update in the old code and
-    # fixing it will change the output data, causing the tests to fail,
-    # (i) open an issue on GitHub, (ii) consult with colleagues, and
-    # then (iii) code a fix:
-    #
-    # if(OLD_DATA_SYSTEM_BEHAVIOR) {
-    #   ... code that replicates old, incorrect behavior
-    # } else {
-    #   ... new code with a fix
-    # }
-    #
-    #
-    # NOTE: This code uses repeat_and_add_vector
-    # This function can be removed; see https://github.com/JGCRI/gcamdata/wiki/Name-That-Function
-    # ===================================================
+
+    # L2041.AgResBio_For <- get_data(all_data, "L2041.AgResBio_For")
+    # L2041.GlobalResBio_Mill <- get_data(all_data, "L2041.GlobalResBio_Mill")
+    # L2041.AgResBio_ag_irr <- get_data(all_data, "L2041.AgResBio_ag_irr")
+    # L2041.AgResBioCurve_For <- get_data(all_data, "L2041.AgResBioCurve_For")
+    # L2041.StubResBioCurve_Mill <- get_data(all_data, "L2041.StubResBioCurve_Mill")
+    # L2041.AgResBioCurve_ag_irr <- get_data(all_data, "L2041.AgResBioCurve_ag_irr")
+
 
     # Produce outputs
-    # Temporary code below sends back empty data frames marked "don't test"
-    # Note that all precursor names (in `add_precursor`) must be in this chunk's inputs
-    # There's also a `same_precursors_as(x)` you can use
-    # If no precursors (very rare) don't call `add_precursor` at all
     tibble() %>%
       add_title("descriptive title of data") %>%
       add_units("units") %>%
       add_comments("comments describing how data generated") %>%
       add_comments("can be multiple lines") %>%
       add_legacy_name("L2042.AgResBio_For") %>%
-      add_precursors("precursor1", "precursor2", "etc") %>%
+      add_precursors("common/GCAM_region_names",
+                     "L2041.AgResBio_For",
+                     "L2041.GlobalResBio_Mill",
+                     "L2041.AgResBio_ag_irr",
+                     "L2041.AgResBioCurve_For",
+                     "L2041.StubResBioCurve_Mill",
+                     "L2041.AgResBioCurve_ag_irr") %>%
       # typical flags, but there are others--see `constants.R`
       add_flags(FLAG_LONG_YEAR_FORM, FLAG_NO_XYEAR) ->
       L2042.AgResBio_For
@@ -83,7 +79,13 @@ module_aglu_L2042.resbio_input_irr_mgmt_DISABLED <- function(command, ...) {
       add_comments("comments describing how data generated") %>%
       add_comments("can be multiple lines") %>%
       add_legacy_name("L2042.AgResBioCurve_For") %>%
-      add_precursors("precursor1", "precursor2", "etc") %>%
+      add_precursors("common/GCAM_region_names",
+                     "L2041.AgResBio_For",
+                     "L2041.GlobalResBio_Mill",
+                     "L2041.AgResBio_ag_irr",
+                     "L2041.AgResBioCurve_For",
+                     "L2041.StubResBioCurve_Mill",
+                     "L2041.AgResBioCurve_ag_irr") %>%
       # typical flags, but there are others--see `constants.R`
       add_flags(FLAG_LONG_YEAR_FORM, FLAG_NO_XYEAR) ->
       L2042.AgResBioCurve_For
@@ -93,7 +95,13 @@ module_aglu_L2042.resbio_input_irr_mgmt_DISABLED <- function(command, ...) {
       add_comments("comments describing how data generated") %>%
       add_comments("can be multiple lines") %>%
       add_legacy_name("L2042.GlobalResBio_Mill") %>%
-      add_precursors("precursor1", "precursor2", "etc") %>%
+      add_precursors("common/GCAM_region_names",
+                     "L2041.AgResBio_For",
+                     "L2041.GlobalResBio_Mill",
+                     "L2041.AgResBio_ag_irr",
+                     "L2041.AgResBioCurve_For",
+                     "L2041.StubResBioCurve_Mill",
+                     "L2041.AgResBioCurve_ag_irr") %>%
       # typical flags, but there are others--see `constants.R`
       add_flags(FLAG_LONG_YEAR_FORM, FLAG_NO_XYEAR) ->
       L2042.GlobalResBio_Mill
@@ -103,7 +111,13 @@ module_aglu_L2042.resbio_input_irr_mgmt_DISABLED <- function(command, ...) {
       add_comments("comments describing how data generated") %>%
       add_comments("can be multiple lines") %>%
       add_legacy_name("L2042.StubResBioCurve_Mill") %>%
-      add_precursors("precursor1", "precursor2", "etc") %>%
+      add_precursors("common/GCAM_region_names",
+                     "L2041.AgResBio_For",
+                     "L2041.GlobalResBio_Mill",
+                     "L2041.AgResBio_ag_irr",
+                     "L2041.AgResBioCurve_For",
+                     "L2041.StubResBioCurve_Mill",
+                     "L2041.AgResBioCurve_ag_irr") %>%
       # typical flags, but there are others--see `constants.R`
       add_flags(FLAG_LONG_YEAR_FORM, FLAG_NO_XYEAR) ->
       L2042.StubResBioCurve_Mill
@@ -113,7 +127,13 @@ module_aglu_L2042.resbio_input_irr_mgmt_DISABLED <- function(command, ...) {
       add_comments("comments describing how data generated") %>%
       add_comments("can be multiple lines") %>%
       add_legacy_name("L2042.AgResBio_ag_irr_mgmt") %>%
-      add_precursors("precursor1", "precursor2", "etc") %>%
+      add_precursors("common/GCAM_region_names",
+                     "L2041.AgResBio_For",
+                     "L2041.GlobalResBio_Mill",
+                     "L2041.AgResBio_ag_irr",
+                     "L2041.AgResBioCurve_For",
+                     "L2041.StubResBioCurve_Mill",
+                     "L2041.AgResBioCurve_ag_irr") %>%
       # typical flags, but there are others--see `constants.R`
       add_flags(FLAG_LONG_YEAR_FORM, FLAG_NO_XYEAR) ->
       L2042.AgResBio_ag_irr_mgmt
@@ -123,7 +143,13 @@ module_aglu_L2042.resbio_input_irr_mgmt_DISABLED <- function(command, ...) {
       add_comments("comments describing how data generated") %>%
       add_comments("can be multiple lines") %>%
       add_legacy_name("L2042.AgResBioCurve_ag_irr_mgmt") %>%
-      add_precursors("precursor1", "precursor2", "etc") %>%
+      add_precursors("common/GCAM_region_names",
+                     "L2041.AgResBio_For",
+                     "L2041.GlobalResBio_Mill",
+                     "L2041.AgResBio_ag_irr",
+                     "L2041.AgResBioCurve_For",
+                     "L2041.StubResBioCurve_Mill",
+                     "L2041.AgResBioCurve_ag_irr") %>%
       # typical flags, but there are others--see `constants.R`
       add_flags(FLAG_LONG_YEAR_FORM, FLAG_NO_XYEAR) ->
       L2042.AgResBioCurve_ag_irr_mgmt
