58343863b4809f4d70ed3f204def55529bc27799
russellhz <russell.horowitz@pnnl.gov>
2017-07-06 21:20:03 -0400

    Ready for translation

--- R/zchunk_L232.prc_nonco2.R
+++ R/zchunk_L232.prc_nonco2.R
@@ -12,15 +12,15 @@
 #' @importFrom assertthat assert_that
 #' @importFrom dplyr filter mutate select
 #' @importFrom tidyr gather spread
-#' @author YourInitials CurrentMonthName 2017
+#' @author RH July 2017
 #' @export
-module_emissions_L232.prc_nonco2_DISABLED <- function(command, ...) {
+module_emissions_L232.prc_nonco2 <- function(command, ...) {
   if(command == driver.DECLARE_INPUTS) {
     return(c(FILE = "common/GCAM_region_names",
              FILE = "emissions/A_regions",
              "L131.nonco2_tg_R_prc_S_S_Yh",
-             FILE = "emissions/A32.max_reduction",
-             FILE = "emissions/A32.steepness"))
+             FILE = "emissions/A32.max_reduction", # Source and Units
+             FILE = "emissions/A32.steepness")) # Source and Units
   } else if(command == driver.DECLARE_OUTPUTS) {
     return(c("L232.nonco2_prc",
              "L232.nonco2_max_reduction",
@@ -37,41 +37,18 @@ module_emissions_L232.prc_nonco2_DISABLED <- function(command, ...) {
     A32.steepness <- get_data(all_data, "emissions/A32.steepness")
 
     # ===================================================
-    # TRANSLATED PROCESSING CODE GOES HERE...
-    #
-    # If you find a mistake/thing to update in the old code and
-    # fixing it will change the output data, causing the tests to fail,
-    # (i) open an issue on GitHub, (ii) consult with colleagues, and
-    # then (iii) code a fix:
-    #
-    # if(OLD_DATA_SYSTEM_BEHAVIOR) {
-    #   ... code that replicates old, incorrect behavior
-    # } else {
-    #   ... new code with a fix
-    # }
-    #
-    #
-    # NOTE: there are 'match' calls in this code. You probably want to use left_join_error_no_match
-    # For more information, see https://github.com/JGCRI/gcamdata/wiki/Name-That-Function
-    # NOTE: This code uses vecpaste
-    # This function can be removed; see https://github.com/JGCRI/gcamdata/wiki/Name-That-Function
-    # NOTE: This code uses repeat_and_add_vector
-    # This function can be removed; see https://github.com/JGCRI/gcamdata/wiki/Name-That-Function
+
     # ===================================================
 
     # Produce outputs
-    # Temporary code below sends back empty data frames marked "don't test"
-    # Note that all precursor names (in `add_precursor`) must be in this chunk's inputs
-    # There's also a `same_precursors_as(x)` you can use
-    # If no precursors (very rare) don't call `add_precursor` at all
     tibble() %>%
       add_title("descriptive title of data") %>%
       add_units("units") %>%
       add_comments("comments describing how data generated") %>%
       add_comments("can be multiple lines") %>%
       add_legacy_name("L232.nonco2_prc") %>%
-      add_precursors("precursor1", "precursor2", "etc") %>%
-      # typical flags, but there are others--see `constants.R`
+      add_precursors("common/GCAM_region_names", "emissions/A_regions", "L131.nonco2_tg_R_prc_S_S_Yh",
+                     "emissions/A32.max_reduction", "emissions/A32.steepness") %>%
       add_flags(FLAG_LONG_YEAR_FORM, FLAG_NO_XYEAR) ->
       L232.nonco2_prc
     tibble() %>%
@@ -80,8 +57,8 @@ module_emissions_L232.prc_nonco2_DISABLED <- function(command, ...) {
       add_comments("comments describing how data generated") %>%
       add_comments("can be multiple lines") %>%
       add_legacy_name("L232.nonco2_max_reduction") %>%
-      add_precursors("precursor1", "precursor2", "etc") %>%
-      # typical flags, but there are others--see `constants.R`
+      add_precursors("common/GCAM_region_names", "emissions/A_regions", "L131.nonco2_tg_R_prc_S_S_Yh",
+                     "emissions/A32.max_reduction", "emissions/A32.steepness") %>%
       add_flags(FLAG_LONG_YEAR_FORM, FLAG_NO_XYEAR) ->
       L232.nonco2_max_reduction
     tibble() %>%
@@ -90,8 +67,8 @@ module_emissions_L232.prc_nonco2_DISABLED <- function(command, ...) {
       add_comments("comments describing how data generated") %>%
       add_comments("can be multiple lines") %>%
       add_legacy_name("L232.nonco2_steepness") %>%
-      add_precursors("precursor1", "precursor2", "etc") %>%
-      # typical flags, but there are others--see `constants.R`
+      add_precursors("common/GCAM_region_names", "emissions/A_regions", "L131.nonco2_tg_R_prc_S_S_Yh",
+                     "emissions/A32.max_reduction", "emissions/A32.steepness") %>%
       add_flags(FLAG_LONG_YEAR_FORM, FLAG_NO_XYEAR) ->
       L232.nonco2_steepness
 
