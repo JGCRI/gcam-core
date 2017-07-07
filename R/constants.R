d40d9b988db207d1ec2d7b0717e7d18d2d8801ab
Ben Bond-Lamberty <bondlamberty@pnnl.gov>
2017-07-07 08:38:03 -0400

    Update constants.R

--- R/constants.R
+++ R/constants.R
@@ -10,15 +10,15 @@ YEAR_PATTERN <- "^(1|2)[0-9]{3}$"   # a 1 or 2 followed by three digits, and not
 
 # Flags ======================================================================
 # Flags used by chunks
-FLAG_INPUT_DATA      <- "FLAG_INPUT_DATA"
-FLAG_LONG_YEAR_FORM  <- "FLAG_LONG_YEAR_FORM"
-FLAG_NO_OUTPUT       <- "FLAG_NO_OUTPUT"
-FLAG_NO_XYEAR        <- "FLAG_NO_XYEAR"
-FLAG_NO_TEST         <- "FLAG_NO_TEST"
-FLAG_SUM_TEST        <- "FLAG_SUM_TEST"
-FLAG_PROTECT_FLOAT   <- "FLAG_PROTECT_FLOAT"
-FLAG_XML             <- "FLAG_XML"
-FLAG_YEAR_COL_XYEARS <- "FLAG_YEAR_COL_XYEARS"
+FLAG_INPUT_DATA      <- "FLAG_INPUT_DATA"       # input data, don't output
+FLAG_LONG_YEAR_FORM  <- "FLAG_LONG_YEAR_FORM"   # 'year' column but original data are wide
+FLAG_NO_OUTPUT       <- "FLAG_NO_OUTPUT"        # don't output
+FLAG_NO_XYEAR        <- "FLAG_NO_XYEAR"         # year names don't have X's in front
+FLAG_NO_TEST         <- "FLAG_NO_TEST"          # don't test
+FLAG_SUM_TEST        <- "FLAG_SUM_TEST"         # use less-restrictive sum test
+FLAG_PROTECT_FLOAT   <- "FLAG_PROTECT_FLOAT"    # protect float columns from readr bug
+FLAG_XML             <- "FLAG_XML"              # xml data
+FLAG_YEAR_COL_XYEARS <- "FLAG_YEAR_COL_XYEARS"  # 'year' column without X's in front
 
 
 # Time constants======================================================================
