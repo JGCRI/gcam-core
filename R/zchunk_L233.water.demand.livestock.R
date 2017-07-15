7ef9845a79f2b18d5c02829dce1202cdf0e0f41d
yalingliupnnl <yaling.liu@pnnl.gov>
2017-07-15 13:47:16 -0400

    Yaling - L233.water.demand.livestock

--- R/zchunk_L233.water.demand.livestock.R
+++ R/zchunk_L233.water.demand.livestock.R
@@ -1,6 +1,6 @@
 #' module_water_L233.water.demand.livestock
 #'
-#' Briefly describe what this chunk does.
+#' This chunk is to generate water coefficients for region-specific livestock for model years.
 #'
 #' @param command API command to execute
 #' @param ... other optional parameters, depending on command
@@ -8,13 +8,14 @@
 #' a vector of output names, or (if \code{command} is "MAKE") all
 #' the generated outputs: \code{L233.TechCoef}. The corresponding file in the
 #' original data system was \code{L233.water.demand.livestock.R} (water level2).
-#' @details Describe in detail what this chunk does.
+#' @details This chunk is to generate water coefficients for region-specific livestock for
+#' model years (1975, 1990,2005, 2010....2100), with the information of supplysector, subsector,
+#' technology, and energy input.
 #' @importFrom assertthat assert_that
 #' @importFrom dplyr filter mutate select
 #' @importFrom tidyr gather spread
-#' @author YourInitials CurrentMonthName 2017
-#' @export
-module_water_L233.water.demand.livestock_DISABLED <- function(command, ...) {
+#' @author YL July 2017
+module_water_L233.water.demand.livestock <- function(command, ...) {
   if(command == driver.DECLARE_INPUTS) {
     return(c(FILE = "common/GCAM_region_names",
              FILE = "water/A03.sector",
@@ -34,37 +35,34 @@ module_water_L233.water.demand.livestock_DISABLED <- function(command, ...) {
 
     # ===================================================
     # TRANSLATED PROCESSING CODE GOES HERE...
-    #
-    # If you find a mistake/thing to update in the old code and
-    # fixing it will change the output data, causing the tests to fail,
-    # (i) open an issue on GitHub, (ii) consult with colleagues, and
-    # then (iii) code a fix:
-    #
-    # if(OLD_DATA_SYSTEM_BEHAVIOR) {
-    #   ... code that replicates old, incorrect behavior
-    # } else {
-    #   ... new code with a fix
-    # }
-    #
-    #
-    # NOTE: there are `merge` calls in this code. Be careful!
-    # For more information, see https://github.com/JGCRI/gcamdata/wiki/Name-That-Function
-    # NOTE: there are 'match' calls in this code. You probably want to use left_join_error_no_match
-    # For more information, see https://github.com/JGCRI/gcamdata/wiki/Name-That-Function
+    #Just read in water coefficients for all years
+    L133.water_demand_livestock_R_C_W_km3_Mt %>%
+      rename(supplysector = GCAM_commodity) %>%
+      inner_join(select(A_an_technology, supplysector, subsector, technology), by = "supplysector") %>%
+      mutate(water_sector = "Livestock") %>%
+      mutate(minicam.energy.input = set_water_input_name(water_sector, water_type, A03.sector)) %>%
+      left_join(GCAM_region_names, by = "GCAM_region_ID") %>%
+      mutate(market.name = region) -> L233.TechCoef_tmp
+      # Set the coef for all years
+    L233.TechCoef_tmp[rep(1:nrow(L233.TechCoef_tmp), times = length(MODEL_YEARS)), ] %>%
+      mutate(year = MODEL_YEARS[sort(rep(1:length(MODEL_YEARS), times = nrow(L233.TechCoef_tmp)))]) %>%
+      select(-GCAM_region_ID, -water_type, -water_sector) -> results
+
     # ===================================================
 
     # Produce outputs
-    # Temporary code below sends back empty data frames marked "don't test"
-    # Note that all precursor names (in `add_precursor`) must be in this chunk's inputs
-    # There's also a `same_precursors_as(x)` you can use
-    # If no precursors (very rare) don't call `add_precursor` at all
-    tibble() %>%
-      add_title("descriptive title of data") %>%
-      add_units("units") %>%
-      add_comments("comments describing how data generated") %>%
+    results %>%
+      add_title("water coefficient for region-specific livestock") %>%
+      add_units("NA") %>%
+      add_comments("The data is generated through:
+                   1) read in the file L133.water_demand_livestock_R_C_W_km3_Mt and left join with the file A_an_technology;
+                   2) generate new fields water_sector and minicam.energy.input:
+                   3) left join with GCAM_region_names;
+                   4) replicate the water coefficients for all MODEL_YEARS") %>%
       add_comments("can be multiple lines") %>%
       add_legacy_name("L233.TechCoef") %>%
-      add_precursors("precursor1", "precursor2", "etc") %>%
+      add_precursors("common/GCAM_region_names", "water/A03.sector", "aglu/A_an_technology",
+                     "temp-data-inject/L133.water_demand_livestock_R_C_W_km3_Mt") %>%
       # typical flags, but there are others--see `constants.R`
       add_flags(FLAG_LONG_YEAR_FORM, FLAG_NO_XYEAR) ->
       L233.TechCoef
