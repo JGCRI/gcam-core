53862b039a1b48c013b39691cdaa2c554375a572
russellhz <russell.horowitz@pnnl.gov>
2017-07-19 19:28:23 -0400

    Removed L2111 Animal emissions

--- R/zchunk_L2111.ag_nonco2_IRR.R
+++ R/zchunk_L2111.ag_nonco2_IRR.R
@@ -7,7 +7,7 @@
 #' @param ... other optional parameters, depending on command
 #' @return Depends on \code{command}: either a vector of required inputs,
 #' a vector of output names, or (if \code{command} is "MAKE") all
-#' the generated outputs: \code{L2111.AWBEmissions}, \code{L2111.AGREmissions}, \code{L2111.AnEmissions}, \code{L2111.AnNH3Emissions}, \code{L2111.AGRBio}, \code{L2111.AWB_BCOC_EmissCoeff}, \code{L2111.nonghg_max_reduction}, \code{L2111.nonghg_steepness}. The corresponding file in the
+#' the generated outputs: \code{L2111.AWBEmissions}, \code{L2111.AGREmissions}, \code{L2111.AGRBio}, \code{L2111.AWB_BCOC_EmissCoeff}, \code{L2111.nonghg_max_reduction}, \code{L2111.nonghg_steepness}. The corresponding file in the
 #' original data system was \code{L2111.ag_nonco2_IRR.R} (emissions level2).
 #' @details Writes out agricultural, agricultural waste burning, and animal non-CO2 emissions. Writes biomass N2O coefficients,
 #' ag waste burning BC/OC coefficients, non-GHG maximum coefficient reduction, and non-GHG steepness by agricultural technology.
@@ -15,7 +15,6 @@
 #' @importFrom dplyr filter mutate select
 #' @importFrom tidyr gather spread
 #' @author RLH July 2017
-#' @export
 module_emissions_L2111.ag_nonco2_IRR <- function(command, ...) {
   if(command == driver.DECLARE_INPUTS) {
     return(c(FILE = "common/GCAM_region_names",
@@ -23,8 +22,6 @@ module_emissions_L2111.ag_nonco2_IRR <- function(command, ...) {
              FILE = "emissions/A_regions",
              "L1211.nonco2_tg_R_awb_C_Y_GLU_IRR",
              FILE = "temp-data-inject/L1221.ghg_tg_R_agr_C_Y_GLU_IRR",
-             FILE = "temp-data-inject/L211.AnEmissions",
-             FILE = "temp-data-inject/L211.AnNH3Emissions",
              FILE = "temp-data-inject/L211.AGRBio",
              FILE = "temp-data-inject/L211.AWB_BCOC_EmissCoeff",
              FILE = "temp-data-inject/L211.nonghg_max_reduction",
@@ -32,8 +29,6 @@ module_emissions_L2111.ag_nonco2_IRR <- function(command, ...) {
   } else if(command == driver.DECLARE_OUTPUTS) {
     return(c("L2111.AWBEmissions",
              "L2111.AGREmissions",
-             "L2111.AnEmissions",
-             "L2111.AnNH3Emissions",
              "L2111.AGRBio",
              "L2111.AWB_BCOC_EmissCoeff",
              "L2111.nonghg_max_reduction",
@@ -43,7 +38,7 @@ module_emissions_L2111.ag_nonco2_IRR <- function(command, ...) {
     # Silence package checks
     SO2_name <- Non.CO2 <- AgProductionTechnology <- AgSupplySector <-
       AgSupplySubsector <- GCAM_commodity <- GLU <- GLU_code <- Non.CO2 <-
-      input.emissions <- region <- year <- NULL
+      input.emissions <- region <- year <- GLU_name <- value <- NULL
 
     all_data <- list(...)[[1]]
 
@@ -66,8 +61,6 @@ module_emissions_L2111.ag_nonco2_IRR <- function(command, ...) {
                                by = "GLU") %>%
       select(-GLU) %>%
       rename(GLU = GLU_name)
-    L211.AnEmissions <- get_data(all_data, "temp-data-inject/L211.AnEmissions")
-    L211.AnNH3Emissions <- get_data(all_data, "temp-data-inject/L211.AnNH3Emissions")
     L211.AGRBio <- get_data(all_data, "temp-data-inject/L211.AGRBio")
     L211.AWB_BCOC_EmissCoeff <- get_data(all_data, "temp-data-inject/L211.AWB_BCOC_EmissCoeff")
     L211.nonghg_max_reduction <- get_data(all_data, "temp-data-inject/L211.nonghg_max_reduction")
@@ -104,12 +97,6 @@ module_emissions_L2111.ag_nonco2_IRR <- function(command, ...) {
       select(region, AgSupplySector, AgSupplySubsector, AgProductionTechnology, year,
              Non.CO2, input.emissions)
 
-    # L2111.AnEmissions and L2111.AnNH3Emissions: Copy tables exactly (irr/rfd irrelevant)
-    L2111.AnEmissions <- L211.AnEmissions %>%
-      mutate(year = year)
-    L2111.AnNH3Emissions <- L211.AnNH3Emissions %>%
-      mutate(year = year)
-
     # L2111.AGRBio, L2111.AWB_BCOC_EmissCoeff, L2111.nonghg_max_reduction, L2111.nonghg_steepness: repeat by irr/rfd and copy
     Irr_Rfd <- tibble(Irr_Rfd = c("IRR", "RFD"))
     L2111.AGRBio <- repeat_add_columns(L211.AGRBio, Irr_Rfd) %>%
@@ -145,20 +132,6 @@ module_emissions_L2111.ag_nonco2_IRR <- function(command, ...) {
         add_precursors("temp-data-inject/L1221.ghg_tg_R_agr_C_Y_GLU_IRR", "water/basin_to_country_mapping",
                        "common/GCAM_region_names") ->
         L2111.AGREmissions
-      L2111.AnEmissions %>%
-        add_title("Animal Emissions") %>%
-        add_units("Tg") %>%
-        add_comments("Copy of L2111.AnEmissions") %>%
-        add_legacy_name("L2111.AnEmissions") %>%
-        add_precursors("temp-data-inject/L211.AnEmissions") ->
-        L2111.AnEmissions
-      L2111.AnNH3Emissions %>%
-        add_title("Animal NH3 Emissions") %>%
-        add_units("Tg") %>%
-        add_comments("Copy of L211.AnNH3Emissions") %>%
-        add_legacy_name("L2111.AnNH3Emissions") %>%
-        add_precursors("temp-data-inject/L211.AnNH3Emissions") ->
-        L2111.AnNH3Emissions
       L2111.AGRBio %>%
         add_title("Bio N2O Coefficients by region and technology") %>%
         add_units("Unitless") %>% # should this be labeled in emissions/A_regions
@@ -188,7 +161,7 @@ module_emissions_L2111.ag_nonco2_IRR <- function(command, ...) {
         add_precursors("temp-data-inject/L211.nonghg_steepness") ->
         L2111.nonghg_steepness
 
-    return_data(L2111.AWBEmissions, L2111.AGREmissions, L2111.AnEmissions, L2111.AnNH3Emissions, L2111.AGRBio, L2111.AWB_BCOC_EmissCoeff, L2111.nonghg_max_reduction, L2111.nonghg_steepness)
+    return_data(L2111.AWBEmissions, L2111.AGREmissions, L2111.AGRBio, L2111.AWB_BCOC_EmissCoeff, L2111.nonghg_max_reduction, L2111.nonghg_steepness)
   } else {
     stop("Unknown command")
   }
