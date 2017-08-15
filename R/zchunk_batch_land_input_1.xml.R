8495c89302ebef47769dc8b6e8c7cbbc5033dc56
abigailsnyder <abigail.snyder@pnnl.gov>
2017-08-15 08:55:00 -0400

    Actually doing the batch xml files for land_input_1

--- R/zchunk_batch_land_input_1.xml.R
+++ R/zchunk_batch_land_input_1.xml.R
@@ -8,9 +8,10 @@
 #' a vector of output names, or (if \code{command} is "MAKE") all
 #' the generated outputs: \code{land_input_1.xml}. The corresponding file in the
 #' original data system was \code{batch_land_input_1.xml.R} (aglu XML).
-module_aglu_batch_land_input_1.xml_DISABLED <- function(command, ...) {
+module_aglu_batch_land_input_1.xml <- function(command, ...) {
   if(command == driver.DECLARE_INPUTS) {
-    return(c( "L221.LN0_Land",
+    return(c( "L221.LN0_Logit",
+              "L221.LN0_Land",
               "L221.LN0_SoilTimeScale",
               "L221.LN1_ValueLogit",
               "L221.LN1_HistUnmgdAllocation",
@@ -23,6 +24,7 @@ module_aglu_batch_land_input_1.xml_DISABLED <- function(command, ...) {
     all_data <- list(...)[[1]]
 
     # Load required inputs
+    L221.LN0_Logit <- get_data(all_data, "L221.LN0_Logit")
     L221.LN0_Land <- get_data(all_data, "L221.LN0_Land")
     L221.LN0_SoilTimeScale <- get_data(all_data, "L221.LN0_SoilTimeScale")
     L221.LN1_ValueLogit <- get_data(all_data, "L221.LN1_ValueLogit")
@@ -34,13 +36,14 @@ module_aglu_batch_land_input_1.xml_DISABLED <- function(command, ...) {
 
     # Produce outputs
     create_xml("land_input_1.xml") %>%
+      add_logit_tables_xml(L221.LN0_Logit, "LN0_Logit") %>%
       add_xml_data(L221.LN0_Land, "LN0_Land") %>%
       add_xml_data(L221.LN0_SoilTimeScale, "LN0_SoilTimeScale") %>%
-      add_xml_data(L221.LN1_ValueLogit, "LN1_ValueLogit") %>%
+      add_logit_tables_xml(L221.LN1_ValueLogit, "LN1_ValueLogit", "LN1_Logit") %>%
       add_xml_data(L221.LN1_HistUnmgdAllocation, "LN1_HistUnmgdAllocation") %>%
       add_xml_data(L221.LN1_UnmgdAllocation, "LN1_UnmgdAllocation") %>%
       add_xml_data(L221.LN1_UnmgdCarbon, "LN1_UnmgdCarbon") %>%
-      add_precursors("L221.LN0_Land", "L221.LN0_SoilTimeScale", "L221.LN1_ValueLogit", "L221.LN1_HistUnmgdAllocation", "L221.LN1_UnmgdAllocation", "L221.LN1_UnmgdCarbon") ->
+      add_precursors("L221.LN0_Logit", "L221.LN0_Land", "L221.LN0_SoilTimeScale", "L221.LN1_ValueLogit", "L221.LN1_HistUnmgdAllocation", "L221.LN1_UnmgdAllocation", "L221.LN1_UnmgdCarbon") ->
       land_input_1.xml
 
     return_data(land_input_1.xml)
