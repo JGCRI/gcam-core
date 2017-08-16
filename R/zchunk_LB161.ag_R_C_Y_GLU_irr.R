a7cf42b9dad8d3db0cc3ff31abf8c5f967cd42b8
rynacui <yiyun.cui@pnnl.gov>
2017-08-16 15:10:46 -0400

    translate code and pass tests

--- R/zchunk_LB161.ag_R_C_Y_GLU_irr.R
+++ R/zchunk_LB161.ag_R_C_Y_GLU_irr.R
@@ -14,14 +14,14 @@
 #' @importFrom tidyr gather spread
 #' @author YourInitials CurrentMonthName 2017
 #' @export
-module_aglu_LB161.ag_R_C_Y_GLU_irr_DISABLED <- function(command, ...) {
+module_aglu_LB161.ag_R_C_Y_GLU_irr <- function(command, ...) {
   if(command == driver.DECLARE_INPUTS) {
-    return(c( "L103.ag_Prod_Mt_R_C_Y_GLU",
-              "L103.ag_HA_bm2_R_C_Y_GLU",
-              "L152.ag_irrProd_Mt_R_C_GLU",
-              "L152.ag_irrHA_bm2_R_C_GLU",
-              "L152.ag_rfdProd_Mt_R_C_GLU",
-              "L152.ag_rfdHA_bm2_R_C_GLU"))
+    return(c("L103.ag_Prod_Mt_R_C_Y_GLU",
+             "L103.ag_HA_bm2_R_C_Y_GLU",
+             "L152.ag_irrProd_Mt_R_C_GLU",
+             "L152.ag_irrHA_bm2_R_C_GLU",
+             "L152.ag_rfdProd_Mt_R_C_GLU",
+             "L152.ag_rfdHA_bm2_R_C_GLU"))
   } else if(command == driver.DECLARE_OUTPUTS) {
     return(c("L161.ag_irrProd_Mt_R_C_Y_GLU",
              "L161.ag_rfdProd_Mt_R_C_Y_GLU",
@@ -42,111 +42,123 @@ module_aglu_LB161.ag_R_C_Y_GLU_irr_DISABLED <- function(command, ...) {
     L152.ag_rfdProd_Mt_R_C_GLU <- get_data(all_data, "L152.ag_rfdProd_Mt_R_C_GLU")
     L152.ag_rfdHA_bm2_R_C_GLU <- get_data(all_data, "L152.ag_rfdHA_bm2_R_C_GLU")
 
-    # ===================================================
-    # TRANSLATED PROCESSING CODE GOES HERE...
-    #
-    # If you find a mistake/thing to update in the old code and
-    # fixing it will change the output data, causing the tests to fail,
-    # (i) open an issue on GitHub, (ii) consult with colleagues, and
-    # then (iii) code a fix:
-    #
-    # if(OLD_DATA_SYSTEM_BEHAVIOR) {
-    #   ... code that replicates old, incorrect behavior
-    # } else {
-    #   ... new code with a fix
-    # }
-    #
-    #
-    # NOTE: there are `merge` calls in this code. Be careful!
-    # For more information, see https://github.com/JGCRI/gcamdata/wiki/Name-That-Function
-    # NOTE: there are 'match' calls in this code. You probably want to use left_join_error_no_match
-    # For more information, see https://github.com/JGCRI/gcamdata/wiki/Name-That-Function
-    # NOTE: This code uses vecpaste
-    # This function can be removed; see https://github.com/JGCRI/gcamdata/wiki/Name-That-Function
-    # NOTE: This code uses repeat_and_add_vector
-    # This function can be removed; see https://github.com/JGCRI/gcamdata/wiki/Name-That-Function
-    # ===================================================
+    # Compute fraction of production that is irrigated for each GCAM region / commodity / GLU
+    L152.ag_irrProd_Mt_R_C_GLU %>%
+      left_join_error_no_match(L152.ag_rfdProd_Mt_R_C_GLU, by = c("GCAM_region_ID", "GCAM_commodity", "GLU")) %>%
+      mutate(irrProd_frac = irrProd / (irrProd + rfdProd)) %>%
+      # Repeat fraction table by number of historical years, and multiply fraction table by total annual production
+      repeat_add_columns(tibble::tibble(year = HISTORICAL_YEARS)) %>%
+      right_join(L103.ag_Prod_Mt_R_C_Y_GLU, by = c("GCAM_region_ID", "GCAM_commodity", "GLU", "year")) %>%
+      # Calculate irrigated (rainfed) production by multiplying total by fraction irrigated (rainfed)
+      mutate(irrProd = value * irrProd_frac, rfdProd = value * (1 - irrProd_frac)) %>%
+      select(-irrProd_frac, -value) ->
+      L161.ag_Prod_Mt_R_C_Y_GLU
+
+    # Compute fraction of harvested area that is irrigated for each GCAM region / commodity / GLU
+    L152.ag_irrHA_bm2_R_C_GLU %>%
+      left_join_error_no_match(L152.ag_rfdHA_bm2_R_C_GLU, by = c("GCAM_region_ID", "GCAM_commodity", "GLU")) %>%
+      mutate(irrHA_frac = irrHA / (irrHA + rfdHA)) ->
+      L161.ag_irrHA_frac_R_C_GLU
+
+    L161.ag_irrHA_frac_R_C_GLU %>%
+      # Repeat fraction table by number of historical years, and multiply fraction table by total annual production
+      repeat_add_columns(tibble::tibble(year = HISTORICAL_YEARS)) %>%
+      right_join(L103.ag_HA_bm2_R_C_Y_GLU, by = c("GCAM_region_ID", "GCAM_commodity", "GLU", "year")) %>%
+      # Calculate irrigated (rainfed) production by multiplying total by fraction irrigated (rainfed)
+      mutate(irrHA = value * irrHA_frac, rfdHA = value * (1 - irrHA_frac)) %>%
+      select(-irrHA_frac, -value) ->
+      L161.ag_HA_bm2_R_C_Y_GLU
+
+    # Calculate yield in kilograms per square meter by region / crop / year / GLU
+    L161.ag_Prod_Mt_R_C_Y_GLU %>%
+      left_join_error_no_match(L161.ag_HA_bm2_R_C_Y_GLU, by = c("GCAM_region_ID", "GCAM_commodity", "GLU", "year")) %>%
+      mutate(irrYield = irrProd / irrHA, rfdYield = rfdProd / rfdHA) %>%
+      replace_na(list(irrYield = 0, rfdYield = 0)) %>%
+      select(-irrProd, -rfdProd, -irrHA, -rfdHA) ->
+      L161.ag_Yield_kgm2_R_C_Y_GLU
 
     # Produce outputs
-    # Temporary code below sends back empty data frames marked "don't test"
-    # Note that all precursor names (in `add_precursor`) must be in this chunk's inputs
-    # There's also a `same_precursors_as(x)` you can use
-    # If no precursors (very rare) don't call `add_precursor` at all
-    tibble() %>%
+    L161.ag_Prod_Mt_R_C_Y_GLU %>%
+      select(GCAM_region_ID, GCAM_commodity, GLU, year, value = irrProd) %>%
       add_title("descriptive title of data") %>%
       add_units("units") %>%
       add_comments("comments describing how data generated") %>%
       add_comments("can be multiple lines") %>%
       add_legacy_name("L161.ag_irrProd_Mt_R_C_Y_GLU") %>%
-      add_precursors("precursor1", "precursor2", "etc") %>%
-      # typical flags, but there are others--see `constants.R`
+      add_precursors("L103.ag_Prod_Mt_R_C_Y_GLU",
+                     "L152.ag_irrProd_Mt_R_C_GLU") %>%
       add_flags(FLAG_LONG_YEAR_FORM, FLAG_NO_XYEAR) ->
       L161.ag_irrProd_Mt_R_C_Y_GLU
 
-    tibble() %>%
+    L161.ag_Prod_Mt_R_C_Y_GLU %>%
+      select(GCAM_region_ID, GCAM_commodity, GLU, year, value = rfdProd) %>%
       add_title("descriptive title of data") %>%
       add_units("units") %>%
       add_comments("comments describing how data generated") %>%
       add_comments("can be multiple lines") %>%
       add_legacy_name("L161.ag_rfdProd_Mt_R_C_Y_GLU") %>%
-      add_precursors("precursor1", "precursor2", "etc") %>%
-      # typical flags, but there are others--see `constants.R`
+      add_precursors("L103.ag_Prod_Mt_R_C_Y_GLU",
+                     "L152.ag_rfdProd_Mt_R_C_GLU") %>%
       add_flags(FLAG_LONG_YEAR_FORM, FLAG_NO_XYEAR) ->
       L161.ag_rfdProd_Mt_R_C_Y_GLU
 
-    tibble() %>%
+    L161.ag_HA_bm2_R_C_Y_GLU %>%
+      select(GCAM_region_ID, GCAM_commodity, GLU, year, value = irrHA) %>%
       add_title("descriptive title of data") %>%
       add_units("units") %>%
       add_comments("comments describing how data generated") %>%
       add_comments("can be multiple lines") %>%
       add_legacy_name("L161.ag_irrHA_bm2_R_C_Y_GLU") %>%
-      add_precursors("precursor1", "precursor2", "etc") %>%
-      # typical flags, but there are others--see `constants.R`
+      add_precursors("L103.ag_HA_bm2_R_C_Y_GLU",
+                     "L152.ag_irrHA_bm2_R_C_GLU") %>%
       add_flags(FLAG_LONG_YEAR_FORM, FLAG_NO_XYEAR) ->
       L161.ag_irrHA_bm2_R_C_Y_GLU
 
-    tibble() %>%
+    L161.ag_HA_bm2_R_C_Y_GLU %>%
+      select(GCAM_region_ID, GCAM_commodity, GLU, year, value = rfdHA) %>%
       add_title("descriptive title of data") %>%
       add_units("units") %>%
       add_comments("comments describing how data generated") %>%
       add_comments("can be multiple lines") %>%
       add_legacy_name("L161.ag_rfdHA_bm2_R_C_Y_GLU") %>%
-      add_precursors("precursor1", "precursor2", "etc") %>%
-      # typical flags, but there are others--see `constants.R`
+      add_precursors("L103.ag_HA_bm2_R_C_Y_GLU",
+                     "L152.ag_rfdHA_bm2_R_C_GLU") %>%
       add_flags(FLAG_LONG_YEAR_FORM, FLAG_NO_XYEAR) ->
       L161.ag_rfdHA_bm2_R_C_Y_GLU
 
-    tibble() %>%
+    L161.ag_Yield_kgm2_R_C_Y_GLU %>%
+      select(GCAM_region_ID, GCAM_commodity, GLU, year, value = irrYield) %>%
       add_title("descriptive title of data") %>%
       add_units("units") %>%
       add_comments("comments describing how data generated") %>%
       add_comments("can be multiple lines") %>%
       add_legacy_name("L161.ag_irrYield_kgm2_R_C_Y_GLU") %>%
-      add_precursors("precursor1", "precursor2", "etc") %>%
-      # typical flags, but there are others--see `constants.R`
+      same_precursors_as("L161.ag_irrProd_Mt_R_C_Y_GLU") %>%
+      same_precursors_as("L161.ag_irrHA_bm2_R_C_Y_GLU") %>%
       add_flags(FLAG_LONG_YEAR_FORM, FLAG_NO_XYEAR) ->
       L161.ag_irrYield_kgm2_R_C_Y_GLU
 
-    tibble() %>%
+    L161.ag_Yield_kgm2_R_C_Y_GLU %>%
+      select(GCAM_region_ID, GCAM_commodity, GLU, year, value = rfdYield) %>%
       add_title("descriptive title of data") %>%
       add_units("units") %>%
       add_comments("comments describing how data generated") %>%
       add_comments("can be multiple lines") %>%
       add_legacy_name("L161.ag_rfdYield_kgm2_R_C_Y_GLU") %>%
-      add_precursors("precursor1", "precursor2", "etc") %>%
-      # typical flags, but there are others--see `constants.R`
+      same_precursors_as("L161.ag_rfdProd_Mt_R_C_Y_GLU") %>%
+      same_precursors_as("L161.ag_rfdHA_bm2_R_C_Y_GLU") %>%
       add_flags(FLAG_LONG_YEAR_FORM, FLAG_NO_XYEAR) ->
       L161.ag_rfdYield_kgm2_R_C_Y_GLU
 
-    tibble() %>%
+    L161.ag_irrHA_frac_R_C_GLU %>%
       add_title("descriptive title of data") %>%
       add_units("units") %>%
       add_comments("comments describing how data generated") %>%
       add_comments("can be multiple lines") %>%
       add_legacy_name("L161.ag_irrHA_frac_R_C_GLU") %>%
-      add_precursors("precursor1", "precursor2", "etc") %>%
-      # typical flags, but there are others--see `constants.R`
-      add_flags(FLAG_LONG_YEAR_FORM, FLAG_NO_XYEAR) ->
+      add_precursors("L152.ag_irrHA_bm2_R_C_GLU",
+                     "L152.ag_rfdHA_bm2_R_C_GLU") %>%
+      add_flags(FLAG_PROTECT_FLOAT) ->
       L161.ag_irrHA_frac_R_C_GLU
 
     return_data(L161.ag_irrProd_Mt_R_C_Y_GLU, L161.ag_rfdProd_Mt_R_C_Y_GLU, L161.ag_irrHA_bm2_R_C_Y_GLU, L161.ag_rfdHA_bm2_R_C_Y_GLU, L161.ag_irrYield_kgm2_R_C_Y_GLU, L161.ag_rfdYield_kgm2_R_C_Y_GLU, L161.ag_irrHA_frac_R_C_GLU)
