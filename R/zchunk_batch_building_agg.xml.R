40d1692223710730c4efffed59f94b2712b25868
stan656 <aaron.staniszewski@pnnl.gov>
2017-08-17 23:26:43 -0400

    Commit for PR

--- R/zchunk_batch_building_agg.xml.R
+++ R/zchunk_batch_building_agg.xml.R
@@ -8,13 +8,12 @@
 #' a vector of output names, or (if \code{command} is "MAKE") all
 #' the generated outputs: \code{building_agg.xml}. The corresponding file in the
 #' original data system was \code{batch_building_agg.xml.R} (energy XML).
-module_energy_batch_building_agg.xml_DISABLED <- function(command, ...) {
+module_energy_batch_building_agg.xml <- function(command, ...) {
   if(command == driver.DECLARE_INPUTS) {
     return(c( "L242.FinalEnergyKeyword_bld",
               "L242.SubsectorLogit_bld",
-              "L242.SubsectorShrwt_bld",
+              "L242.SubsectorShrwtFllt_bld",
               "L242.SubsectorInterp_bld",
-              "L242.SubsectorInterpTo_bld",
               "L242.StubTech_bld",
               "L242.GlobalTechInterp_bld",
               "L242.GlobalTechShrwt_bld",
@@ -34,9 +33,8 @@ module_energy_batch_building_agg.xml_DISABLED <- function(command, ...) {
     # Load required inputs
     L242.FinalEnergyKeyword_bld <- get_data(all_data, "L242.FinalEnergyKeyword_bld")
     L242.SubsectorLogit_bld <- get_data(all_data, "L242.SubsectorLogit_bld")
-    L242.SubsectorShrwt_bld <- get_data(all_data, "L242.SubsectorShrwt_bld")
+    L242.SubsectorShrwtFllt_bld <- get_data(all_data, "L242.SubsectorShrwtFllt_bld")
     L242.SubsectorInterp_bld <- get_data(all_data, "L242.SubsectorInterp_bld")
-    L242.SubsectorInterpTo_bld <- get_data(all_data, "L242.SubsectorInterpTo_bld")
     L242.StubTech_bld <- get_data(all_data, "L242.StubTech_bld")
     L242.GlobalTechInterp_bld <- get_data(all_data, "L242.GlobalTechInterp_bld")
     L242.GlobalTechShrwt_bld <- get_data(all_data, "L242.GlobalTechShrwt_bld")
@@ -54,9 +52,8 @@ module_energy_batch_building_agg.xml_DISABLED <- function(command, ...) {
     create_xml("building_agg.xml") %>%
       add_xml_data(L242.FinalEnergyKeyword_bld,"FinalEnergyKeyword") %>%
       add_xml_data(L242.SubsectorLogit_bld,"SubsectorLogit") %>%
-      add_xml_data(L242.SubsectorShrwt_bld,"SubsectorShrwt") %>%
+      add_xml_data(L242.SubsectorShrwtFllt_bld,"L242.SubsectorShrwtFllt_bld") %>%
       add_xml_data(L242.SubsectorInterp_bld,"SubsectorInterp") %>%
-      add_xml_data(L242.SubsectorInterpTo_bld,"SubsectorInterpTo") %>%
       add_xml_data(L242.StubTech_bld,"StubTech") %>%
       add_xml_data(L242.GlobalTechInterp_bld,"GlobalTechInterp") %>%
       add_xml_data(L242.GlobalTechShrwt_bld,"GlobalTechShrwt") %>%
@@ -67,7 +64,7 @@ module_energy_batch_building_agg.xml_DISABLED <- function(command, ...) {
       add_xml_data(L242.PerCapitaBased_bld,"PerCapitaBased") %>%
       add_xml_data(L242.PriceElasticity_bld,"PriceElasticity") %>%
       add_xml_data(L242.BaseService_bld,"BaseService") %>%
-      add_precursors("L242.FinalEnergyKeyword_bld", "L242.SubsectorLogit_bld", "L242.SubsectorShrwt_bld", "L242.SubsectorInterp_bld", "L242.SubsectorInterpTo_bld", "L242.StubTech_bld", "L242.GlobalTechInterp_bld", "L242.GlobalTechShrwt_bld", "L242.GlobalTechEff_bld", "L242.GlobalTechCost_bld", "L242.StubTechCalInput_bld", "L242.FuelPrefElast_bld", "L242.PerCapitaBased_bld", "L242.PriceElasticity_bld", "L242.BaseService_bld") ->
+      add_precursors("L242.FinalEnergyKeyword_bld", "L242.SubsectorLogit_bld", "L242.SubsectorShrwtFllt_bld", "L242.SubsectorInterp_bld", "L242.StubTech_bld", "L242.GlobalTechInterp_bld", "L242.GlobalTechShrwt_bld", "L242.GlobalTechEff_bld", "L242.GlobalTechCost_bld", "L242.StubTechCalInput_bld", "L242.FuelPrefElast_bld", "L242.PerCapitaBased_bld", "L242.PriceElasticity_bld", "L242.BaseService_bld") ->
       building_agg.xml
 
     return_data(building_agg.xml)
