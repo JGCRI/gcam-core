18a8e9306d868a10ac011f21c887e9b0e1633397
cwroney <christopher.roney@pnnl.gov>
2017-08-23 16:07:53 -0400

    whywontmyifswork

--- R/zchunk_L226.en_distribution.R
+++ R/zchunk_L226.en_distribution.R
@@ -67,47 +67,64 @@ module_energy_L226.en_distribution <- function(command, ...) {
     # ===================================================
     browser()
     # 2. Build tables for CSVs
+
     # 2a. Supplysector information
     # "L226.Supplysector_en: Supply sector information for energy distribution sectors"
-    L226.Supplysector_en <- write_to_all_regions( A26.sector, c(names_Supplysector, "logit.type"))
-
-
-
-    #OLD=======================start
-    printlog( "L226.SubsectorShrwt_en and L226.SubsectorShrwtFllt_en: Subsector shareweights of energy distribution sectors" )
-    if( any( !is.na( A26.subsector_shrwt$year ) ) ){
-      L226.SubsectorShrwt_en <- write_to_all_regions( A26.subsector_shrwt[ !is.na( A26.subsector_shrwt$year ), ], names_SubsectorShrwt )
-    }
-    if( any( !is.na( A26.subsector_shrwt$year.fillout ) ) ){
-      L226.SubsectorShrwtFllt_en <- write_to_all_regions( A26.subsector_shrwt[ !is.na( A26.subsector_shrwt$year.fillout ), ], names_SubsectorShrwtFllt )
-    }
-
-    printlog( "L226.SubsectorInterp_en and L226.SubsectorInterpTo_en: Subsector shareweight interpolation of energy distribution sectors" )
-    if( any( is.na( A26.subsector_interp$to.value ) ) ){
-      L226.SubsectorInterp_en <- write_to_all_regions( A26.subsector_interp[ is.na( A26.subsector_interp$to.value ), ], names_SubsectorInterp )
-    }
-    if( any( !is.na( A26.subsector_interp$to.value ) ) ){
-      L226.SubsectorInterpTo_en <- write_to_all_regions( A26.subsector_interp[ !is.na( A26.subsector_interp$to.value ), ], names_SubsectorInterpTo )
-    }
-    #OLD=======================end
-
-
-
+    A26.sector %>%
+      write_to_all_regions(c("region", "supplysector", "output.unit", "input.unit", "price.unit", "logit.year.fillout", "logit.exponent", "logit.type"), GCAM_region_names) ->
+      L226.Supplysector_en
 
     # 2b. Subsector information
     # "L226.SubsectorLogit_en: Subsector logit exponents of energy distribution sectors"
-    L226.SubsectorLogit_en <- write_to_all_regions( A26.subsector_logit, c(names_SubsectorLogit, "logit.type"))
+    A26.subsector_logit %>%
+      write_to_all_regions(c("region", "supplysector", "subsector", "logit.year.fillout", "logit.exponent", "logit.type"), GCAM_region_names) ->
+      L226.SubsectorLogit_en
 
+    # set names for subsector shareweight and interpolation in energy distribution sectors
+    NAMES_SUBSECTORSHRWT <- c("region", "supplysector", "subsector", "year", "share.weight")
+    NAMES_SUBSECTORSHRWTFLLT <- c("region", "supplysector", "subsector", "year.fillout", "share.weight")
+    NAMES_SUBSECTORINTERP <- c("region", "supplysector", "subsector", "apply.to", "from.year", "to.year", "interpolation.function")
+    NAMES_SUBSECTORINTERPTO <- c("region", "supplysector", "subsector", "apply.to", "from.year", "to.year", "to.value", "interpolation.function")
 
-    #OLD=======================start
+    # L226.SubsectorShrwt_en and L226.SubsectorShrwtFllt_en: Subsector shareweights of energy distribution sectors
+
+    if (any(!is.na(A26.subsector_shrwt$year))) {
+      A26.subsector_shrwt %>%
+        filter(!is.na(year)) %>%
+        write_to_all_regions(NAMES_SUBSECTORSHRWT, GCAM_region_names) ->
+        L226.SubsectorShrwt_en
+    }
+
+    if (any(!is.na(A26.subsector_shrwt$year.fillout))) {
+      A26.subsector_shrwt %>%
+        filter(!is.na(year.fillout)) %>%
+        write_to_all_regions(NAMES_SUBSECTORSHRWTFLLT, GCAM_region_names) ->
+        L226.SubsectorShrwtFllt_en
+    }
+
+    # L226.SubsectorInterp_en and L226.SubsectorInterpTo_en: Subsector shareweight interpolation of energy distribution sectors
+
+    if (any(is.na(A26.subsector_interp$to.value))){
+      A26.subsector_interp %>%
+        filter(is.na(to.value)) %>%
+        write_to_all_regions(NAMES_SUBSECTORINTERP, GCAM_region_names) ->
+        L226.SubsectorInterp_en
+    }
+
+    if (any(!is.na(A26.subsector_interp$to.value))) {
+      A26.subsector_interp %>%
+        filter(!is.na(to.value)) %>%
+        write_to_all_regions(NAMES_SUBSECTORINTERPTO, GCAM_region_names) ->
+        L226.SubsectorInterpTo_en
+    }
+
     # 2c. Technology information
-    printlog( "L226.StubTech_en: Identification of stub technologies of energy distribution" )
-    #Note: assuming that technology list in the shareweight table includes the full set (any others would default to a 0 shareweight)
-    L226.StubTech_en <- write_to_all_regions( A26.globaltech_shrwt, names_Tech )
-    names( L226.StubTech_en ) <- names_StubTech
-    #OLD=======================end
-
-
+    # L226.StubTech_en: Identification of stub technologies of energy distribution
+    # Note: assuming that technology list in the shareweight table includes the full set (any others would default to a 0 shareweight)
+    A26.globaltech_shrwt %>%
+      write_to_all_regions(c("region", "supplysector", "subsector", "technology"), GCAM_region_names) %>%
+      rename(stub.technology = technology) ->
+      L226.StubTech_en
 
     #Efficiencies of global technologies
     #"L226.GlobalTechEff_en: Energy inputs and efficiencies of global technologies for energy distribution"
@@ -116,7 +133,8 @@ module_energy_L226.en_distribution <- function(command, ...) {
       complete(nesting(supplysector, subsector, technology, minicam.energy.input), year = c(year, BASE_YEARS, FUTURE_YEARS)) %>%
       arrange(supplysector, year) %>%
       group_by(supplysector) %>%
-      mutate (efficiency = approx_fun(as.numeric(year), efficiency)) %>%
+      mutate(efficiency = approx_fun(as.numeric(year), efficiency)) %>%
+      ungroup() %>%
       filter(year %in% c(BASE_YEARS, FUTURE_YEARS)) %>%
       # Assign the columns "sector.name" and "subsector.name", consistent with the location info of a global technology
       rename(sector.name = supplysector, subsector.name = subsector) ->
@@ -129,6 +147,7 @@ module_energy_L226.en_distribution <- function(command, ...) {
       arrange(supplysector, year) %>%
       group_by(supplysector) %>%
       mutate (input.cost = approx_fun(as.numeric(year), input.cost)) %>%
+      ungroup() %>%
       filter(year %in% c(BASE_YEARS, FUTURE_YEARS)) %>%
       #Assign the columns "sector.name" and "subsector.name", consistent with the location info of a global technology
       rename(sector.name = supplysector, subsector.name = subsector) ->
@@ -141,48 +160,101 @@ module_energy_L226.en_distribution <- function(command, ...) {
       arrange(supplysector, year) %>%
       group_by(supplysector) %>%
       mutate (share.weight = approx_fun(as.numeric(year), share.weight)) %>%
+      ungroup() %>%
       filter(year %in% c(BASE_YEARS, FUTURE_YEARS)) %>%
       #Assign the columns "sector.name" and "subsector.name", consistent with the location info of a global technology
       rename(sector.name = supplysector, subsector.name = subsector) ->
       L226.GlobalTechShrwt_en
 
     #2d. Calibration and region-specific data
-    # Electricity ownuse IO coefs
+    # Electricity ownuse IO coefs - filter down to the base years and append region IDs
     L126.IO_R_elecownuse_F_Yh %>%
-      complete(nesting(GCAM_region_ID, sector, fuel), year = c(year, BASE_YEARS)) %>%
-      arrange(GCAM_region_ID, sector, fuel, year) %>%
-      group_by(GCAM_region_ID, sector, fuel) %>%
-      mutate (value = approx_fun(as.numeric(year), value)) %>%
       filter(year %in% c(BASE_YEARS)) %>%
       left_join_error_no_match(GCAM_region_names, by = "GCAM_region_ID") %>%
       left_join(calibrated_techs, by = c("sector", "fuel")) %>%
       select(-calibration, -secondary.output) ->
       L226.IO_R_elecownuse_F_Yh
 
+    #repeat final year's ownuse ratio into future years and append future years to base years (could perhaps be tied to industrial CHP...but also AUTOELEC)
+    L226.IO_R_elecownuse_F_Yh %>%
+      filter(year == max(BASE_YEARS)) %>%
+      repeat_add_columns(tibble("year" = FUTURE_YEARS)) %>%
+      select(-year.x) %>%
+      rename(year = year.y) %>%
+      bind_rows(L226.IO_R_elecownuse_F_Yh, .) ->
+      L226.IO_R_elecownuse_F_Y
 
+    #Set number of digits to round elecownuse coefficient
+    DIGITS_COEFFICIENT <- 7
 
+    #rename columns and round coefficients - L226.StubTechCoef_elecownuse: calibrated coefficients on electricity net ownuse
+    L226.IO_R_elecownuse_F_Y %>%
+      rename(coefficient = value, market.name = region, stub.technology = technology) %>%
+      mutate(coefficient = round(coefficient, DIGITS_COEFFICIENT)) ->
+      L226.StubTechCoef_elecownuse
 
-    #Electricity transmission and distribution (#this works now but may need optional interpolation)
+    #Electricity transmission and distribution (#this works now but may need optional interpolation if the assumptions file changes)
     L126.IO_R_electd_F_Yh %>%
       filter(year %in% BASE_YEARS) ->
       L226.IO_R_electd_F_Yh
 
-    # If you find a mistake/thing to update in the old code and
-    # fixing it will change the output data, causing the tests to fail,
-    # (i) open an issue on GitHub, (ii) consult with colleagues, and
-    # then (iii) code a fix:
-    #
-    # if(OLD_DATA_SYSTEM_BEHAVIOR) {
-    #   ... code that replicates old, incorrect behavior
-    # } else {
-    #   ... new code with a fix
-    # }
-    #
-    #
-    # NOTE: there are 'match' calls in this code. You probably want to use left_join_error_no_match
-    # For more information, see https://github.com/JGCRI/gcamdata/wiki/Name-That-Function
-    # NOTE: This code uses repeat_and_add_vector
-    # This function can be removed; see https://github.com/JGCRI/gcamdata/wiki/Name-That-Function
+    # Copy to future periods
+    L226.IO_R_electd_F_Yh %>%
+      filter(year == max(BASE_YEARS)) %>%
+      repeat_add_columns(tibble("year" = FUTURE_YEARS)) %>%
+      select(-year.x) %>%
+      rename(year = year.y) ->
+      L226.IO_R_electd_F_Yfut
+
+    # append assumed techchange and calculate future values
+    L226.IO_R_electd_F_Yfut %>%
+      left_join(A_regions, by = "GCAM_region_ID") %>%
+      mutate(value = value * (1 - elect_td_techchange ^ (year - max(BASE_YEARS)))) %>%
+      select(GCAM_region_ID, sector, fuel, value, year) %>%
+      bind_rows(L226.IO_R_electd_F_Yh, .) ->
+      L226.IO_R_electd_F_Y
+
+    # L226.StubTechCoef_electd: calibrated coefficients on electricity transmission and distribution
+    # Electricity T&D: Because this is written out to multiple sectors, need to start with the list in calibrated_techs
+    calibrated_techs %>%
+      filter(paste(sector, fuel) %in% paste(L226.IO_R_electd_F_Y$sector, L226.IO_R_electd_F_Y$fuel)) %>%
+      repeat_add_columns(tibble("GCAM_region_ID" = unique(L226.IO_R_electd_F_Y$GCAM_region_ID))) %>%
+      left_join_error_no_match(GCAM_region_names, by = "GCAM_region_ID") %>%
+      repeat_add_columns(tibble("year" = unique(L226.IO_R_electd_F_Y$year))) %>%
+      left_join_error_no_match(L226.IO_R_electd_F_Y, by = c("sector", "fuel", "GCAM_region_ID", "year")) %>%
+      rename(stub.technology = technology, coefficient = value) %>%
+      mutate(market.name = region) %>%
+      select(-sector, -fuel, -calibration, -secondary.output, -GCAM_region_ID) ->
+      L226.StubTechCoef_electd
+
+    # Gas pipeline IO coefs
+    L126.IO_R_gaspipe_F_Yh %>%
+      complete(nesting(GCAM_region_ID, sector, fuel), year = c(year, BASE_YEARS)) %>%
+      arrange(GCAM_region_ID, sector, year) %>%
+      group_by(GCAM_region_ID, sector) %>%
+      mutate (value = approx_fun(as.numeric(year), value)) %>%
+      ungroup() %>%
+      filter(year %in% BASE_YEARS) %>%
+      left_join_error_no_match(GCAM_region_names, by = "GCAM_region_ID") %>%
+      left_join(calibrated_techs, by = c("sector", "fuel")) %>%
+      select(-calibration, -secondary.output, -GCAM_region_ID, -sector, -fuel) ->
+      L226.IO_R_gaspipe_F_Yh
+
+    # Gas pipeline energy use ratios are held constant in the future
+    L226.IO_R_gaspipe_F_Yh %>%
+      filter(year == max(BASE_YEARS)) %>%
+      repeat_add_columns(tibble("year" = FUTURE_YEARS)) %>%
+      select(-year.x) %>%
+      rename(year = year.y) ->
+      L226.IO_R_gaspipe_F_Yfut
+
+    # L226.StubTechCoef_gaspipe: calibrated coefficients on gas pipeline energy use
+    # Append future years to base years, add a market.name column, and round the values for the xml
+    L226.IO_R_gaspipe_F_Yfut %>%
+      bind_rows(L226.IO_R_gaspipe_F_Yh, .) %>%
+      mutate(market.name = region, value = round(value, DIGITS_COEFFICIENT)) ->
+      L226.StubTechCoef_gaspipe
+
     # ===================================================
 
     # Produce outputs
