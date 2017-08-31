953f31e4cf6ae09e10ea6bf670349a9e948e4971
abigailsnyder <abigail.snyder@pnnl.gov>
2017-08-31 11:08:11 -0400

    remove disabled from l2231

--- R/zchunk_L2231.land_input_3_irr.R
+++ R/zchunk_L2231.land_input_3_irr.R
@@ -14,7 +14,7 @@
 #' @importFrom tidyr gather spread
 #' @author YourInitials CurrentMonthName 2017
 #' @export
-module_aglu_L2231.land_input_3_irr_DISABLED <- function(command, ...) {
+module_aglu_L2231.land_input_3_irr <- function(command, ...) {
   if(command == driver.DECLARE_INPUTS) {
     return(c(FILE = "common/GCAM_region_names",
              FILE = "water/basin_to_country_mapping",
