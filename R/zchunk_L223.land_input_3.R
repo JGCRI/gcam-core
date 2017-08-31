734972ef57f35d42529a40a1cffebbc8ce701137
abigailsnyder <abigail.snyder@pnnl.gov>
2017-08-31 11:07:32 -0400

    add disabled back to L223

--- R/zchunk_L223.land_input_3.R
+++ R/zchunk_L223.land_input_3.R
@@ -14,7 +14,7 @@
 #' @importFrom tidyr gather spread
 #' @author YourInitials CurrentMonthName 2017
 #' @export
-module_aglu_L223.land_input_3 <- function(command, ...) {
+module_aglu_L223.land_input_3_DISABLED <- function(command, ...) {
   if(command == driver.DECLARE_INPUTS) {
     return(c(FILE = "common/GCAM_region_names",
              FILE = "water/basin_to_country_mapping",
