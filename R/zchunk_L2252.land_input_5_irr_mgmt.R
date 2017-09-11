4ceb1260e95fe606c6d766a834e19fc8a649b0b2
abigailsnyder <abigail.snyder@pnnl.gov>
2017-09-11 10:52:57 -0400

    remove disabled

--- R/zchunk_L2252.land_input_5_irr_mgmt.R
+++ R/zchunk_L2252.land_input_5_irr_mgmt.R
@@ -14,7 +14,7 @@
 #' @importFrom tidyr gather spread
 #' @author YourInitials CurrentMonthName 2017
 #' @export
-module_aglu_L2252.land_input_5_irr_mgmt_DISABLED <- function(command, ...) {
+module_aglu_L2252.land_input_5_irr_mgmt <- function(command, ...) {
   if(command == driver.DECLARE_INPUTS) {
     return(c(FILE = "common/GCAM_region_names",
              FILE = "water/basin_to_country_mapping",
@@ -60,41 +60,78 @@ module_aglu_L2252.land_input_5_irr_mgmt_DISABLED <- function(command, ...) {
     L2241.LN4_LeafGhostShare <- get_data(all_data, "L2241.LN4_LeafGhostShare")
     L2012.AgProduction_ag_irr_mgmt <- get_data(all_data, "L2012.AgProduction_ag_irr_mgmt")
 
-    # ===================================================
-    # TRANSLATED PROCESSING CODE GOES HERE...
-    #
-    # If you find a mistake/thing to update in the old code and
-    # fixing it will change the output data, causing the tests to fail,
-    # (i) open an issue on GitHub, (ii) consult with colleagues, and
-    # then (iii) code a fix:
-    #
-    # if(OLD_DATA_SYSTEM_BEHAVIOR) {
-    #   ... code that replicates old, incorrect behavior
-    # } else {
-    #   ... new code with a fix
-    # }
-    #
-    #
-    # NOTE: there are 'match' calls in this code. You probably want to use left_join_error_no_match
-    # For more information, see https://github.com/JGCRI/gcamdata/wiki/Name-That-Function
-    # NOTE: This code uses vecpaste
-    # This function can be removed; see https://github.com/JGCRI/gcamdata/wiki/Name-That-Function
-    # NOTE: This code uses repeat_and_add_vector
-    # This function can be removed; see https://github.com/JGCRI/gcamdata/wiki/Name-That-Function
-    # ===================================================
+
+    # silence package check notes
+    GCAM_commodity <- GCAM_region_ID <- region <- value <- year <- GLU <- GLU_name <- GLU_code <-
+      LandLeaf <- Land_Type <- LandNode <- LandNode1 <- LandNode2 <- LandNode3 <- UnmanagedLandLeaf <-
+      logit.year.fillout <- logit.exponent <- logit.type <- soilTimeScale <- `mature age` <- mature.age <-
+      soil_c <- veg_c <- LC_bm2 <- LV_milUSD75 <- LV_USD75_bm2 <- LV_USD75_m2 <- HarvCropLand_bm2 <-
+      unManagedLandValue <- LandAllocatorRoot <- hist.veg.carbon.density <- hist.soil.carbon.density <-
+      veg.carbon.density <- soil.carbon.density <- allocation <- Land_Type.y <- mature.age.year.fillout <-
+      min.veg.carbon.density <- min.soil.carbon.density <- . <- NULL
+
+
+    # 1. Process inputs
+
+    # Replace GLU names and Add region names
+    L181.LandShare_R_bio_GLU_irr %>%
+      left_join_error_no_match(GCAM_region_names, by = "GCAM_region_ID") %>%
+      replace_GLU(map = basin_to_country_mapping) ->
+      L181.LandShare_R_bio_GLU_irr
+
+    L181.LC_bm2_R_C_Yh_GLU_irr_level %>%
+      left_join_error_no_match(GCAM_region_names, by = "GCAM_region_ID") %>%
+      replace_GLU(map = basin_to_country_mapping) ->
+      L181.LC_bm2_R_C_Yh_GLU_irr_level
+
+    L181.YieldMult_R_bio_GLU_irr %>%
+      left_join_error_no_match(GCAM_region_names, by = "GCAM_region_ID") ->
+      L181.YieldMult_R_bio_GLU_irr
+
+    # Write a function to carry LN4 information down to LN5
+    convert_LN4_to_LN5 <- function( data, names ){
+      data %>%
+        repeat_add_columns(tibble::tibble(level = c( "lo", "hi" ))) %>%
+        mutate(LandNode5 = LandLeaf,
+               LandLeaf = paste(data_new$LandNode5, data_new[[lvl]], sep = aglu.MGMT_DELIMITER)) ->
+        data_new
+      # data_new <- repeat_and_add_vector( data, lvl, c( "lo", "hi" ) )
+      # data_new$LandNode5 <- data_new$LandLeaf
+      # data_new$LandLeaf <- paste( data_new$LandNode5, data_new[[lvl]], sep = mgmt_delimiter )
+
+      data_new <- data_new[names]
+      return( data_new )
+    }
+
+
+
+
+
+    # The methods in this code file will be to start with existing (landnode4) tables, and add another level of detail
+    # Some tables will require post-hoc adjustments, and for others the information simply needs to be passed down to another level
+
 
     # Produce outputs
-    # Temporary code below sends back empty data frames marked "don't test"
-    # Note that all precursor names (in `add_precursor`) must be in this chunk's inputs
-    # There's also a `same_precursors_as(x)` you can use
-    # If no precursors (very rare) don't call `add_precursor` at all
     tibble() %>%
       add_title("descriptive title of data") %>%
       add_units("units") %>%
       add_comments("comments describing how data generated") %>%
       add_comments("can be multiple lines") %>%
       add_legacy_name("L2252.LN5_Logit") %>%
-      add_precursors("precursor1", "precursor2", "etc") %>%
+      add_precursors("common/GCAM_region_names",
+                     "water/basin_to_country_mapping",
+                     "L181.LandShare_R_bio_GLU_irr",
+                     "L181.LC_bm2_R_C_Yh_GLU_irr_level",
+                     "L181.YieldMult_R_bio_GLU_irr",
+                     "L2241.LN4_Logit",
+                     "L2241.LN4_HistMgdAllocation_crop",
+                     "L2241.LN4_MgdAllocation_crop",
+                     "L2241.LN4_HistMgdAllocation_bio",
+                     "L2241.LN4_MgdAllocation_bio",
+                     "L2241.LN4_MgdCarbon_crop",
+                     "L2241.LN4_MgdCarbon_bio",
+                     "L2241.LN4_LeafGhostShare",
+                     "L2012.AgProduction_ag_irr_mgmt") %>%
       # typical flags, but there are others--see `constants.R`
       add_flags(FLAG_LONG_YEAR_FORM, FLAG_NO_XYEAR) ->
       L2252.LN5_Logit
@@ -105,7 +142,20 @@ module_aglu_L2252.land_input_5_irr_mgmt_DISABLED <- function(command, ...) {
       add_comments("comments describing how data generated") %>%
       add_comments("can be multiple lines") %>%
       add_legacy_name("L2252.LN5_HistMgdAllocation_crop") %>%
-      add_precursors("precursor1", "precursor2", "etc") %>%
+      add_precursors("common/GCAM_region_names",
+                     "water/basin_to_country_mapping",
+                     "L181.LandShare_R_bio_GLU_irr",
+                     "L181.LC_bm2_R_C_Yh_GLU_irr_level",
+                     "L181.YieldMult_R_bio_GLU_irr",
+                     "L2241.LN4_Logit",
+                     "L2241.LN4_HistMgdAllocation_crop",
+                     "L2241.LN4_MgdAllocation_crop",
+                     "L2241.LN4_HistMgdAllocation_bio",
+                     "L2241.LN4_MgdAllocation_bio",
+                     "L2241.LN4_MgdCarbon_crop",
+                     "L2241.LN4_MgdCarbon_bio",
+                     "L2241.LN4_LeafGhostShare",
+                     "L2012.AgProduction_ag_irr_mgmt") %>%
       # typical flags, but there are others--see `constants.R`
       add_flags(FLAG_LONG_YEAR_FORM, FLAG_NO_XYEAR) ->
       L2252.LN5_HistMgdAllocation_crop
@@ -116,7 +166,20 @@ module_aglu_L2252.land_input_5_irr_mgmt_DISABLED <- function(command, ...) {
       add_comments("comments describing how data generated") %>%
       add_comments("can be multiple lines") %>%
       add_legacy_name("L2252.LN5_MgdAllocation_crop") %>%
-      add_precursors("precursor1", "precursor2", "etc") %>%
+      add_precursors("common/GCAM_region_names",
+                     "water/basin_to_country_mapping",
+                     "L181.LandShare_R_bio_GLU_irr",
+                     "L181.LC_bm2_R_C_Yh_GLU_irr_level",
+                     "L181.YieldMult_R_bio_GLU_irr",
+                     "L2241.LN4_Logit",
+                     "L2241.LN4_HistMgdAllocation_crop",
+                     "L2241.LN4_MgdAllocation_crop",
+                     "L2241.LN4_HistMgdAllocation_bio",
+                     "L2241.LN4_MgdAllocation_bio",
+                     "L2241.LN4_MgdCarbon_crop",
+                     "L2241.LN4_MgdCarbon_bio",
+                     "L2241.LN4_LeafGhostShare",
+                     "L2012.AgProduction_ag_irr_mgmt") %>%
       # typical flags, but there are others--see `constants.R`
       add_flags(FLAG_LONG_YEAR_FORM, FLAG_NO_XYEAR) ->
       L2252.LN5_MgdAllocation_crop
@@ -127,7 +190,20 @@ module_aglu_L2252.land_input_5_irr_mgmt_DISABLED <- function(command, ...) {
       add_comments("comments describing how data generated") %>%
       add_comments("can be multiple lines") %>%
       add_legacy_name("L2252.LN5_HistMgdAllocation_bio") %>%
-      add_precursors("precursor1", "precursor2", "etc") %>%
+      add_precursors("common/GCAM_region_names",
+                     "water/basin_to_country_mapping",
+                     "L181.LandShare_R_bio_GLU_irr",
+                     "L181.LC_bm2_R_C_Yh_GLU_irr_level",
+                     "L181.YieldMult_R_bio_GLU_irr",
+                     "L2241.LN4_Logit",
+                     "L2241.LN4_HistMgdAllocation_crop",
+                     "L2241.LN4_MgdAllocation_crop",
+                     "L2241.LN4_HistMgdAllocation_bio",
+                     "L2241.LN4_MgdAllocation_bio",
+                     "L2241.LN4_MgdCarbon_crop",
+                     "L2241.LN4_MgdCarbon_bio",
+                     "L2241.LN4_LeafGhostShare",
+                     "L2012.AgProduction_ag_irr_mgmt") %>%
       # typical flags, but there are others--see `constants.R`
       add_flags(FLAG_LONG_YEAR_FORM, FLAG_NO_XYEAR) ->
       L2252.LN5_HistMgdAllocation_bio
@@ -138,7 +214,20 @@ module_aglu_L2252.land_input_5_irr_mgmt_DISABLED <- function(command, ...) {
       add_comments("comments describing how data generated") %>%
       add_comments("can be multiple lines") %>%
       add_legacy_name("L2252.LN5_MgdAllocation_bio") %>%
-      add_precursors("precursor1", "precursor2", "etc") %>%
+      add_precursors("common/GCAM_region_names",
+                     "water/basin_to_country_mapping",
+                     "L181.LandShare_R_bio_GLU_irr",
+                     "L181.LC_bm2_R_C_Yh_GLU_irr_level",
+                     "L181.YieldMult_R_bio_GLU_irr",
+                     "L2241.LN4_Logit",
+                     "L2241.LN4_HistMgdAllocation_crop",
+                     "L2241.LN4_MgdAllocation_crop",
+                     "L2241.LN4_HistMgdAllocation_bio",
+                     "L2241.LN4_MgdAllocation_bio",
+                     "L2241.LN4_MgdCarbon_crop",
+                     "L2241.LN4_MgdCarbon_bio",
+                     "L2241.LN4_LeafGhostShare",
+                     "L2012.AgProduction_ag_irr_mgmt") %>%
       # typical flags, but there are others--see `constants.R`
       add_flags(FLAG_LONG_YEAR_FORM, FLAG_NO_XYEAR) ->
       L2252.LN5_MgdAllocation_bio
@@ -149,7 +238,20 @@ module_aglu_L2252.land_input_5_irr_mgmt_DISABLED <- function(command, ...) {
       add_comments("comments describing how data generated") %>%
       add_comments("can be multiple lines") %>%
       add_legacy_name("L2252.LN5_MgdCarbon_crop") %>%
-      add_precursors("precursor1", "precursor2", "etc") %>%
+      add_precursors("common/GCAM_region_names",
+                     "water/basin_to_country_mapping",
+                     "L181.LandShare_R_bio_GLU_irr",
+                     "L181.LC_bm2_R_C_Yh_GLU_irr_level",
+                     "L181.YieldMult_R_bio_GLU_irr",
+                     "L2241.LN4_Logit",
+                     "L2241.LN4_HistMgdAllocation_crop",
+                     "L2241.LN4_MgdAllocation_crop",
+                     "L2241.LN4_HistMgdAllocation_bio",
+                     "L2241.LN4_MgdAllocation_bio",
+                     "L2241.LN4_MgdCarbon_crop",
+                     "L2241.LN4_MgdCarbon_bio",
+                     "L2241.LN4_LeafGhostShare",
+                     "L2012.AgProduction_ag_irr_mgmt") %>%
       # typical flags, but there are others--see `constants.R`
       add_flags(FLAG_LONG_YEAR_FORM, FLAG_NO_XYEAR) ->
       L2252.LN5_MgdCarbon_crop
@@ -160,7 +262,20 @@ module_aglu_L2252.land_input_5_irr_mgmt_DISABLED <- function(command, ...) {
       add_comments("comments describing how data generated") %>%
       add_comments("can be multiple lines") %>%
       add_legacy_name("L2252.LN5_MgdCarbon_bio") %>%
-      add_precursors("precursor1", "precursor2", "etc") %>%
+      add_precursors("common/GCAM_region_names",
+                     "water/basin_to_country_mapping",
+                     "L181.LandShare_R_bio_GLU_irr",
+                     "L181.LC_bm2_R_C_Yh_GLU_irr_level",
+                     "L181.YieldMult_R_bio_GLU_irr",
+                     "L2241.LN4_Logit",
+                     "L2241.LN4_HistMgdAllocation_crop",
+                     "L2241.LN4_MgdAllocation_crop",
+                     "L2241.LN4_HistMgdAllocation_bio",
+                     "L2241.LN4_MgdAllocation_bio",
+                     "L2241.LN4_MgdCarbon_crop",
+                     "L2241.LN4_MgdCarbon_bio",
+                     "L2241.LN4_LeafGhostShare",
+                     "L2012.AgProduction_ag_irr_mgmt") %>%
       # typical flags, but there are others--see `constants.R`
       add_flags(FLAG_LONG_YEAR_FORM, FLAG_NO_XYEAR) ->
       L2252.LN5_MgdCarbon_bio
@@ -171,7 +286,20 @@ module_aglu_L2252.land_input_5_irr_mgmt_DISABLED <- function(command, ...) {
       add_comments("comments describing how data generated") %>%
       add_comments("can be multiple lines") %>%
       add_legacy_name("L2252.LN5_LeafGhostShare") %>%
-      add_precursors("precursor1", "precursor2", "etc") %>%
+      add_precursors("common/GCAM_region_names",
+                     "water/basin_to_country_mapping",
+                     "L181.LandShare_R_bio_GLU_irr",
+                     "L181.LC_bm2_R_C_Yh_GLU_irr_level",
+                     "L181.YieldMult_R_bio_GLU_irr",
+                     "L2241.LN4_Logit",
+                     "L2241.LN4_HistMgdAllocation_crop",
+                     "L2241.LN4_MgdAllocation_crop",
+                     "L2241.LN4_HistMgdAllocation_bio",
+                     "L2241.LN4_MgdAllocation_bio",
+                     "L2241.LN4_MgdCarbon_crop",
+                     "L2241.LN4_MgdCarbon_bio",
+                     "L2241.LN4_LeafGhostShare",
+                     "L2012.AgProduction_ag_irr_mgmt") %>%
       # typical flags, but there are others--see `constants.R`
       add_flags(FLAG_LONG_YEAR_FORM, FLAG_NO_XYEAR) ->
       L2252.LN5_LeafGhostShare
@@ -182,7 +310,20 @@ module_aglu_L2252.land_input_5_irr_mgmt_DISABLED <- function(command, ...) {
       add_comments("comments describing how data generated") %>%
       add_comments("can be multiple lines") %>%
       add_legacy_name("L2252.LN5_NodeGhostShare") %>%
-      add_precursors("precursor1", "precursor2", "etc") %>%
+      add_precursors("common/GCAM_region_names",
+                     "water/basin_to_country_mapping",
+                     "L181.LandShare_R_bio_GLU_irr",
+                     "L181.LC_bm2_R_C_Yh_GLU_irr_level",
+                     "L181.YieldMult_R_bio_GLU_irr",
+                     "L2241.LN4_Logit",
+                     "L2241.LN4_HistMgdAllocation_crop",
+                     "L2241.LN4_MgdAllocation_crop",
+                     "L2241.LN4_HistMgdAllocation_bio",
+                     "L2241.LN4_MgdAllocation_bio",
+                     "L2241.LN4_MgdCarbon_crop",
+                     "L2241.LN4_MgdCarbon_bio",
+                     "L2241.LN4_LeafGhostShare",
+                     "L2012.AgProduction_ag_irr_mgmt") %>%
       # typical flags, but there are others--see `constants.R`
       add_flags(FLAG_LONG_YEAR_FORM, FLAG_NO_XYEAR) ->
       L2252.LN5_NodeGhostShare
