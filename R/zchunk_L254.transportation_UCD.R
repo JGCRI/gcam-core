b92e97070f7e00f37617ee04d88dc47b86da0caa
Ben Bond-Lamberty <bondlamberty@pnnl.gov>
2017-09-19 17:48:22 -0400

    Misc changes for debugging

--- R/zchunk_L254.transportation_UCD.R
+++ R/zchunk_L254.transportation_UCD.R
@@ -16,6 +16,7 @@
 #' @importFrom dplyr filter mutate select
 #' @importFrom tidyr gather spread
 #' @author AJS September 2017
+#' @export
 module_energy_L254.transportation_UCD <- function(command, ...) {
   if(command == driver.DECLARE_INPUTS) {
     return(c(FILE = "common/GCAM_region_names",
