1692242622f4f8ca9a0eb2cf6a6791c2461ad0b5
Jill Horing <jill.horing@pnnl.gov>
2017-09-22 15:13:43 -0400

    updated code and assumption files

--- R/constants.R
+++ R/constants.R
@@ -415,6 +415,12 @@ emissions.AGR_GASES            <- c("CH4_AGR", "N2O_AGR", "NH3_AGR", "NOx_AGR")
 emissions.AG_MACC_GHG_NAMES    <- c("CH4_AGR", "N2O_AGR")
 emissions.GHG_NAMES            <- c("CH4", "N2O")
 emissions.USE_GV_MAC           <- 1
+
+emissions.USE_GCAM3_CCOEFS     <- 1 #Select whether to use GCAM3 fuel carbon coefficients
+emissions.USE_GLOBAL_CCOEFS    <- 1 #Select whether to use global average carbon coefficients on fuels, or region-specific carbon coefficients
+emissions.INVENTORY_MATCH_YEAR <- 2009 #Select year from which to calculate fuel emissions coefficients (2009 is currently the most recent)
+emissions.DIGITS_CO2COEF <- 1
+
 # Uncomment these lines to run under 'timeshift' conditions
 # HISTORICAL_YEARS <- 1971:2005       # normally 1971:2010
 # FUTURE_YEARS <- seq(2010, 2100, 5)  # normally seq(2015, 2100, 5)
--- R/zchunk_L202.Ccoef.R
+++ R/zchunk_L202.Ccoef.R
@@ -1,6 +1,6 @@
 #' module_energy_L202.Ccoef
 #'
-#' Briefly describe what this chunk does.
+#' This chunk creates the file of CO2 coefficients for primary energy sources.
 #'
 #' @param command API command to execute
 #' @param ... other optional parameters, depending on command
@@ -8,17 +8,19 @@
 #' a vector of output names, or (if \code{command} is "MAKE") all
 #' the generated outputs: \code{L202.CarbonCoef}. The corresponding file in the
 #' original data system was \code{L202.Ccoef.R} (energy level2).
-#' @details Describe in detail what this chunk does.
+#' @details Based on user-defined switches (in constants.R), this code produces the CO2 coefficients for primary energy sources
+#' # based on 1. GCAM 3.0 assumptions 2. global average emissions coefficients based on CDIAC inventory and IEA energy balances
+#' # or 3. region-specific average emissions coefficients based on CDIAC inventory and IEA energy balances.
 #' @importFrom assertthat assert_that
 #' @importFrom dplyr filter mutate select
 #' @importFrom tidyr gather spread
-#' @author YourInitials CurrentMonthName 2017
+#' @author JDH Sept 2017
 #' @export
-module_energy_L202.Ccoef_DISABLED <- function(command, ...) {
+module_energy_L202.Ccoef <- function(command, ...) {
   if(command == driver.DECLARE_INPUTS) {
     return(c(FILE = "common/GCAM_region_names",
              FILE = "emissions/A_PrimaryFuelCCoef",
-             FILE = "emissions/fuel_to_Ccoef",
+             FILE = "emissions/mappings/fuel_to_Ccoef",
              "L102.Ccoef_kgCGJ_F_Yh",
              "L102.Ccoef_kgCGJ_R_F_Yh"))
   } else if(command == driver.DECLARE_OUTPUTS) {
@@ -30,43 +32,63 @@ module_energy_L202.Ccoef_DISABLED <- function(command, ...) {
     # Load required inputs
     GCAM_region_names <- get_data(all_data, "common/GCAM_region_names")
     A_PrimaryFuelCCoef <- get_data(all_data, "emissions/A_PrimaryFuelCCoef")
-    fuel_to_Ccoef <- get_data(all_data, "emissions/fuel_to_Ccoef")
+    fuel_to_Ccoef <- get_data(all_data, "emissions/mappings/fuel_to_Ccoef")
     L102.Ccoef_kgCGJ_F_Yh <- get_data(all_data, "L102.Ccoef_kgCGJ_F_Yh")
     L102.Ccoef_kgCGJ_R_F_Yh <- get_data(all_data, "L102.Ccoef_kgCGJ_R_F_Yh")
 
     # ===================================================
-    # TRANSLATED PROCESSING CODE GOES HERE...
-    #
-    # If you find a mistake/thing to update in the old code and
-    # fixing it will change the output data, causing the tests to fail,
-    # (i) open an issue on GitHub, (ii) consult with colleagues, and
-    # then (iii) code a fix:
-    #
-    # if(OLD_DATA_SYSTEM_BEHAVIOR) {
-    #   ... code that replicates old, incorrect behavior
-    # } else {
-    #   ... new code with a fix
-    # }
-    #
-    #
-    # NOTE: there are 'match' calls in this code. You probably want to use left_join_error_no_match
-    # For more information, see https://github.com/JGCRI/gcamdata/wiki/Name-That-Function
+    # Carbon contents of fuels in all regions
+    A_PrimaryFuelCCoef %>%
+      write_to_all_regions(c(LEVEL2_DATA_NAMES[["CarbonCoef"]]), GCAM_region_names = GCAM_region_names, has_traded = TRUE) -> L202.CarbonCoef
+
+
+    # For regions with no agricultural and land use sector (Taiwan),
+    # need to remove the passthrough supplysectors for first-gen biofuels
+    ag_en <- c( "regional corn for ethanol", "regional sugar for ethanol", "regional biomassOil" )
+    L202.CarbonCoef %>%
+      filter(!region %in% aglu.NO_AGLU_REGIONS | !PrimaryFuelCO2Coef.name %in% ag_en) -> L202.CarbonCoef
+
+    # if emissions.USE_GCAM3_CCOEFS = 1, using exogenous C coefs from GCAM 3.0
+    if(emissions.USE_GCAM3_CCOEFS == 0) {
+      L202.CarbonCoef %>%
+        left_join(fuel_to_Ccoef, by = "PrimaryFuelCO2Coef.name") -> L202.CarbonCoef
+      if(emissions.USE_GLOBAL_CCOEFS == 1) {
+        # Using global average emissions coefficients based on CDIAC inventory and IEA energy balances
+        L102.Ccoef_kgCGJ_F_Yh %>%
+          filter(year %in% HISTORICAL_YEARS) %>%
+          filter(year == emissions.INVENTORY_MATCH_YEAR) -> L202.Ccoef_kgCGJ_F_fby
+
+        L202.CarbonCoef %>%
+          left_join(L202.Ccoef_kgCGJ_F_fby, by = "fuel") %>%
+          mutate(PrimaryFuelCO2Coef = if_else(!is.na(fuel), round(value, emissions.DIGITS_CO2COEF), PrimaryFuelCO2Coef)) %>%
+          select(one_of(LEVEL2_DATA_NAMES[["CarbonCoef"]])) -> L202.CarbonCoef
+      }
+      if(emissions.USE_GLOBAL_CCOEFS == 0) {
+        # Using region-specific average emissions coefficients based on CDIAC inventory and IEA energy balances
+        L102.Ccoef_kgCGJ_R_F_Yh %>%
+          filter(year %in% HISTORICAL_YEARS) %>%
+          filter(year == emissions.INVENTORY_MATCH_YEAR) %>%
+          left_join(GCAM_region_names, by = "GCAM_region_ID") -> L202.Ccoef_kgCGJ_R_F_fby
+
+        L202.CarbonCoef %>%
+          left_join(L202.Ccoef_kgCGJ_R_F_fby, by = c("fuel", "region")) %>%
+          mutate(PrimaryFuelCO2Coef = if_else(!is.na(fuel), round(value, emissions.DIGITS_CO2COEF), PrimaryFuelCO2Coef)) %>%
+          select(one_of(LEVEL2_DATA_NAMES[["CarbonCoef"]])) -> L202.CarbonCoef
+      }
+}
+
+
     # ===================================================
 
     # Produce outputs
-    # Temporary code below sends back empty data frames marked "don't test"
-    # Note that all precursor names (in `add_precursor`) must be in this chunk's inputs
-    # There's also a `same_precursors_as(x)` you can use
-    # If no precursors (very rare) don't call `add_precursor` at all
-    tibble() %>%
-      add_title("descriptive title of data") %>%
-      add_units("units") %>%
-      add_comments("comments describing how data generated") %>%
-      add_comments("can be multiple lines") %>%
+
+    L202.CarbonCoef %>%
+      add_title("Primary Energy CO2 Coefficient") %>%
+      add_units("kgC/GJ") %>%
+      add_comments("Data can be generated in 3 ways. 1. Using GCAM 3.0 assumptions 2. Using global average emissions") %>%
+      add_comments("coefficients based on CDIAC inventory and IEA energy balances or 3. Using region-specific average emissions coefficients based on CDIAC inventory and IEA energy balances") %>%
       add_legacy_name("L202.CarbonCoef") %>%
-      add_precursors("precursor1", "precursor2", "etc") %>%
-      # typical flags, but there are others--see `constants.R`
-      add_flags(FLAG_LONG_YEAR_FORM, FLAG_NO_XYEAR) ->
+      add_precursors("common/GCAM_region_names", "emissions/A_PrimaryFuelCCoef", "emissions/mappings/fuel_to_Ccoef", "L102.Ccoef_kgCGJ_F_Yh", "L102.Ccoef_kgCGJ_R_F_Yh") ->
       L202.CarbonCoef
 
     return_data(L202.CarbonCoef)
