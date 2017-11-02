1445fca5e3bb781719f3feced4ec7fd11bc6e484
Leyang Feng <leyang.feng@pnnl.gov>
2017-11-02 17:10:39 -0400

    change xml ouptut names to match old chunk

--- R/zchunk_batch_industry_SSP_xml.R
+++ R/zchunk_batch_industry_SSP_xml.R
@@ -6,9 +6,9 @@
 #' @param ... other optional parameters, depending on command
 #' @return Depends on \code{command}: either a vector of required inputs,
 #' a vector of output names, or (if \code{command} is "MAKE") all
-#' the generated outputs: \code{industry_incelas_GCAM3.xml}, \code{industry_incelas_SSP1.xml}, \code{industry_incelas_SSP2.xml}, \code{industry_incelas_SSP3.xml},
-#' \code{industry_incelas_SSP4.xml}, \code{industry_incelas_SSP5.xml}, \code{industry_incelas_gSSP1.xml}, \code{industry_incelas_gSSP2.xml},
-#' \code{industry_incelas_gSSP3.xml}, \code{industry_incelas_gSSP4.xml}, and \code{industry_incelas_gSSP5.xml}.
+#' the generated outputs: \code{industry_incelas_gcam3.xml}, \code{industry_incelas_ssp1.xml}, \code{industry_incelas_ssp2.xml}, \code{industry_incelas_ssp3.xml},
+#' \code{industry_incelas_ssp4.xml}, \code{industry_incelas_ssp5.xml}, \code{industry_incelas_gssp1.xml}, \code{industry_incelas_gssp2.xml},
+#' \code{industry_incelas_gssp3.xml}, \code{industry_incelas_gssp4.xml}, and \code{industry_incelas_gssp5.xml}.
 module_energy_batch_industry_incelas_SSP_xml <- function(command, ...) {
 
   INCOME_ELASTICITY_INPUTS <- c("GCAM3",
@@ -18,20 +18,20 @@ module_energy_batch_industry_incelas_SSP_xml <- function(command, ...) {
   if(command == driver.DECLARE_INPUTS) {
     return(c(paste("L232.IncomeElasticity_ind", tolower(INCOME_ELASTICITY_INPUTS), sep = "_")))
   } else if(command == driver.DECLARE_OUTPUTS) {
-    return(c(paste0(paste("industry_incelas", toupper(INCOME_ELASTICITY_INPUTS), sep = "_"), '.xml')))
+    return(c(paste0(paste("industry_incelas", tolower(INCOME_ELASTICITY_INPUTS), sep = "_"), '.xml')))
   } else if(command == driver.MAKE) {
 
     # Silence package checks
-    industry_incelas_GCAM3.xml <- industry_incelas_SSP1.xml <- industry_incelas_SSP2.xml <- industry_incelas_SSP3.xml <-
-      industry_incelas_SSP4.xml <- industry_incelas_SSP5.xml<- industry_incelas_gSSP1.xml<- industry_incelas_gSSP2.xml<-
-      industry_incelas_gSSP3.xml<- industry_incelas_gSSP4.xml <- industry_incelas_gSSP5.xml <- NULL
+    industry_incelas_gcam3.xml <- industry_incelas_ssp1.xml <- industry_incelas_ssp2.xml <- industry_incelas_ssp3.xml <-
+      industry_incelas_ssp4.xml <- industry_incelas_ssp5.xml<- industry_incelas_gssp1.xml<- industry_incelas_gssp2.xml<-
+      industry_incelas_gssp3.xml<- industry_incelas_gssp4.xml <- industry_incelas_gssp5.xml <- NULL
 
     all_data <- list(...)[[1]]
 
     # Loop through all the GCAM3, SSP, and gSSP objects and build the corresponding XML structure
     for(iei in INCOME_ELASTICITY_INPUTS) {
       data_obj <- paste0("L232.IncomeElasticity_ind_", tolower(iei))
-      xmlfn <- paste0("industry_incelas_", toupper(iei), '.xml')
+      xmlfn <- paste0("industry_incelas_", tolower(iei), '.xml')
 
       create_xml(xmlfn) %>%
         add_xml_data(get_data(all_data, data_obj), "IncomeElasticity") %>%
@@ -42,9 +42,9 @@ module_energy_batch_industry_incelas_SSP_xml <- function(command, ...) {
       assign(xmlfn, xml_obj)
     }
 
-    return_data(industry_incelas_GCAM3.xml,
-                industry_incelas_SSP1.xml, industry_incelas_SSP2.xml, industry_incelas_SSP3.xml, industry_incelas_SSP4.xml, industry_incelas_SSP5.xml,
-                industry_incelas_gSSP1.xml, industry_incelas_gSSP2.xml, industry_incelas_gSSP3.xml, industry_incelas_gSSP4.xml, industry_incelas_gSSP5.xml)
+    return_data(industry_incelas_gcam3.xml,
+                industry_incelas_ssp1.xml, industry_incelas_ssp2.xml, industry_incelas_ssp3.xml, industry_incelas_ssp4.xml, industry_incelas_ssp5.xml,
+                industry_incelas_gssp1.xml, industry_incelas_gssp2.xml, industry_incelas_gssp3.xml, industry_incelas_gssp4.xml, industry_incelas_gssp5.xml)
   } else {
     stop("Unknown command")
   }
