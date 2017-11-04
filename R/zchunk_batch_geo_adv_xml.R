8da0621d1315642f88375a526177f2d02b10149f
russellhz <russell.horowitz@pnnl.gov>
2017-11-04 19:34:30 -0400

    Passes checks

---
+++ R/zchunk_batch_geo_adv_xml.R
@@ -0,0 +1,38 @@
+#' module_energy_batch_geo_adv_xml
+#'
+#' Construct XML data structure for \code{geo_adv.xml}.
+#'
+#' @param command API command to execute
+#' @param ... other optional parameters, depending on command
+#' @return Depends on \code{command}: either a vector of required inputs,
+#' a vector of output names, or (if \code{command} is "MAKE") all
+#' the generated outputs: \code{geo_adv.xml}. The corresponding file in the
+#' original data system was \code{batch_geo_adv_xml.R} (energy XML).
+module_energy_batch_geo_adv_xml <- function(command, ...) {
+  if(command == driver.DECLARE_INPUTS) {
+    return(c("L210.GrdRenewRsrcCurves_EGS",
+             "L210.GrdRenewRsrcMax_EGS"))
+  } else if(command == driver.DECLARE_OUTPUTS) {
+    return(c(XML = "geo_adv.xml"))
+  } else if(command == driver.MAKE) {
+
+    all_data <- list(...)[[1]]
+
+    # Load required inputs
+    L210.GrdRenewRsrcCurves_EGS <- get_data(all_data, "L210.GrdRenewRsrcCurves_EGS")
+    L210.GrdRenewRsrcMax_EGS <- get_data(all_data, "L210.GrdRenewRsrcMax_EGS")
+
+    # ===================================================
+
+    # Produce outputs
+    create_xml("geo_adv.xml") %>%
+      add_xml_data(L210.GrdRenewRsrcCurves_EGS,"GrdRenewRsrcCurves") %>%
+      add_xml_data(L210.GrdRenewRsrcMax_EGS,"GrdRenewRsrcMax") %>%
+      add_precursors("L210.GrdRenewRsrcCurves_EGS", "L210.GrdRenewRsrcMax_EGS") ->
+      geo_adv.xml
+
+    return_data(geo_adv.xml)
+  } else {
+    stop("Unknown command")
+  }
+}
--- R/zchunk_batch_resources_SSP_xml.R
+++ R/zchunk_batch_resources_SSP_xml.R
@@ -1,37 +1,54 @@
 #' module_energy_batch_resources_SSP_xml
 #'
-#' Construct XML data structure for \code{resources_SSP5.xml}.
+#' Construct XML data structure for all the \code{resources_SSP.xml} files.
 #'
 #' @param command API command to execute
 #' @param ... other optional parameters, depending on command
 #' @return Depends on \code{command}: either a vector of required inputs,
 #' a vector of output names, or (if \code{command} is "MAKE") all
-#' the generated outputs: \code{resources_SSP5.xml}. The corresponding file in the
-#' original data system was \code{batch_resources_SSP5_xml.R} (energy XML).
-module_energy_batch_resources_SSP_xml_DISABLED <- function(command, ...) {
+#' the generated outputs: \code{resources_SSP1.xml}, \code{resources_SSP2.xml}, \code{resources_SSP3.xml}, \code{resources_SSP4.xml}, \code{resources_SSP5.xml}.
+module_energy_batch_resources_SSP_xml <- function(command, ...) {
+
+  SSP_nums <- seq(1,5)
+
   if(command == driver.DECLARE_INPUTS) {
-    return(c("L210.DepRsrcTechChange_SSP5",
-             "L210.DepRsrcEnvironCost_SSP5"))
+    return(c(paste0("L210.DepRsrcTechChange_SSP", SSP_nums),
+             paste0("L210.DepRsrcEnvironCost_SSP", SSP_nums)))
   } else if(command == driver.DECLARE_OUTPUTS) {
-    return(c(XML = "resources_SSP5.xml"))
+    return(c(XML = "resources_SSP1.xml",
+             XML = "resources_SSP2.xml",
+             XML = "resources_SSP3.xml",
+             XML = "resources_SSP4.xml",
+             XML = "resources_SSP5.xml"))
   } else if(command == driver.MAKE) {
+
+    # Silence package checks
+    L210.DepRsrcEnvironCost_SSP1 <- L210.DepRsrcEnvironCost_SSP2 <- L210.DepRsrcEnvironCost_SSP3 <-
+      L210.DepRsrcEnvironCost_SSP4 <- L210.DepRsrcEnvironCost_SSP5 <- L210.DepRsrcTechChange_SSP1 <-
+      L210.DepRsrcTechChange_SSP2 <- L210.DepRsrcTechChange_SSP3 <-  L210.DepRsrcTechChange_SSP4 <-
+      L210.DepRsrcTechChange_SSP5 <- resources_SSP1.xml <- resources_SSP2.xml <- resources_SSP3.xml <-
+      resources_SSP4.xml <- resources_SSP5.xml <- NULL
 
     all_data <- list(...)[[1]]
 
-    # Load required inputs
-    L210.DepRsrcTechChange_SSP5 <- get_data(all_data, "L210.DepRsrcTechChange_SSP5")
-    L210.DepRsrcEnvironCost_SSP5 <- get_data(all_data, "L210.DepRsrcEnvironCost_SSP5")
+    for(i in SSP_nums){
+      # Load required inputs
+      L210.DepRsrcTechChange_SSP <- get_data(all_data, paste0("L210.DepRsrcTechChange_SSP", i))
+      L210.DepRsrcEnvironCost_SSP <- get_data(all_data, paste0("L210.DepRsrcEnvironCost_SSP", i))
 
-    # ===================================================
+      xmlfn <- paste0("resources_SSP", i, '.xml')
 
-    # Produce outputs
-    create_xml("resources_SSP5.xml") %>%
-      add_xml_data(L210.DepRsrcTechChange_SSP5,"DepRsrcTechChange") %>%
-      add_xml_data(L210.DepRsrcEnvironCost_SSP5,"DepRsrcEnvironCost") %>%
-      add_precursors("L210.DepRsrcTechChange_SSP5", "L210.DepRsrcEnvironCost_SSP5") ->
-      resources_SSP5.xml
+      create_xml(xmlfn) %>%
+        add_xml_data(L210.DepRsrcTechChange_SSP, "DepRsrcTechChange") %>%
+        add_xml_data(L210.DepRsrcEnvironCost_SSP, "DepRsrcEnvironCost") %>%
+        add_precursors(paste0("L210.DepRsrcTechChange_SSP", i), paste0("L210.DepRsrcEnvironCost_SSP", i)) ->
+        xml_obj
 
-    return_data(resources_SSP5.xml)
+      # Assign output to output name
+      assign(xmlfn, xml_obj)
+    }
+
+    return_data(resources_SSP1.xml, resources_SSP2.xml, resources_SSP3.xml, resources_SSP4.xml, resources_SSP5.xml)
   } else {
     stop("Unknown command")
   }
