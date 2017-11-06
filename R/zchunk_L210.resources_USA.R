35513851f452c0c840210cef6bbe2c4ee62036ce
Russell Horowitz <russell.horowitz@pnnl.gov>
2017-11-06 15:44:21 -0500

    Initial set-up

--- R/zchunk_L210.resources_USA.R
+++ R/zchunk_L210.resources_USA.R
@@ -6,20 +6,24 @@
 #' @param ... other optional parameters, depending on command
 #' @return Depends on \code{command}: either a vector of required inputs,
 #' a vector of output names, or (if \code{command} is "MAKE") all
-#' the generated outputs: \code{L210.DeleteRenewRsrc_USArsrc}, \code{L210.DeleteUnlimitRsrc_USArsrc}, \code{L210.RenewRsrc_USA}, \code{L210.UnlimitRsrc_USA}, \code{L210.UnlimitRsrcPrice_USA}, \code{L210.SmthRenewRsrcTechChange_USA}, \code{L210.SmthRenewRsrcCurves_wind_USA}, \code{L210.GrdRenewRsrcCurves_geo_USA}, \code{L210.GrdRenewRsrcMax_geo_USA}, \code{L210.SmthRenewRsrcCurvesGdpElastCapFac_roofPV_USA}, \code{L210.DeleteUnlimitRsrc_USAlimestone}, \code{L210.UnlimitRsrc_limestone_USA}, \code{L210.UnlimitRsrcPrice_limestone_USA}. The corresponding file in the
+#' the generated outputs: \code{L210.DeleteRenewRsrc_USArsrc}, \code{L210.DeleteUnlimitRsrc_USArsrc}, \code{L210.RenewRsrc_USA},
+#' \code{L210.UnlimitRsrc_USA}, \code{L210.UnlimitRsrcPrice_USA}, \code{L210.SmthRenewRsrcTechChange_USA},
+#' \code{L210.SmthRenewRsrcCurves_wind_USA}, \code{L210.GrdRenewRsrcCurves_geo_USA}, \code{L210.GrdRenewRsrcMax_geo_USA},
+#' \code{L210.SmthRenewRsrcCurvesGdpElastCapFac_roofPV_USA}, \code{L210.DeleteUnlimitRsrc_USAlimestone},
+#' \code{L210.UnlimitRsrc_limestone_USA}, \code{L210.UnlimitRsrcPrice_limestone_USA}. The corresponding file in the
 #' original data system was \code{L210.resources_USA.R} (gcam-usa level2).
 #' @details Describe in detail what this chunk does.
 #' @importFrom assertthat assert_that
 #' @importFrom dplyr filter mutate select
 #' @importFrom tidyr gather spread
-#' @author YourInitials CurrentMonthName 2017
+#' @author RLH November 2017
 #' @export
-module_gcam.usa_L210.resources_USA_DISABLED <- function(command, ...) {
+module_gcam.usa_L210.resources_USA <- function(command, ...) {
   if(command == driver.DECLARE_INPUTS) {
     return(c(FILE = "gcam-usa/states_subregions",
              FILE = "energy/calibrated_techs",
              FILE = "gcam-usa/NREL_us_re_technical_potential",
-             "us_state_wind",
+             FILE = "gcam-usa/us_state_wind",
              "L115.rsrc_state_rooftopPV",
              "L1231.out_EJ_state_elec_F_tech",
              "L1321.out_Mt_state_cement_Yh",
@@ -53,7 +57,7 @@ module_gcam.usa_L210.resources_USA_DISABLED <- function(command, ...) {
     states_subregions <- get_data(all_data, "gcam-usa/states_subregions")
     calibrated_techs <- get_data(all_data, "energy/calibrated_techs")
     NREL_us_re_technical_potential <- get_data(all_data, "gcam-usa/NREL_us_re_technical_potential")
-    us_state_wind <- get_data(all_data, "us_state_wind")
+    us_state_wind <- get_data(all_data, "gcam-usa/us_state_wind")
     L115.rsrc_state_rooftopPV <- get_data(all_data, "L115.rsrc_state_rooftopPV")
     L1231.out_EJ_state_elec_F_tech <- get_data(all_data, "L1231.out_EJ_state_elec_F_tech")
     L1321.out_Mt_state_cement_Yh <- get_data(all_data, "L1321.out_Mt_state_cement_Yh")
@@ -67,42 +71,17 @@ module_gcam.usa_L210.resources_USA_DISABLED <- function(command, ...) {
     L210.GrdRenewRsrcMax_geo <- get_data(all_data, "L210.GrdRenewRsrcMax_geo")
 
     # ===================================================
-    # TRANSLATED PROCESSING CODE GOES HERE...
-    #
-    # If you find a mistake/thing to update in the old code and
-    # fixing it will change the output data, causing the tests to fail,
-    # (i) open an issue on GitHub, (ii) consult with colleagues, and
-    # then (iii) code a fix:
-    #
-    # if(OLD_DATA_SYSTEM_BEHAVIOR) {
-    #   ... code that replicates old, incorrect behavior
-    # } else {
-    #   ... new code with a fix
-    # }
-    #
-    #
-    # NOTE: there are 'match' calls in this code. You probably want to use left_join_error_no_match
-    # For more information, see https://github.com/JGCRI/gcamdata/wiki/Name-That-Function
-    # NOTE: This code uses vecpaste
-    # This function can be removed; see https://github.com/JGCRI/gcamdata/wiki/Name-That-Function
-    # NOTE: This code uses repeat_and_add_vector
-    # This function can be removed; see https://github.com/JGCRI/gcamdata/wiki/Name-That-Function
+
     # ===================================================
 
     # Produce outputs
-    # Temporary code below sends back empty data frames marked "don't test"
-    # Note that all precursor names (in `add_precursor`) must be in this chunk's inputs
-    # There's also a `same_precursors_as(x)` you can use
-    # If no precursors (very rare) don't call `add_precursor` at all
     tibble() %>%
       add_title("descriptive title of data") %>%
       add_units("units") %>%
       add_comments("comments describing how data generated") %>%
       add_comments("can be multiple lines") %>%
       add_legacy_name("L210.DeleteRenewRsrc_USArsrc") %>%
-      add_precursors("precursor1", "precursor2", "etc") %>%
-      # typical flags, but there are others--see `constants.R`
-      add_flags(FLAG_LONG_YEAR_FORM, FLAG_NO_XYEAR) ->
+      add_precursors("energy/calibrated_techs") ->
       L210.DeleteRenewRsrc_USArsrc
 
     tibble() %>%
@@ -111,9 +90,7 @@ module_gcam.usa_L210.resources_USA_DISABLED <- function(command, ...) {
       add_comments("comments describing how data generated") %>%
       add_comments("can be multiple lines") %>%
       add_legacy_name("L210.DeleteUnlimitRsrc_USArsrc") %>%
-      add_precursors("precursor1", "precursor2", "etc") %>%
-      # typical flags, but there are others--see `constants.R`
-      add_flags(FLAG_LONG_YEAR_FORM, FLAG_NO_XYEAR) ->
+      add_precursors("energy/calibrated_techs") ->
       L210.DeleteUnlimitRsrc_USArsrc
 
     tibble() %>%
@@ -122,9 +99,7 @@ module_gcam.usa_L210.resources_USA_DISABLED <- function(command, ...) {
       add_comments("comments describing how data generated") %>%
       add_comments("can be multiple lines") %>%
       add_legacy_name("L210.RenewRsrc_USA") %>%
-      add_precursors("precursor1", "precursor2", "etc") %>%
-      # typical flags, but there are others--see `constants.R`
-      add_flags(FLAG_LONG_YEAR_FORM, FLAG_NO_XYEAR) ->
+      add_precursors("energy/calibrated_techs") ->
       L210.RenewRsrc_USA
 
     tibble() %>%
@@ -133,9 +108,7 @@ module_gcam.usa_L210.resources_USA_DISABLED <- function(command, ...) {
       add_comments("comments describing how data generated") %>%
       add_comments("can be multiple lines") %>%
       add_legacy_name("L210.UnlimitRsrc_USA") %>%
-      add_precursors("precursor1", "precursor2", "etc") %>%
-      # typical flags, but there are others--see `constants.R`
-      add_flags(FLAG_LONG_YEAR_FORM, FLAG_NO_XYEAR) ->
+      add_precursors("energy/calibrated_techs") ->
       L210.UnlimitRsrc_USA
 
     tibble() %>%
@@ -144,9 +117,7 @@ module_gcam.usa_L210.resources_USA_DISABLED <- function(command, ...) {
       add_comments("comments describing how data generated") %>%
       add_comments("can be multiple lines") %>%
       add_legacy_name("L210.UnlimitRsrcPrice_USA") %>%
-      add_precursors("precursor1", "precursor2", "etc") %>%
-      # typical flags, but there are others--see `constants.R`
-      add_flags(FLAG_LONG_YEAR_FORM, FLAG_NO_XYEAR) ->
+      add_precursors("energy/calibrated_techs") ->
       L210.UnlimitRsrcPrice_USA
 
     tibble() %>%
@@ -155,9 +126,7 @@ module_gcam.usa_L210.resources_USA_DISABLED <- function(command, ...) {
       add_comments("comments describing how data generated") %>%
       add_comments("can be multiple lines") %>%
       add_legacy_name("L210.SmthRenewRsrcTechChange_USA") %>%
-      add_precursors("precursor1", "precursor2", "etc") %>%
-      # typical flags, but there are others--see `constants.R`
-      add_flags(FLAG_LONG_YEAR_FORM, FLAG_NO_XYEAR) ->
+      add_precursors("energy/calibrated_techs") ->
       L210.SmthRenewRsrcTechChange_USA
 
     tibble() %>%
@@ -166,9 +135,7 @@ module_gcam.usa_L210.resources_USA_DISABLED <- function(command, ...) {
       add_comments("comments describing how data generated") %>%
       add_comments("can be multiple lines") %>%
       add_legacy_name("L210.SmthRenewRsrcCurves_wind_USA") %>%
-      add_precursors("precursor1", "precursor2", "etc") %>%
-      # typical flags, but there are others--see `constants.R`
-      add_flags(FLAG_LONG_YEAR_FORM, FLAG_NO_XYEAR) ->
+      add_precursors("energy/calibrated_techs") ->
       L210.SmthRenewRsrcCurves_wind_USA
 
     tibble() %>%
@@ -177,9 +144,7 @@ module_gcam.usa_L210.resources_USA_DISABLED <- function(command, ...) {
       add_comments("comments describing how data generated") %>%
       add_comments("can be multiple lines") %>%
       add_legacy_name("L210.GrdRenewRsrcCurves_geo_USA") %>%
-      add_precursors("precursor1", "precursor2", "etc") %>%
-      # typical flags, but there are others--see `constants.R`
-      add_flags(FLAG_LONG_YEAR_FORM, FLAG_NO_XYEAR) ->
+      add_precursors("energy/calibrated_techs") ->
       L210.GrdRenewRsrcCurves_geo_USA
 
     tibble() %>%
@@ -188,9 +153,7 @@ module_gcam.usa_L210.resources_USA_DISABLED <- function(command, ...) {
       add_comments("comments describing how data generated") %>%
       add_comments("can be multiple lines") %>%
       add_legacy_name("L210.GrdRenewRsrcMax_geo_USA") %>%
-      add_precursors("precursor1", "precursor2", "etc") %>%
-      # typical flags, but there are others--see `constants.R`
-      add_flags(FLAG_LONG_YEAR_FORM, FLAG_NO_XYEAR) ->
+      add_precursors("energy/calibrated_techs") ->
       L210.GrdRenewRsrcMax_geo_USA
 
     tibble() %>%
@@ -199,9 +162,7 @@ module_gcam.usa_L210.resources_USA_DISABLED <- function(command, ...) {
       add_comments("comments describing how data generated") %>%
       add_comments("can be multiple lines") %>%
       add_legacy_name("L210.SmthRenewRsrcCurvesGdpElastCapFac_roofPV_USA") %>%
-      add_precursors("precursor1", "precursor2", "etc") %>%
-      # typical flags, but there are others--see `constants.R`
-      add_flags(FLAG_LONG_YEAR_FORM, FLAG_NO_XYEAR) ->
+      add_precursors("energy/calibrated_techs") ->
       L210.SmthRenewRsrcCurvesGdpElastCapFac_roofPV_USA
 
     tibble() %>%
@@ -210,9 +171,7 @@ module_gcam.usa_L210.resources_USA_DISABLED <- function(command, ...) {
       add_comments("comments describing how data generated") %>%
       add_comments("can be multiple lines") %>%
       add_legacy_name("L210.DeleteUnlimitRsrc_USAlimestone") %>%
-      add_precursors("precursor1", "precursor2", "etc") %>%
-      # typical flags, but there are others--see `constants.R`
-      add_flags(FLAG_LONG_YEAR_FORM, FLAG_NO_XYEAR) ->
+      add_precursors("energy/calibrated_techs") ->
       L210.DeleteUnlimitRsrc_USAlimestone
 
     tibble() %>%
@@ -221,9 +180,7 @@ module_gcam.usa_L210.resources_USA_DISABLED <- function(command, ...) {
       add_comments("comments describing how data generated") %>%
       add_comments("can be multiple lines") %>%
       add_legacy_name("L210.UnlimitRsrc_limestone_USA") %>%
-      add_precursors("precursor1", "precursor2", "etc") %>%
-      # typical flags, but there are others--see `constants.R`
-      add_flags(FLAG_LONG_YEAR_FORM, FLAG_NO_XYEAR) ->
+      add_precursors("energy/calibrated_techs") ->
       L210.UnlimitRsrc_limestone_USA
 
     tibble() %>%
@@ -232,9 +189,7 @@ module_gcam.usa_L210.resources_USA_DISABLED <- function(command, ...) {
       add_comments("comments describing how data generated") %>%
       add_comments("can be multiple lines") %>%
       add_legacy_name("L210.UnlimitRsrcPrice_limestone_USA") %>%
-      add_precursors("precursor1", "precursor2", "etc") %>%
-      # typical flags, but there are others--see `constants.R`
-      add_flags(FLAG_LONG_YEAR_FORM, FLAG_NO_XYEAR) ->
+      add_precursors("energy/calibrated_techs") ->
       L210.UnlimitRsrcPrice_limestone_USA
 
     return_data(L210.DeleteRenewRsrc_USArsrc, L210.DeleteUnlimitRsrc_USArsrc, L210.RenewRsrc_USA, L210.UnlimitRsrc_USA,
