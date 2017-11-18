3a7fde2a37862b0d44153ac41ceac3adc54e2b86
Ben Bond-Lamberty <bondlamberty@pnnl.gov>
2017-11-18 07:10:52 -0500

    Files for #836 #837 #838 #839 #840

--- R/zchunk_LA119.solar.R
+++ R/zchunk_LA119.solar.R
@@ -1,112 +1,73 @@
-#' module_gcam.usa_LA119.Solar
+#' module_energy_LA119.solar
 #'
-#' Compute scalars by state to vary capacity factors by state.
+#' Briefly describe what this chunk does.
 #'
 #' @param command API command to execute
 #' @param ... other optional parameters, depending on command
 #' @return Depends on \code{command}: either a vector of required inputs,
 #' a vector of output names, or (if \code{command} is "MAKE") all
-#' the generated outputs: \code{L119.CapFacScaler_PV_state}, \code{L119.CapFacScaler_CSP_state}. The corresponding file in the
-#' original data system was \code{LA119.Solar.R} (gcam-usa level1).
-#' @details This chunk computes scalars by state to vary capacity factors for central station PV and CSP technologies by state.
+#' the generated outputs: \code{L119.Irradiance_rel_R}. The corresponding file in the
+#' original data system was \code{LA119.solar.R} (energy level1).
+#' @details Describe in detail what this chunk does.
 #' @importFrom assertthat assert_that
 #' @importFrom dplyr filter mutate select
 #' @importFrom tidyr gather spread
-#' @author GI, FF, AS Apr 2017
-module_gcam.usa_LA119.Solar <- function(command, ...) {
+#' @author YourInitials CurrentMonthName 2017
+#' @export
+module_energy_LA119.solar_DISABLED <- function(command, ...) {
   if(command == driver.DECLARE_INPUTS) {
-    return(c(FILE = "gcam-usa/states_subregions",
-             FILE = "gcam-usa/NREL_us_re_capacity_factors"))
+    return(c(FILE = "common/iso_GCAM_regID",
+             "Land_type_area_ha",
+             FILE = "energy/Smith_irradiance_ctry_kwh"))
   } else if(command == driver.DECLARE_OUTPUTS) {
-    return(c("L119.CapFacScaler_PV_state",
-             "L119.CapFacScaler_CSP_state"))
+    return(c("L119.Irradiance_rel_R"))
   } else if(command == driver.MAKE) {
-
-    fuel <- value <- State <- . <- value.x <- value.y <- sector <- scaler <-
-        state <- state_name <- NULL     # silence package check.
 
     all_data <- list(...)[[1]]
 
     # Load required inputs
-    states_subregions <- get_data(all_data, "gcam-usa/states_subregions")
-    NREL_us_re_capacity_factors <- get_data(all_data, "gcam-usa/NREL_us_re_capacity_factors")
+    iso_GCAM_regID <- get_data(all_data, "common/iso_GCAM_regID")
+    Land_type_area_ha <- get_data(all_data, "Land_type_area_ha")
+    Smith_irradiance_ctry_kwh <- get_data(all_data, "energy/Smith_irradiance_ctry_kwh")
 
     # ===================================================
-    # Create scalers to scale capacity factors read in the assumptions file in the energy folder.
-    # These scalers will then be used to create capacity factors by state.
-    # The idea is to vary capacity factors for solar technologies by state depending on the varying solar irradiance by state.
-
-    if(OLD_DATA_SYSTEM_BEHAVIOR) {
-      # Old data used the read-in value for the average, while we actually want to calculate it
-      # The difference between read-in value and calculated are slightly different
-      NREL_us_re_capacity_factors %>%
-        gather(fuel, value, -State) %>%
-        filter(State != "Average") ->
-        NREL_us_re_capacity_factors_longform
-
-      NREL_us_re_capacity_factors %>%
-        gather(fuel, value, -State) %>%
-        filter(State == "Average") %>%
-        select(-State) ->
-        Capacityfactor_average
-
-    } else {
-
-    # Converting NREL_us_re_capacity_factors to long-form and removing read-in value for the average
-    NREL_us_re_capacity_factors %>%
-      gather(fuel, value, -State) %>%
-      filter(State != "Average") ->
-      NREL_us_re_capacity_factors_longform
-
-    # Calculate average capacity factor by fuel (not including the 0 capacity factors)
-    NREL_us_re_capacity_factors_longform %>%
-      group_by(fuel) %>%
-      summarise_all(funs(mean(.[. != 0])), -State) -> # Average does not include 0 capacity factors
-      Capacityfactor_average
-    }
-
-    # Creating scalers by state by dividing capacity factor by the average
-    # Using state name abbreviations instead of full names
-    NREL_us_re_capacity_factors_longform %>%
-      left_join_error_no_match(Capacityfactor_average, by = "fuel") %>%
-      mutate(scaler = value.x / value.y, sector = "electricity generation") %>%
-      select(State, sector, fuel, scaler) %>%
-      left_join_error_no_match(
-        select(states_subregions, state, state_name),
-        by = c("State" = "state_name")) %>% # Need to rename to match with base dataframe
-      select(state, sector, fuel, scaler, -State) -> # Removing full state name
-      Capacityfactors_scaled
-
-    # Creating solar PV table by using Urban_Utility_scale_PV fuel values
-    Capacityfactors_scaled %>%
-      filter(fuel == "Urban_Utility_scale_PV") %>%
-      mutate(fuel = "solar PV") ->
-      L119.CapFacScaler_PV_state
-
-    # Creating solar CSP table by using CSP fuel values
-    Capacityfactors_scaled %>%
-      filter(fuel == "CSP") %>%
-      mutate(fuel = "solar CSP") ->
-      L119.CapFacScaler_CSP_state
-
+    # TRANSLATED PROCESSING CODE GOES HERE...
+    #
+    # If you find a mistake/thing to update in the old code and
+    # fixing it will change the output data, causing the tests to fail,
+    # (i) open an issue on GitHub, (ii) consult with colleagues, and
+    # then (iii) code a fix:
+    #
+    # if(OLD_DATA_SYSTEM_BEHAVIOR) {
+    #   ... code that replicates old, incorrect behavior
+    # } else {
+    #   ... new code with a fix
+    # }
+    #
+    #
+    # NOTE: there are `merge` calls in this code. Be careful!
+    # For more information, see https://github.com/JGCRI/gcamdata/wiki/Name-That-Function
+    # NOTE: there are 'match' calls in this code. You probably want to use left_join_error_no_match
+    # For more information, see https://github.com/JGCRI/gcamdata/wiki/Name-That-Function
     # ===================================================
-    L119.CapFacScaler_PV_state %>%
-      add_title("Scalar to vary PV capacity factors by state") %>%
-      add_units("Unitless") %>%
-      add_comments("The scalars are generated by dividing data on capacity factors by state by national average capacity factor from rNREL") %>%
-      add_legacy_name("L119.CapFacScaler_PV_state") %>%
-      add_precursors("gcam-usa/states_subregions", "gcam-usa/NREL_us_re_capacity_factors") ->
-      L119.CapFacScaler_PV_state
 
-    L119.CapFacScaler_CSP_state %>%
-      add_title("Scalar to vary CSP capacity factors by state") %>%
-      add_units("Unitless") %>%
-      add_comments("The scalars are generated by dividing data on capacity factors by state by national average capacity factor from NREL") %>%
-      add_legacy_name("L119.CapFacScaler_CSP_state") %>%
-      add_precursors("gcam-usa/states_subregions", "gcam-usa/NREL_us_re_capacity_factors") ->
-      L119.CapFacScaler_CSP_state
+    # Produce outputs
+    # Temporary code below sends back empty data frames marked "don't test"
+    # Note that all precursor names (in `add_precursor`) must be in this chunk's inputs
+    # There's also a `same_precursors_as(x)` you can use
+    # If no precursors (very rare) don't call `add_precursor` at all
+    tibble() %>%
+      add_title("descriptive title of data") %>%
+      add_units("units") %>%
+      add_comments("comments describing how data generated") %>%
+      add_comments("can be multiple lines") %>%
+      add_legacy_name("L119.Irradiance_rel_R") %>%
+      add_precursors("precursor1", "precursor2", "etc") %>%
+      # typical flags, but there are others--see `constants.R`
+      add_flags(FLAG_LONG_YEAR_FORM, FLAG_NO_XYEAR) ->
+      L119.Irradiance_rel_R
 
-    return_data(L119.CapFacScaler_PV_state, L119.CapFacScaler_CSP_state)
+    return_data(L119.Irradiance_rel_R)
   } else {
     stop("Unknown command")
   }
