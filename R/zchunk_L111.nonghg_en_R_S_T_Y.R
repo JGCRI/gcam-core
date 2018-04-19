7b35d5548444ffd1d1f6cc10ee041708e2f2fa9c
Ben Bond-Lamberty <bondlamberty@pnnl.gov>
2018-04-19 10:47:11 -0400

    Overwrite 115 rows in L111.nonghg_tgej_R_en_S_F_Yh with old data

--- R/zchunk_L111.nonghg_en_R_S_T_Y.R
+++ R/zchunk_L111.nonghg_en_R_S_T_Y.R
@@ -191,7 +191,7 @@ module_emissions_L111.nonghg_en_R_S_T_Y <- function(command, ...) {
     # Map in final GCAM sector, technology, and driver type
     L111.nonghg_tg_R_en_S_F_Yh <- L111.nonghg_tg_R_en_Si_F_Yh %>%
       left_join_keep_first_only(GCAM_sector_tech %>% select(sector, fuel, technology, supplysector, subsector, stub.technology) %>% unique,
-                               by = c("sector", "fuel", "technology")) %>%
+                                by = c("sector", "fuel", "technology")) %>%
       group_by(GCAM_region_ID, Non.CO2, supplysector, subsector, stub.technology, year) %>%
       # Calculate total input emissions
       summarise(value = sum(emissions)) %>%
@@ -215,9 +215,100 @@ module_emissions_L111.nonghg_en_R_S_T_Y <- function(command, ...) {
     L111.nonghg_tgej_R_en_S_F_Yh <- L111.nonghg_tg_R_en_S_F_Yh %>%
       left_join_error_no_match(L111.in_EJ_R_en_S_F_Yh, by = c("GCAM_region_ID", "supplysector", "subsector", "stub.technology", "year")) %>%
       # Calculate emission factors
-      mutate(emfact = value / energy) %>%
+      mutate(value = value / energy) %>%
       replace_na(list(value = 0)) %>%
       select(-energy)
+
+    if(OLD_DATA_SYSTEM_BEHAVIOR) {
+
+      # There are 115 rows in L111.nonghg_tgej_R_en_S_F_Yh where, because of the line 218 division of very small
+      # numbers, the old data system had a non-zero value but now we have a zero value. Replace them, for now.
+      # These all occur in supplysector "industrial energy use", subsector "gas", stub.technology "gas"
+      # TODO: need to remove this and deal with the more general problem of data system numeric precision (#931)
+      old_ds_values <- structure(list(
+        GCAM_region_ID = c(5L, 5L, 5L, 5L, 5L, 16L, 16L,
+                           16L, 16L, 16L, 24L, 24L, 24L, 24L, 24L, 16L, 16L, 16L, 16L, 16L,
+                           17L, 17L, 17L, 17L, 17L, 24L, 24L, 24L, 24L, 24L, 15L, 15L, 15L,
+                           15L, 15L, 24L, 24L, 24L, 24L, 24L, 15L, 15L, 15L, 15L, 15L, 4L,
+                           4L, 4L, 4L, 4L, 4L, 4L, 4L, 4L, 4L, 24L, 24L, 24L, 24L, 24L,
+                           4L, 4L, 4L, 4L, 4L, 24L, 24L, 24L, 24L, 24L, 4L, 4L, 4L, 4L,
+                           4L, 24L, 24L, 24L, 24L, 24L, 4L, 4L, 4L, 4L, 4L, 15L, 15L, 15L,
+                           15L, 15L, 24L, 24L, 24L, 24L, 24L, 4L, 4L, 4L, 4L, 4L, 28L, 28L,
+                           28L, 28L, 28L, 28L, 28L, 28L, 28L, 28L, 28L, 28L, 28L, 28L, 28L),
+        Non.CO2 = c("CO", "NH3", "NMVOC", "NOx", "SO2", "CO", "NH3",
+                    "NMVOC", "NOx", "SO2", "CO", "NH3", "NMVOC", "NOx", "SO2", "CO",
+                    "NH3", "NMVOC", "NOx", "SO2", "CO", "NH3", "NMVOC", "NOx", "SO2",
+                    "CO", "NH3", "NMVOC", "NOx", "SO2", "CO", "NH3", "NMVOC", "NOx",
+                    "SO2", "CO", "NH3", "NMVOC", "NOx", "SO2", "CO", "NH3", "NMVOC",
+                    "NOx", "SO2", "CO", "NH3", "NMVOC", "NOx", "SO2", "CO", "NH3",
+                    "NMVOC", "NOx", "SO2", "CO", "NH3", "NMVOC", "NOx", "SO2", "CO",
+                    "NH3", "NMVOC", "NOx", "SO2", "CO", "NH3", "NMVOC", "NOx", "SO2",
+                    "CO", "NH3", "NMVOC", "NOx", "SO2", "CO", "NH3", "NMVOC", "NOx",
+                    "SO2", "CO", "NH3", "NMVOC", "NOx", "SO2", "CO", "NH3", "NMVOC",
+                    "NOx", "SO2", "CO", "NH3", "NMVOC", "NOx", "SO2", "CO", "NH3",
+                    "NMVOC", "NOx", "SO2", "CO", "NH3", "NMVOC", "NOx", "SO2", "CO",
+                    "NH3", "NMVOC", "NOx", "SO2", "CO", "NH3", "NMVOC", "NOx", "SO2"),
+        year = c(1971L, 1971L, 1971L, 1971L, 1971L, 1971L, 1971L,
+                 1971L, 1971L, 1971L, 1972L, 1972L, 1972L, 1972L, 1972L, 1973L,
+                 1973L, 1973L, 1973L, 1973L, 1973L, 1973L, 1973L, 1973L, 1973L,
+                 1974L, 1974L, 1974L, 1974L, 1974L, 1977L, 1977L, 1977L, 1977L,
+                 1977L, 1977L, 1977L, 1977L, 1977L, 1977L, 1978L, 1978L, 1978L,
+                 1978L, 1978L, 1979L, 1979L, 1979L, 1979L, 1979L, 1980L, 1980L,
+                 1980L, 1980L, 1980L, 1981L, 1981L, 1981L, 1981L, 1981L, 1983L,
+                 1983L, 1983L, 1983L, 1983L, 1984L, 1984L, 1984L, 1984L, 1984L,
+                 1985L, 1985L, 1985L, 1985L, 1985L, 1986L, 1986L, 1986L, 1986L,
+                 1986L, 1987L, 1987L, 1987L, 1987L, 1987L, 1988L, 1988L, 1988L,
+                 1988L, 1988L, 1988L, 1988L, 1988L, 1988L, 1988L, 1989L, 1989L,
+                 1989L, 1989L, 1989L, 1990L, 1990L, 1990L, 1990L, 1990L, 1991L,
+                 1991L, 1991L, 1991L, 1991L, 1993L, 1993L, 1993L, 1993L, 1993L),
+        old.value = c(0.0944966537391371, 7.48711895167565e-06, 0.00245707572998905,
+                      0.371018946880322, 0.0170680423058009, 0.0184187889857578, 1.09748806246084e-05,
+                      0.000408664210090361, 0.134698572451476, 0.00881336961174388,
+                      0.289346323619755, 2.4835452072382e-05, 0.00435725102011136,
+                      0.270299128327986, 0.0115191426957133, 0.0165197889503319, 1.1553611714149e-05,
+                      0.000407084020972717, 0.140777695071841, 0.00846451393166871,
+                      0.197479474867471, 6.42773499296107e-06, 0.00266397841073204,
+                      0.160382542724924, 0.00742225739407049, 0.279874958922219, 2.2431331774062e-05,
+                      0.00422717165804295, 0.262121327382084, 0.0127141247868168, 0.0398804776036695,
+                      7.36602841670277e-06, 0.00106040738116434, 0.127830288769087,
+                      0.0163896567561995, 0.351764382243981, 3.36006672201343e-05,
+                      0.00825842003664506, 0.247678636658149, 0.0153760633022136, 0.0345830922221782,
+                      7.50030179066614e-06, 0.000930036731957963, 0.120350265760437,
+                      0.0159279528286064, 0.10334097125801, 7.64979273264422e-06, 0.00385115570161794,
+                      0.200626921115142, 0.0154379411213319, 0.111470140411742, 6.18261606928307e-06,
+                      0.00349616332801964, 0.181967640458583, 0.0159585851277602, 0.252992398198855,
+                      2.73010119244019e-05, 0.0055879866086533, 0.179413534564387,
+                      0.0131310575075609, 0.0622145970075885, 4.86878345919341e-06,
+                      0.00203556368870375, 0.117483172303343, 0.0121323494019424, 0.192846594881294,
+                      2.92675367407579e-05, 0.00413856068970847, 0.124022921275247,
+                      0.00961005880624691, 0.0346467940212395, 6.97138118767614e-06,
+                      0.00129449169520505, 0.0688929806288163, 0.0202542369002338,
+                      0.112869399340003, 3.00754689073321e-05, 0.00411161395648741,
+                      0.0604880471491012, 0.0122813392563423, 0.0293849496585887, 6.06738480765586e-06,
+                      0.00105565037644863, 0.0596979167365746, 0.0169765314906373,
+                      0.0190040075430393, 5.4222121466013e-06, 0.000480907493470135,
+                      0.0504764054634695, 0.0224753721217179, 0.111733595994142, 2.69010646086247e-05,
+                      0.00335595499962896, 0.0632549379615608, 0.0112305376689313,
+                      0.0226126001186248, 5.34577361134216e-06, 0.00148440900381583,
+                      0.0603324932575327, 0.0154938068400773, 0.00740220836241231,
+                      9.40448241575728e-06, 0.000502908894169819, 0.0364730474973357,
+                      0.0166392416686058, 0.0102315877079933, 1.04221043523334e-05,
+                      0.000537092119380972, 0.0417549144952055, 0.0173640226592427,
+                      0.0105412898432555, 9.78027379718356e-06, 0.00038595036679195,
+                      0.0452797988201461, 0.020990322210803)),
+        row.names = c(NA, -115L),
+        class = c("tbl_df", "tbl", "data.frame"),
+        .Names = c("GCAM_region_ID", "Non.CO2", "year", "old.value"))
+
+      old_ds_values$supplysector <- "industrial energy use"
+      old_ds_values$subsector <- old_ds_values$stub.technology <-  "gas"
+
+      L111.nonghg_tgej_R_en_S_F_Yh %>%
+        left_join(old_ds_values, by = c("GCAM_region_ID", "Non.CO2", "supplysector", "subsector", "stub.technology", "year")) %>%
+        mutate(value = if_else(is.na(old.value), value, old.value)) %>%
+        select(-old.value) ->
+        L111.nonghg_tgej_R_en_S_F_Yh
+    }
 
     # Produce outputs
 
