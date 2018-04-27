a4c427bcf717ab83793c6623b3c275f00877c75e
Dorheim <kalyn.dorheim@pnnl.gov>
2018-04-27 14:02:51 -0400

    fix land_input_3_IRR.xml

--- R/zchunk_batch_land_input_3_IRR_xml.R
+++ R/zchunk_batch_land_input_3_IRR_xml.R
@@ -10,7 +10,8 @@
 #' original data system was \code{batch_land_input_3_IRR.xml.R} (aglu XML).
 module_aglu_batch_land_input_3_IRR_xml <- function(command, ...) {
   if(command == driver.DECLARE_INPUTS) {
-    return(c("L2231.LN3_HistUnmgdAllocation",
+    return(c( "L2231.LN3_Logit",
+              "L2231.LN3_HistUnmgdAllocation",
               "L2231.LN3_UnmgdAllocation",
               "L2231.LN3_NoEmissCarbon",
               "L2231.LN3_NodeCarbon",
@@ -25,6 +26,7 @@ module_aglu_batch_land_input_3_IRR_xml <- function(command, ...) {
     all_data <- list(...)[[1]]
 
     # Load required inputs
+    L2231.LN3_Logit <- get_data(all_data, "L2231.LN3_Logit")
     L2231.LN3_HistUnmgdAllocation <- get_data(all_data, "L2231.LN3_HistUnmgdAllocation")
     L2231.LN3_UnmgdAllocation <- get_data(all_data, "L2231.LN3_UnmgdAllocation")
     L2231.LN3_NoEmissCarbon <- get_data(all_data, "L2231.LN3_NoEmissCarbon")
@@ -38,15 +40,20 @@ module_aglu_batch_land_input_3_IRR_xml <- function(command, ...) {
 
     # Produce outputs
     create_xml("land_input_3_IRR.xml") %>%
+      add_logit_tables_xml(L2231.LN3_Logit, "LN3_Logit") %>%
       add_xml_data(L2231.LN3_HistUnmgdAllocation,"LN3_HistUnmgdAllocation") %>%
       add_xml_data(L2231.LN3_UnmgdAllocation,"LN3_UnmgdAllocation") %>%
+      add_node_equiv_xml("LandLeaf") %>%
+      add_node_equiv_xml("carbon-calc") %>%
       add_xml_data(L2231.LN3_NoEmissCarbon,"LN3_NoEmissCarbon") %>%
       add_xml_data(L2231.LN3_NodeCarbon,"LN3_NodeCarbon") %>%
       add_xml_data(L2231.LN3_HistMgdAllocation_noncrop,"LN3_HistMgdAllocation") %>%
       add_xml_data(L2231.LN3_MgdAllocation_noncrop,"LN3_MgdAllocation") %>%
       add_xml_data(L2231.LN3_UnmgdCarbon,"LN3_UnmgdCarbon") %>%
       add_xml_data(L2231.LN3_MgdCarbon_noncrop,"LN3_MgdCarbon") %>%
-      add_precursors("L2231.LN3_HistUnmgdAllocation",
+      add_rename_landnode_xml() %>%
+      add_precursors("L2231.LN3_Logit",
+                     "L2231.LN3_HistUnmgdAllocation",
                      "L2231.LN3_UnmgdAllocation",
                      "L2231.LN3_NoEmissCarbon",
                      "L2231.LN3_NodeCarbon",
