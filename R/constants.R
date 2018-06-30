31cc38d425cfba82ad1f11756df8d2d8f42694fa
Pralit Patel <pralit.patel@pnnl.gov>
2018-06-29 21:54:26 -0400

    Remove OLD_DATA_SYSTEM_BEHAVIOR flag as there are no more uses of it.

--- R/constants.R
+++ R/constants.R
@@ -3,7 +3,6 @@
 OUTPUTS_DIR              <- "outputs/"
 XML_DIR                  <- "xml/"
 COMMENT_CHAR             <- "#"
-OLD_DATA_SYSTEM_BEHAVIOR <- TRUE
 UNDER_TIMESHIFT          <- FALSE
 YEAR_PATTERN             <- "^(1|2)[0-9]{3}$"   # a 1 or 2 followed by three digits, and nothing else
 LOGIT_TYPE_COLNAME        <- "logit.type"        # will be removed by test code before old-new comparison
