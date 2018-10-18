3ada6c828d82d393f0c6b167ee18ffb4bc4195a8
Turner, Sean W <sean.turner@pnnl.gov>
2018-10-18 14:51:41 -0400

    L102.water.supply.unlimied

--- R/zchunk_L102.water.supply.unlimited.R
+++ R/zchunk_L102.water.supply.unlimited.R
@@ -1,24 +1,26 @@
 #' module_water_L102.water.supply.unlimited
 #'
-#' Summarize the water price values by water type and GCAM region.
+#' Build out unlimited water prices using defaults in Constants.R.
 #'
 #' @param command API command to execute
 #' @param ... other optional parameters, depending on command
 #' @return Depends on \code{command}: either a vector of required inputs,
 #' a vector of output names, or (if \code{command} is "MAKE") all
-#' the generated outputs: \code{L102.unlimited_water_price_R_W_Y_75USDm3}. The corresponding file in the
+#' the generated outputs: \code{L102.unlimited_mapped_water_price_B_W_Y_75USDm3},
+#' \code{L102.unlimited_nonmapped_water_price_R_W_Y_75USDm3}. The corresponding file in the
 #' original data system was \code{L102.water.supply.unlimited.R} (water level1).
-#' @details  We generate all the possible combinations between the 32 GCAM regions, 4 water types, and all the defined years.
+#' @details  Generates all combinations of basin / region and water_type.
 #' @importFrom assertthat assert_that
 #' @importFrom dplyr filter mutate select
 #' @importFrom tidyr gather spread
-#' @author MH May 2017
+#' @author ST Oct 2018
 module_water_L102.water.supply.unlimited <- function(command, ...) {
   if(command == driver.DECLARE_INPUTS) {
-    return(c(FILE = "common/GCAM_region_names",
-             FILE = "water/A_unlimited_water_price"))
+    return(c(FILE = "water/basin_ID",
+             FILE = "common/GCAM_region_names"))
   } else if(command == driver.DECLARE_OUTPUTS) {
-    return(c("L102.unlimited_water_price_R_W_Y_75USDm3"))
+    return(c("L102.unlimited_mapped_water_price_B_W_Y_75USDm3",
+             "L102.unlimited_nonmapped_water_price_R_W_Y_75USDm3"))
   } else if(command == driver.MAKE) {
 
     region <- NULL                      # silence package check.
@@ -26,42 +28,56 @@ module_water_L102.water.supply.unlimited <- function(command, ...) {
     all_data <- list(...)[[1]]
 
     # Load required inputs
+    basin_ids <- get_data(all_data, "water/basin_ID")
     GCAM_region_names <- get_data(all_data, "common/GCAM_region_names")
-    A_unlimited_water_price <- get_data(all_data, "water/A_unlimited_water_price")
 
-    # all 4 water types
-    water_type <- c("water consumption", "water withdrawals", "seawater", "biophysical water consumption")
+    # get vector of nonmapped water types
+    water.ALL_WATER_TYPES[!(water.ALL_WATER_TYPES %in% water.MAPPED_WATER_TYPES)] ->
+      nonmapped_water_types
 
-    # copy for ordering later
-    water_type_order <- water_type
+    # mapped water types
+    expand.grid(basin_id = basin_ids[["basin_id"]],
+                year = MODEL_YEARS,
+                water_type = water.MAPPED_WATER_TYPES) %>%
+      as_tibble() %>%
+      mutate(price = case_when(
+        water_type == "water consumption" ~ water.DEFAULT_UNLIMITED_WATER_PRICE,
+        water_type == "water withdrawals" ~ water.DEFAULT_UNLIMITED_WITHD_WATER_PRICE
+      )) ->
+      L102.unlimited_mapped_water_price_R_W_Y_75USDm3
 
-    # all historical and future years
-    year <- c(HISTORICAL_YEARS, FUTURE_YEARS)
-
-    # generate a long table format with the all the possible combinations between the 32 GCAM regions, 4 water types, and all the defined years
-    GCAM_region_names %>%
-      repeat_add_columns(tibble(water_type)) %>%
-      select(-region) %>%
-      repeat_add_columns(tibble(year)) %>%
-      mutate(value = if_else(water_type != "water withdrawals",
-                             water.DEFAULT_UNLIMITED_WATER_PRICE,
-                             water.DEFAULT_UNLIMITED_WITHD_WATER_PRICE),
-             year = as.integer(year)) ->
-      L102.all_region_water_type
+    # nonmapped water types
+    expand.grid(GCAM_region_ID = GCAM_region_names[["GCAM_region_ID"]],
+                year = MODEL_YEARS,
+                water_type = nonmapped_water_types) %>%
+      as_tibble() %>%
+      mutate(price = water.DEFAULT_UNLIMITED_WATER_PRICE) ->
+      L102.unlimited_nonmapped_water_price_R_W_Y_75USDm3
 
     # ===================================================
 
     # Produce outputs
 
-    L102.all_region_water_type %>%
-      add_title("Water price data for different water types") %>%
-      add_units("$/m3") %>%
+    L102.unlimited_mapped_water_price_R_W_Y_75USDm3 %>%
+      add_title("Water price assumptions for mapped water types") %>%
+      add_units("1975$/m3") %>%
       add_comments("Nominal default water prices") %>%
-      add_legacy_name("L102.unlimited_water_price_R_W_Y_75USDm3") %>%
-      add_precursors("common/GCAM_region_names", "water/A_unlimited_water_price") ->
-      L102.unlimited_water_price_R_W_Y_75USDm3
+      add_legacy_name("L102.unlimited_mapped_water_price_R_W_Y_75USDm3") %>%
+      add_precursors("water/basin_ID") ->
+      L102.unlimited_mapped_water_price_R_W_Y_75USDm3
 
-    return_data(L102.unlimited_water_price_R_W_Y_75USDm3)
+    L102.unlimited_nonmapped_water_price_R_W_Y_75USDm3 %>%
+      add_title("Water price assumptions for nonmapped water types") %>%
+      add_units("1975$/m3") %>%
+      add_comments("Nominal default water prices") %>%
+      add_legacy_name("L102.unlimited_mapped_water_price_R_W_Y_75USDm3") %>%
+      add_precursors("common/GCAM_region_names") ->
+      L102.unlimited_nonmapped_water_price_R_W_Y_75USDm3
+
+      return_data(L102.unlimited_mapped_water_price_R_W_Y_75USDm3,
+                  L102.unlimited_nonmapped_water_price_R_W_Y_75USDm3)
+
+
   } else {
     stop("Unknown command")
   }
