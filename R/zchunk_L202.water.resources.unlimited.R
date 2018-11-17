69e7839cd0d44c29a86eaf5c3dc6663b28119c42
swd-turner <sean.turner@pnnl.gov>
2018-11-16 17:22:17 -0800

    L202.water.resources.unlimited

--- R/zchunk_L202.water.resources.unlimited.R
+++ R/zchunk_L202.water.resources.unlimited.R
@@ -6,22 +6,30 @@
 #' @param ... other optional parameters, depending on command
 #' @return Depends on \code{command}: either a vector of required inputs,
 #' a vector of output names, or (if \code{command} is "MAKE") all
-#' the generated outputs: \code{L202.UnlimitRsrc}, \code{L202.UnlimitRsrcPrice}. The corresponding file in the
+#' the generated outputs: \code{L202.UnlimitRsrc_mapped}, \code{L202.UnlimitRsrc_nonmapped},
+#' \code{L202.UnlimitRsrcPrice_mapped}, \code{L202.UnlimitRsrcPrice_nonmapped}. The corresponding file in the
 #' original data system was \code{L202.water.resources.unlimited.R} (water level2).
 #' @details Create unlimited resource markets (i.e., 32 GCAM regions) for water types (i.e., water consumption, withdrawals, biophysical water consumption and seawater),
 #' and read in fixed prices for water types.
 #' @importFrom assertthat assert_that
 #' @importFrom dplyr filter mutate select
 #' @importFrom tidyr gather spread
-#' @author YL July 2017
+#' @author YL July 2017 / ST Oct 2018
 module_water_L202.water.resources.unlimited <- function(command, ...) {
   if(command == driver.DECLARE_INPUTS) {
     return(c(FILE = "common/GCAM_region_names",
-             "L102.unlimited_water_price_R_W_Y_75USDm3"))
+             FILE = "water/basin_ID",
+             "L102.unlimited_mapped_water_price_B_W_Y_75USDm3",
+             "L102.unlimited_nonmapped_water_price_R_W_Y_75USDm3",
+             "L103.water_mapping_R_GLU_B_W_Ws_share",
+             "L103.water_mapping_R_B_W_Ws_share"))
   } else if(command == driver.DECLARE_OUTPUTS) {
-    return(c("L202.UnlimitRsrc",
-             "L202.UnlimitRsrcPrice"))
+    return(c("L202.UnlimitRsrc_mapped",
+             "L202.UnlimitRsrc_nonmapped",
+             "L202.UnlimitRsrcPrice_mapped",
+             "L202.UnlimitRsrcPrice_nonmapped"))
   } else if(command == driver.MAKE) {
+
 
     all_data <- list(...)[[1]]
 
@@ -30,64 +38,113 @@ module_water_L202.water.resources.unlimited <- function(command, ...) {
 
     # Load required inputs
     GCAM_region_names <- get_data(all_data, "common/GCAM_region_names")
-    L102.unlimited_water_price_R_W_Y_75USDm3 <- get_data(all_data, "L102.unlimited_water_price_R_W_Y_75USDm3")
+    basin_ID <- get_data(all_data, "water/basin_ID")
+    L102.unlimited_mapped_water_price_B_W_Y_75USDm3 <- get_data(all_data, "L102.unlimited_mapped_water_price_B_W_Y_75USDm3")
+    L102.unlimited_nonmapped_water_price_R_W_Y_75USDm3 <- get_data(all_data, "L102.unlimited_nonmapped_water_price_R_W_Y_75USDm3")
+    L103.water_mapping_R_GLU_B_W_Ws_share <- get_data(all_data, "L103.water_mapping_R_GLU_B_W_Ws_share")
+    L103.water_mapping_R_B_W_Ws_share <- get_data(all_data, "L103.water_mapping_R_B_W_Ws_share")
 
-    # Create unlimited resource markets for water types
-    L102.unlimited_water_price_R_W_Y_75USDm3 %>%
-      # the file is in long year format (58 years), and the intent is to select one year
-      # (the year order does not matter) since all other years are duplicate and not needed.
-      filter(year == first(year)) %>%
-      select(GCAM_region_ID, water_type) %>%
-      left_join_error_no_match(GCAM_region_names, by = "GCAM_region_ID") %>%
+    # Create a list of region + basin that actually exist with names
+    bind_rows(L103.water_mapping_R_GLU_B_W_Ws_share %>% rename(basin_id = GLU),
+              L103.water_mapping_R_B_W_Ws_share) %>%
+      select(-share, -water_sector) %>%
+      unique() %>%
+      right_join(GCAM_region_names, by = "GCAM_region_ID") %>%
+      right_join(basin_ID, by = "basin_id") ->
+      L202.region_basin
+
+    # Create unlimied research markets for mapped water types
+    L202.region_basin %>%
+      mutate(unlimited.resource = paste(basin_name, water_type, sep = "-"),
+             output.unit = water.WATER_UNITS_QUANTITY,
+             price.unit = water.WATER_UNITS_PRICE,
+             capacity.factor = 1) %>%
+      ## ^^ capacity factor is not used for water resources
+      rename(market = basin_name) %>%
+      select(one_of(LEVEL2_DATA_NAMES[["UnlimitRsrc"]])) ->
+      L202.UnlimitRsrc_mapped
+
+    # Create unlimited resource markets for non-mapped water types
+    L102.unlimited_nonmapped_water_price_R_W_Y_75USDm3 %>%
+      select(GCAM_region_ID, water_type) %>% unique() %>%
+      left_join(GCAM_region_names, by = "GCAM_region_ID") %>%
       mutate(market = region) %>%
       rename(unlimited.resource = water_type) %>%
-      # Capacity factor is not used for water resources
-      mutate(output.unit = water.WATER_UNITS_QUANTITY, price.unit = water.WATER_UNITS_PRICE, capacity.factor = 1) %>%
-      select(-GCAM_region_ID) %>%
-      # Remove water goods that are only used by ag technologies, in regions with no aglu module
-      filter(!region %in% aglu.NO_AGLU_REGIONS | !unlimited.resource %in% water.AG_ONLY_WATER_TYPES) %>%
-      select(region, unlimited.resource,	output.unit,	price.unit,	market) %>%
-      arrange(region, unlimited.resource, output.unit, price.unit, market) ->
-      UnlimitRsrc
+      mutate(output.unit = water.WATER_UNITS_QUANTITY,
+             price.unit = water.WATER_UNITS_PRICE,
+             capacity.factor = 1) %>%
+      select(one_of(LEVEL2_DATA_NAMES[["UnlimitRsrc"]])) %>%
+      filter(!(region %in% aglu.NO_AGLU_REGIONS) |
+               !(unlimited.resource %in% water.AG_ONLY_WATER_TYPES)) ->
+    # ^^ remove water goods that are used only by ag technologies, in regions with no aglu module
+    L202.UnlimitRsrc_nonmapped
 
-    # Read in fixed prices for water types
-    L102.unlimited_water_price_R_W_Y_75USDm3 %>%
-      filter(year %in% MODEL_YEARS) %>%
-      left_join_keep_first_only(GCAM_region_names, by = "GCAM_region_ID") %>%
-      rename(unlimited.resource = water_type, price = value) %>%
-      filter(!region %in% aglu.NO_AGLU_REGIONS | !unlimited.resource %in% water.AG_ONLY_WATER_TYPES) %>%
-      select(-GCAM_region_ID) %>%
-      arrange(region, unlimited.resource, year, price) ->
-      UnlimitRsrcPrice
+    # Read in fixed prices for mapped water types
+    L202.region_basin %>%
+      group_by(basin_name) %>%
+      filter(GCAM_region_ID == min(GCAM_region_ID)) %>% ungroup() %>%
+      left_join(L102.unlimited_mapped_water_price_B_W_Y_75USDm3,
+                by = c("basin_id", "water_type")) %>%
+      mutate(unlimited.resource = paste(basin_name, water_type, sep = "-")) %>%
+      select(one_of(LEVEL2_DATA_NAMES$UnlimitRsrcPrice)) ->
+      L202.UnlimitRsrcPrice_mapped
+
+    # Read in fixed prices for non-mapped water types
+    L102.unlimited_nonmapped_water_price_R_W_Y_75USDm3 %>%
+      left_join(GCAM_region_names, by = "GCAM_region_ID") %>%
+      rename(unlimited.resource = water_type) %>%
+      select(one_of(LEVEL2_DATA_NAMES$UnlimitRsrcPrice)) %>%
+      filter(!(region %in% aglu.NO_AGLU_REGIONS) |
+               !(unlimited.resource %in% water.AG_ONLY_WATER_TYPES)) ->
+    # ^^ remove water goods that are used only by ag technologies, in regions with no aglu module
+      L202.UnlimitRsrcPrice_nonmapped
 
     #========================================================================
+
+
     # Produce outputs
-    UnlimitRsrc %>%
-      add_title("unlimited resources of water (i.e., water consumption, withdrawals, biophysical water consumption and seawater)") %>%
+    L202.UnlimitRsrc_mapped %>%
+      add_title("Unlimited water resources (mapped)") %>%
       add_units("NA") %>%
-      add_comments("1) select the 1st year (1971) data records from the long format of L102.unlimited_water_price_R_W_Y_75USDm3
-                      because only  records for 1 year are needed.
-                   2) left_join with GCAM_region_names by GCAM_region_ID;
-                   3) assign WATER_UNITS_QUANTITY, WATER_UNITS_PRICE and capacity.factor;
-                   4) remove water goods that are only used by ag technologies, in regions with no aglu module.") %>%
       add_comments("The removed record is biophysical water consumption for Taiwan") %>%
       add_legacy_name("L202.UnlimitRsrc") %>%
-      add_precursors("common/GCAM_region_names", "L102.unlimited_water_price_R_W_Y_75USDm3") ->
-      L202.UnlimitRsrc
+      add_precursors("common/GCAM_region_names",
+                     "L103.water_mapping_R_GLU_B_W_Ws_share",
+                     "water/basin_ID") ->
+      L202.UnlimitRsrc_mapped
 
-    UnlimitRsrcPrice %>%
-      add_title("price for unlimited resources of water (i.e., water consumption, withdrawals, biophysical water consumption and seawater)") %>%
+    L202.UnlimitRsrcPrice_mapped %>%
+      add_title("Price for unlimited water resources (mapped)") %>%
       add_units("1975$/m^3") %>%
-      add_comments("1) select the records that cover the MODEL_YEARS period;
-                   2) left_join with GCAM_region_names by GCAM_region_ID;
-                   3) rename field names;
-                   4) remove water goods that are only used by ag technologies, in regions with no aglu module.") %>%
       add_comments("The removed record is biophysical water consumption for Taiwan") %>%
       add_legacy_name("L202.UnlimitRsrcPrice") %>%
-      add_precursors("common/GCAM_region_names", "L102.unlimited_water_price_R_W_Y_75USDm3") ->
-      L202.UnlimitRsrcPrice
+      add_precursors("common/GCAM_region_names",
+                     "L102.unlimited_mapped_water_price_B_W_Y_75USDm3",
+                     "water/basin_ID") ->
+      L202.UnlimitRsrcPrice_mapped
 
-    return_data(L202.UnlimitRsrc, L202.UnlimitRsrcPrice)
+    L202.UnlimitRsrc_nonmapped %>%
+      add_title("Unlimited water resources (non-mapped)") %>%
+      add_units("NA") %>%
+      add_comments("The removed record is biophysical water consumption for Taiwan") %>%
+      add_legacy_name("L202.UnlimitRsrc") %>%
+      add_precursors("common/GCAM_region_names",
+                     "L103.water_mapping_R_B_W_Ws_share",
+                     "water/basin_ID") ->
+      L202.UnlimitRsrc_nonmapped
+
+    L202.UnlimitRsrcPrice_nonmapped %>%
+      add_title("Price for unlimited water resources (non-mapped)") %>%
+      add_units("1975$/m^3") %>%
+      add_comments("The removed record is biophysical water consumption for Taiwan") %>%
+      add_legacy_name("L202.UnlimitRsrcPrice") %>%
+      add_precursors("common/GCAM_region_names",
+                     "L102.unlimited_nonmapped_water_price_R_W_Y_75USDm3",
+                     "water/basin_ID") ->
+      L202.UnlimitRsrcPrice_nonmapped
+
+    return_data(L202.UnlimitRsrc_mapped, L202.UnlimitRsrcPrice_mapped,
+                L202.UnlimitRsrc_nonmapped, L202.UnlimitRsrcPrice_nonmapped)
   } else {
     stop("Unknown command")
   }
