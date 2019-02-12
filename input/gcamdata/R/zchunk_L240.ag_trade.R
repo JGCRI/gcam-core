efd5f3a25b70e05dd7146097cf347293b4be9721
Kyle, G Page <pkyle@pnnl.gov>
2019-02-12 16:26:56 -0500

    regional ag markets: input data files from FAO, assumptions tables, constants, R code, package data, and documentation. * revisions to existing FAO data generally involve extending the data to the year 2012 and dealing with changes in country and commodity names * R code changes compute regional prices, replace default global prices with regional prices, update the fertilizer prices (for accuracy, * consistency with model output), revise the method of ensuring positive land profit rates (subsidy instead of revision to water coefs), * reduce fertilizer demands in several nations with extremely high coefs (Morocco, Pakistan "other" crops), compute gross trade volumes * from FAO bi-lateral trade data, and add in all necessary sectors and technologies for tracking gross trade by GCAM region in a new * xml file. this is done as a single commit because a partial version of these changes wouldn't build and run.

---
+++ input/gcamdata/R/zchunk_L240.ag_trade.R
@@ -0,0 +1,310 @@
+#' module_aglu_L240.ag_trade
+#'
+#' Model input for regional and (globally) traded agricultural commodities
+#'
+#' @param command API command to execute
+#' @param ... other optional parameters, depending on command
+#' @return Depends on \code{command}: either a vector of required inputs, a vector of output names, or (if
+#'   \code{command} is "MAKE") all the generated outputs: \code{L240.Supplysector_tra},
+#'   \code{L240.SectorUseTrialMarket_tra}, \code{L240.SubsectorAll_tra}, \code{L240.TechShrwt_tra},
+#'   \code{L240.TechCost_tra}, \code{L240.TechCoef_tra}, \code{L240.Production_tra}, \code{L240.Supplysector_reg},
+#'   \code{L240.SubsectorAll_reg}, \code{L240.TechShrwt_reg}, \code{L240.TechCoef_reg}, \code{L240.Production_reg_imp},
+#'   \code{L240.Production_reg_dom}.
+#' @details Build datasets for ssp4 agricultural trade: food and nonfood trade coefficients, feed trade
+#' coefficients, restricted agricultural trade, and trade regions.
+#' @importFrom assertthat assert_that
+#' @importFrom dplyr filter if_else left_join mutate rename select
+#' @importFrom tibble tibble
+#' @author GPK February 2019
+module_aglu_L240.ag_trade <- function(command, ...) {
+  if(command == driver.DECLARE_INPUTS) {
+    return(c(FILE = "common/GCAM_region_names",
+             FILE = "aglu/A_agRegionalSector",
+             FILE = "aglu/A_agRegionalSubsector",
+             FILE = "aglu/A_agRegionalTechnology",
+             FILE = "aglu/A_agTradedSector",
+             FILE = "aglu/A_agTradedSubsector",
+             FILE = "aglu/A_agTradedTechnology",
+             "L109.ag_ALL_Mt_R_C_Y",
+             "L1091.GrossTrade_Mt_R_C_Y"))
+  } else if(command == driver.DECLARE_OUTPUTS) {
+    return(c("L240.Supplysector_tra",
+             "L240.SectorUseTrialMarket_tra",
+             "L240.SubsectorAll_tra",
+             "L240.TechShrwt_tra",
+             "L240.TechCost_tra",
+             "L240.TechCoef_tra",
+             "L240.Production_tra",
+             "L240.Supplysector_reg",
+             "L240.SubsectorAll_reg",
+             "L240.TechShrwt_reg",
+             "L240.TechCoef_reg",
+             "L240.Production_reg_imp",
+             "L240.Production_reg_dom"))
+  } else if(command == driver.MAKE) {
+
+    all_data <- list(...)[[1]]
+
+    year <- region <- supplysector <- subsector <- GCAM_commodity <- GrossExp_Mt <-
+      calOutputValue <- subs.share.weight <- market.name <- minicam.energy.input <-
+      GrossImp_Mt <- Prod_Mt <- NULL # silence package check notes
+
+    # Load required inputs
+    GCAM_region_names <- get_data(all_data, "common/GCAM_region_names")
+    A_agRegionalSector <- get_data(all_data, "aglu/A_agRegionalSector")
+    A_agRegionalSubsector <- get_data(all_data, "aglu/A_agRegionalSubsector")
+    A_agRegionalTechnology <- get_data(all_data, "aglu/A_agRegionalTechnology")
+    A_agTradedSector <- get_data(all_data, "aglu/A_agTradedSector")
+    A_agTradedSubsector <- get_data(all_data, "aglu/A_agTradedSubsector")
+    A_agTradedTechnology <- get_data(all_data, "aglu/A_agTradedTechnology")
+    L109.ag_ALL_Mt_R_C_Y <- get_data(all_data, "L109.ag_ALL_Mt_R_C_Y")
+    L1091.GrossTrade_Mt_R_C_Y <- get_data(all_data, "L1091.GrossTrade_Mt_R_C_Y")
+
+    # 1. TRADED SECTOR / SUBSECTOR / TECHNOLOGY")
+    # L240.Supplysector_tra: generic supplysector info for traded ag commodities
+    # By convention, traded commodity information is contained within the USA region (could be within any)
+    A_agTradedSector$region <- gcam.USA_REGION
+
+    # L240.Supplysector_tra: generic supplysector info for traded ag commodities
+    L240.Supplysector_tra <- mutate(A_agTradedSector, logit.year.fillout = min(MODEL_BASE_YEARS)) %>%
+      select(c(LEVEL2_DATA_NAMES[["Supplysector"]], "logit.type"))
+
+    # L240.SectorUseTrialMarket_tra: Create solved markets for the traded sectors
+    L240.SectorUseTrialMarket_tra <- select(A_agTradedSector, region, supplysector) %>%
+      mutate(use.trial.market = 1)
+
+    # L240.SubsectorAll_tra: generic subsector info for traded ag commodities
+    # Traded commodities have the region set to USA and the subsector gets the region name pre-pended
+    L240.SubsectorAll_tra <- write_to_all_regions(A_agTradedSubsector,
+                                                  c(LEVEL2_DATA_NAMES[["SubsectorAll"]], "logit.type"),
+                                                  filter(GCAM_region_names, !region %in% aglu.NO_AGLU_REGIONS),
+                                                  has_traded = TRUE)
+
+    # Base technology-level table for several tables to be written out")
+    A_agTradedTechnology_R_Y <- repeat_add_columns(A_agTradedTechnology,
+                                                   tibble(year = MODEL_YEARS)) %>%
+      repeat_add_columns(filter(GCAM_region_names, !region %in% aglu.NO_AGLU_REGIONS)) %>%
+      mutate(subsector = paste(region, subsector, sep = " "),
+             technology = subsector,
+             market.name = region,
+             region = gcam.USA_REGION)
+
+    # L240.TechShrwt_tra: Share-weights of traded technologies
+    L240.TechShrwt_tra <- select(A_agTradedTechnology_R_Y, LEVEL2_DATA_NAMES[["TechShrwt"]])
+
+    # L240.TechCost_tra: Costs of traded technologies
+    L240.TechCost_tra <- A_agTradedTechnology_R_Y %>%
+      mutate(minicam.non.energy.input = "trade costs") %>%
+      select(LEVEL2_DATA_NAMES[["TechCost"]])
+
+    # L240.TechCoef_tra: Coefficient and market name of traded technologies
+    L240.TechCoef_tra <- select(A_agTradedTechnology_R_Y, LEVEL2_DATA_NAMES[["TechCoef"]])
+
+    # L240.Production_tra: Output (gross exports) of traded technologies
+    L240.GrossExports_Mt_R_C_Y <- left_join_error_no_match(L1091.GrossTrade_Mt_R_C_Y,
+                                                           GCAM_region_names,
+                                                           by = "GCAM_region_ID") %>%
+      select(region, GCAM_commodity, year, GrossExp_Mt)
+
+    L240.Production_tra <- filter(A_agTradedTechnology_R_Y, year %in% MODEL_BASE_YEARS) %>%
+      left_join_error_no_match(L240.GrossExports_Mt_R_C_Y,
+                               by = c(market.name = "region", minicam.energy.input = "GCAM_commodity", "year")) %>%
+      rename(calOutputValue = GrossExp_Mt) %>%
+      mutate(share.weight.year = year,
+             subs.share.weight = if_else(calOutputValue > 0, 1, 0),
+             tech.share.weight = subs.share.weight) %>%
+      select(LEVEL2_DATA_NAMES[["Production"]])
+
+    # PART 2: DOMESTIC SUPPLY SECTOR / SUBSECTOR / TECHNOLOGY")
+    # L240.Supplysector_reg: generic supplysector info for regional ag commodities
+    L240.Supplysector_reg <- mutate(A_agRegionalSector, logit.year.fillout = min(MODEL_BASE_YEARS)) %>%
+      write_to_all_regions(c(LEVEL2_DATA_NAMES[["Supplysector"]], "logit.type"),
+                           filter(GCAM_region_names, !region %in% aglu.NO_AGLU_REGIONS))
+
+    # L240.SubsectorAll_reg: generic subsector info for regional ag commodities (competing domestic prod vs intl imports)
+    L240.SubsectorAll_reg <- write_to_all_regions(A_agRegionalSubsector,
+                                                  c(LEVEL2_DATA_NAMES[["SubsectorAll"]], "logit.type"),
+                                                  filter(GCAM_region_names, !region %in% aglu.NO_AGLU_REGIONS))
+
+    # Base technology-level table for several tables to be written out")
+    A_agRegionalTechnology_R_Y <- repeat_add_columns(A_agRegionalTechnology,
+                                                     tibble(year = MODEL_YEARS)) %>%
+      repeat_add_columns(filter(GCAM_region_names["region"], !region %in% aglu.NO_AGLU_REGIONS)) %>%
+      mutate(market.name = if_else(market.name == "regional", region, market.name))
+
+    # L240.TechShrwt_tra: Share-weights of traded technologies
+    L240.TechShrwt_reg <- select(A_agRegionalTechnology_R_Y, LEVEL2_DATA_NAMES[["TechShrwt"]])
+
+    # L240.TechCoef_reg: Coefficient and market name of traded technologies
+    L240.TechCoef_reg <- select(A_agRegionalTechnology_R_Y, LEVEL2_DATA_NAMES[["TechCoef"]])
+
+    # L240.Production_reg_imp: Output (flow) of gross imports
+    # Imports are equal to the gross imports calculated in L1091
+    L240.GrossImports_Mt_R_C_Y <- left_join_error_no_match(L1091.GrossTrade_Mt_R_C_Y,
+                                                           GCAM_region_names,
+                                                           by = "GCAM_region_ID") %>%
+      left_join(select(A_agTradedTechnology, supplysector, minicam.energy.input),
+                by = c(GCAM_commodity = "minicam.energy.input")) %>%
+      select(region, supplysector, year, GrossImp_Mt)
+    L240.Production_reg_imp <- A_agRegionalTechnology_R_Y %>%
+      filter(year %in% MODEL_BASE_YEARS,
+             grepl( "import", subsector)) %>%
+      left_join_error_no_match(L240.GrossImports_Mt_R_C_Y,
+                               by = c("region", minicam.energy.input = "supplysector", "year")) %>%
+      rename(calOutputValue = GrossImp_Mt) %>%
+      mutate(share.weight.year = year,
+             subs.share.weight = if_else(calOutputValue > 0, 1, 0),
+             tech.share.weight = subs.share.weight) %>%
+      select(LEVEL2_DATA_NAMES[["Production"]])
+
+    # L240.Production_reg_dom: Output (flow) of domestic
+    # Domestic "output" is equal to production (Prod_Mt in L109) minus gross exports (calculated in L1091)
+
+    #### DOMESTIC TECHNOLOGY OUTPUT = AG PRODUCTION - GROSS EXPORTS
+    L240.GrossExports_Mt_R_C_Y <- left_join_error_no_match(L1091.GrossTrade_Mt_R_C_Y,
+                                                           GCAM_region_names,
+                                                           by = "GCAM_region_ID") %>%
+      select(region, GCAM_commodity, year, GrossExp_Mt)
+    L240.Prod_Mt_R_C_Y <- left_join_error_no_match(L109.ag_ALL_Mt_R_C_Y,
+                                                   GCAM_region_names,
+                                                   by = "GCAM_region_ID") %>%
+      select(region, GCAM_commodity, year, Prod_Mt)
+    L240.Production_reg_dom <- A_agRegionalTechnology_R_Y %>%
+      filter(year %in% MODEL_BASE_YEARS,
+             grepl( "domestic", subsector)) %>%
+      left_join_error_no_match(L240.GrossExports_Mt_R_C_Y,
+                               by = c("region", minicam.energy.input = "GCAM_commodity", "year")) %>%
+      left_join_error_no_match(L240.Prod_Mt_R_C_Y,
+                               by = c("region", minicam.energy.input = "GCAM_commodity", "year")) %>%
+      mutate(calOutputValue = Prod_Mt - GrossExp_Mt,
+             share.weight.year = year,
+             subs.share.weight = if_else(calOutputValue > 0, 1, 0),
+             tech.share.weight = subs.share.weight) %>%
+      select(LEVEL2_DATA_NAMES[["Production"]])
+
+    # Produce outputs
+    L240.Supplysector_tra %>%
+      add_title("Supplysector info for traded ag commodities") %>%
+      add_units("None") %>%
+      add_comments("We remove any regions for which agriculture and land use are not modeled.") %>%
+      add_precursors("common/GCAM_region_names",
+                     "aglu/A_agTradedSector") ->
+      L240.Supplysector_tra
+
+    L240.SectorUseTrialMarket_tra %>%
+      add_title("Supplysector flag indicating to make trial markets") %>%
+      add_units("None") %>%
+      add_comments("This helps model solution when running with ag trade") %>%
+      add_precursors("common/GCAM_region_names",
+                     "aglu/A_agTradedSector") ->
+      L240.SectorUseTrialMarket_tra
+
+    L240.SubsectorAll_tra %>%
+      add_title("Subsector info for traded ag commodities") %>%
+      add_units("None") %>%
+      add_comments("We remove any regions for which agriculture and land use are not modeled.") %>%
+      add_precursors("common/GCAM_region_names",
+                     "aglu/A_agTradedSubsector") ->
+      L240.SubsectorAll_tra
+
+    L240.TechShrwt_tra %>%
+      add_title("Technology share-weights for traded ag commodities") %>%
+      add_units("None") %>%
+      add_comments("We remove any regions for which agriculture and land use are not modeled.") %>%
+      add_precursors("common/GCAM_region_names",
+                     "aglu/A_agTradedTechnology") ->
+      L240.TechShrwt_tra
+
+    L240.TechCost_tra %>%
+      add_title("Technology costs for traded ag commodities") %>%
+      add_units("1975$/kg") %>%
+      add_comments("Exogenous cost to reflect shipping + handling of traded commodities") %>%
+      add_precursors("common/GCAM_region_names",
+                     "aglu/A_agTradedTechnology") ->
+      L240.TechCost_tra
+
+    L240.TechCoef_tra %>%
+      add_title("Technology input-output coefficients for traded ag commodities") %>%
+      add_units("Unitless IO") %>%
+      add_comments("Pass-through; 1 unless some portion is assumed lost/spoiled in shipping") %>%
+      add_precursors("common/GCAM_region_names",
+                     "aglu/A_agTradedTechnology") ->
+      L240.TechCoef_tra
+
+    L240.Production_tra %>%
+      add_title("Technology calibration for traded ag commodities") %>%
+      add_units("Mt") %>%
+      add_comments("Regional exports of commodities that are traded between GCAM regions") %>%
+      add_precursors("common/GCAM_region_names",
+                     "aglu/A_agTradedTechnology",
+                     "L1091.GrossTrade_Mt_R_C_Y") ->
+      L240.Production_tra
+
+    L240.Supplysector_reg %>%
+      add_title("Supplysector info for regional ag commodities") %>%
+      add_units("None") %>%
+      add_comments("These sectors are used for sharing between consumption of domestically produced crops versus imports") %>%
+      add_precursors("common/GCAM_region_names",
+                     "aglu/A_agRegionalSector") ->
+      L240.Supplysector_reg
+
+    L240.SubsectorAll_reg %>%
+      add_title("Subsector info for traded ag commodities") %>%
+      add_units("None") %>%
+      add_comments("We remove any regions for which agriculture and land use are not modeled.") %>%
+      add_precursors("common/GCAM_region_names",
+                     "aglu/A_agRegionalSubsector") ->
+      L240.SubsectorAll_reg
+
+    L240.TechShrwt_reg %>%
+      add_title("Technology share-weights for traded ag commodities") %>%
+      add_units("None") %>%
+      add_comments("We remove any regions for which agriculture and land use are not modeled.") %>%
+      add_precursors("common/GCAM_region_names",
+                     "aglu/A_agRegionalTechnology") ->
+      L240.TechShrwt_reg
+
+    L240.TechCoef_reg %>%
+      add_title("Technology input-output coefficients for regional ag commodities") %>%
+      add_units("Unitless IO") %>%
+      add_comments("Pass-through; 1 unless some portion is assumed lost/spoiled in shipping") %>%
+      add_precursors("common/GCAM_region_names",
+                     "aglu/A_agRegionalTechnology") ->
+      L240.TechCoef_reg
+
+    L240.Production_reg_imp %>%
+      add_title("Technology calibration for regional ag commodities: imports") %>%
+      add_units("Mt") %>%
+      add_comments("Consumption of commodities that are traded between GCAM regions") %>%
+      add_precursors("common/GCAM_region_names",
+                     "aglu/A_agRegionalTechnology",
+                     "L1091.GrossTrade_Mt_R_C_Y") ->
+      L240.Production_reg_imp
+
+    L240.Production_reg_dom %>%
+      add_title("Technology calibration for regional ag commodities: consumption of domestic production") %>%
+      add_units("Mt") %>%
+      add_comments("Consumption of commodities produced within-region") %>%
+      add_precursors("common/GCAM_region_names",
+                     "aglu/A_agRegionalTechnology",
+                     "L109.ag_ALL_Mt_R_C_Y",
+                     "L1091.GrossTrade_Mt_R_C_Y") ->
+      L240.Production_reg_dom
+
+    return_data(L240.Supplysector_tra,
+                L240.SectorUseTrialMarket_tra,
+                L240.SubsectorAll_tra,
+                L240.TechShrwt_tra,
+                L240.TechCost_tra,
+                L240.TechCoef_tra,
+                L240.Production_tra,
+                L240.Supplysector_reg,
+                L240.SubsectorAll_reg,
+                L240.TechShrwt_reg,
+                L240.TechCoef_reg,
+                L240.Production_reg_imp,
+                L240.Production_reg_dom)
+  } else {
+    stop("Unknown command")
+  }
+}
