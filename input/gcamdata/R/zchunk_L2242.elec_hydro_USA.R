bba85ff8df9cf70bde8e16a51743dd564821c916
Matthew Binsted <matthew.binsted@pnnl.gov>
2019-02-28 13:08:19 -0500

    This commit includes: - Updates to zchunk_LB1235.elec_load_segments_USA.R and zchunk_L2242.elec_hydro_USA.R based on feedback from Pull Request #83: 271 Consolidated GCAM-USA updates 	- NOTE:  for zchunk_LB1235.elec_load_segments_USA.R, several outputs which are not used anywhere in gcamdata were removed - Removal of gcamusa.USE_REGIONAL_ELEC_MARKETS from constants.R, and all places in gcamdata where it was used.  This is no longer a user configuration option we wish to maintain. - Modification of the way gcamusa.USE_REGIONAL_FUEL_MARKETS (constants.R) is utilized.  We now always create regional fuel markets (at the grid region level) which pull from the national equivalent, and use this variable only to determine whether or not a regional cost “adder” is created (to match historical regional prices from EIA). There are a number of corresponding changes to various chunks where gcamusa.USE_REGIONAL_FUEL_MARKETS was previously used to indicate whether regional or national fuel markets should be created.  This simplifies the maintenance of this user configuration option substantially. - Renaming several modules to use "module_gcamusa" rather tham "module_gcam.usa", plus associated documentation updates.

--- input/gcamdata/R/zchunk_L2242.elec_hydro_USA.R
+++ input/gcamdata/R/zchunk_L2242.elec_hydro_USA.R
@@ -1,4 +1,4 @@
-#' module_gcam.usa_L2242.elec_hydro_USA
+#' module_gcamusa_L2242.elec_hydro_USA
 #'
 #' Update hydro-electricity fixed output to match historical data for 2015 and AEO-2018 for the future.
 #'
@@ -13,7 +13,7 @@
 #' @importFrom dplyr filter mutate select
 #' @importFrom tidyr gather spread
 #' @author MTB September 2018
-module_gcam.usa_L2242.elec_hydro_USA <- function(command, ...) {
+module_gcamusa_L2242.elec_hydro_USA <- function(command, ...) {
   if(command == driver.DECLARE_INPUTS) {
     return(c(FILE = 'gcam-usa/states_subregions',
              FILE = 'gcam-usa/A23.elecS_tech_associations',
@@ -45,34 +45,25 @@ module_gcam.usa_L2242.elec_hydro_USA <- function(command, ...) {
     # Isolate 2010 hydro fixedOutput
     L2234.StubTechFixOut_elecS_USA %>%
       filter(subsector == "hydro",
-             year == max(MODEL_BASE_YEARS)) %>%
-      filter(fixedOutput != 0) -> L2242.hydro_2010_fixedOutput
+             year == max(MODEL_BASE_YEARS),
+             fixedOutput != 0) -> L2242.hydro_2010_fixedOutput
 
     # Filter EIA data to get annual hydro net generation for 2010, 2015
     # Compute ratio of 2015 EIA to 2010 EIA
     EIA_elec_gen_hydro %>%
       filter(year %in% c(max(MODEL_BASE_YEARS), min(MODEL_FUTURE_YEARS))) %>%
       group_by(state) %>%
-      mutate(EIA_ratio =  EIA[year == min(MODEL_FUTURE_YEARS)] /EIA[year == max(MODEL_BASE_YEARS)] ) %>%
+      mutate(EIA_ratio =  EIA[year == min(MODEL_FUTURE_YEARS)] / EIA[year == max(MODEL_BASE_YEARS)] ) %>%
       distinct(state, EIA_ratio) %>%
       ungroup() -> L2242.hydro_EIA_ratio
 
     # Apply the ratio to GCAM 2010 fixed output to calculate 2015 fixed output
     L2242.hydro_2010_fixedOutput %>%
       left_join_error_no_match(L2242.hydro_EIA_ratio, by = c("region" = "state")) %>%
-      mutate(fixedOutput_2015 = fixedOutput * EIA_ratio) %>%
-      mutate(fixedOutput = fixedOutput_2015,
-             year = 2015,
-             share.weight.year = year) %>%
-      select(-EIA_ratio, -fixedOutput_2015) -> L2242.hydro_fixedOutput_2015
-
-    L2242.hydro_2010_fixedOutput %>%
-      left_join_error_no_match(L2242.hydro_EIA_ratio, by = c("region" = "state")) %>%
-      mutate(fixedOutput_2015 = fixedOutput * EIA_ratio,
+      mutate(fixedOutput = fixedOutput * EIA_ratio,
              year = min(MODEL_FUTURE_YEARS),
-             share.weight.year = year,
-             fixedOutput = fixedOutput_2015) %>%
-      select(-EIA_ratio, -fixedOutput_2015) -> L2242.hydro_fixedOutput_2015
+             share.weight.year = year) %>%
+      select(-EIA_ratio) -> L2242.hydro_fixedOutput_2015
 
     # Compute ratio of AEO-2018 hydro generation relative to EIA 2015 at the national level
     EIA_elec_gen_hydro %>%
@@ -86,7 +77,7 @@ module_gcam.usa_L2242.elec_hydro_USA <- function(command, ...) {
     # The same ratio is assumed for all states
     AEO_2018_elec_gen_hydro %>%
       mutate(AEO_2015_ratio = (AEO * CONV_TWH_EJ) / L2242.hydro_EIA_US_2015) %>%
-      filter(year %in%MODEL_FUTURE_YEARS) %>%
+      filter(year %in% MODEL_FUTURE_YEARS) %>%
       select(year, AEO_2015_ratio) %>%
       repeat_add_columns(tibble::tibble(region = gcamusa.STATES)) %>%
       # filtering out states with no hydro generation
@@ -95,11 +86,11 @@ module_gcam.usa_L2242.elec_hydro_USA <- function(command, ...) {
                                  rename(fixedOutput_2015 = fixedOutput) %>%
                                  select(-year),
                                by = c("region")) %>%
-      mutate(fixedOutput = fixedOutput_2015 * AEO_2015_ratio) %>%
-      mutate(share.weight.year = year) %>%
+      mutate(fixedOutput = fixedOutput_2015 * AEO_2015_ratio,
+             share.weight.year = year) %>%
       select(region, supplysector, subsector, stub.technology, year, fixedOutput,
              share.weight.year, subs.share.weight, tech.share.weight) %>%
-      bind_rows (L2242.hydro_fixedOutput_2015) %>%
+      bind_rows(L2242.hydro_fixedOutput_2015) %>%
       arrange(region, year) -> L2242.StubTechFixOut_hydro_USA_2050
 
     # Copy 2050 values for the remaining years
@@ -107,7 +98,8 @@ module_gcam.usa_L2242.elec_hydro_USA <- function(command, ...) {
       complete(year = MODEL_FUTURE_YEARS, nesting(region, supplysector, subsector, stub.technology,
                                             subs.share.weight, tech.share.weight)) %>%
       group_by(region, supplysector, subsector, stub.technology) %>%
-      mutate(fixedOutput = replace(fixedOutput, year > 2050, fixedOutput[year == 2050])) %>%
+      mutate(fixedOutput = replace(fixedOutput, year > gcamusa.FINAL_AEO_HYDRO_YEAR,
+                                   fixedOutput[year == gcamusa.FINAL_AEO_HYDRO_YEAR])) %>%
       ungroup() %>%
       mutate(share.weight.year = year,
              fixedOutput = round(fixedOutput, energy.DIGITS_CALOUTPUT)) %>%
