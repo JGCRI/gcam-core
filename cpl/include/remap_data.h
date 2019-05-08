c8923546b6b69fe419c8f77d7fc2c1ec0715c0bd
Pralit Patel <pralit.patel@pnnl.gov>
2019-05-08 17:50:44 -0400

    First cut at adding GCAMFusion capabilities to get data out of GCAM for E3SM.  Given how we will need to produce data in E3SM we modified the GetData helper to use a "ReMapData" utility which: * Will handle mapping a GCAM name to GLM name, for instance. * Aggregate results * Collapse dimensions into a 1D vector which assumes "complete" all dimensions

---
+++ cpl/include/remap_data.h
@@ -0,0 +1,33 @@
+#ifndef __REMAP_DATA__
+#define __REMAP_DATA__
+
+#include <string>
+#include <vector>
+#include <map>
+
+template<typename T>
+struct ReMapDataHelper {
+public:
+    std::string mDataName;
+    std::vector<T> mInOrderOutputNames;
+    std::map<T, T> mGCAMToOutputNameMap;
+    size_t getIndex(const T& aGCAMName) const;
+    size_t getStrideLength() const;
+};
+
+class ReMapData {
+public:
+    ReMapData();
+    void addColumn(std::string aDataName, std::vector<std::string> aInOrderOutputNames, std::map<std::string, std::string> aGCAMToOutputNameMap);
+    void addYearColumn(std::string aDataName, std::vector<int> aInOrderOutputNames, std::map<int, int> aGCAMToOutputNameMap);
+    void finalizeColumns();
+    void setData(const std::vector<std::string>& aColValues, const int aYearValue, const double aValue);
+    double* getData();
+private:
+    bool mIsInitialized;
+    std::vector<ReMapDataHelper<std::string> > mColumns;
+    ReMapDataHelper<int> mYearColumn;
+    double* mData;
+};
+
+#endif // __REMAP_DATA__
