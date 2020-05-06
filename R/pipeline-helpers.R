9b7e2fe4063b074ed7bc4c165a0e8d9a1202e7b6
Kyle, G Page <pkyle@pnnl.gov>
2020-05-06 11:34:18 -0400

    switch from is.tibble() to is_tibble() as per warning message of tibble 2.0.0

--- R/pipeline-helpers.R
+++ R/pipeline-helpers.R
@@ -27,7 +27,7 @@ PH_year_value_historical <- function(d) {
 #' @importFrom dplyr left_join
 #' @export
 left_join_error_no_match <- function(d, ..., ignore_columns = NULL) {
-  assertthat::assert_that(tibble::is.tibble(d))
+  assertthat::assert_that(tibble::is_tibble(d))
   dnames <- names(d)
   drows <- nrow(d)
   d <- left_join(d, ...)
