3541175d6f543c2028c74307ced735d5da69d4d6
Kyle, G Page <pkyle@pnnl.gov>
2020-05-08 20:40:40 -0400

    specify the default value for the 'ties' argument of approx() in our approx_fun() wrapper function this avoids a warning message in versions of R >= 3.6

--- input/gcamdata/R/pipeline-helpers.R
+++ input/gcamdata/R/pipeline-helpers.R
@@ -161,7 +161,7 @@ approx_fun <- function(year, value, rule = 1) {
   assert_that(is.numeric(value))
 
   if(rule == 1 | rule == 2) {
-    tryCatch(stats::approx(as.vector(year), value, rule = rule, xout = year)$y,
+    tryCatch(stats::approx(as.vector(year), value, rule = rule, xout = year, ties = mean)$y,
              error = function(e) NA)
 
   } else {
