40badbf43b5350032c30516c8271a2514e5f28aa
Pralit Patel <pralit.patel@pnnl.gov>
2020-06-18 15:58:49 -0400

    Fix old/new test where we explicitly need to sort rows before testing.
    Fix fail on L2242.StubTechFixOut_hydro_USA which called complete for
    MODEL_FUTURE_YEARS but didn't filter to those years after

--- input/gcamdata/R/zchunk_L2242.elec_hydro_USA.R
+++ input/gcamdata/R/zchunk_L2242.elec_hydro_USA.R
@@ -77,6 +77,8 @@ module_gcamusa_L2242.elec_hydro_USA <- function(command, ...) {
     L2242.StubTechFixOut_hydro_USA_2050 %>%
       complete(year = MODEL_FUTURE_YEARS, nesting(region, supplysector, subsector, stub.technology,
                                             subs.share.weight, tech.share.weight)) %>%
+      filter(year >= gcamusa.HYDRO_HIST_YEAR,
+             year %in% MODEL_FUTURE_YEARS) %>%
       group_by(region, supplysector, subsector, stub.technology) %>%
       mutate(fixedOutput = approx_fun(year, fixedOutput, rule = 2)) %>%
       ungroup() %>%
