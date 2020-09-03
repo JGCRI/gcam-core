1bbcac4756b178452e085bdeb999464acdc459f3
elochner74 <ellie.lochner@pnnl.gov>
2020-09-03 07:54:18 -0500

    Get rid of arrange in round_df

--- tests/testthat/test_driver_drake.R
+++ tests/testthat/test_driver_drake.R
@@ -60,11 +60,6 @@ test_that("load_from_cache works", {
        numeric_columns <- sapply(x, class) == "numeric"
        x[numeric_columns] <- round(x[numeric_columns], digits)
 
-       # expect_equivalent no longer accepts rows in different order but we
-       # do want to allow this so we will sort all columns before testing
-       # arrange_columns <- select(x, dplyr::everything())
-       # arrange(x, arrange_columns)
-
        return(x)
      } # end round_df
 
