fabac5c9a9c1704fea2dbb91017e15a0b602cc96
elochner74 <ellie.lochner@pnnl.gov>
2020-09-03 14:35:41 -0500

    Clean up comments

--- tests/testthat/test_driver_drake.R
+++ tests/testthat/test_driver_drake.R
@@ -60,11 +60,6 @@ test_that("load_from_cache works", {
        numeric_columns <- sapply(x, class) == "numeric"
        x[numeric_columns] <- round(x[numeric_columns], digits)
 
-       # expect_equivalent no longer accepts rows in different order but we
-       # do want to allow this so we will sort all columns before testing
-       # arrange_columns <- select(x, dplyr::everything())
-       # arrange(x, arrange_columns)
-
        return(x)
      } # end round_df
 
@@ -74,9 +69,8 @@ test_that("load_from_cache works", {
        old_data <- get_comparison_data(data_name)
        expect_is(old_data, "data.frame", info = paste("No comparison data found for", data_name))
 
-       # Compare old data and data from cache - ignore order
-       # expect_true(all.equal(round_df(old_data), round_df(data[[data_name]]), ignore_col_order = TRUE, ignore_row_order = TRUE))
-        expect_equivalent(round_df(old_data), round_df(data[[data_name]]))
+       # Compare old data and data from cache
+       expect_equivalent(round_df(old_data), round_df(data[[data_name]]))
        }
    }
 })
--- tests/testthat/test_oldnew.R
+++ tests/testthat/test_oldnew.R
@@ -104,10 +104,6 @@ test_that("matches old data system output", {
         numeric_columns <- sapply(x, class) == "numeric"
         x[numeric_columns] <- round(x[numeric_columns], digits)
 
-        # expect_equivalent no longer accepts rows in different order but we
-        # do want to allow this so we will sort all columms before testing
-        #arrange_columns <- select(x, dplyr::everything())
-        #arrange(x, arrange_columns)
         return(x)
       }
 
@@ -125,8 +121,7 @@ test_that("matches old data system output", {
                           info = paste(basename(newf), "doesn't match (sum test)"))
       } else {
         expect_equivalent(round_df(olddata), round_df(newdata), info = paste(basename(newf), "doesn't match"))
-        #expect_true(all.equal(round_df(olddata), round_df(newdata), ignore_col_order = TRUE, ignore_row_order = TRUE))
-        }
+      }
     }
   }
 })
