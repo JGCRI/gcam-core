e1c1133114dd9adfad2dd5f2dd02e43a8a741f6d
elochner74 <ellie.lochner@pnnl.gov>
2020-10-02 08:33:23 -0500

    Let driver_drake create xml folder

--- input/gcamdata/R/driver.R
+++ input/gcamdata/R/driver.R
@@ -507,7 +507,8 @@ driver_drake <- function(
    }
    else {
     chunks_to_run <- c(unfound_inputs$input, chunklist$name)
-  }
+   }
+  dir.create(xmldir, showWarnings = FALSE, recursive = TRUE)
 
   # Loop over each chunk and add a target for it and the command to build it
   # as appropriate for if it is just loading a FILE or running an actual chunk.
