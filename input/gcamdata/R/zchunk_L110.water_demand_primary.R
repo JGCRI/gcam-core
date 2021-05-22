c9425aab622104e38a9762df34f6a1b7b05a950f
Pralit Patel <pralit.patel@pnnl.gov>
2021-05-21 21:17:18 -0400

    A couple of fixes for unconventional oil water use including:  * We used the energy into unconventional oil (L121.in_EJ_R_unoil_F_Yh) instead of the production of unconventional oil (L121.in_EJ_R_TPES_unoil_Yh) for calculating the water coef  * Needed to tweak so map unconventional oil back in with regional oil to deal with the new oil structure since the fossil trade CMP.

--- input/gcamdata/R/zchunk_L110.water_demand_primary.R
+++ input/gcamdata/R/zchunk_L110.water_demand_primary.R
@@ -27,7 +27,7 @@ module_water_L110.water_demand_primary <- function(command, ...) {
              FILE = "energy/A23.globaltech_eff",
              "L1012.en_bal_EJ_R_Si_Fi_Yh",
              "L103.water_mapping_R_B_W_Ws_share",
-             "L121.in_EJ_R_unoil_F_Yh",
+             "L121.in_EJ_R_TPES_unoil_Yh",
              "L121.in_EJ_R_TPES_crude_Yh",
              "L122.in_EJ_R_gasproc_F_Yh"))
   } else if(command == driver.DECLARE_OUTPUTS) {
@@ -52,7 +52,7 @@ module_water_L110.water_demand_primary <- function(command, ...) {
     A23.globaltech_eff <- get_data(all_data, "energy/A23.globaltech_eff")
     L1012.en_bal_EJ_R_Si_Fi_Yh <- get_data(all_data, "L1012.en_bal_EJ_R_Si_Fi_Yh", strip_attributes = TRUE)
     L103.water_mapping_R_B_W_Ws_share <- get_data(all_data, "L103.water_mapping_R_B_W_Ws_share")
-    L121.in_EJ_R_unoil_F_Yh <- get_data(all_data, "L121.in_EJ_R_unoil_F_Yh")
+    L121.in_EJ_R_TPES_unoil_Yh <- get_data(all_data, "L121.in_EJ_R_TPES_unoil_Yh")
     L121.in_EJ_R_TPES_crude_Yh <- get_data(all_data, "L121.in_EJ_R_TPES_crude_Yh")
     L122.in_EJ_R_gasproc_F_Yh <- get_data(all_data, "L122.in_EJ_R_gasproc_F_Yh")
 
@@ -112,8 +112,8 @@ module_water_L110.water_demand_primary <- function(command, ...) {
     L110.energy_flow_oil <- L121.in_EJ_R_TPES_crude_Yh %>%
       mutate(supplysector = "regional oil") %>%
       select(GCAM_region_ID, supplysector, year, value)
-    L110.energy_flow_unoilprod <- subset(L121.in_EJ_R_unoil_F_Yh, fuel == "unconventional oil") %>%
-      mutate(supplysector = "unconventional oil production") %>%
+    L110.energy_flow_unoilprod <- subset(L121.in_EJ_R_TPES_unoil_Yh, fuel == "unconventional oil") %>%
+      mutate(supplysector = "regional oil") %>%
       select(GCAM_region_ID, supplysector, year, value)
     L110.energy_flow_gas <- subset(L122.in_EJ_R_gasproc_F_Yh, fuel == "gas") %>%
       mutate(supplysector = "regional natural gas") %>%
@@ -171,7 +171,7 @@ module_water_L110.water_demand_primary <- function(command, ...) {
       add_precursors("energy/calibrated_techs",
                      "energy/A23.globaltech_eff",
                      "L1012.en_bal_EJ_R_Si_Fi_Yh",
-                     "L121.in_EJ_R_unoil_F_Yh",
+                     "L121.in_EJ_R_TPES_unoil_Yh",
                      "L121.in_EJ_R_TPES_crude_Yh",
                      "L122.in_EJ_R_gasproc_F_Yh") ->
       L110.in_km3_water_primary
