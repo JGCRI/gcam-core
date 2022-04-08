d16c9874404bb9e1ee74ee45fbad25f677a30a2d
samp699 <jon.sampedro@pnnl.gov>
2022-04-08 10:10:02 -0400

    Adjustments to GCAM-USA to be consistent with the new building sector in the global version

--- input/gcamdata/R/zchunk_batch_building_USA_xml.R
+++ input/gcamdata/R/zchunk_batch_building_USA_xml.R
@@ -42,7 +42,14 @@ module_gcamusa_batch_building_USA_xml <- function(command, ...) {
              "L244.GlobalTechShrwt_bld_gcamusa",
              "L244.GlobalTechCost_bld_gcamusa",
              "L244.GompFnParam_gcamusa",
-             "L244.GlobalTechSCurve_bld"))
+             "L244.Satiation_impedance_gcamusa",
+             "L244.GlobalTechSCurve_bld",
+             "L244.GenericServiceImpedance_gcamusa",
+             "L244.GenericServiceCoef_gcamusa",
+             "L244.GenericServiceAdder_gcamusa",
+             "L244.ThermalServiceImpedance_gcamusa",
+             "L244.ThermalServiceCoef_gcamusa",
+             "L244.ThermalServiceAdder_gcamusa"))
   } else if(command == driver.DECLARE_OUTPUTS) {
     return(c(XML = "building_USA.xml"))
   } else if(command == driver.MAKE) {
@@ -58,11 +65,18 @@ module_gcamusa_batch_building_USA_xml <- function(command, ...) {
     L244.DemandFunction_serv <- get_data(all_data, "L244.DemandFunction_serv_gcamusa")
     L244.DemandFunction_flsp <- get_data(all_data, "L244.DemandFunction_flsp_gcamusa")
     L244.Satiation_flsp <- get_data(all_data, "L244.Satiation_flsp_gcamusa")
+    L244.Satiation_impedance <- get_data(all_data, "L244.Satiation_impedance_gcamusa")
     L244.SatiationAdder <- get_data(all_data, "L244.SatiationAdder_gcamusa")
     L244.ThermalBaseService <- get_data(all_data, "L244.ThermalBaseService_gcamusa")
     L244.GenericBaseService <- get_data(all_data, "L244.GenericBaseService_gcamusa")
     L244.ThermalServiceSatiation <- get_data(all_data, "L244.ThermalServiceSatiation_gcamusa")
     L244.GenericServiceSatiation <- get_data(all_data, "L244.GenericServiceSatiation_gcamusa")
+    L244.GenericServiceImpedance<-get_data(all_data, "L244.GenericServiceImpedance_gcamusa")
+    L244.ThermalServiceImpedance<-get_data(all_data, "L244.ThermalServiceImpedance_gcamusa")
+    L244.GenericServiceAdder<-get_data(all_data, "L244.GenericServiceAdder_gcamusa")
+    L244.ThermalServiceAdder<-get_data(all_data, "L244.ThermalServiceAdder_gcamusa")
+    L244.GenericServiceCoef<-get_data(all_data, "L244.GenericServiceCoef_gcamusa")
+    L244.ThermalServiceCoef<-get_data(all_data, "L244.ThermalServiceCoef_gcamusa")
     L244.Intgains_scalar <- get_data(all_data, "L244.Intgains_scalar_gcamusa")
     L244.ShellConductance_bld <- get_data(all_data, "L244.ShellConductance_bld_gcamusa")
     L244.Supplysector_bld <- get_data(all_data, "L244.Supplysector_bld_gcamusa")
@@ -94,12 +108,19 @@ module_gcamusa_batch_building_USA_xml <- function(command, ...) {
       add_xml_data(L244.DemandFunction_serv, "DemandFunction_serv") %>%
       add_xml_data(L244.DemandFunction_flsp, "DemandFunction_flsp") %>%
       add_xml_data(L244.Satiation_flsp, "Satiation_flsp") %>%
+      add_xml_data(L244.Satiation_impedance, "SatiationImpedance") %>%
       add_xml_data(L244.SatiationAdder, "SatiationAdder") %>%
       add_xml_data(L244.GompFnParam_gcamusa, "GompFnParam")  %>%
       add_xml_data(L244.ThermalBaseService, "ThermalBaseService") %>%
       add_xml_data(L244.GenericBaseService, "GenericBaseService") %>%
       add_xml_data(L244.ThermalServiceSatiation, "ThermalServiceSatiation") %>%
       add_xml_data(L244.GenericServiceSatiation, "GenericServiceSatiation") %>%
+      add_xml_data(L244.GenericServiceImpedance, "GenericServiceImpedance") %>%
+      add_xml_data(L244.ThermalServiceImpedance, "ThermalServiceImpedance") %>%
+      add_xml_data(L244.GenericServiceAdder, "GenericServiceAdder") %>%
+      add_xml_data(L244.ThermalServiceAdder, "ThermalServiceAdder") %>%
+      add_xml_data(L244.GenericServiceCoef, "GenericServiceCoef") %>%
+      add_xml_data(L244.ThermalServiceCoef, "ThermalServiceCoef") %>%
       add_xml_data(L244.Intgains_scalar, "Intgains_scalar") %>%
       add_xml_data(L244.ShellConductance_bld, "ShellConductance") %>%
       add_logit_tables_xml(L244.Supplysector_bld, "Supplysector") %>%
@@ -146,7 +167,14 @@ module_gcamusa_batch_building_USA_xml <- function(command, ...) {
                      "L244.GlobalTechShrwt_bld_gcamusa",
                      "L244.GlobalTechCost_bld_gcamusa",
                      "L244.GompFnParam_gcamusa",
-                     "L244.GlobalTechSCurve_bld") ->
+                     "L244.GlobalTechSCurve_bld",
+                     "L244.Satiation_impedance_gcamusa",
+                     "L244.GenericServiceImpedance_gcamusa",
+                     "L244.GenericServiceCoef_gcamusa",
+                     "L244.GenericServiceAdder_gcamusa",
+                     "L244.ThermalServiceImpedance_gcamusa",
+                     "L244.ThermalServiceCoef_gcamusa",
+                     "L244.ThermalServiceAdder_gcamusa") ->
       building_USA.xml
 
     # # Some data inputs may not actually contain data. If so, do not add_xml_data.
