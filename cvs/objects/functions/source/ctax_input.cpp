6339d1eca1c91f9193e293dfaea640dbb5b4cc46
Pralit Patel <pralit.patel@pnnl.gov>
2023-05-10 12:58:09 -0400

    Prior attempts to avoid prices > 1 for the negative emissions budget have provent to be insufficient.  While prior attempts deal with it in preconditioning, there is nothing to stop a solver component for running into and derailing the solver inbetween precondition checks.
    A better approach is to remove any caps in GCAM/CTaxInput.  Doing so
    will create negative demands which does not make sense. However given
    the supply will always be positive it could not solve to such a value.
    And more importantly, we will always have continuous behavior even at "invalid" prices

--- cvs/objects/functions/source/ctax_input.cpp
+++ cvs/objects/functions/source/ctax_input.cpp
@@ -182,7 +182,12 @@ void CTaxInput::setPhysicalDemand( double aPhysicalDemand,
     double ctax = marketplace->getPrice( "CO2", aRegionName, aPeriod, false );
     double priceAdjust = 0.0;
     if( taxFraction != Marketplace::NO_MARKET_PRICE && ctax != Marketplace::NO_MARKET_PRICE ) {
-        priceAdjust = (1.0 - std::min( taxFraction, 1.0 )) * ctax * mCachedCCoef;
+        // Note: we do not attempt to ensure taxFraction is within bounds
+        // and we could get thrown a price > 1, for instance.  However, that
+        // will send the demand negative and given the supply will always be
+        // positive it could not solve to such a value.  And more importantly,
+        // we will always have continuous behavior even at "invalid" prices
+        priceAdjust = (1.0 - taxFraction) * ctax * mCachedCCoef;
     }
     mNetTransferAdjust = aPhysicalDemand * priceAdjust;
     marketplace->addToDemand( mName, aRegionName, mNetTransferAdjust, aPeriod );
