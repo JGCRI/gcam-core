80ce4d6705bf56b9c1cd629d051eee44cf222516
Pralit Patel <pralit.patel@pnnl.gov>
2023-05-17 00:28:14 -0400

    Ensure emissions unit tag is always copied forward for consistent reporting

--- cvs/objects/emissions/source/nonco2_emissions.cpp
+++ cvs/objects/emissions/source/nonco2_emissions.cpp
@@ -122,6 +122,8 @@ void NonCO2Emissions::copyGHGParameters( const AGHG* aPrevGHG ){
         abort();
     }
     
+    mEmissionsUnit = prevComplexGHG->mEmissionsUnit;
+    
     if( !mEmissionsDriver ) {
         mEmissionsDriver = prevComplexGHG->mEmissionsDriver->clone();
     }
