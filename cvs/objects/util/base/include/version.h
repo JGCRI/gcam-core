dd934a9dbfdb7e1565263ec901ff99c688c3c38d
Pralit Patel <pralit.patel@pnnl.gov>
2025-11-24 11:06:42 -0500

    Bump version number

--- cvs/objects/util/base/include/version.h
+++ cvs/objects/util/base/include/version.h
@@ -41,10 +41,10 @@
  * NOTE: ADD 1 TO LATEST SUBVERSION REVISION NUMBER
  */
 //! The latest SVN revision number for identification of the build.
-#define __REVISION_NUMBER__ "gcam-v8.3"
+#define __REVISION_NUMBER__ "gcam-v8.5"
 /*****************************************************************************/
 
 //! GCAM model version.
-#define __ObjECTS_VER__ "8.3"
+#define __ObjECTS_VER__ "8.5"
 
 #endif // _VERSION_H_ 
